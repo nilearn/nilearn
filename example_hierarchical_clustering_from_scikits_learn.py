576f9754cdd2bbf50a5f07279cd9906ed6f23616
Jean Kossaifi <kossaifi@is208616.intra.cea.fr>
2011-07-21 12:19:34 +0200

    Best display of the example

--- example_hierarchical_clustering_from_scikits_learn.py
+++ example_hierarchical_clustering_from_scikits_learn.py
@@ -16,19 +16,28 @@ import hierarchical_clustering
 import numpy as np
 import pylab as pl
 from scipy import linalg, ndimage
+from time import clock
+import timeit
 
 from scikits.learn.feature_extraction.image import grid_to_graph
 from scikits.learn.linear_model import BayesianRidge
-#from scikits.learn.linear_model import RidgeCV
+from scikits.learn.linear_model import RidgeCV
 #from scikits.learn import svm
 
 ###############################################################################
-# Generate data
-n_iterations = 25
+# Set parameters
+estimator_name = 'BayesianRidge'
+#clf = RidgeCV(alphas=[0.1, 1.0, 10.0])
+clf = BayesianRidge(compute_score=True, verbose=False)
+#clf = svm.SVR(kernel='linear')
+n_iterations = 30
+#
 n_samples = 200
 size = 40 # image size
 roi_size = 15
 snr = 5.
+
+# Generate data
 np.random.seed(0)
 mask = np.ones([size, size], dtype=np.bool)
 
@@ -49,12 +58,12 @@ y += noise_coef * noise # add noise
 
 ###############################################################################
 # Hierarchical clustering
-#clf = RidgeCV(alphas=[0.1, 1.0, 10.0])
-clf = BayesianRidge()
-#clf = svm.SVR(kernel='linear')
 A = grid_to_graph(n_x=size, n_y=size)
 hc = hierarchical_clustering.HierarchicalClustering(clf, A)
+time1 = clock()
 tab = hc.fit(X, y, n_iterations, verbose=1)
+time2 = clock()
+execution_time = (time2 -time1)
 
 # Plotting the result
 # Computing an array of the fitted coefs
@@ -75,8 +84,9 @@ pl.title("True weights")
 # tab = tab.reshape((size, size)), then plot tab
 pl.subplot(2, 2, 2)
 pl.imshow(tab2, interpolation='nearest', cmap=pl.cm.RdBu_r)
-pl.title("Hierarchical Clustering, \n%d iterations,\n %d parcels " % 
-        (n_iterations, len(np.unique(tab))))
+pl.title("Hierarchical Clustering, \n%d iterations,\
+        %d parcels\n estimator : %s\n execution time : %d s." % 
+        (n_iterations, len(np.unique(tab)), estimator_name, execution_time))
 # Plotting the score of the best parcellation at each iteration
 pl.subplot(2, 1, 2)
 pl.bar(np.arange(len(hc.scores)), hc.scores)
