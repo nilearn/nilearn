05a045cb3d8561cb172263cfa73a6a7bcd9daa77
Jean Kossaifi <jean.kossaifi@gmail.com>
2011-08-12 16:12:03 +0200

    Decoding example

--- decoding_example.py
+++ decoding_example.py
@@ -1,5 +1,6 @@
 ### All the imports
 import numpy as np
+import pylab as pl
 from scipy import signal
 import nibabel as ni
 from scikits.learn.svm import SVC
@@ -7,6 +8,9 @@ from scikits.learn.feature_selection import SelectKBest, f_classif
 from scikits.learn.pipeline import Pipeline
 from scikits.learn.cross_val import LeaveOneLabelOut, cross_val_score
 from scikits.learn.feature_extraction.image import grid_to_graph
+from  scikits.learn.externals.joblib.memory import Memory
+
+mem = Memory('./')
 
 from supervised_clustering import SupervisedClusteringClassifier
 
@@ -25,9 +29,14 @@ for s in np.unique(session):
     X[session==s] = signal.detrend(X[session==s], axis=0)
 
 # Remove volumes corresponding to rest
+X = X[y<=2]
+y = y[y<=2]
+session = session[y<=2]
+
 X, y, session = X[y!=0], y[y!=0], session[y!=0]
 n_samples, n_features = X.shape
 n_conditions = np.size(np.unique(y))
+
 
 ### Define the prediction function to be used.
 # Here we use a Support Vector Classification, with a linear kernel and C=1
@@ -44,6 +53,7 @@ anova_svc = Pipeline([('anova', feature_selection), ('svc', clf)])
 ### Define the cross-validation scheme used for validation.
 # Here we use a LeaveOneLabelOut cross-validation on the session, which
 # corresponds to a leave-one-session-out
+session /= 5
 cv = LeaveOneLabelOut(session)
 
 ### Compute the prediction accuracy for the different folds (i.e. session)
@@ -58,10 +68,20 @@ print "Classification accuracy: %f" % classification_accuracy, \
 
 ### Same test using the supervised clustering
 A =  grid_to_graph(n_x=shape[0], n_y=shape[1], n_z=shape[2], mask=mask)
-sc = SupervisedClusteringClassifier(connectivity=A, n_jobs=8)
-cv_scores = cross_val_score(sc, X, y, cv=cv, n_jobs=-1,
+sc = SupervisedClusteringClassifier(connectivity=A, n_jobs=8, n_iterations=100)
+cv_scores = cross_val_score(sc, X, y, cv=cv, n_jobs=1,
                             verbose=1, iid=True)
 classification_accuracy = np.sum(cv_scores) / float(n_samples)
 print "=== SUPERVISED CLUSTERING ==="
 print "Classification accuracy: %f" % classification_accuracy, \
     " / Chance level: %f" % (1. / n_conditions)
+sc.fit(X, y)
+print "Number of parcellations : %d" % len(sc.coef_)
+pl.figure()
+pl.subplot(2, 1, 1)
+pl.plot(sc.scores_)
+pl.title('scores')
+pl.subplot(2, 1, 2)
+pl.plot(sc.delta_scores_)
+pl.title('delta')
+pl.show()
