0baa90278196e955f73f32aa16eb7155e4812c34
Jean Kossaifi <jean.kossaifi@gmail.com>
2011-08-30 19:15:49 +0200

    Plotting only relevant coefs. Deleted the shuffle split (replaced by KFold)

--- simulated_example.py
+++ simulated_example.py
@@ -15,7 +15,8 @@ from scipy import linalg, ndimage
 from scikits.learn.utils import check_random_state
 from scikits.learn.feature_extraction.image import grid_to_graph
 from scikits.learn.linear_model import BayesianRidge
-from scikits.learn.cross_val import ShuffleSplit
+from scikits.learn.cross_val import KFold
+#from scikits.learn.cross_val import ShuffleSplit
 
 
 import supervised_clustering
@@ -80,12 +81,12 @@ X_train, Xtest, y_train, y_test, snr, noise, coefs, size =\
 A = grid_to_graph(n_x=size, n_y=size, n_z=size)
 clf = BayesianRidge(fit_intercept=True, normalize=True)
 
-#sc = supervised_clustering.SupervisedClusteringRegressor(clf, connectivity=A,
-#        n_iterations=30, verbose=1, n_jobs=8, cv=KFold(X_train.shape[0], 25))
 sc = supervised_clustering.SupervisedClusteringRegressor(clf, connectivity=A,
-        n_iterations=15, verbose=1, n_jobs=8,
-        cv=ShuffleSplit(X_train.shape[0], n_splits=25, test_fraction=0.1,
-            random_state=0))
+        n_iterations=30, verbose=1, n_jobs=8, cv=KFold(X_train.shape[0], 10))
+#sc = supervised_clustering.SupervisedClusteringRegressor(clf, connectivity=A,
+#        n_iterations=30, verbose=1, n_jobs=8,
+#        cv=ShuffleSplit(X_train.shape[0], n_splits=25, test_fraction=0.1,
+#            random_state=0))
 sc.fit(X_train, y_train)
 
 computed_coefs = sc.inverse_transform()
@@ -95,19 +96,19 @@ score = sc.score(Xtest, y_test)
 ###############################################################################
 # Plot the results
 
-pl.close('all')
-pl.figure()
-pl.title('Scores of the supervised clustering')
-pl.subplot(2, 1, 1)
-pl.bar(np.arange(len(sc.scores_)), sc.scores_)
-pl.xlabel('score')
-pl.ylabel('iteration')
-pl.title('Score of the best parcellation of each iteration')
-pl.subplot(2, 1, 2)
-pl.bar(np.arange(len(sc.delta_scores_)), sc.delta_scores_)
-pl.xlabel('delta_score')
-pl.ylabel('iteration')
-pl.title('Delta_Score of the best parcellation of each iteration')
+#pl.close('all')
+#pl.figure()
+#pl.title('Scores of the supervised clustering')
+#pl.subplot(2, 1, 1)
+#pl.bar(np.arange(len(sc.scores_)), sc.scores_)
+#pl.xlabel('score')
+#pl.ylabel('iteration')
+#pl.title('Score of the best parcellation of each iteration')
+#pl.subplot(2, 1, 2)
+#pl.bar(np.arange(len(sc.delta_scores_)), sc.delta_scores_)
+#pl.xlabel('delta_score')
+#pl.ylabel('iteration')
+#pl.title('Delta_Score of the best parcellation of each iteration')
 
 
 print "Score of the supervised_clustering: ", score
@@ -122,18 +123,22 @@ vmin = -vminmax
 vmax = +vminmax
 pl.axes()
 
-for i in range(size):
-    pl.subplot(4, 7, i+1+(i/3)*4)
+for i in [0, 5, 11]:
+    pl.subplot(3, 3, i/5+1)
     pl.imshow(computed_coefs[:, :, i], vmin=vmin, vmax=vmax,
             interpolation="nearest", cmap=pl.cm.RdBu_r)
+    if i==5:
+        pl.title('computed coefs')
     pl.xticks(())
     pl.yticks(())
 
 coefs = coefs.reshape((size, size, size))
-for i in range(size):
-    pl.subplot(4, 7, i+(i/3+1)*4)
+for i in [0, 5, 11]:
+    pl.subplot(3, 3, i/5+7)
     pl.imshow(coefs[:, :, i], vmin=vmin, vmax=vmax,
             interpolation="nearest", cmap=pl.cm.RdBu_r)
+    if i == 5:
+        pl.title('real coefs')
     pl.xticks(())
     pl.yticks(())
 
