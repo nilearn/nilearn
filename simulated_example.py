d646253656eac4334abb5fd4db1745c9a30bca4b
Jean Kossaifi <jean.kossaifi@gmail.com>
2011-09-02 16:32:24 +0200

    Nice plot for the scores

--- simulated_example.py
+++ simulated_example.py
@@ -2,9 +2,9 @@
 =============================
 Supervised clustering example
 =============================
-
-Example of a supervised clustering on simulated data
 """
+
+# Licence : BSD
 
 print __doc__
 
@@ -15,16 +15,13 @@ from scipy import linalg, ndimage
 from scikits.learn.utils import check_random_state
 from scikits.learn.feature_extraction.image import grid_to_graph
 from scikits.learn.linear_model import BayesianRidge
-from scikits.learn.cross_val import KFold
-from scikits.learn.cross_val import ShuffleSplit
+
 
 
 import supervised_clustering
 
-
 ###############################################################################
 # Fonction to generate data
-
 def create_simulation_data(snr=5, n_samples=2*100, size=12, random_state=0):
     generator = check_random_state(random_state)
     roi_size = 2  # size / 3
@@ -76,85 +73,91 @@ X_train, X_test, y_train, y_test, snr, noise, coefs, size =\
 
 
 ###############################################################################
-# Compute the results
-
+# Compute the results for supervised clustering
 A = grid_to_graph(n_x=size, n_y=size, n_z=size)
-clf = BayesianRidge(fit_intercept=True, normalize=True, overwrite_X=False)
-
-sc = supervised_clustering.SupervisedClusteringRegressor(clf, connectivity=A,
-        n_iterations=30, verbose=1, n_jobs=8, cv=KFold(X_train.shape[0], 25))
+clf = BayesianRidge(fit_intercept=True, normalize=True, tol=1.e-3)
+sc = supervised_clustering.SupervisedClusteringRegressor(estimator=clf,
+        connectivity=A, n_iterations=30, cv=25, verbose=1, n_jobs=8)
 #sc = supervised_clustering.SupervisedClusteringRegressor(clf, connectivity=A,
 #        n_iterations=30, verbose=1, n_jobs=8,
 #        cv=ShuffleSplit(X_train.shape[0], n_splits=10, test_fraction=0.6,
 #            random_state=0))
 sc.fit(X_train, y_train)
-
 computed_coefs = sc.inverse_transform()
+computed_coefs = np.reshape(computed_coefs, [size, size, size])
 score = sc.score(X_test, y_test)
+
+
+###############################################################################
+# Compute the results for simple BayesianRidge
+clf.fit(X_train, y_train)
+bayes_coefs = clf.coef_
+bayes_score = clf.score(X_test, y_test)
+bayes_coefs = bayes_coefs.reshape((size, size, size))
 
 
 ###############################################################################
 # Plot the results
 
-#pl.close('all')
-#pl.figure()
-#pl.title('Scores of the supervised clustering')
-#pl.subplot(2, 1, 1)
-#pl.bar(np.arange(len(sc.scores_)), sc.scores_)
-#pl.xlabel('score')
-#pl.ylabel('iteration')
-#pl.title('Score of the best parcellation of each iteration')
-#pl.subplot(2, 1, 2)
-#pl.bar(np.arange(len(sc.delta_scores_)), sc.delta_scores_)
-#pl.xlabel('delta_score')
-#pl.ylabel('iteration')
-#pl.title('Delta_Score of the best parcellation of each iteration')
+pl.close('all')
+pl.figure()
+pl.title('Scores of the supervised clustering')
+pl.subplot(2, 1, 1)
+pl.plot(np.arange(len(sc.scores_)), sc.scores_)
+pl.xlabel('score')
+pl.ylabel('iteration')
+pl.title('Score of the best parcellation of each iteration')
+pl.subplot(2, 1, 2)
+pl.plot(np.arange(len(sc.delta_scores_)), sc.delta_scores_)
+pl.xlabel('delta_score')
+pl.ylabel('iteration')
+pl.title('Delta_Score of the best parcellation of each iteration')
 
 
-print "Score of the supervised_clustering: ", score
-print "Number of parcels : %d" % len(np.unique(sc.labels_))
-computed_coefs = np.reshape(computed_coefs, [size, size, size])
-pl.figure(figsize=[5, 4])
-pl.subplots_adjust(left=0., right=1., bottom=0.05, top=0.8, wspace=0.05,
-        hspace=0.05)
+
+pl.figure(figsize=(3*2, 3*1.5))
 vminmax = np.max(np.abs(computed_coefs))
 vmin = 0
 vmin = -vminmax
 vmax = +vminmax
-pl.axes()
-computed_coefs *= 4
-
-for i in [0, 6, 11]:
-    pl.subplot(5, 3, i/5+1)
-    pl.imshow(computed_coefs[:, :, i], vmin=vmin, vmax=vmax,
-            interpolation="nearest", cmap=pl.cm.RdBu_r)
-    if i==6:
-        pl.title('computed coefs')
-    pl.xticks(())
-    pl.yticks(())
+computed_coefs *= 3
+pl.suptitle('Supervised Clustering VS simple estimator,\nSNR = %d' % snr, size=27)
 
 coefs = coefs.reshape((size, size, size))
 for i in [0, 6, 11]:
-    pl.subplot(5, 3, i/5+7)
+    pl.subplot(3, 3, i/5+4)
     pl.imshow(coefs[:, :, i], vmin=vmin, vmax=vmax,
             interpolation="nearest", cmap=pl.cm.RdBu_r)
-    if i == 6:
-        pl.title('real coefs')
+    if i == 0:
+        pl.ylabel('real coefs', size=19)
     pl.xticks(())
     pl.yticks(())
 
-bayes = BayesianRidge()
-bayes.fit(X_train, y_train)
-bayes_score = bayes.score(X_test, y_test)
-coefs_clf = bayes.coef_.reshape((size, size, size))
-coefs_clf *= 4
-print "Score of the simple BayesianRidge :", bayes_score
 for i in [0, 6, 11]:
-    pl.subplot(5, 3, i/5+13)
-    pl.imshow(coefs_clf[:, :, i], vmin=vmin, vmax=vmax,
+    pl.subplot(3, 3, i/5+1)
+    pl.imshow(computed_coefs[:, :, i], vmin=vmin, vmax=vmax,
             interpolation="nearest", cmap=pl.cm.RdBu_r)
-    if i == 6:
-        pl.title('simple bayesian ridge')
+    if i==0:
+        pl.ylabel('Supervised Clustering coefs,\n\
+                score = %f,\n\
+                %d parcels' % (score, len(np.unique(sc.labels_))), size=19)
+    pl.xticks(())
+    pl.yticks(())
+
+# Plotting the BayesianRidge's coefs
+vminmax = np.max(np.abs(bayes_coefs))
+vmin = 0
+vmin = -vminmax
+vmax = +vminmax
+
+bayes_coefs *= 3
+
+for i in [0, 6, 11]:
+    pl.subplot(3, 3, i/5+7)
+    pl.imshow(bayes_coefs[:, :, i], vmin=vmin, vmax=vmax,
+            interpolation="nearest", cmap=pl.cm.RdBu_r)
+    if i == 0:
+        pl.ylabel('simple BayesianRidge,\n score = %f' % bayes_score, size=19)
     pl.xticks(())
     pl.yticks(())
 
