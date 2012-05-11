0ba8bea888a0a0571da394438cc5df120c6d736f
Gael Varoquaux <gael.varoquaux@normalesup.org>
2012-05-12 01:13:15 +0200

    ENH: restore a simulated data example that works
    The simulated data example was using the supervised clustering, which had
    been removed.

---
+++ plot_simulated_data.py
@@ -0,0 +1,113 @@
+"""
+=================================================
+Example of pattern recognition on simulated data
+=================================================
+"""
+
+# Licence : BSD
+
+print __doc__
+
+from time import time
+
+import numpy as np
+import pylab as pl
+from scipy import linalg, ndimage
+
+from sklearn import linear_model, svm
+
+
+###############################################################################
+# Fonction to generate data
+def create_simulation_data(snr=5, n_samples=2*100, size=12, random_state=0):
+    from sklearn.utils import check_random_state
+    generator = check_random_state(random_state)
+    roi_size = 2  # size / 3
+    smooth_X = 2
+    ### Coefs
+    w = np.zeros((size, size, size))
+    w[0:roi_size, 0:roi_size, 0:roi_size] = -0.6
+    w[-roi_size:, -roi_size:, 0:roi_size] = 0.5
+    w[0:roi_size, -roi_size:, -roi_size:] = -0.6
+    w[-roi_size:, 0:roi_size:, -roi_size:] = 0.5
+    w = w.ravel()
+    ### Images
+    XX = generator.randn(n_samples, size, size, size)
+    X = []
+    y = []
+    for i in range(n_samples):
+        Xi = ndimage.filters.gaussian_filter(XX[i, :, :, :], smooth_X)
+        Xi = Xi.ravel()
+        X.append(Xi)
+        y.append(np.dot(Xi, w))
+    X = np.array(X)
+    y = np.array(y)
+    norm_noise = linalg.norm(y, 2) / np.exp(snr/20.)
+    orig_noise = generator.randn(y.shape[0])
+    noise_coef = norm_noise / linalg.norm(orig_noise, 2)
+    # Add additive noise
+    noise = noise_coef * orig_noise
+    snr = 20 * np.log(linalg.norm(y, 2) / linalg.norm(noise, 2))
+    print "SNR : %d " % snr
+    y += noise
+
+    X -= X.mean(axis=-1)[:, np.newaxis]
+    X /= X.std(axis=-1)[:, np.newaxis]
+    X_test = X[n_samples/2:, :]
+    X_train = X[:n_samples/2, :]
+    y_test = y[n_samples/2:]
+    y = y[:n_samples/2]
+
+    return X_train, X_test, y, y_test, snr, noise, w, size
+
+
+def plot_slices(data, title=None):
+    pl.figure(figsize=(5.5, 2))
+    vmax = np.abs(data).max()
+    for i in (0, 6, 11):
+        pl.subplot(1, 3, i/5+1)
+        pl.imshow(data[:, :, i], vmin=-vmax, vmax=vmax,
+                interpolation="nearest", cmap=pl.cm.RdBu_r)
+        pl.xticks(())
+        pl.yticks(())
+    pl.subplots_adjust(hspace=0.05, wspace=0.05, left=.03, right=.97)
+    if title is not None:
+        pl.suptitle(title)
+
+
+###############################################################################
+# Create data
+size = 12
+n_samples = 400
+X_train, X_test, y_train, y_test, snr, noise, coefs, size =\
+        create_simulation_data(snr=10, n_samples=n_samples, size=size)
+
+
+###############################################################################
+# Compute the results and coef maps for different estimators
+classifiers = {
+    'bayesian_ridge': linear_model.BayesianRidge(normalize=True),
+    'svr': svm.SVR(kernel='linear'),
+    'ridge_cv': linear_model.RidgeCV(),
+}
+
+coefs = np.reshape(coefs, [size, size, size])
+plot_slices(coefs, title="Ground truth")
+
+for name, classifier in sorted(classifiers.iteritems()):
+    t1 = time()
+    classifier.fit(X_train, y_train)
+    elapsed_time = time() - t1
+    coefs = classifier.coef_
+    coefs = np.reshape(coefs, [size, size, size])
+    score = classifier.score(X_test, y_test)
+
+    title = '%s: prediction score %.3f, training time: %.2fs' % (
+                classifier.__class__.__name__, score,
+                elapsed_time)
+
+    plot_slices(coefs, title=title)
+
+    print title
+
+pl.show()
