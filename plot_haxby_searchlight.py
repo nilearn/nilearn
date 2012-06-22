106b551d1b3fa1308ccd3eb8a26dba459d7be110
Alexandre Abraham <alexandre.abraham@cea.fr>
2012-06-22 09:05:41 +0200

    Remove not working starplus example. Add haxby example (for the moment, computing is very long)

---
+++ plot_haxby_searchlight.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy import sparse, signal
+
+from sklearn import neighbors
+from sklearn.cross_validation import KFold
+from sklearn.metrics import precision_score
+from sklearn import svm
+
+from nisl import searchlight, datasets
+
+### Download the data from the web
+dataset = datasets.fetch_haxby()
+
+X = dataset.data
+mask = dataset.mask
+y = dataset.target
+session = dataset.session
+X = X[mask != 0].T
+
+mask = np.asarray(np.where(mask)).T
+print "detrending data"
+for s in np.unique(session):
+    X[session == s] = signal.detrend(X[session == s], axis=0)
+
+# Remove volumes corresponding to rest
+X, y, session = X[y != 0], y[y != 0], session[y != 0]
+X, y, session = X[y < 3], y[y < 3], session[y < 3]
+
+### Create the adjacency matrix
+clf = neighbors.NearestNeighbors(radius=4., n_neighbors=30)
+dist, ind = clf.fit(mask).kneighbors(mask)
+A = sparse.lil_matrix((mask.shape[0], mask.shape[0]))
+for i, li in enumerate(ind):
+    A[i, list(li[1:])] = np.ones(len(li[1:]))
+
+### Instanciate the searchlight model
+n_jobs = 7
+estimator = svm.SVC(kernel='linear', C=1)
+searchlight = searchlight.SearchLight(A, estimator, n_jobs=n_jobs)
+score_func = precision_score
+cv = KFold(y.size, k=8)
+# cv = None
+scores = searchlight.fit(X, y, score_func=score_func, verbose=True, cv=cv)
