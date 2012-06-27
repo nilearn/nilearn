cfba4f7c496a43d711883bf4d94e8d4f7ed84075
Alexandre Abraham <abraham.alexandre@gmail.com>
2012-06-27 02:25:24 +0200

    Restrain computation, add documentation
    This is still a work in progress and is only run on Windows
    Comments added to plot_haxby_searchlight
    Restrain computation to the back of the brain
    F_score computation is commented out because generates an error on
    Windows :
    
    C:\Python27\lib\site-packages\sklearn\feature_selection\univariate_selection.py:92:
    RuntimeWarning: invalid value encountered in divide
    f = msb / msw
    
    C:\Python27\lib\site-packages\sklearn\feature_selection\univariate_selection.py:93:
    RuntimeWarning: invalid value encountered in fdtrc
    prob = stats.fprob(dfbn, dfwn, f)
    
    C:\Users\Goulagman\Documents\GitHub\tutorial\plot_haxby_searchlight.py:95:
    RuntimeWarning: invalid value encountered in less
    p_values[p_values < 1e-10] = 1e-10
    C:\Python27\lib\site-packages\matplotlib\colors.py:533: RuntimeWarning:
    invalid value encountered in less
    cbook._putmask(xa, xa<0.0, -1)

--- plot_haxby_searchlight.py
+++ plot_haxby_searchlight.py
@@ -1,3 +1,15 @@
+"""
+The haxby dataset: face vs house in object recognition using Searchlight
+========================================================================
+
+A significant part of the running time of this example is actually spent
+in loading the data: we load all the data but only use the face and
+houses conditions.
+
+Plus, in order to speed up computing, Searchlight is run only on one slice
+od the fMRI (see the generated figures).
+"""
+
 ### Load Haxby dataset ########################################################
 from nisl import datasets
 dataset = datasets.fetch_haxby()
@@ -15,6 +27,9 @@ import numpy as np
 X = np.rollaxis(fmri_data, 3)
 # X.shape is (1452, 41, 40, 49)
 
+# Mean image: used as background in visualisation
+mean_img = np.mean(X, axis=0)
+
 # Detrend data on each session independently
 from scipy import signal
 for s in np.unique(session):
@@ -22,13 +37,15 @@ for s in np.unique(session):
 
 ### Prepare the masks #########################################################
 # Here we will use several masks :
-# * mask is the original mask
+# * mask is the originalmask
 # * process_mask is a subset of mask, it contains voxels that should be
-#   processed (we only keep the slice z = 26)
+#   processed (we only keep the slice z = 26 and the back of the brain to speed
+#   up computation)
 mask = (dataset.mask != 0)
 process_mask = mask.copy()
 process_mask[..., 27:] = False
 process_mask[..., :25] = False
+process_mask[:, 23:] = False
 
 ### Restrict to faces and houses ##############################################
 
@@ -42,30 +59,46 @@ conditions = conditions[condition_mask]
 ### Searchlight ###############################################################
 
 from nisl import searchlight
-from sklearn.metrics import f1_score
-from sklearn.cross_validation import KFold
 
 # Make processing parallel
-# /!\ As each thread will print its progress, this could mess up a little
+# /!\ As each thread will print its progress, n_jobs > 1 could mess up the
 #     information output.
 n_jobs = 1
 
-score_func = f1_score
+### Define the score function used to evaluate classifiers
+# Here we use precision which mesures proportion of true positives among
+# all positives results for one class.
+from sklearn.metrics import precision_score
+score_func = precision_score
 
-# A cross validation method is needed to measure precision of each voxel
+### Define the cross-validation scheme used for validation.
+# Here we use a KFold cross-validation on the session, which corresponds to
+# splitting the samples in 4 folds and make 4 runs using each fold as a test
+# set once and the others as learning sets
+from sklearn.cross_validation import KFold
 cv = KFold(y.size, k=4)
+
+### Fit #######################################################################
+
+# The radius is the one of the Searchlight sphere that will scan the volume
 searchlight = searchlight.SearchLight(mask, process_mask, radius=1.5,
         n_jobs=n_jobs, score_func=score_func, verbose=True, cv=cv)
-# scores.scores is an array containing per voxel cross validation scores
+		
+# scores.scores_ is an array containing per voxel cross validation scores
 scores = searchlight.fit(X, y)
 
-### Unmask the data and display it
+### Visualization #############################################################
 import pylab as pl
-pl.imshow(np.rot90(scores.scores_[..., 26]), interpolation='nearest',
+slice = np.ma.array(scores.scores_, mask=np.logical_not(process_mask))
+pl.imshow(np.rot90(mean_img[..., 26]), interpolation='nearest',
+        cmap=pl.cm.gray)
+pl.imshow(np.rot90(slice[..., 26]), interpolation='nearest',
         cmap=pl.cm.spectral, vmin=0, vmax=1)
 pl.colorbar()
+pl.axis('off')
 pl.show()
 
+"""
 ### Show the F_score
 from sklearn.feature_selection import f_classif
 f_values, p_values = f_classif(X, y)
@@ -73,4 +106,6 @@ p_values[p_values < 1e-10] = 1e-10
 p_values = -np.log10(p_values)
 pl.imshow(np.rot90(p_values[..., 26]), interpolation='nearest', cmap=pl.cm.jet)
 pl.colorbar()
+pl.axis('off')
 pl.show()
+"""
