Fix bug in affine computation making the transformer run slowly

nisl/resampling.py
@@ -96,7 +96,7 @@ def as_volume_img(data, affine, new_affine=None, new_shape=None,
         data = copy.copy(data)
         affine = copy.copy(affine)
     if new_affine is None and new_shape is None:
-            return (data, affine)
+        return (data, affine)
     if new_affine is None:
         new_affine = np.eye(4)
     if new_shape is None:

plot_haxby_decoding.py
@@ -44,7 +44,7 @@ n_conditions = np.size(np.unique(y))
 from nisl import mri_transformer
 # Detrending is disabled as we are not yet able to do it by session
 mri_loader = mri_transformer.MRITransformer(mask=mask, detrend=True,
-        sessions=session, verbose=1)
+        copy=False, sessions=session)
 
 ### Prediction function #######################################################
 