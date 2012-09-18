34975a415726be9bf53068cc4d9d6418d2f54e74
Gael Varoquaux <gael.varoquaux@normalesup.org>
2012-09-18 22:17:39 +0200

    TEST: setup testing framework

--- nisl/__init__.py
+++ nisl/__init__.py
@@ -21,34 +21,5 @@ try:
 except ImportError:
     print 'Scikit-learn could not be found, please install it properly to use nisl.'
 """
-try:
-    from numpy.testing import nosetester
-
-    class NoseTester(nosetester.NoseTester):
-        """ Subclass numpy's NoseTester to add doctests by default
-        """
-
-        def test(self, label='fast', verbose=1, extra_argv=['--exe'],
-                        doctests=True, coverage=False):
-            """Run the full test suite
-
-            Examples
-            --------
-            This will run the test suite and stop at the first failing
-            example
-            >>> from nisl import test
-            >>> test(extra_argv=['--exe', '-sx']) #doctest: +SKIP
-            """
-            return super(NoseTester, self).test(label=label, verbose=verbose,
-                                    extra_argv=extra_argv,
-                                    doctests=doctests, coverage=coverage)
-
-    test = NoseTester().test
-    del nosetester
-except:
-    pass
-
-
-__all__ = ['datasets']
 
 __version__ = 0.1
