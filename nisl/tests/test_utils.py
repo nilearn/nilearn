40b127489a363eed66164d76b96757b2ebfad86a
Alexandre Abraham <alexandre.abraham@cea.fr>
2012-09-19 16:16:43 +0200

    Increase utils code coverage

--- nisl/tests/test_utils.py
+++ nisl/tests/test_utils.py
@@ -2,13 +2,32 @@
 Test the utils module
 """
 
+import os
+
 import nose
 
 import numpy as np
 
+import nibabel
 from nibabel import Nifti1Image
 
 from .. import utils
+
+currdir = os.path.dirname(os.path.abspath(__file__))
+
+
+class PhonyNiimage:
+
+    def __init__(self):
+        self.data = np.ones((9, 9, 9, 9))
+        self.affine = np.ones((4, 4))
+
+    def get_data(self):
+        return self.data
+
+    def get_affine(self):
+        return self.affine
+
 
 def test_check_niimg():
     nose.tools.assert_raises(TypeError, utils.check_niimg, 0)
@@ -25,6 +44,16 @@ def test_check_niimgs():
                              niimg)
     # This shouldn't raise an error
     utils.check_niimgs(niimg, accept_3d=True)
+    
+    # Test a Niimage that does not hold a shape attribute
+    phony_niimg = PhonyNiimage()
+    utils.check_niimgs(phony_niimg)
+
+
+def _remove_if_exists(file):
+    if os.path.exists(file):
+        os.remove(file)
+
 
 
 def test_concat_niimgs():
@@ -35,3 +64,15 @@ def test_concat_niimgs():
     nose.tools.assert_raises(ValueError, utils.concat_niimgs,
                             [niimg1, niimg2])
 
+    tmpimg1 = os.path.join(currdir, "tmp1.nii")
+    tmpimg2 = os.path.join(currdir, "tmp2.nii")
+    try:
+        _remove_if_exists(tmpimg1)
+        _remove_if_exists(tmpimg2)
+        nibabel.save(niimg1, tmpimg1)
+        nibabel.save(niimg2, tmpimg2)
+        nose.tools.assert_raises(ValueError, utils.concat_niimgs,
+                                [tmpimg1, tmpimg2])
+    finally:
+        _remove_if_exists(tmpimg1)
+        _remove_if_exists(tmpimg2)
