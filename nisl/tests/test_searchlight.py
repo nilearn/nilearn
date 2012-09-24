1c60ead40d11f548be8c0814fc23d93f31c19450
Alexandre Abraham <alexandre.abraham@cea.fr>
2012-09-24 09:17:42 +0200

    Fix header in test_searchlight

--- nisl/tests/test_searchlight.py
+++ nisl/tests/test_searchlight.py
@@ -1,6 +1,12 @@
+"""
+Test the searchlight module
+"""
+# Author: Alexandre Abraham
+# License: simplified BSD
+
 import numpy as np
 
-from nose.tools import assert_true
+from nose.tools import assert_equal, assert_true
 
 # Create a toy dataset to run searchlight on
 
@@ -14,9 +20,9 @@ cond = np.array([int(i > (frames / 2)) for i in range(frames)])
 
 # Create some activation pixels. Points are not connected.
 mask = np.zeros((10, 10, 10), np.bool)
-for i in range(2, 5, 2):
-    for j in range(3, 6, 2):
-        for k in range(5, 8, 2):
+for i in [2, 4, 6]:
+    for j in [3, 5, 7]:
+        for k in [5, 7, 9]:
             mask[i, j, k] = True
 
 # Apply a fake activation when condition is 1 (consider 0 as resting state)
@@ -34,23 +40,23 @@ from nisl import searchlight
 n_jobs = 1
 
 # The radius is the one of the Searchlight sphere that will scan the volume
-# Small radius : we only get 1 point
+# Small radius
 sl = searchlight.SearchLight(mask, mask, radius=1.5,
                 n_jobs=n_jobs, score_func=score_func, cv=cv)
 sl.fit(data, cond)
-assert_true(np.nonzero(sl.scores_)[0].size == 1)
+assert_equal(np.nonzero(sl.scores_)[0].size, 6)
 
 # Medium radius : Searchlight start to grab some points
 sl = searchlight.SearchLight(mask, mask, radius=2.5,
                 n_jobs=n_jobs, score_func=score_func, cv=cv)
 sl.fit(data, cond)
-assert_true(np.nonzero(sl.scores_)[0].size == 6)
+assert_equal(np.nonzero(sl.scores_)[0].size, 22)
 
 # Big radius (> sqrt(2) * 2), we get all activation points
 sl = searchlight.SearchLight(mask, mask, radius=3,
                 n_jobs=n_jobs, score_func=score_func, cv=cv)
 sl.fit(data, cond)
-assert_true(np.nonzero(sl.scores_)[0].size == 8)
+assert_equal(np.nonzero(sl.scores_)[0].size, 27)
 
 assert_true(sl.scores_[mask].all() > 0.1)
 assert_true(sl.scores_[-mask].all() == 0)
