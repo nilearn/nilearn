dff9045253b412eb0c05252a49312886c3eef99b
Alexandre Abraham <alexandre.abraham@cea.fr>
2012-11-30 15:14:22 +0100

    Add test for reading md5 sum files

--- nisl/tests/test_datasets.py
+++ nisl/tests/test_datasets.py
@@ -10,7 +10,7 @@ from tempfile import mkdtemp, mktemp
 import numpy as np
 
 from nose import with_setup
-from nose.tools import assert_true, assert_equal
+from nose.tools import assert_true, assert_false, assert_equal
 
 from .. import datasets
 from ..testing import mock_urllib2, mock_chunk_read_, mock_uncompress_file, \
@@ -40,6 +40,21 @@ def test_md5_sum_file():
     out.write('abcfeg')
     out.close()
     assert_equal(datasets._md5_sum_file(f), '18f32295c556b2a1a3a8e68fe1ad40f7')
+    os.remove(f)
+
+
+def test_read_md5_sum_file():
+    # Create dummy temporary file
+    f = mktemp()
+    out = open(f, 'w')
+    out.write('20861c8c3fe177da19a7e9539a5dbac  /tmp/test\n'
+              '70886dcabe7bf5c5a1c24ca24e4cbd94  test/some_image.nii')
+    out.close()
+    h = datasets._read_md5_sum_file(f)
+    assert_true('/tmp/test' in h)
+    assert_false('/etc/test' in h)
+    assert_equal(h['test/some_image.nii'], '70886dcabe7bf5c5a1c24ca24e4cbd94')
+    assert_equal(h['/tmp/test'], '20861c8c3fe177da19a7e9539a5dbac')
     os.remove(f)
 
 
