f89d396b8a18b1cec4f9a1639d9efbb0238729c5
Philippe Gervais <philippe.gervais@inria.fr>
2013-04-08 13:49:06 +0200

    Added conversion function for ROI data structures
    Handling of ROI can be made using different kinds of data structures,
    namely a 4D array, a list of 3D arrays, and a 3D array of labels.
    Conversion functions between these formats have been written (six
    functions).

--- nisl/roi.py
+++ nisl/roi.py
@@ -27,11 +27,13 @@ def apply_roi(timeseries, regions, normalize_regions=False):
     timeseries (2D numpy array)
         Masked data (e.g. output of apply_mask())
         shape: (instant number, voxel number)
+
     regions (2D numpy array)
         shape: (voxel number, region number)
         Region definitions. One column of this array defines one
         region, given by its weight on each voxel. Voxel numbering
         must match that of `timeseries`.
+
     normalize_regions (boolean)
         If True, normalize output by
         (regions ** 2).sum(axis=0) / regions.sum(axis=0)
@@ -58,3 +60,280 @@ def unapply_roi(timeseries_roi, regions):
     See also apply_roi().
     """
     return np.dot(timeseries_roi, regions.T)
+
+# region array: 4D array, (x, y, z, region number) values are weights (nifti-like)
+# region list: list of 3D arrays [(x, y, z)] values are weights
+# region labels: 3D array, (x, y, z) values are labels.
+
+# masked regions: 2D array, (region number, voxel number) values are weights.
+# apply_mask/unmask to convert to 4D array
+
+
+def _regions_are_overlapping_masked(regions_masked):
+    """Predicate telling if any two regions are overlapping.
+
+    Parameters
+    ==========
+    regions_masked (numpy.ndarray)
+        shape (region number, voxel number). Values are weights.
+
+    """
+    count = np.where(regions_masked > 0, 1, 0).sum(axis=1)
+    return np.any(count > 1)
+
+
+def _regions_are_overlapping_array(regions_array):
+    regions = np.where(regions_array != 0, 1, 0)
+    return regions.sum(axis=-1).max() > 1
+
+
+def _regions_are_overlapping_list(regions_list):
+    """Predicate telling if any two regions are overlapping.
+
+    Parameters
+    ==========
+    regions_list (list)
+        Region definition as a list of 3D arrays
+
+    Returns
+    =======
+    predicate (boolean)
+        True if two regions are overlapping, False otherwise.
+    """
+
+    count = np.zeros(regions_list[0].shape)
+    for region in regions_list:
+        count += region > 0
+
+    return count.max() > 1
+
+
+def regions_are_overlapping(regions):
+    """Predicate telling if any two regions are overlapping."""
+
+    if isinstance(regions, list):
+        predicate = _regions_are_overlapping_list(regions)
+    elif isinstance(regions, np.ndarray):
+        if regions.ndim == 2:
+            predicate = _regions_are_overlapping_masked(regions)
+        elif regions.ndim == 3:  # labeled array
+            predicate = False
+        elif regions.ndim == 4:  # arrays of 3D-arrays
+            predicate = _regions_are_overlapping_array(regions)
+        else:
+            raise TypeError("input array may have 2 to 4 dimensions.")
+
+    else:
+        raise TypeError("type not understood")
+
+    return predicate
+
+
+def regions_labels_to_array(region_labels, dtype=np.bool):
+    """Convert regions expressed as labels to a 4D array with weights.
+
+    Parameters
+    ==========
+    region_labels (numpy.ndarray)
+        3D array, with integer labels as values
+
+    dtype (numpy dtype)
+        dtype of the returned array. Defaults to boolean.
+
+    Returns
+    =======
+    region_array (numpy.ndarray)
+        shape: region_labels.shape + (len(labels),)
+    labels (list)
+        labels[i] is the label for the region region_array[..., i]
+
+    See also
+    ========
+    nisl.roi.regions_array_to_labels
+    """
+
+    # FIXME: should display a warning if array has not integer values
+    labels = np.unique(region_labels)
+    regions_array = np.zeros(region_labels.shape + (len(labels), ),
+                             dtype=dtype)
+
+    for n, label in enumerate(labels):
+        regions_array[..., n] = np.where(region_labels == label, 1, 0)
+
+    return regions_array, list(labels)
+
+
+def regions_array_to_labels(region_array, labels=None, background_label=0):
+    """Convert regions expressed as a 4D array into a labeled array.
+
+    region_array can contain overlapping regions, but return values cannot.
+    A point in space is always labeled with the region with highest weight.
+    Zero is "outside".
+
+    Parameters
+    ==========
+    region_array (numpy.ndarray)
+        shape: region_labels.shape + (len(labels),)
+    labels (list or numpy.ndarray, optional)
+        labels[i] is the label for the region region_array[..., i]
+        Defaults to range(1, region_array.shape(-1) + 1)
+    background_label (number)
+        label used for points contained in no region.
+
+    Returns
+    =======
+    region_labels (numpy.ndarray)
+        3D array, with integer labels as values
+
+    See also
+    ========
+    nisl.roi.regions_labels_to_array
+
+    """
+    if labels is not None and (len(labels) != region_array.shape[-1]):
+        raise ValueError("number of labels and number of regions do not match")
+
+    region_labels = region_array.argmax(axis=-1)
+    if labels is None:
+        region_labels += 1
+    else:
+        if not isinstance(labels, np.ndarray):
+            labels = np.asarray(labels)
+        region_labels = labels[region_labels]
+
+    # FIXME: use a different scheme for short dtype (like int8) for which
+    # an overflow can occur in sum()
+    mask = region_array.sum(axis=-1)  # every weight should be positive
+    region_labels[mask < 10. * np.finfo(float).eps] = background_label
+    return region_labels
+
+
+def regions_array_to_list(regions_array, copy=False):
+    """Convert a numpy array into an list of arrays with one dimension less.
+
+    By default, data are NOT copied. Arrays in list are views of the
+    input array. If a copy is required, use the `copy` keyword.
+
+    Parameters
+    ==========
+    regions_array (numpy.ndarray)
+        arbitrary input array, with two or more dimensions.
+
+    Returns
+    =======
+    regions_list (list on numpy.ndarray)
+        list of slices of regions_array such that:
+        regions_list[n] == regions_array[..., n]
+
+    """
+    regions_list = []
+    for n in xrange(regions_array.shape[-1]):
+        if copy:
+            regions_list.append(regions_array[..., n].copy())
+        else:
+            regions_list.append(regions_array[..., n])
+    return regions_list
+
+
+def regions_list_to_array(regions_list):
+    """Turn a list of arrays into one array with an extra dimension.
+
+    Data are copied.
+
+    Parameters
+    ==========
+    regions_list (list on numpy.ndarray)
+        Every array in the list must have the same shape.
+
+    Returns
+    =======
+    regions_array (numpy.ndarray)
+        Concatenation of all input arrays, along a new last dimension.
+    """
+    regions_array = np.ndarray(regions_list[0].shape + (len(regions_list),))
+    for n, arr in enumerate(regions_list):
+        regions_array[..., n] = arr
+    return regions_array
+
+
+def regions_list_to_labels(regions_list, labels=None, background_label=0):
+    """Convert a list of regions into an array of labels.
+
+    If regions overlap, they are made non-overlapping (see
+    regions_array_to_labels for details)
+
+    Parameters
+    ==========
+    regions_list (list of 3D numpy.ndarray)
+        Every array must have the same shape.
+
+    labels (list of number)
+        labels[n] gives the label to use for region defined by regions_list[n]
+        If not provided, defaults to range(1, len(regions_list) + 1)
+
+    background_label (number)
+        gives the "no region" label
+
+    Returns
+    =======
+    region_labels (numpy.ndarray)
+        3D numpy array, with integer labels as values
+        shape: regions_list[0].shape
+
+    See also
+    ========
+    regions_array_to_labels
+    """
+
+    regions_labels = background_label * np.ones(regions_list[0].shape)
+    regions_max = np.zeros(regions_list[0].shape)
+
+    if labels is None:
+        labels = range(1, len(regions_list) + 1)
+
+    for region, label in zip(regions_list, labels):
+        mask = region > regions_max
+        regions_labels[mask] = label
+        regions_max[mask] = region[mask]
+
+    return regions_labels
+
+
+def regions_labels_to_list(regions_labels, background_label=0,
+                           dtype=np.bool):
+    """Convert an array of labels into a list of region arrays.
+
+    Parameters
+    ==========
+    regions_labels (numpy.ndarray)
+        regions defined using labels in an array (a label is a number)
+    background_label (number)
+        label used in regions_labels for "background". No output is generated
+        for this label. "None" means no background.
+    dtype (numpy dtype)
+        dtype of returned arrays in list (defaults to numpy.bool)
+
+    Returns
+    =======
+    regions_list (list on numpy.ndarray)
+        Region definition using weights.
+    labels (list)
+        Label used for each region in input array. labels[n] is the label
+        used in regions_labels for regions_list[n].
+    """
+
+    # get region labels
+    labels = list(np.unique(regions_labels))
+    if background_label is not None:
+        # FIXME: catch ValueError (raised when background_label is not
+        # in labels)
+        labels.remove(background_label)
+
+    regions_list = []
+    for label in labels:
+        region = regions_labels == label
+        if dtype != np.bool:
+            region = region.astype(dtype)  # copy
+        regions_list.append(region)
+    return regions_list, labels
+
--- nisl/tests/test_roi.py
+++ nisl/tests/test_roi.py
@@ -6,6 +6,8 @@ Test for roi module.
 import numpy as np
 import scipy.signal as spsignal
 from .. import roi
+from .. import masking
+from nose.tools import assert_raises
 
 
 def generate_timeseries(instant_number, feature_number,
@@ -23,6 +25,11 @@ def generate_hb_regions(feature_number, region_number,
     ==========
     window (str)
         Name of a window in scipy.signal. e.g. "hamming".
+
+    Returns
+    =======
+    regions (numpy.ndarray)
+        shape (feature_number, region_number)
     """
 
     assert(feature_number > region_number)
@@ -51,6 +58,16 @@ def generate_hb_regions(feature_number, region_number,
     return regions
 
 
+def generate_labeled_regions(shape, region_number):
+    """Generate a 3D volume with labeled regions"""
+    voxel_number = shape[0] * shape[1] * shape[2]
+    regions = generate_hb_regions(voxel_number, region_number)
+    # replace weights with labels
+    for n, col in enumerate(regions.T):
+        col[col > 0] = n + 1
+    return masking.unmask(regions.sum(axis=1), np.ones(shape, dtype=np.bool))
+
+
 def test_apply_roi():
     instant_number = 101
     voxel_number = 54
@@ -74,3 +91,162 @@ def test_apply_roi():
                                normalize_regions=True)
     region_signals = timeseries.T[indices].T
     np.testing.assert_almost_equal(recovered2, region_signals)
+
+
+def test_regions_convert():
+    """Test of conversion functions between different regions structures.
+    Function tested:
+    regions_labels_to_array
+    regions_array_to_labels
+    """
+
+    shape = (4, 5, 6)
+    region_number = 11
+
+    ## labels <-> 4D array
+    regions_labels = generate_labeled_regions(shape, region_number)
+
+    # FIXME: test dtype argument
+    # FIXME: test labels argument
+    regions_4D, labels = roi.regions_labels_to_array(regions_labels)
+    assert(regions_4D.shape == shape + (region_number,))
+    regions_labels_recovered = roi.regions_array_to_labels(regions_4D)
+
+    np.testing.assert_almost_equal(regions_labels_recovered, regions_labels)
+
+    ## 4D array <-> list of 3D arrays
+    regions_list = roi.regions_array_to_list(regions_4D, copy=False)
+    assert (len(regions_list) == regions_4D.shape[-1])
+    for n in xrange(regions_4D.shape[-1]):
+        np.testing.assert_almost_equal(regions_list[n], regions_4D[..., n])
+
+    regions_4D_recovered = roi.regions_list_to_array(regions_list)
+    np.testing.assert_almost_equal(regions_4D_recovered, regions_4D)
+    # check that arrays in list are views (modifies arrays)
+    for n in xrange(regions_4D.shape[-1]):
+        regions_list[n][0, 0, 0] = False
+        regions_4D[0, 0, 0, n] = True
+        assert(regions_list[n][0, 0, 0] == regions_4D[0, 0, 0, n])
+
+    # Assert that data have been copied
+    regions_list = roi.regions_array_to_list(regions_4D, copy=True)
+    for n in xrange(regions_4D.shape[-1]):
+        regions_list[n][0, 0, 0] = False
+        regions_4D[0, 0, 0, n] = True
+        assert(regions_list[n][0, 0, 0] != regions_4D[0, 0, 0, n])
+
+    # Use "labels" argument
+    regions_labels += 3
+    regions_4D, labels = roi.regions_labels_to_array(regions_labels)
+    regions_labels_recovered = roi.regions_array_to_labels(regions_4D,
+                                                           labels=labels)
+    np.testing.assert_almost_equal(regions_labels_recovered, regions_labels)
+    regions_labels_recovered = roi.regions_array_to_labels(regions_4D,
+                                                           labels=np.asarray(labels))
+    np.testing.assert_almost_equal(regions_labels_recovered, regions_labels)
+
+    assert_raises(ValueError, roi.regions_array_to_labels,
+                  regions_4D, labels=[])
+
+    ## list of 3D arrays <-> labels
+    # First case
+    regions_labels = generate_labeled_regions(shape, region_number)
+    regions_list, labels = roi.regions_labels_to_list(regions_labels,
+                                              background_label=1)
+    assert(len(labels) == len(regions_list))
+    assert(len(regions_list) == region_number - 1)
+    assert(regions_list[0].shape == regions_labels.shape)
+    assert(regions_list[0].dtype == np.bool)
+    regions_labels_recovered = roi.regions_list_to_labels(regions_list,
+                                                          labels=labels,
+                                                          background_label=1)
+    np.testing.assert_almost_equal(regions_labels, regions_labels_recovered)
+
+    # same with different dtype
+    regions_list, labels = roi.regions_labels_to_list(regions_labels,
+                                                      background_label=1,
+                                                      dtype=np.float)
+    assert(regions_list[0].dtype == np.float)
+    regions_labels_recovered = roi.regions_list_to_labels(regions_list,
+                                                          labels=labels,
+                                                          background_label=1)
+    np.testing.assert_almost_equal(regions_labels, regions_labels_recovered)
+
+
+    # second case: no background
+    regions_labels = generate_labeled_regions(shape, region_number)
+    regions_list, labels = roi.regions_labels_to_list(regions_labels,
+                                                      background_label=None)
+    assert(len(labels) == len(regions_list))
+    assert(len(regions_list) == region_number)
+    assert(regions_list[0].shape == regions_labels.shape)
+
+    regions_labels_recovered = roi.regions_list_to_labels(regions_list)
+    np.testing.assert_almost_equal(regions_labels, regions_labels_recovered)
+
+    ## check conversion consistency (labels -> 4D -> list -> labels)
+    # loop one way
+    regions_labels = generate_labeled_regions(shape, region_number)
+    regions_array, _ = roi.regions_labels_to_array(regions_labels)
+    regions_list = roi.regions_array_to_list(regions_array)
+    regions_labels_recovered = roi.regions_list_to_labels(regions_list)
+
+    np.testing.assert_almost_equal(regions_labels_recovered, regions_labels)
+
+    # loop the other way
+    regions_list, _ = roi.regions_labels_to_list(regions_labels,
+                                                 background_label=None)
+    regions_array = roi.regions_list_to_array(regions_list)
+    regions_labels_recovered = roi.regions_array_to_labels(regions_array)
+
+    np.testing.assert_almost_equal(regions_labels_recovered, regions_labels)
+
+
+def test_regions_are_overlapping():
+    """Test of regions_are_overlapping()"""
+
+    shape = (4, 5, 6)
+    voxel_number = shape[0] * shape[1] * shape[2]
+    region_number = 11
+
+
+    # masked array of labels
+    regions = generate_hb_regions(voxel_number, region_number,
+                                  window="hamming")
+    assert(not roi.regions_are_overlapping(regions))
+
+    regions[0, :2] = 1  # make regions overlap
+    assert(roi.regions_are_overlapping(regions))
+
+    # 3D volume with labels. No possible overlap.
+    regions_labels = generate_labeled_regions(shape, region_number)
+    assert(not roi.regions_are_overlapping(regions_labels))
+
+    # 4D volume, with weights
+    regions_4D, labels = roi.regions_labels_to_array(regions_labels)
+    assert(not roi.regions_are_overlapping(regions_4D))
+
+    regions_4D[0, 0, 0, :2] = 1  # Make regions overlap
+    assert(roi.regions_are_overlapping(regions_4D))
+
+    # List of arrays
+    regions_list = roi.regions_array_to_list(regions_4D)
+    assert(roi.regions_are_overlapping(regions_list))
+
+    regions_4D, labels = roi.regions_labels_to_array(regions_labels)
+    regions_list = roi.regions_array_to_list(regions_4D)
+    assert(not roi.regions_are_overlapping(regions_list))
+
+    # Bad input
+    assert_raises(TypeError, roi.regions_are_overlapping, None)
+    assert_raises(TypeError, roi.regions_are_overlapping,
+                  np.zeros((2, 2, 2, 2, 2)))
+
+
+
+    # TODO:
+    # - check with / without labels
+    # - check overlapping / not overlapping
+    # - check with / without holes
+    # - check length consistency assertion
+
