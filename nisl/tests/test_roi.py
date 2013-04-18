f283e27ae5811ad65867b64915575a9fb4bfc596
Philippe Gervais <philippe.gervais@inria.fr>
2013-04-18 13:23:45 +0200

    Added overlap parameter to signal generator
    Coding-style fixes in test_roi.py

--- nisl/tests/test_roi.py
+++ nisl/tests/test_roi.py
@@ -10,58 +10,69 @@ from .. import masking
 from nose.tools import assert_raises
 
 
-def generate_timeseries(instant_number, feature_number,
-                        randgen=np.random.RandomState(0)):
+def generate_timeseries(n_instants, n_features,
+                        randgen=None):
     """Generate some random timeseries. """
-    return randgen.randn(instant_number, feature_number)
+    if randgen is None:
+        randgen = np.random.RandomState(0)
+    return randgen.randn(n_instants, n_features)
 
 
-def generate_hb_regions(feature_number, region_number,
-                        randgen=np.random.RandomState(0),
-                        window=None):
-    """Generate some non-overlapping regions.
+def generate_regions_ts(n_features, n_regions,
+                        overlap=0,
+                        randgen=None,
+                        window="boxcar"):
+    """Generate some regions.
 
     Parameters
     ==========
+    overlap (int)
+        Number of overlapping voxels between two regions (more or less)
     window (str)
         Name of a window in scipy.signal. e.g. "hamming".
 
     Returns
     =======
     regions (numpy.ndarray)
-        shape (feature_number, region_number)
+        timeseries representing regions.
+        shape (n_features, n_regions)
     """
 
-    assert(feature_number > region_number)
+    if randgen is None:
+        randgen = np.random.RandomState(0)
+    if window is None:
+        window = "boxcar"
+
+    assert(n_features > n_regions)
 
     # Compute region boundaries indices.
     # Start at 1 to avoid getting an empty region
-    boundaries = np.zeros(region_number + 1)
-    boundaries[-1] = feature_number
-    boundaries[1:-1] = randgen.permutation(range(1, feature_number)
-                                           )[:region_number - 1]
+    boundaries = np.zeros(n_regions + 1)
+    boundaries[-1] = n_features
+    boundaries[1:-1] = randgen.permutation(range(1, n_features)
+                                           )[:n_regions - 1]
     boundaries.sort()
 
-    regions = np.zeros((feature_number, region_number))
+    regions = np.zeros((n_features, n_regions))
+    overlap_end = int((overlap + 1) / 2)
+    overlap_start = int(overlap / 2)
     for n in xrange(len(boundaries) - 1):
-        if window is not None:
-            win = spsignal.get_window(window,
-                                      boundaries[n + 1] - boundaries[n])
-            win /= win.mean()  # unity mean
-            regions[boundaries[n]:boundaries[n + 1], n] = win
-        else:
-            regions[boundaries[n]:boundaries[n + 1], n] = 1
+        start = max(0, boundaries[n] - overlap_start)
+        end = min(n_features, boundaries[n + 1] + overlap_end)
+        win = spsignal.get_window(window, end - start)
+        win /= win.mean()  # unity mean
+        regions[start:end, n] = win
 
-    # Check that no regions overlap
-    np.testing.assert_array_less((regions > 0).sum(axis=-1) - 0.1,
-                                 np.ones(regions.shape[0]))
+    ## # Check that no regions overlap
+    ## np.testing.assert_array_less((regions > 0).sum(axis=-1) - 0.1,
+    ##                              np.ones(regions.shape[0]))
     return regions
 
 
-def generate_labeled_regions(shape, region_number):
+def generate_labeled_regions(shape, n_regions, randgen=None):
     """Generate a 3D volume with labeled regions"""
-    voxel_number = shape[0] * shape[1] * shape[2]
-    regions = generate_hb_regions(voxel_number, region_number)
+    n_voxels = shape[0] * shape[1] * shape[2]
+    regions = generate_regions_ts(n_voxels, n_regions, randgen=randgen)
     # replace weights with labels
     for n, col in enumerate(regions.T):
         col[col > 0] = n + 1
@@ -69,14 +80,14 @@ def generate_labeled_regions(shape, region_number):
 
 
 def test_apply_roi():
-    instant_number = 101
-    voxel_number = 54
-    region_number = 11
+    n_instants = 101
+    n_voxels = 54
+    n_regions = 11
 
     # First generate signal based on _non-overlapping_ regions, then do
     # the reverse. Check that the starting signals are recovered.
-    ts_roi = generate_timeseries(instant_number, region_number)
-    regions_nonflat = generate_hb_regions(voxel_number, region_number,
+    ts_roi = generate_timeseries(n_instants, n_regions)
+    regions_nonflat = generate_regions_ts(n_voxels, n_regions,
                                           window="hamming")
     regions = np.where(regions_nonflat > 0, 1, 0)
     timeseries = roi.unapply_roi(ts_roi, regions)
@@ -101,15 +112,15 @@ def test_regions_convert():
     """
 
     shape = (4, 5, 6)
-    region_number = 11
+    n_regions = 11
 
     ## labels <-> 4D array
-    regions_labels = generate_labeled_regions(shape, region_number)
+    regions_labels = generate_labeled_regions(shape, n_regions)
 
     # FIXME: test dtype argument
     # FIXME: test labels argument
     regions_4D, labels = roi.regions_labels_to_array(regions_labels)
-    assert(regions_4D.shape == shape + (region_number,))
+    assert(regions_4D.shape == shape + (n_regions,))
     regions_labels_recovered = roi.regions_array_to_labels(regions_4D)
 
     np.testing.assert_almost_equal(regions_labels_recovered, regions_labels)
@@ -142,7 +153,7 @@ def test_regions_convert():
                                                            labels=labels)
     np.testing.assert_almost_equal(regions_labels_recovered, regions_labels)
     regions_labels_recovered = roi.regions_array_to_labels(regions_4D,
-                                                           labels=np.asarray(labels))
+                                                 labels=np.asarray(labels))
     np.testing.assert_almost_equal(regions_labels_recovered, regions_labels)
 
     assert_raises(ValueError, roi.regions_array_to_labels,
@@ -150,11 +161,11 @@ def test_regions_convert():
 
     ## list of 3D arrays <-> labels
     # First case
-    regions_labels = generate_labeled_regions(shape, region_number)
+    regions_labels = generate_labeled_regions(shape, n_regions)
     regions_list, labels = roi.regions_labels_to_list(regions_labels,
                                               background_label=1)
     assert(len(labels) == len(regions_list))
-    assert(len(regions_list) == region_number - 1)
+    assert(len(regions_list) == n_regions - 1)
     assert(regions_list[0].shape == regions_labels.shape)
     assert(regions_list[0].dtype == np.bool)
     regions_labels_recovered = roi.regions_list_to_labels(regions_list,
@@ -172,13 +183,12 @@ def test_regions_convert():
                                                           background_label=1)
     np.testing.assert_almost_equal(regions_labels, regions_labels_recovered)
 
-
     # second case: no background
-    regions_labels = generate_labeled_regions(shape, region_number)
+    regions_labels = generate_labeled_regions(shape, n_regions)
     regions_list, labels = roi.regions_labels_to_list(regions_labels,
                                                       background_label=None)
     assert(len(labels) == len(regions_list))
-    assert(len(regions_list) == region_number)
+    assert(len(regions_list) == n_regions)
     assert(regions_list[0].shape == regions_labels.shape)
 
     regions_labels_recovered = roi.regions_list_to_labels(regions_list)
@@ -186,7 +196,7 @@ def test_regions_convert():
 
     ## check conversion consistency (labels -> 4D -> list -> labels)
     # loop one way
-    regions_labels = generate_labeled_regions(shape, region_number)
+    regions_labels = generate_labeled_regions(shape, n_regions)
     regions_array, _ = roi.regions_labels_to_array(regions_labels)
     regions_list = roi.regions_array_to_list(regions_array)
     regions_labels_recovered = roi.regions_list_to_labels(regions_list)
@@ -206,12 +216,11 @@ def test_regions_are_overlapping():
     """Test of regions_are_overlapping()"""
 
     shape = (4, 5, 6)
-    voxel_number = shape[0] * shape[1] * shape[2]
-    region_number = 11
-
+    n_voxels = shape[0] * shape[1] * shape[2]
+    n_regions = 11
 
     # masked array of labels
-    regions = generate_hb_regions(voxel_number, region_number,
+    regions = generate_regions_ts(n_voxels, n_regions,
                                   window="hamming")
     assert(not roi.regions_are_overlapping(regions))
 
@@ -219,7 +228,7 @@ def test_regions_are_overlapping():
     assert(roi.regions_are_overlapping(regions))
 
     # 3D volume with labels. No possible overlap.
-    regions_labels = generate_labeled_regions(shape, region_number)
+    regions_labels = generate_labeled_regions(shape, n_regions)
     assert(not roi.regions_are_overlapping(regions_labels))
 
     # 4D volume, with weights
@@ -249,4 +258,3 @@ def test_regions_are_overlapping():
     # - check overlapping / not overlapping
     # - check with / without holes
     # - check length consistency assertion
-
