852a4c268f60e2836ee052461f0a0561bd61d94d
Alexandre Abraham <abraham.alexandre@gmail.com>
2013-06-12 14:57:26 +0200

    Make test work for new CanICA

--- nisl/decomposition/tests/test_canica.py
+++ nisl/decomposition/tests/test_canica.py
@@ -2,14 +2,15 @@
 import nibabel
 import numpy as np
 from numpy.testing import assert_array_equal
-from nisl.decomposition.old_canica import CanICA
+from nisl.decomposition.canica import CanICA
 
 
 def test_canica_square_img():
-    shape = (20, 20)
+    shape = (20, 20, 1)
+    affine = np.eye(4)
     rng = np.random.RandomState(0)
 
-    # Create four images with "activated regions"
+    # Create two images with "activated regions"
     component1 = np.zeros(shape)
     component1[:5, :10] = 1
     component1[5:10, :10] = -1
@@ -34,14 +35,21 @@ def test_canica_square_img():
     for i in range(8):
         this_data = np.dot(rng.normal(size=(40, 4)), components)
         this_data += .01 * rng.normal(size=this_data.shape)
-        data.append(this_data)
+        # Get back into 3D for CanICA
+        this_data = np.reshape(this_data, (40,) + shape)
+        this_data = np.rollaxis(this_data, 0, 4)
+        data.append(nibabel.Nifti1Image(this_data, affine))
 
-    canica = CanICA(n_components=4, random_state=rng)
+    mask_img = nibabel.Nifti1Image(np.ones(shape, dtype=np.int8), affine)
+
+    canica = CanICA(n_components=4, random_state=rng, mask=mask_img,
+                    smoothing_fwhm=0.)
     sparsity = np.infty
     for rs in range(50):
         canica.random_state = np.random.RandomState(rs)
         canica.fit(data)
-        maps_ = canica.components_
+        maps_ = canica.components_img_.get_data()
+        maps_ = np.rollaxis(maps_, 3, 0)
         sparsity_ = np.sum(np.abs(maps_), 1).max()
         if sparsity_ < sparsity:
             sparsity = sparsity_
@@ -55,7 +63,7 @@ def test_canica_square_img():
         map = np.abs(maps[i]) > np.abs(maps[i]).max() * 0.95
         for j in indices:
             ref_map = components[j].ravel() != 0
-            if np.all(map == ref_map):
+            if np.all(map.ravel() == ref_map):
                 indices.remove(j)
                 break
         else:
