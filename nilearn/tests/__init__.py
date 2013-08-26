07fca2f048f20488be669b5390274e23e1a2fdc5
Philippe Gervais <philippe.gervais@inria.fr>
2013-08-26 11:09:42 +0200

    Code cleanup

--- nilearn/tests/__init__.py
+++ nilearn/tests/__init__.py
@@ -3,10 +3,11 @@
 
 # These imports are useful to launch a single test with nosetests
 from . import test_masking
+from . import test_class_inspect
 from . import test_datasets
-from . import test_resampling
-from . import test_signal
-from . import test_image
-from . import test_region
 from . import test_logger
+from . import test_masking
 from . import test_ndimage
+from . import test_niimg_conversions
+from . import test_region
+from . import test_signal
