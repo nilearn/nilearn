fe5cc54e36d3037f6288ae6f21a7d601e02445e1
Michael Eickenberg <michael.eickenberg@gmail.com>
2013-10-31 16:29:47 +0100

    added crop_img to nilearn.image.__init__.all

--- nilearn/image/__init__.py
+++ nilearn/image/__init__.py
@@ -3,7 +3,7 @@ Mathematical operations working on niimgs like -a (3+n)-D block of data,
 and an affine.
 """
 from .resampling import resample_img
-from .image import high_variance_confounds, smooth
+from .image import high_variance_confounds, smooth, crop_img
 
-__all__ = ['resample_img', 'high_variance_confounds', 'smooth']
+__all__ = ['resample_img', 'high_variance_confounds', 'smooth', 'crop_img']
 
