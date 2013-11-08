b3c2b20359da85d57657378e4c251f957a6c1da1
Michael <michael.eickenberg@gmail.com>
2013-11-08 14:39:14 +0100

    scoped numpy, scipy, sklearn imports, added import for input_data

--- nilearn/__init__.py
+++ nilearn/__init__.py
@@ -9,20 +9,27 @@ visualisation tools.
 See http://nilearn.github.com for complete documentation.
 """
 
-try:
-    import numpy
-except ImportError:
-    print 'Numpy could not be found, please install it properly to use nilearn.'
 
-try:
-    import scipy
-except ImportError:
-    print 'Scipy could not be found, please install it properly to use nilearn.'
+def _check_dependencies():
+    try:
+        import numpy
+    except ImportError:
+        print ('Numpy could not be found,'
+        ' please install it properly to use nilearn.')
 
-try:
-    import sklearn
-except ImportError:
-    print ('Scikit-learn could not be found,'
-           ' please install it properly to use nilearn.')
+    try:
+        import scipy
+    except ImportError:
+        print ('Scipy could not be found,'
+               ' please install it properly to use nilearn.')
+
+    try:
+        import sklearn
+    except ImportError:
+        print ('Scikit-learn could not be found,'
+               ' please install it properly to use nilearn.')
 
 __version__ = 0.1
+_check_dependencies()
+
+import input_data
