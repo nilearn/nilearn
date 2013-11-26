0847521ea28655895174cbb0ff20e90129395dc7
Alexandre Abraham <abraham.alexandre@gmail.com>
2013-11-27 00:08:44 +0100

    Add spectral clustering example

---
+++ plot_rest_spectral.py
@@ -0,0 +1,66 @@
+"""
+Spectral clustering to segment brain from resting state fMRI
+============================================================
+
+We build an affinity matrix between voxels using Pearson correlation coefficient
+and then use it to segment the brain into functional regions.
+"""
+
+from sklearn.feature_extraction.image import grid_to_graph
+
+### Load adhd dataset #########################################################
+
+import numpy as np
+import scipy as sp
+import pylab as pl
+from nilearn import datasets, input_data
+import time
+
+
+print 'Fetching dataset... '
+t0 = time.time()
+dataset = datasets.fetch_adhd(n_subjects=1)
+print '... done (%.2fs)' % (time.time() - t0)
+print 'Masking data... '
+t0 = time.time()
+nifti_masker = input_data.NiftiMasker(memory='nilearn_cache', memory_level=1,
+                              smoothing_fwhm=6., standardize=False)
+X = nifti_masker.fit_transform(dataset.func[0])
+mask = nifti_masker.mask_img_.get_data().astype(np.bool)
+print '... done (%.2fs)' % (time.time() - t0)
+
+### Spectral clustering #######################################################
+
+print 'Computing affinity matrix... '
+t0 = time.time()
+# Compute the connecitvity graph (it is sparse)
+connectivity = grid_to_graph(*mask.shape, mask=mask)
+
+# Compute the Pearson correlation matrix from data and connectivity
+rows, cols = connectivity.nonzero()
+values = np.zeros(rows.shape)
+for i, (r, c) in enumerate(zip(rows, cols)):
+    corr = sp.stats.pearsonr(X[:, r], X[:, c])[0]
+    if np.isnan(corr):
+        continue
+    if corr <= 0.5:
+        # Suppress negative values and sparsify matrix
+        corr = 0.
+    values[i] = corr
+correlation = sp.sparse.coo_matrix((values, (rows, cols)))
+print '... done (%.2fs)' % (time.time() - t0)
+
+# Apply spectral clustering on correlation matrix
+from sklearn.cluster import spectral_clustering
+
+print 'Running spectral clustering... '
+t0 = time.time()
+clustering = spectral_clustering(correlation,
+        n_clusters=50, assign_labels='discretize')
+print '... done (%.2fs)' % (time.time() - t0)
+
+clustering = nifti_masker.inverse_transform(clustering)
+
+pl.imshow(clustering.get_data()[..., 25] + 1, cmap='Set1',
+          interpolation='nearest')
+pl.show()
