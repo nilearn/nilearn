59755c88e407fa106682b470f810a8b98db93c36
Michael Eickenberg <michael.eickenberg@gmail.com>
2013-12-03 00:22:22 +0100

    Correlation Matrix and rudimentary svm in cross_val

--- haxby_full_analysis.py
+++ haxby_full_analysis.py
@@ -73,23 +73,9 @@ for subject_id in subject_ids:
     vt_timecourses = ventral_temporal_mask.transform(
         data_files.func[subject_id])
 
-    # load labels
-    labels = np.recfromcsv(data_files.session_target[subject_id],
-                           delimiter=" ")
-    # Possibly take a quick look at them
-    print "Label names in dataset:"
-    print np.unique(labels['labels'])
-    # This outputs:
-    # ['bottle' 'cat' 'chair' 'face' 'house' 'rest'
-    # 'scissors' 'scrambledpix' 'shoe']
-
-    # @@@@@@ Michael: If I understand the paper correctly, they work on the BOLD data. But maybe I am wrong. In the following, I will remove resting state by hand and divide into conditions. However, the design is orthogonal, and would almost stay that way even after convolution with an HRF, so maybe the way to go for this tutorial would rather be via Beta maps?
-    # find non resting state episodes
-    non_rest = labels["labels"] != "rest"
-
-    # The data consists of 12 concatenated runs of 121 timepoints each,
+    # The data consist of 12 concatenated runs of 121 timepoints each,
     # and we will reshape the data to reflect this. This is important, since
-    # the original paper correlates activations from even and odd runs
+    # detrending and standardizing needs to respect this structure
 
     vt_timecourses = vt_timecourses.reshape(12, 121, 
                                             vt_timecourses.shape[-1])
@@ -101,52 +87,128 @@ for subject_id in subject_ids:
     vt_timecourses = ((vt_timecourses
                        - vt_timecourses.mean(axis=1)[:, np.newaxis, :])
                        / vt_timecourses.std(axis=1)[:, np.newaxis, :])
-
+    # reshape back
+    vt_timecourses = vt_timecourses.reshape(12 * 121, -1)
     # We could take a look at some BOLD time courses
-    take_a_look_at_bold = True
+    take_a_look_at_bold = False
     if take_a_look_at_bold:
         import pylab as pl
         pl.figure()
-        pl.plot(vt_timecourses[0])
+        pl.plot(vt_timecourses[:121])
         pl.xlabel("TRs")
         pl.ylabel("Normalized BOLD")
         pl.title("Some BOLD time courses.")
-    # we reshape the non_rest indicator accordingly
-    non_rest = non_rest.reshape(12, 121)
 
-    # and find -- possibly by plotting it ...
-    plot_non_rest = False
-    if plot_non_rest:
-        import pylab as pl
-        pl.figure()
-        pl.plot(non_rest.T)
+    # load labels
+    labels = np.recfromcsv(data_files.session_target[subject_id],
+                           delimiter=" ")
+    # Possibly take a quick look at them
+    print "Label names in dataset:"
+    print np.unique(labels['labels'])
+    # This outputs:
+    # ['bottle' 'cat' 'chair' 'face' 'house' 'rest'
+    # 'scissors' 'scrambledpix' 'shoe']
 
-    # ... that the rest-non_rest structure is the same over all runs. We can
-    # assert the fact that all runs look like the first one like so:
-    assert (non_rest == non_rest[0:1]).all()
+    # Citing the paper:
+    """ To identify object-selective cortex, we used an eight-regressor model. The first regressor was the contrast between stimulus blocks and rest. The remaining seven regressors modeled the response to each meaningful category."""
+    # Let's do it:
+    unique_conditions = np.unique(labels)
+    # We first make a design matrix containing a regressor for each
+    # condition per run [e.g. ("shoe", 3), ("face", 12) and so on]
+    independent_design = (labels[:, np.newaxis]
+                  == unique_conditions[np.newaxis, :]).astype(np.float64)
 
-    # Now remove the resting state data
-    active_timecourses = vt_timecourses[:, non_rest[0], :]
+    # now we merge the regressors of the same category over different runs
+    unique_categories = np.unique(unique_conditions["labels"])
+    merge_table = (unique_conditions["labels"][:, np.newaxis]
+                   == unique_categories[np.newaxis, :])
 
-    active_labels = labels['labels'][non_rest.ravel()].reshape(12, 72)
+    category_design = independent_design.dot(merge_table)
 
-    # In printing the active labels, we find that within a run there is
-    # always the same block structure of 8 blocks of 9 TRs each, where 
-    # each type of stimulus is shown. However: The order is shuffled
-    # from run to run.
+    # category_design is almost what we want. We need to modify one 
+    # regressor, rest to rest_vs_active, and drop the scrambled regressor
 
-    print "Active labels"
-    print active_labels
+    rest_vs_active = (category_design[:, unique_categories == "rest"] -
+                      category_design[:, unique_categories != "rest"
+                                         ].sum(axis=1)[:, np.newaxis])
 
-    # We exploit the inner block structure of the runs by reshaping our
-    # data once more: 12 runs of 8 blocks each consisting of 9 TRs.
-    active_timecourses = active_timecourses.reshape(12, 8, 9,
-                                          active_timecourses.shape[-1])
+    active_categories = np.array([s not in ["rest", "scrambledpix"] 
+                                  for s in unique_categories])
 
-    # In reshaping the label array according to this same structure, 
-    # we can extract the label pertaining to each block
+    eight_regressor_design = np.hstack([rest_vs_active,
+                             category_design[:, active_categories]])
 
-    block_labels = active_labels.reshape(12, 8, 9)[..., 0]
-    # @@@@@ Michael: I am realizing more and more, that I may be doing too much acrobatics with ndarrays. More and more convinced that Beta maps are the way to go.
+    # Fit a GLM using this matrix
+    from sklearn.linear_model import LinearRegression
+    glm_eight = LinearRegression(fit_intercept=True)
 
-    # According to the paper, BOLD activity is 
+    glm_eight.fit(eight_regressor_design, vt_timecourses)
+
+    beta_maps = ventral_temporal_mask.inverse_transform(glm_eight.coef_.T)
+
+    # Citing the paper again
+    """To determine the patterns of response to each category on even-numbered and odd-numbered runs, we used a 16-regressor model - eight regressors to model the response to  each category relative to rest on even runs and eight regressors to model the response to each category on odd runs with no regressor that contrasteed all stimulus blocks to rest"""
+    # As unclear as this paragraph is, let us still try to make this matrix
+    even_runs = labels["chunks"] % 2 == 0
+    odd_runs = labels["chunks"] % 2 == 1
+
+    even_design = (even_runs[:, np.newaxis] * 
+                   independent_design).dot(merge_table)
+    odd_design = (odd_runs[:, np.newaxis] * 
+                   independent_design).dot(merge_table)
+
+    # now subtract the rest regressor from all the others
+    even_category_vs_rest = (even_design[:, unique_categories != "rest"] -
+                even_design[:, unique_categories == "rest"])
+    odd_category_vs_rest = (odd_design[:, unique_categories != "rest"] -
+                odd_design[:, unique_categories == "rest"])
+
+    sixteen_regressor_design = np.hstack([even_category_vs_rest,
+                                          odd_category_vs_rest])
+
+
+    # use this matrix in a GLM
+    glm_sixteen = LinearRegression(fit_intercept=True)
+    glm_sixteen.fit(sixteen_regressor_design, vt_timecourses)
+
+    activations = glm_sixteen.coef_
+    # as indicated in the paper, normalize activations across categories,
+    # (but not across even/odd runs)
+    normalized_activations = ((activations.reshape(-1, 2, 8)
+           - activations.reshape(-1, 2, 8).mean(-1)[..., np.newaxis])
+           / activations.reshape(-1, 2, 8).std(-1)[..., np.newaxis]
+                              ).reshape(-1, 16)
+
+    correlation_matrix = np.corrcoef(normalized_activations.T)
+
+    plot_labels = list(unique_categories[unique_categories != "rest"])
+
+    import pylab as pl
+    pl.figure()
+    pl.imshow(correlation_matrix, interpolation="nearest")
+    pl.title("Full correlation matrix, \nodd/even correlation on off diagonal blocks")
+    pl.yticks(range(16), plot_labels * 2)
+    pl.xticks(range(16), plot_labels * 2, rotation=90)
+    pl.jet()
+    pl.colorbar()
+    pl.show()
+
+
+
+    # Now try a quick and dirty SVM on the whole thing
+    from sklearn.svm import SVC
+    # from sklearn.feature_selection import f_classif, SelectKBest
+    # from sklearn.pipeline import Pipeline
+    from sklearn.multiclass import OneVsRestClassifier
+    from sklearn.cross_validation import cross_val_score
+    # feature_selection = SelectKBest(f_classif, 500)
+    classifier = OneVsRestClassifier(SVC(C=1.))
+
+    scores = cross_val_score(classifier, vt_timecourses, labels['labels'],
+                             cv=12, n_jobs=12, verbose=True)
+
+    # mean score around .8, chance level is at around .11111
+    # Note that I didn't even remove resting state here.
+
+
+
