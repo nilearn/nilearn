c1edcd06db88e40c4158af7feabfe23b7209f552
Alexandre Abraham <abraham.alexandre@gmail.com>
2013-12-06 16:41:04 +0100

    Add title to figures in image manipulation example.

--- plot_image_manipulation.py
+++ plot_image_manipulation.py
@@ -15,11 +15,12 @@ labels = numpy.genfromtxt(haxby.session_target[0], skip_header=1, usecols=[0],
 import matplotlib.pyplot as plt
 
 
-def display_axial(brain, i, cmap='hot'):
+def display_axial(brain, i, title, cmap='hot'):
     plt.figure()
     plt.imshow(brain[:, :, i].T.copy(), origin='lower', interpolation='nearest',
                cmap=cmap)
     plt.axis('off')
+    plt.title(title)
     plt.tight_layout()
 
 
@@ -28,7 +29,7 @@ from nilearn.image import smooth
 img = smooth(haxby.func[0], fwhm=6)
 data = img.get_data()
 affine = img.get_affine()
-display_axial(data.mean(axis=-1), z)
+display_axial(data.mean(axis=-1), z, 'Mean EPI')
 
 # Run a T-test for face and houses
 from scipy import stats
@@ -37,27 +38,27 @@ tv, pv = stats.ttest_ind(data[..., labels == 'face'],
 pv = - numpy.log10(pv)
 pv[numpy.isnan(pv)] = 0.
 pv[pv > 10] = 10
-display_axial(pv, z)
+display_axial(pv, z, 'p-values')
 
 # Thresholding
 pv[pv < 5] = 0
-display_axial(pv, z)
+display_axial(pv, z, 'Thresholded p-values')
 
 # Binarization and intersection with VT mask
 pv = (pv != 0)
 vt = nibabel.load(haxby.mask_vt[0]).get_data().astype(bool)
 pv = numpy.logical_and(pv, vt)
-display_axial(pv, z)
+display_axial(pv, z, 'Intersection with ventral temporal mask')
 
 # Dilation
 from scipy.ndimage import binary_dilation
 pv = binary_dilation(pv)
-display_axial(pv, z)
+display_axial(pv, z, 'Dilated mask')
 
 # Identification of connected components
 from scipy.ndimage import label
 labels, n_labels = label(pv)
-display_axial(labels, z, 'gnuplot')
+display_axial(labels, z, 'Connected components', 'gnuplot')
 
 # Save the result
 nibabel.save(nibabel.Nifti1Image(labels, affine), 'mask.nii')
