6ff5806349d569d980407a960951baf688673c3c
Gael Varoquaux <gael.varoquaux@normalesup.org>
2013-12-11 21:32:37 -0700

    Add mask plotting example

--- plot_haxby_full_analysis.py
+++ plot_haxby_full_analysis.py
@@ -10,87 +10,78 @@ Objects in Ventral Temporal Cortex"
 
 
 ### Fetch data using nilearn dataset fetcher ################################
-# specify how many subjects we want to load
-n_subjects = 2
-
 from nilearn import datasets
-data_files = datasets.fetch_haxby(n_subjects=n_subjects, fetch_stimuli=True)
-
-### Do first similarity analysis for all subjects in subjects list ##########
-subject_ids = [1]
+data_files = datasets.fetch_haxby(n_subjects=1)
 
 # Load nilearn NiftiMasker, the practical masking and unmasking tool
 from nilearn.input_data import NiftiMasker
+
+
+# load labels
 import numpy as np
+labels = np.recfromcsv(data_files.session_target[0], delimiter=" ")
+# identify resting state labels in order to be able to remove them
+resting_state = labels['labels'] == "rest"
+
+# find names of remaining active labels
+categories = np.unique(labels['labels'][resting_state == False])
+
+# extract tags indicating to which acquisition run a tag belongs
+session_labels = labels["chunks"][resting_state == False]
 
 
-for subject_id in subject_ids:
-
-    # load labels
-    labels = np.recfromcsv(data_files.session_target[subject_id],
-                           delimiter=" ")
-    # identify resting state labels in order to be able to remove them
-    resting_state = labels['labels'] == "rest"
-
-    # find names of remaining active labels
-    unique_labels = np.unique(labels['labels'][resting_state == False])
-
-    # extract tags indicating to which acquisition run a tag belongs
-    session_labels = labels["chunks"][resting_state == False]
+from sklearn.svm import SVC
+classifier = SVC(C=1., kernel="linear")
 
 
-    ### Let us now check the provided masks and do decoding on them
+# Make a data splitting object for cross validation
+from sklearn.cross_validation import LeaveOneLabelOut, cross_val_score
+cv = LeaveOneLabelOut(session_labels)
 
-    # Make a data splitting object for cross validation
-    from sklearn.cross_validation import LeaveOneLabelOut
-    cv = LeaveOneLabelOut(session_labels)
+mask_names = ['mask_vt', 'mask_face', 'mask_house']
 
-    from sklearn.svm import SVC
-    from sklearn.multiclass import OneVsRestClassifier
-    from sklearn.cross_validation import cross_val_score
-    classifier = OneVsRestClassifier(SVC(C=1., kernel="linear"))
+mask_scores = {}
+for mask_name in mask_names:
+    print "Working on mask %s" % mask_name
+    masker = NiftiMasker(mask=data_files[mask_name][0])
+    masked_timecourses = masker.fit_transform(
+        data_files.func[0])[resting_state == False]
 
+    mask_scores[mask_name] = {}
 
-    mask_names = ['mask_vt', 'mask_face', 'mask_face_little',
-                  'mask_house', 'mask_house_little']
+    for category in categories:
+        print "Processing %s %s" % (mask_name, category)
+        classification_target = \
+            labels['labels'][resting_state == False] == category
+        mask_scores[mask_name][category] = cross_val_score(
+            classifier,
+            masked_timecourses,
+            classification_target,
+            cv=cv, scoring="f1")
 
-    mask_scores = {}
-    for mask_name in mask_names:
-        print "Working on mask %s" % mask_name
-        masker = NiftiMasker(data_files[mask_name][subject_id])
-        masked_timecourses = masker.fit_transform(
-            data_files.func[subject_id])[resting_state == False]
+        print "Scores: %1.2f +- %1.2f" % (
+            mask_scores[mask_name][category].mean(),
+            mask_scores[mask_name][category].std())
 
-        mask_scores[mask_name] = {}
+# make a rudimentary diagram
+import matplotlib.pyplot as plt
+plt.figure()
 
-        for label in unique_labels:
-            print "Treating %s %s" % (mask_name, label)
-            classification_target = \
-                labels['labels'][resting_state == False] == label
-            mask_scores[mask_name][label] = cross_val_score(
-                classifier, 
-                masked_timecourses,
-                classification_target,
-                cv=cv,
-                n_jobs=1,
-                verbose=True,
-                scoring="f1")
+tick_position = np.arange(len(categories))
+plt.xticks(tick_position, categories, rotation=45)
 
-            print "Scores: %1.2f +- %1.2f" % (
-                mask_scores[mask_name][label].mean(),
-                mask_scores[mask_name][label].std())
+for color, (mask_name, mask_score) in zip('rgb', mask_scores.items()):
+    score_means = [mask_score[category].mean()
+                for category in categories]
+    plt.bar(tick_position, score_means, label=mask_name,
+            width=.25, color=color)
+    tick_position = tick_position + .2
 
-    # make a rudimentary diagram
-    import matplotlib.pyplot as plt
-    score_means = np.array([[mask_scores[mask_name][label].mean()
-                for label in unique_labels]
-                for mask_name in mask_names])
-    plt.figure(figsize=(10, 6))
-    plt.imshow(score_means, interpolation="nearest")
-    plt.xticks(np.arange(len(unique_labels)), unique_labels, rotation=45)
-    plt.yticks(np.arange(len(mask_names)) + .5, mask_names, rotation=45)
-    plt.colorbar()
-    plt.hot()
+plt.ylabel('Classification accurancy (f1 score)')
+plt.legend(loc='best')
+plt.title('Category-specific classification accuracy for different masks')
+plt.tight_layout()
+
 
 plt.show()
 
