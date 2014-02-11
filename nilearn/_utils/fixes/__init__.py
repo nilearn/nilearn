b7728d5b71ae5aca9ad06f0a4298b19db8b74f0d
VirgileFritsch <virgile.fritsch@gmail.com>
2014-02-11 13:43:48 +0100

    permuted OLS: add support for sklearn < 0.12 (no sparse matrix support)

--- nilearn/_utils/fixes/__init__.py
+++ nilearn/_utils/fixes/__init__.py
@@ -1,7 +1,10 @@
 from distutils.version import LooseVersion
 import sklearn
 
-if LooseVersion(sklearn.__version__) < '0.15':
+if LooseVersion(sklearn.__version__) < '0.12':
+    from .sklearn_f_regression_nosparse import (
+        f_regression_nosparse as f_regression)
+elif LooseVersion(sklearn.__version__) < '0.15':
     from .sklearn_f_regression import f_regression
 else:
     from sklearn.feature_selection import f_regression
