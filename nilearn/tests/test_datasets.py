676fa51e677fe0d25a0510ce577e0e5b9920b7f6
VirgileFritsch <virgile.fritsch@gmail.com>
2014-05-21 16:00:52 +0200

    Localizer: test without csv files.

--- nilearn/tests/test_datasets.py
+++ nilearn/tests/test_datasets.py
@@ -350,6 +350,11 @@ def test_fetch_yeo_2011_atlas():
 @with_setup(setup_tmpdata_and_mock, teardown_tmpdata)
 def test_fetch_localizer_contrasts():
     local_url = "file://" + datadir
+    ids = np.asarray(['S%2d' % i for i in range(94)])
+    ids = ids.view(dtype=[('subject_id', 'S3')])
+    file_mock.add_csv('cubicwebexport.csv', ids)
+    file_mock.add_csv('cubicwebexport2.csv', ids)
+
     # Disabled: cannot be tested without actually fetching covariates CSV file
     # All subjects
     dataset = datasets.fetch_localizer_contrasts(["checkerboard"],
