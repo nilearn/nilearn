a95b1534489781e2ce9a6469f996ff47865e44e0
Gael Varoquaux <gael.varoquaux@normalesup.org>
2014-06-17 10:22:04 +0200

    TEST: be robust to lack of X display

--- nilearn/plotting/__init__.py
+++ nilearn/plotting/__init__.py
@@ -3,6 +3,25 @@ Plotting code for nilearn
 """
 # Authors: Chris Filo Gorgolewski, Gael Varoquaux
 
+###############################################################################
+# Make sure that we don't get DISPLAY problems when running without X on
+# unices
+
+def _set_mpl_backend():
+    import os
+    if not os.name == 'posix':
+        # We have the problem only on posix systems
+        return
+    if 'DISPLAY' in os.environ:
+        return
+    import matplotlib
+    # Agg is a backend that will do PNGs and PDFs
+    matplotlib.use('Agg')
+
+_set_mpl_backend()
+
+###############################################################################
+
 from . import cm
 from .img_plotting import plot_img, plot_anat, demo_plot_roi, plot_epi, \
         plot_roi
