49bfd406103815c13f3285c1f41ca08c9508fd4b
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2014-06-17 22:58:52 +0200

    Removed code dealing with NaNs from the haxby searchlight example

--- plot_haxby_searchlight.py
+++ plot_haxby_searchlight.py
@@ -78,7 +78,6 @@ fmri_masked = nifti_masker.fit_transform(fmri_img)
 from sklearn.feature_selection import f_classif
 f_values, p_values = f_classif(fmri_masked, y)
 p_values = -np.log10(p_values)
-p_values[np.isnan(p_values)] = 0
 p_values[p_values > 10] = 10
 p_unmasked = nifti_masker.inverse_transform(p_values).get_data()
 
