60f1f55f354c18cf6f4a684d9d925904a9bad5ff
Gael Varoquaux <gael.varoquaux@normalesup.org>
2014-06-18 14:03:23 +0200

    DOC: richer plotting example

--- plot_demo_plotting.py
+++ plot_demo_plotting.py
@@ -2,20 +2,22 @@
 Demoing Plotting functions of nilearn
 ======================================
 
-Nilearn comes with a set of plotting function for Nifti-like images
+Nilearn comes with a set of plotting function for Nifti-like images. Here
+we showcase the major plotting modes.
 
 """
 
-from nilearn import plotting, datasets
+from nilearn import plotting, datasets, image
 
 
 ###############################################################################
-# Retrieve the data
+# Retrieve the data: haxby dataset to have EPI images and masks, and
+# localizer dataset to have contrast maps
 
-data = datasets.fetch_haxby(n_subjects=1)
+haxby = datasets.fetch_haxby(n_subjects=1)
 
 localizer = datasets.fetch_localizer_contrasts(["left vs right button press"],
-                                               get_anats=True,)
+                                               get_anats=True)
 
 
 ###############################################################################
@@ -26,10 +28,13 @@ plotting.plot_stat_map(localizer.cmaps[3], bg_img=localizer.anats[3],
                        title="plot_stat_map")
 
 # Plotting anatomical maps
-plotting.plot_anat(data.anat[0], title="plot_anat")
+plotting.plot_anat(haxby.anat[0], title="plot_anat")
 
 # Plotting ROIs (here the mask)
-plotting.plot_roi(data.mask_vt[0], bg_img=data.anat[0], title="plot_roi")
+plotting.plot_roi(haxby.mask_vt[0], bg_img=haxby.anat[0], title="plot_roi")
+
+# Plotting EPI haxby
+plotting.plot_epi(image.mean_img(haxby.func[0]), title="plot_epi")
 
 ###############################################################################
 # demo the different display_mode
@@ -43,8 +48,22 @@ plotting.plot_stat_map(localizer.cmaps[3], display_mode='x', cut_coords=(-36, 36
 plotting.plot_stat_map(localizer.cmaps[3], display_mode='y', cut_coords=1,
                        title="display_mode='x', cut_coords=(-36, 36)")
 
+plotting.plot_stat_map(localizer.cmaps[3], display_mode='z',
+                       cut_coords=1, colorbar=False,
+                       title="display_mode='z', cut_coords=1, colorbar=False")
+
+plotting.plot_stat_map(localizer.cmaps[3], display_mode='xz',
+                       cut_coords=(36, 60),
+                       title="display_mode='xz', cut_coords=(36, 60)")
+
+plotting.plot_stat_map(localizer.cmaps[3], display_mode='yx',
+                       cut_coords=(-27, 36),
+                       title="display_mode='yz', cut_coords=(-27, 36)")
+
 plotting.plot_stat_map(localizer.cmaps[3], display_mode='yz',
                        cut_coords=(-27, 60),
                        title="display_mode='yz', cut_coords=(-27, 60)")
+
+
 import pylab as pl
 pl.show()
