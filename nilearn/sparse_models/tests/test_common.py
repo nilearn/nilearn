98be7d3effb5c9591e9598b1bcd851e513dee8ee
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-06-30 09:37:52 +0200

    refactored some tests

--- nilearn/sparse_models/tests/test_common.py
+++ nilearn/sparse_models/tests/test_common.py
@@ -13,7 +13,6 @@ from ..common import (gradient_id, compute_mse, logistic,
                       compute_mse_lipschitz_constant,
                       test_grad_div_adjoint_arbitrary_ndim)
 from ..estimators import _BaseEstimator
-from ..prox_tv_l1 import prox_tv_l1
 from ..operators import prox_l1
 from nose.tools import assert_true, raises
 
@@ -48,45 +47,6 @@ def test_3D_gradient_id():
 
         np.testing.assert_array_equal(
             gid.shape, [img.ndim + 1] + list(img.shape))
-
-
-def test_prox_l1(n_features=10):
-
-    rng = np.random.RandomState(42)
-    x = rng.randn(n_features, 1)
-    tau = .3
-    s = prox_l1(x.copy(), tau)
-    p = x - s  # projection + shrinkage = id
-
-    # We should have ||s(a) - s(b)||^2 <= ||a - b||^2 - ||p(a) - p(b)||^2
-    # for all a and b (this is strong non-expansiveness
-    for (a, b), (pa, pb), (sa, sb) in zip(*[itertools.product(z[0], z[0])
-                                            for z in [x, p, s]]):
-        assert_true((sa - sb) ** 2 <= (a - b) ** 2 - (pa - pb) ** 2)
-
-
-def test_prox_tv_l1_approximates_prox_l1_for_lasso(size=15, random_state=42,
-                                                   decimal=4, dgap_tol=1e-7):
-
-    rng = np.random.RandomState(random_state)
-
-    l1_ratio = 1.  # pure LASSO
-    for ndim in xrange(1, 4):
-        shape = [size] * ndim
-        z = rng.randn(*shape)
-        for weight in np.logspace(-10, 10, num=10):
-            # use prox_tv_l1 approximation to prox_l1
-            a = prox_tv_l1(z.copy(), weight=weight, l1_ratio=l1_ratio,
-                           dgap_tol=dgap_tol, return_info=False,
-                           max_iter=10,
-                           )[-1].ravel()
-
-            # use exact closed-form soft shrinkage formula for prox_l1
-            b = prox_l1(z.copy(), weight)[-1].ravel()
-
-            # results shoud be close in l-infinity norm
-            np.testing.assert_almost_equal(np.abs(a - b).max(),
-                                           0., decimal=decimal)
 
 
 def test_logistic_loss_derivative(n_samples=4, n_features=10, random_state=42,
--- nilearn/sparse_models/tests/test_operators.py
+++ nilearn/sparse_models/tests/test_operators.py
@@ -1,18 +1,42 @@
-from ..operators import prox_l1
-
 import itertools
+from nose.tools import assert_true
 import numpy as np
+from ..operators import prox_l1, prox_tv_l1
 
 
-def test_prox_l1():
-    """Tests non-expansivity of proximal operator"""
-    x = np.arange(10)[np.newaxis, :]
+def test_prox_l1_nonexpansiveness(n_features=10):
+    rng = np.random.RandomState(42)
+    x = rng.randn(n_features, 1)
     tau = .3
     s = prox_l1(x.copy(), tau)
     p = x - s  # projection + shrinkage = id
 
     # We should have ||s(a) - s(b)||^2 <= ||a - b||^2 - ||p(a) - p(b)||^2
-    # for all a and b
+    # for all a and b (this is strong non-expansiveness
     for (a, b), (pa, pb), (sa, sb) in zip(*[itertools.product(z[0], z[0])
                                             for z in [x, p, s]]):
-        assert (sa - sb) ** 2 <= (a - b) ** 2 - (pa - pb) ** 2
+        assert_true((sa - sb) ** 2 <= (a - b) ** 2 - (pa - pb) ** 2)
+
+
+def test_prox_tv_l1_approximates_prox_l1_for_lasso(size=15, random_state=42,
+                                                   decimal=4, dgap_tol=1e-7):
+
+    rng = np.random.RandomState(random_state)
+
+    l1_ratio = 1.  # pure LASSO
+    for ndim in xrange(1, 4):
+        shape = [size] * ndim
+        z = rng.randn(*shape)
+        for weight in np.logspace(-10, 10, num=10):
+            # use prox_tv_l1 approximation to prox_l1
+            a = prox_tv_l1(z.copy(), weight=weight, l1_ratio=l1_ratio,
+                           dgap_tol=dgap_tol, return_info=False,
+                           max_iter=10,
+                           )[-1].ravel()
+
+            # use exact closed-form soft shrinkage formula for prox_l1
+            b = prox_l1(z.copy(), weight)[-1].ravel()
+
+            # results shoud be close in l-infinity norm
+            np.testing.assert_almost_equal(np.abs(a - b).max(),
+                                           0., decimal=decimal)
