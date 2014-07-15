b6dd5a8243ecae4b137788b7f7dd61df254ff15d
lesteve <loic.esteve@ymail.com>
2014-07-15 16:06:07 +0100

    Clearer import errors when dependencies are not installed At the moment you get an error message printed at the top and a stacktrace with a not so clear UnboundLocalError

--- nilearn/__init__.py
+++ nilearn/__init__.py
@@ -11,61 +11,61 @@ See http://nilearn.github.com for complete documentation.
 
 __version__ = "0.1a"
 
+
 def _check_dependencies():
     from distutils.version import LooseVersion
     try:
         import numpy
     except ImportError:
-        print('Numpy could not be found,'
-              ' please install it properly to use nilearn.')
+        raise ImportError('Numpy could not be found,'
+                          ' please install it properly to use nilearn.')
     if not LooseVersion(numpy.__version__) >= LooseVersion('1.6.0'):
         raise ImportError('A numpy version of at least 1.6 is required '
-            'to use nilearn. %s was found. Please upgrade numpy.'
-            % numpy.__version__
-            )
+                          'to use nilearn. %s was found. Please upgrade numpy.'
+                          % numpy.__version__)
 
     try:
         import scipy
     except ImportError:
-        print('Scipy could not be found,'
-              ' please install it properly to use nilearn.')
+        raise ImportError('Scipy could not be found,'
+                          ' please install it properly to use nilearn.')
     if not LooseVersion(scipy.__version__) >= LooseVersion('0.9.0'):
         raise ImportError('A scipy version of at least 0.9 is required '
-            'to use nilearn. %s was found. Please upgrade scipy.'
-            % scipy.__version__
-            )
+                          'to use nilearn. %s was found. Please upgrade scipy.'
+                          % scipy.__version__)
 
     try:
         import sklearn
     except ImportError:
-        print('Scikit-learn could not be found,'
-              ' please install it properly to use nilearn.')
+        raise ImportError('Scikit-learn could not be found,'
+                          ' please install it properly to use nilearn.')
     if not LooseVersion(sklearn.__version__) >= LooseVersion('0.10'):
-        raise ImportError('A scikit-learn version of at least 0.10 is required'
+        raise ImportError(
+            'A scikit-learn version of at least 0.10 is required'
             ' to use nilearn. %s was found. Please upgrade scikit-learn.'
-            % sklearn.__version__
-            )
-
+            % sklearn.__version__)
 
     try:
         import nibabel
     except ImportError:
-        print('nibabel could not be found,'
-              ' please install it properly to use nilearn.')
+        raise ImportError('nibabel could not be found,'
+                          ' please install it properly to use nilearn.')
+
+    if not LooseVersion(nibabel.__version__) >= LooseVersion('1.1.0'):
+        raise ImportError(
+            'A nibabel version of at least 1.1 is required'
+            ' to use nilearn. %s was found. Please upgrade nibabel.'
+            % nibabel.__version__)
+
     try:
         import gzip
         if hasattr(gzip.GzipFile, 'max_read_chunk'):
             # Monkey-patch gzip to have faster reads on large
             # gzip files
-            gzip.GzipFile.max_read_chunk = 100 * 1024 * 1024 # 100Mb
+            gzip.GzipFile.max_read_chunk = 100 * 1024 * 1024  # 100Mb
     except ImportError:
-        print('Python has been compiled without gzip,'
-              ' reading nii.gz files will be impossible.')
-    if not LooseVersion(nibabel.__version__) >= LooseVersion('1.1.0'):
-        raise ImportError('A nibabel version of at least 1.1 is required'
-            ' to use nilearn. %s was found. Please upgrade nibabel.'
-            % nibabel.__version__
-            )
+        raise ImportError('Python has been compiled without gzip,'
+                          ' reading nii.gz files will be impossible.')
 
 
 _check_dependencies()
@@ -79,4 +79,3 @@ del _check_dependencies
 # structures
 # This  is used in nilearn._utils.cache_mixin
 check_cache_version = True
-
