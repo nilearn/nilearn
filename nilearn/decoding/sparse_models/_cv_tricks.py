8ab36625d528120502be16e21aa45a69ab8a87a8
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-09-04 09:38:51 +0200

    ENH: moved _ravel hack from _cv_tricks to _utils.fixes._ravel.py

--- nilearn/decoding/sparse_models/_cv_tricks.py
+++ nilearn/decoding/sparse_models/_cv_tricks.py
@@ -11,28 +11,13 @@ selection, creation of parameter grids (for alpha), etc.
 #         and others.
 # License: simplified BSD
 
-from functools import partial
 import numpy as np
 from scipy import stats
 from ..._utils.fixes import center_data
 from sklearn.feature_selection import (
     f_regression, f_classif, SelectPercentile)
 from sklearn.base import BaseEstimator
-
-np_version = []
-for x in np.__version__.split('.'):
-    try:
-        np_version.append(int(x))
-    except ValueError:
-        # x may be of the form dev-1ea1592
-        np_version.append(x)
-np_version = tuple(np_version)
-
-# Newer NumPy has a ravel that needs less copying.
-if np_version < (1, 7, 1):
-    _ravel = np.ravel
-else:
-    _ravel = partial(np.ravel, order='K')
+from ..._utils.fixes._ravel import _ravel
 
 
 def _squared_norm(x):
