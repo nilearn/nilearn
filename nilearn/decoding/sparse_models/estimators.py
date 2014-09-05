1a067aec4c5e94073b0c487b6c2b743271395ee0
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-09-05 10:50:52 +0200

    sk Removing backports

--- nilearn/decoding/sparse_models/estimators.py
+++ nilearn/decoding/sparse_models/estimators.py
@@ -189,6 +189,25 @@ class _BaseClassifier(_BaseEstimator, BaseEstimator, LinearClassifierMixin):
         self.backtracking = backtracking
         self.n_jobs = n_jobs
 
+    def _predict_proba_lr(self, X):
+        """Probability estimation for OvR logistic regression.
+
+        Positive class probabilities are computed as
+        1. / (1. + np.exp(-self.decision_function(X)));
+        multiclass is handled by normalizing that over all classes.
+        """
+        prob = self.decision_function(X)
+        prob *= -1
+        np.exp(prob, prob)
+        prob += 1
+        np.reciprocal(prob, prob)
+        if len(prob.shape) == 1:
+            return np.vstack([1 - prob, prob]).T
+        else:
+            # OvR normalization, like LibLinear's predict_probability
+            prob /= prob.sum(axis=1).reshape((prob.shape[0], -1))
+            return prob
+
     def _pre_fit(self, X, y):
         X = np.array(X)
         y = np.array(y)
