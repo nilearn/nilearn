54999614684baaa9e60b279f70f4bf329a4abd6d
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-09-05 10:50:52 +0200

    sk Removing backports

--- nilearn/_utils/fixes/sklearn_basic_backports.py
+++ nilearn/_utils/fixes/sklearn_basic_backports.py
@@ -140,25 +140,6 @@ class LinearClassifierMixin(ClassifierMixin):
             indices = scores.argmax(axis=1)
         return self.classes_[indices]
 
-    def _predict_proba_lr(self, X):
-        """Probability estimation for OvR logistic regression.
-
-        Positive class probabilities are computed as
-        1. / (1. + np.exp(-self.decision_function(X)));
-        multiclass is handled by normalizing that over all classes.
-        """
-        prob = self.decision_function(X)
-        prob *= -1
-        np.exp(prob, prob)
-        prob += 1
-        np.reciprocal(prob, prob)
-        if len(prob.shape) == 1:
-            return np.vstack([1 - prob, prob]).T
-        else:
-            # OvR normalization, like LibLinear's predict_probability
-            prob /= prob.sum(axis=1).reshape((prob.shape[0], -1))
-            return prob
-
 
 def is_classifier(estimator):
     """Returns True if the given estimator is (probably) a classifier.
