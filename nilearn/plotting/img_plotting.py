a65bdd27af5e96a124bc4ad0a3808e506158940c
Gael Varoquaux <gael.varoquaux@normalesup.org>
2014-09-08 14:04:36 +0200

    ENH: add support for vmax in plot_stats_map

--- nilearn/plotting/img_plotting.py
+++ nilearn/plotting/img_plotting.py
@@ -553,11 +553,19 @@ def plot_stat_map(stat_map_img, bg_img=MNI152TEMPLATE, cut_coords=None,
                                                     black_bg=black_bg)
 
     # make sure that the color range is symmetrical
-    stat_map_img = _utils.check_niimg(stat_map_img, ensure_3d=True)
-    stat_map_data = stat_map_img.get_data()
-    stat_map_max = np.nanmax(stat_map_data)
-    stat_map_min = np.nanmin(stat_map_data)
-    vmax = max(-stat_map_min, stat_map_max)
+    if 'vmax' in kwargs:
+        vmax = kwargs.pop('vmax')
+    else:
+        stat_map_img = _utils.check_niimg(stat_map_img, ensure_3d=True)
+        stat_map_data = stat_map_img.get_data()
+        stat_map_max = np.nanmax(stat_map_data)
+        stat_map_min = np.nanmin(stat_map_data)
+        vmax = max(-stat_map_min, stat_map_max)
+    if 'vmin' in kwargs:
+        raise ValueError('plot_stat_map does not accept a "vmin" '
+                         'argument, as it uses a symmetrical range '
+                         'defined via the vmax argument. To threshold '
+                         'the map, use the "threshold" argument')
     vmin = -vmax
 
     slicer = _plot_img_with_bg(img=stat_map_img, bg_img=bg_img,
