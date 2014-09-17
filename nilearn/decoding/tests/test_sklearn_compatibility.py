1712b1d1d5ec050b89e24c233f30fdceb26ca1b0
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-09-17 09:48:04 +0200

    EHN: SpaceNet is fine

--- nilearn/decoding/tests/test_sklearn_compatibility.py
+++ nilearn/decoding/tests/test_sklearn_compatibility.py
@@ -1,6 +1,5 @@
 import numpy as np
-from ..space_net import (TVl1Classifier, TVl1Regressor,
-                         SmoothLassoClassifier, SmoothLassoRegressor)
+from ..space_net import SpaceNet
 from nose.tools import assert_true
 import traceback
 
@@ -9,32 +8,21 @@ def test_get_params():
     # Issue #12 (on github) reported that our objects
     # get_params() methods returned empty dicts.
 
-    cv_addition_params = ["n_alphas", "eps", "cv", "alpha_min"]
-    for model in [SmoothLassoClassifier, TVl1Classifier,
-                  TVl1Regressor, SmoothLassoRegressor]:
-        kwargs = {}
-        if model.__name__.endswith('CV'):
-            kwargs['alphas'] = np.logspace(-3, 1, num=5)
-        for param in ["max_iter", "alpha", "l1_ratio", "verbose",
-                      "tol", "mask", "memory", "copy_data",
-                      "fit_intercept", "alphas"] + [
-            [], cv_addition_params][model.__name__.endswith("CV")]:
-            if model.__name__.endswith("CV"):
-                if param == "alpha":
-                    continue
-            elif param == "alphas":
-                continue
+    for penalty in ["smooth-lasso", "tvl1"]:
+        for classif in [True, False]:
+            kwargs = {}
+            for param in ["max_iter", "alpha", "l1_ratio", "verbose",
+                          "tol", "mask", "memory", "copy_data",
+                          "fit_intercept", "alphas"]:
+                m = SpaceNet(penalty=penalty, classif=classif, **kwargs)
+                try:
+                    params = m.get_params()
+                except AttributeError:
+                    if "get_params" in traceback.format_exc():
+                        params = m._get_params()
+                    else:
+                        raise
 
-            m = model(**kwargs)
-
-            try:
-                params = m.get_params()
-            except AttributeError:
-                if "get_params" in traceback.format_exc():
-                    params = m._get_params()
-                else:
-                    raise
-
-            assert_true(param in params,
-                        msg="Class '%s' doesn't have parameter '%s'." % (
-                    model.__name__, param))
+                assert_true(param in params,
+                            msg="%s doesn't have parameter '%s'." % (
+                        m, param))
