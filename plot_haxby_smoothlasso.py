15ba569cf8ecb91ee94cfe5a87e23690ba63565a
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-10-08 13:20:41 +0200

    s-lasso -> tvl1 typos fix in demo script

--- plot_haxby_smoothlasso.py
+++ plot_haxby_smoothlasso.py
@@ -1,6 +1,6 @@
 """
-S-Lasso Simple example of decoding: the Haxby data
-==================================================
+TV-L1 Simple example of decoding: the Haxby data
+================================================
 
 Here is a simple example of decoding, reproducing the Haxby 2001
 study on a face vs house discrimination task in a mask of the ventral
@@ -41,21 +41,23 @@ X = nibabel.Nifti1Image(niimgs.get_data()[:, :, :, condition_mask],
                         niimgs.get_affine())
 y = target[condition_mask]
 
-### Fit S-LASSO classifier and retreive weights map ##########################
+### Fit and predict #########################################################
 from nilearn.decoding import SpaceNet
 decoder = SpaceNet(memory=memory, mask=nifti_masker, classif=True, verbose=1,
-                penalty="tvl1", n_jobs=14)
-decoder.fit(X, y)
+                penalty="tvl1")
+decoder.fit(X, y)  # fit
+y_pred = decoder.predict(X)  # predict
 coef_niimg = decoder.coef_img_
-coef_niimg.to_filename('haxby_slcv_weights.nii')
+coef_niimg.to_filename('haxby_tvl1_weights.nii')
+
 
 ### Visualization #############################################################
 import matplotlib.pyplot as plt
-from nilearn.image.image import mean_img
+from nilearn.image import mean_img
 from nilearn.plotting import plot_stat_map
 
-mean_epi = mean_img(data_files.func[0])
-slicer = plot_stat_map(coef_niimg, mean_epi, title="S-LASSO weights")
-print ("Accurarcy: %g" % ((decoder.predict(X) == y).mean() * 100.)) + "%"
+background_img = mean_img(data_files.func[0])
+slicer = plot_stat_map(coef_niimg, background_img, title="S-LASSO weights")
+print ("Accurarcy: %g" % ((y_pred == y).mean() * 100.)) + "%"
 print "_" * 80
 plt.show()
