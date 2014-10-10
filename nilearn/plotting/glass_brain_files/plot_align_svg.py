ad2dc036111193f373db88dacda35ed27adcc456
lesteve <loic.esteve@ymail.com>
2014-10-10 10:25:43 +0200

    Update plot_align_svg.py now that add_brain_schematics has been removed from displays
    Tweak it to use additional arguments to ease alignment + remove commented code

--- nilearn/plotting/glass_brain_files/plot_align_svg.py
+++ nilearn/plotting/glass_brain_files/plot_align_svg.py
@@ -1,59 +1,48 @@
+import functools
+
 import matplotlib.pyplot as plt
-import matplotlib.transforms
 
 from nilearn import plotting
-from nilearn.plotting import slicers
+from nilearn.plotting import glass_brain
 
 plt.close('all')
 
 
+def add_brain_schematics(display, **kwargs):
+    for axes in display.axes.itervalues():
+        object_bounds = glass_brain.plot_brain_schematics(axes.ax,
+                                                          axes.direction,
+                                                          **kwargs)
+        axes.add_object_bounds(object_bounds)
+
+my_add_brain_schematics = functools.partial(add_brain_schematics,
+                                            alpha=0.5,
+                                            linewidth=1,
+                                            edgecolor='orange')
 # side
 display = plotting.plot_anat(display_mode='x', cut_coords=[-2])
-display.add_brain_schematics()
+my_add_brain_schematics(display)
 
 # top
 display = plotting.plot_anat(display_mode='z', cut_coords=[20])
-display.add_brain_schematics()
+my_add_brain_schematics(display)
 
 # front
 display = plotting.plot_anat(display_mode='y', cut_coords=[-20])
-display.add_brain_schematics()
+my_add_brain_schematics(display)
 
 # all in one
 display = plotting.plot_anat(display_mode='ortho', cut_coords=(-2, -20, 20))
-display.add_brain_schematics()
+my_add_brain_schematics(display)
 
 # Plot multiple slices
 display = plotting.plot_anat(display_mode='x')
-display.add_brain_schematics()
+my_add_brain_schematics(display)
 
 display = plotting.plot_anat(display_mode='y')
-display.add_brain_schematics()
+my_add_brain_schematics(display)
 
 display = plotting.plot_anat(display_mode='z')
-display.add_brain_schematics()
+my_add_brain_schematics(display)
 
 plt.show()
-
-# Leaving that here for legacy params
-# # all in one
-# display = plotting.plot_anat(display_mode='ortho', cut_coords=(-2, -20, 20))
-# axes = [each.ax for each in display.axes.itervalues()]
-
-# ax = axes[0]
-# bp = brain_plotter.BrainPlotter('generated_json/brain_schematics_front.json')
-# transform = matplotlib.transforms.Affine2D.from_values(0.38, 0, 0, 0.38, -71, -73)
-# transform += ax.transData
-# bp.plot(ax, transform)
-
-# ax = axes[1]
-# bp = brain_plotter.BrainPlotter('generated_json/brain_schematics_side.json')
-# transform = matplotlib.transforms.Affine2D.from_values(0.36, 0, 0, 0.36, -103, -67)
-# transform += ax.transData
-# bp.plot(ax, transform)
-
-# ax = axes[2]
-# bp = brain_plotter.BrainPlotter('generated_json/brain_schematics_top.json')
-# transform = matplotlib.transforms.Affine2D.from_values(0.35, 0, 0, 0.35, -70, -103)
-# transform += ax.transData
-# bp.plot(ax, transform)
