0d4b43407e2f84cbb84abc1146190db8795eb9ce
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-10-10 05:51:45 +0200

    BF: broken demo script

--- plot_oasis_vbm_spacenet.py
+++ plot_oasis_vbm_spacenet.py
@@ -17,19 +17,21 @@ age = dataset_files.ext_vars['age'].astype(float)
 
 ### Fit and predict ###########################################################
 from nilearn.decoding import SpaceNet
-decoder = SpaceNet(memory="cache", screening_percentile=20., verbose=1,
-                   smoothing_fwhm=2.)
-decoder.fit(dataset_files.gray_matter_maps, age)
+decoder = SpaceNet(memory="cache", screening_percentile=20., verbose=1)
+decoder.fit(dataset_files.gray_matter_maps, age)  # fit
 coef_img = decoder.coef_img_
-age_pred = decoder.predict(dataset_files.gray_matter_maps).ravel()
+age_pred = decoder.predict(dataset_files.gray_matter_maps).ravel()  # predict
 
 ### Visualization #############################################################
 import matplotlib.pyplot as plt
 from nilearn.plotting import plot_stat_map
+
+# weights map
 background_img = dataset_files.gray_matter_maps[0]
 plot_stat_map(coef_img, background_img, title="Graph-Net weights",
               display_mode="z")
 
+# quality of predictions
 plt.figure()
 linewidth = 3
 ax1 = plt.subplot('211')
@@ -41,4 +43,5 @@ ax2 = plt.subplot("212")
 ax2.plot(age - age_pred, label="True age - fitted age", linewidth=linewidth)
 ax2.set_xlabel("subject")
 plt.legend(loc="best")
+
 plt.show()
