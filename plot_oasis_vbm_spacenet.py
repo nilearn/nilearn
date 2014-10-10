7990528604a19ff9f018ce07949f7f0c697d8585
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-10-10 05:51:45 +0200

    BF: broken demo script

--- plot_oasis_vbm_spacenet.py
+++ plot_oasis_vbm_spacenet.py
@@ -28,10 +28,13 @@ age_pred = decoder.predict(new_images).ravel()
 ### Visualization #############################################################
 import matplotlib.pyplot as plt
 from nilearn.plotting import plot_stat_map
+
+# weights map
 background_img = dataset_files.gray_matter_maps[0]
 plot_stat_map(coef_img, background_img, title="Graph-Net weights",
               display_mode="z")
 
+# quality of predictions
 plt.figure()
 linewidth = 3
 ax1 = plt.subplot('211')
@@ -43,4 +46,5 @@ ax2 = plt.subplot("212")
 ax2.plot(age - age_pred, label="True age - fitted age", linewidth=linewidth)
 ax2.set_xlabel("subject")
 plt.legend(loc="best")
+
 plt.show()
