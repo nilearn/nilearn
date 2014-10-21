5e055605e835c12f5cb5d89314a13470742849cd
banilo <danilobzdok@gmail.com>
2014-10-21 16:14:02 +0200

    PEP8ized + more intuitive variable names

--- plot_roi_extraction.py
+++ plot_roi_extraction.py
@@ -49,7 +49,8 @@ plot_epi(mean_img, title='Smoothed mean EPI', cut_coords=(coronal, sagittal,
 from scipy import stats
 fmri_data = fmri_img.get_data()
 _, pvalues = stats.ttest_ind(fmri_data[..., haxby_labels == 'face'],
-                             fmri_data[..., haxby_labels == 'house'], axis=-1)
+                             fmri_data[..., haxby_labels == 'house'],
+                             axis=-1)
 
 # Use a log scale for p-values
 pvalues = - np.log10(pvalues)
@@ -66,17 +67,18 @@ fig_id = plt.subplot(3, 1, 1)
 # Thresholding
 pvalues[pvalues < 5] = 0
 plot_stat_map(nibabel.Nifti1Image(pvalues, fmri_img.get_affine()), mean_img,
-              title='Thresholded p-values',
+              title='Thresholded p-values', annotate=False, colorbar=False,
               cut_coords=(coronal, sagittal, axial), axes=fig_id)
 
-# Binarization and intersection (i.e., "AND conjunction") with VT mask
+# Binarization and intersection with VT mask
+# (intersection corresponds to an "AND conjunction")
 bin_pvalues = (pvalues != 0)
 vt = nibabel.load(haxby_files.mask_vt[0]).get_data().astype(bool)
 bin_pvalues_and_vt = np.logical_and(bin_pvalues, vt)
 
 fig_id = plt.subplot(3, 1, 2)
 plot_roi(nibabel.Nifti1Image(bin_pvalues_and_vt.astype(np.int), 
-                             fmri_img.get_affine()), 
+         fmri_img.get_affine()), 
          mean_img, title='Intersection with ventral temporal mask',
          cut_coords=(coronal, sagittal, axial), axes=fig_id)
 
@@ -84,10 +86,10 @@ plot_roi(nibabel.Nifti1Image(bin_pvalues_and_vt.astype(np.int),
 fig_id = plt.subplot(3, 1, 3)
 from scipy import ndimage
 dil_bin_pvalues_and_vt = ndimage.binary_dilation(bin_pvalues_and_vt)
-plot_roi(nibabel.Nifti1Image(dil_bin_pvalues_and_vt.astype(np.int), 
-                             fmri_img.get_affine()), 
+plot_roi(nibabel.Nifti1Image(dil_bin_pvalues_and_vt.astype(np.int),
+    fmri_img.get_affine()),
     mean_img, title='Dilated mask', cut_coords=(coronal, sagittal, axial),
-    axes=fig_id)
+    axes=fig_id, annotate=False)
 plt.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0)
 
 # Identification of connected components
@@ -104,10 +106,10 @@ plot_roi(nibabel.Nifti1Image(second_roi_data, fmri_img.get_affine()),
 plt.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0)
 plot_roi(nibabel.Nifti1Image(first_roi_data, fmri_img.get_affine()),
     mean_img, title='Connected components: first ROI_',
-    output_file='snapshot_first_ROI.png')  # no plot, save .PNG to hard disk 
+    output_file='snapshot_first_ROI.png')
 plot_roi(nibabel.Nifti1Image(second_roi_data, fmri_img.get_affine()),
     mean_img, title='Connected components: second ROI',
-    output_file='snapshot_second_ROI.png')  # no plot, save .PNG to hard disk 
+    output_file='snapshot_second_ROI.png')
 
 # use the new ROIs to extract data maps in both ROIs
 masker = NiftiLabelsMasker(
@@ -116,24 +118,27 @@ masker = NiftiLabelsMasker(
             standardize=False,
             detrend=False)
 masker.fit()
-trial_list = list(set(haxby_labels))
-max_trials = 108
-n_conds = len(trial_list)
-X1, X2 = np.zeros((max_trials, n_conds)), np.zeros((max_trials, n_conds))
-for i, cond in enumerate(trial_list):
-    cond_maps = nibabel.Nifti1Image(fmri_data[..., haxby_labels == cond][...,:max_trials],
+condition_names = list(set(haxby_labels))
+n_cond_img = fmri_data[...,haxby_labels == 'house'].shape[-1]
+n_conds = len(condition_names)
+X1, X2 = np.zeros((n_cond_img, n_conds)), np.zeros((n_cond_img, n_conds))
+for i, cond in enumerate(condition_names):
+    cond_maps = nibabel.Nifti1Image(
+        fmri_data[..., haxby_labels == cond][..., :n_cond_img],
         fmri_img.get_affine())
     mask_data = masker.transform(cond_maps)
     X1[:, i], X2[:, i] = mask_data[:, 0], mask_data[:, 1]
-trial_list[trial_list.index('scrambledpix')] = 'scrambled'  # cosmetics
+condition_names[condition_names.index('scrambledpix')] = 'scrambled'
 
 plt.figure(figsize=(15, 7))
 for i in np.arange(2):
     plt.subplot(1, 2, i + 1)
-    plt.boxplot(X1 if i == 0 else X2, labels=trial_list)
+    plt.boxplot(X1 if i == 0 else X2)
+    plt.xticks(np.arange(len(condition_names)) + 1, condition_names)
     plt.title('Boxplots of data in ROI%i per condition' % (i + 1))
 
 plt.show()
 
 # save the ROI 'atlas' to a single output nifti
-nibabel.save(nibabel.Nifti1Image(labels, fmri_img.get_affine()), 'mask_atlas.nii')
+nibabel.save(nibabel.Nifti1Image(labels, fmri_img.get_affine()),
+    'mask_atlas.nii')
