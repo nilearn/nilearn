f67cb7188fc83ef7da66aea2f9bbfc72a34ae32a
Loïc Estève <loic.esteve@ymail.com>
2014-11-12 11:03:36 +0100

    Add nilearn/data as a data dir
    Otherwise the MNI template is missing when installing nilearn with setup.py

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('nilearn/_utils')
     config.add_subpackage('nilearn/_utils/fixes')
     config.add_subpackage("nilearn/plotting")
+    config.add_data_dir("nilearn/data")
     return config
 
 
