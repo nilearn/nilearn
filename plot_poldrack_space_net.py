986aacd1c7ed2f7000cc9816057f96d59701e066
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-11-14 15:24:38 +0100

    ENH: cleanup for poldrack demo script

--- plot_poldrack_space_net.py
+++ plot_poldrack_space_net.py
@@ -2,11 +2,7 @@ import numpy as np
 from joblib import Memory
 
 ### Load data ################################################################
-import os
-import sys
-sys.path.append(
-    os.path.join(os.environ["HOME"], "CODE/FORKED/parietal-python"))
-from examples.proximal.load_data import load_gain_poldrack
+from load_poldrack import load_gain_poldrack
 mem = Memory(cachedir='cache', verbose=3)
 X, y, _, mask, affine = mem.cache(load_gain_poldrack)(smooth=0)
 img_data = np.zeros(list(mask.shape) + [len(X)])
@@ -20,24 +16,21 @@ y_train = y
 
 ### Fit and predict ##########################################################
 from nilearn.decoding import SpaceNetRegressor
-penalty = "TV-L1"
-l1_ratio = .3
-alpha = None
-decoder = SpaceNetRegressor(memory=mem, mask=mask_img, verbose=2,
-                            n_jobs=int(os.environ.get("N_JOBS", 1)),
-                            l1_ratio=l1_ratio, penalty=penalty, alpha=alpha)
-decoder.fit(X_train, y_train)  # fit
-coef_niimg = decoder.coef_img_
-coef_niimg.to_filename('poldrack_%s(l1_ratio=%g, alpha=%s)_weights.nii' % (
-        penalty, l1_ratio, alpha))
+penalties = ['Smooth-LASSO', 'TV-L1']
+decoders = {}
+for penalty in penalties:
+    decoder = SpaceNetRegressor(memory=mem, mask=mask_img, verbose=2,
+                                penalty=penalty)
+    decoder.fit(X_train, y_train)  # fit
+    decoders[penalty] = decoder
 
 ### Visualization #############################################################
 import matplotlib.pyplot as plt
 from nilearn.image import mean_img
 from nilearn.plotting import plot_stat_map
-plt.close('all')
 background_img = mean_img(X_train)
-background_img.to_filename('poldrack_mean.nii')
-slicer = plot_stat_map(coef_niimg, background_img, cut_coords=[20, -2],
-                       display_mode="yz", title="%s weights" % penalty)
+for penalty, decoder in decoders.iteritems():
+    coef_img = mean_img(decoder.coef_img_)
+    plot_stat_map(coef_img, background_img, title=penalty, display_mode="yz",
+                  cut_coords=[20, -2])
 plt.show()
