4db09951b571a122f86cfc6b68282a3d9337bcd8
Loïc Estève <loic.esteve@ymail.com>
2014-12-03 13:59:11 +0100

    Add anatomy in plot_align_svg.py
    Also other minor tweaks

--- nilearn/plotting/glass_brain_files/plot_align_svg.py
+++ nilearn/plotting/glass_brain_files/plot_align_svg.py
@@ -8,46 +8,55 @@ import functools
 import matplotlib.pyplot as plt
 
 from nilearn import plotting
-from nilearn.plotting import glass_brain
+from nilearn.plotting import img_plotting, glass_brain
 
 plt.close('all')
 
 
-def add_brain_schematics(display, **kwargs):
+# plotting anat for coarse alignment
+bg_img, _, _, _ = img_plotting._load_anat()
+display = img_plotting.plot_glass_brain(bg_img, threshold=0, black_bg=True,
+                                        title='anat', alpha=1)
+display = img_plotting.plot_glass_brain(bg_img, threshold=0,
+                                        title='anat', alpha=1)
+
+
+# plotting slices for finer alignment
+# e.g. parieto-occipital sulcus
+
+def add_brain_schematics(display):
     for axes in display.axes.itervalues():
+        kwargs = {'alpha': 0.5, 'linewidth': 1, 'edgecolor': 'orange'}
         object_bounds = glass_brain.plot_brain_schematics(axes.ax,
                                                           axes.direction,
                                                           **kwargs)
         axes.add_object_bounds(object_bounds)
 
-my_add_brain_schematics = functools.partial(add_brain_schematics,
-                                            alpha=0.5,
-                                            linewidth=1,
-                                            edgecolor='orange')
+
 # side
 display = plotting.plot_anat(display_mode='x', cut_coords=[-2])
-my_add_brain_schematics(display)
+add_brain_schematics(display)
 
 # top
 display = plotting.plot_anat(display_mode='z', cut_coords=[20])
-my_add_brain_schematics(display)
+add_brain_schematics(display)
 
 # front
 display = plotting.plot_anat(display_mode='y', cut_coords=[-20])
-my_add_brain_schematics(display)
+add_brain_schematics(display)
 
 # all in one
 display = plotting.plot_anat(display_mode='ortho', cut_coords=(-2, -20, 20))
-my_add_brain_schematics(display)
+add_brain_schematics(display)
 
 # Plot multiple slices
 display = plotting.plot_anat(display_mode='x')
-my_add_brain_schematics(display)
+add_brain_schematics(display)
 
 display = plotting.plot_anat(display_mode='y')
-my_add_brain_schematics(display)
+add_brain_schematics(display)
 
 display = plotting.plot_anat(display_mode='z')
-my_add_brain_schematics(display)
+add_brain_schematics(display)
 
 plt.show()
