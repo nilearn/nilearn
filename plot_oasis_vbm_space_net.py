cf493afadae81e0f15df143e5ed6fb88d1542bf8
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-12-04 15:34:41 +0100

    BF: still fixing alpha grid computation

--- plot_oasis_vbm_space_net.py
+++ plot_oasis_vbm_space_net.py
@@ -17,31 +17,36 @@ age = dataset_files.ext_vars['age'].astype(float)
 
 ### Fit and predict ###########################################################
 from nilearn.decoding import SpaceNetRegressor
-decoder = SpaceNetRegressor(memory="cache", verbose=2)
-decoder.fit(dataset_files.gray_matter_maps, age)  # fit
-coef_img = decoder.coef_img_
-age_pred = decoder.predict(dataset_files.gray_matter_maps).ravel()  # predict
+for penalty in ['TV-L1', 'Smooth-LASSO']:
+    decoder = SpaceNetRegressor(memory="cache", penalty=penalty, verbose=2,
+                                n_jobs=20)
+    decoder.fit(dataset_files.gray_matter_maps, age)  # fit
+    coef_img = decoder.coef_img_
+    age_pred = decoder.predict(dataset_files.gray_matter_maps
+                               ).ravel()  # predict
 
-### Visualization #############################################################
-import matplotlib.pyplot as plt
-from nilearn.plotting import plot_stat_map
+    ### Visualization #########################################################
+    import matplotlib.pyplot as plt
+    from nilearn.plotting import plot_stat_map
 
-# weights map
-background_img = dataset_files.gray_matter_maps[0]
-plot_stat_map(coef_img, background_img, title="Graph-Net weights",
-              display_mode="z")
+    # weights map
+    background_img = dataset_files.gray_matter_maps[0]
+    plot_stat_map(coef_img, background_img, title="%s weights" % penalty,
+                  display_mode="z")
 
-# quality of predictions
-plt.figure()
-linewidth = 3
-ax1 = plt.subplot('211')
-ax1.plot(age, label="True age", linewidth=linewidth)
-ax1.plot(age_pred, '--', c="g", label="Fitted age", linewidth=linewidth)
-ax1.set_ylabel("age")
-plt.legend(loc="best")
-ax2 = plt.subplot("212")
-ax2.plot(age - age_pred, label="True age - fitted age", linewidth=linewidth)
-ax2.set_xlabel("subject")
-plt.legend(loc="best")
+    # quality of predictions
+    plt.figure()
+    plt.suptitle(penalty)
+    linewidth = 3
+    ax1 = plt.subplot('211')
+    ax1.plot(age, label="True age", linewidth=linewidth)
+    ax1.plot(age_pred, '--', c="g", label="Fitted age", linewidth=linewidth)
+    ax1.set_ylabel("age")
+    plt.legend(loc="best")
+    ax2 = plt.subplot("212")
+    ax2.plot(age - age_pred, label="True age - fitted age",
+             linewidth=linewidth)
+    ax2.set_xlabel("subject")
+    plt.legend(loc="best")
 
 plt.show()
