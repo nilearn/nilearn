4ab3c6e15068227660924662725f169b4a764fcc
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-12-09 14:02:37 +0100

    wip

--- plot_oasis_vbm_space_net.py
+++ plot_oasis_vbm_space_net.py
@@ -6,31 +6,41 @@ Voxel-Based Morphometry on Oasis dataset with Space-Net prior
 # Authors: DOHMATOB Elvis
 #          FRITSCH Virgile
 
-from nilearn import datasets
-
 n_subjects = 100  # more subjects requires more memory
+n_subjects_train = 100
 
 ### Load Oasis dataset ########################################################
+import numpy as np
+from nilearn import datasets
 dataset_files = datasets.fetch_oasis_vbm(n_subjects=n_subjects)
 age = dataset_files.ext_vars['age'].astype(float)
+age = np.array(age)
+perm = np.argsort(age)[::-1]
+age = age[perm]
+X = np.array(dataset_files.gray_matter_maps)[perm]
+X_train = X[:n_subjects_train]
+y_train = age[:n_subjects_train]
+X_test = X[n_subjects_train:]
+y_test = age[n_subjects_train:]
+X_test = X_train.copy()
+y_test = y_train.copy()
 
 
 ### Fit and predict ###########################################################
 from nilearn.decoding import SpaceNetRegressor
 for penalty in ['TV-L1', 'Smooth-LASSO'][1:]:
     decoder = SpaceNetRegressor(memory="cache", penalty=penalty, verbose=2,
-                                l1_ratios=[.25, .5, .75], n_jobs=20)
-    decoder.fit(dataset_files.gray_matter_maps, age)  # fit
+                                alphas=8.6E-8 * len(X), n_jobs=20)
+    decoder.fit(X_train, y_train)  # fit
     coef_img = decoder.coef_img_
-    age_pred = decoder.predict(dataset_files.gray_matter_maps
-                               ).ravel()  # predict
+    y_pred = decoder.predict(X_test).ravel()  # predict
 
     ### Visualization #########################################################
     import matplotlib.pyplot as plt
     from nilearn.plotting import plot_stat_map
 
     # weights map
-    background_img = dataset_files.gray_matter_maps[0]
+    background_img = X[0]
     plot_stat_map(coef_img, background_img, title="%s weights" % penalty,
                   display_mode="z")
 
@@ -39,12 +49,12 @@ for penalty in ['TV-L1', 'Smooth-LASSO'][1:]:
     plt.suptitle(penalty)
     linewidth = 3
     ax1 = plt.subplot('211')
-    ax1.plot(age, label="True age", linewidth=linewidth)
-    ax1.plot(age_pred, '--', c="g", label="Fitted age", linewidth=linewidth)
+    ax1.plot(y_test, label="True age", linewidth=linewidth)
+    ax1.plot(y_pred, '--', c="g", label="Fitted age", linewidth=linewidth)
     ax1.set_ylabel("age")
     plt.legend(loc="best")
     ax2 = plt.subplot("212")
-    ax2.plot(age - age_pred, label="True age - fitted age",
+    ax2.plot(y_test - y_pred, label="True age - fitted age",
              linewidth=linewidth)
     ax2.set_xlabel("subject")
     plt.legend(loc="best")
