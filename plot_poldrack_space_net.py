383e0eeac9aaa0d08ecde59fc8c8bcbd6c8512bc
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-12-17 16:16:51 +0100

    EXPERIMENTS: debugging why good alphas don't mean good maps

--- plot_poldrack_space_net.py
+++ plot_poldrack_space_net.py
@@ -17,17 +17,28 @@ from nilearn.decoding import SpaceNetRegressor
 penalties = ["smooth-lasso", "tv-l1"]
 decoders = {}
 for penalty in penalties:
-    decoder = SpaceNetRegressor(mask=mask_img, penalty=penalty, verbose=2)
+    decoder = SpaceNetRegressor(mask=mask_img, penalty=penalty,
+                                eps=1e-1,  # prefer large alphas
+                                memory="cache", verbose=2)
     decoder.fit(X, y)  # fit
     decoders[penalty] = decoder
 
 
 ### Visualization #############################################################
 import matplotlib.pyplot as plt
-from nilearn.image import mean_img
 from nilearn.plotting import plot_stat_map
+from nilearn.image import mean_img
 background_img = mean_img(X)
 for penalty, decoder in decoders.iteritems():
     plot_stat_map(mean_img(decoder.coef_img_), background_img, title=penalty,
                   display_mode="yz", cut_coords=[20, -2])
+    for f, ((best_alpha, best_l1_ratio), best_w) in enumerate(
+        zip(decoder.best_model_params_, decoder.all_coef_[0])):
+        plot_stat_map(decoder.masker_.inverse_transform(best_w),
+                      background_img,
+                      title=("%s: fold=%i, best alpha: %g, best "
+                             "l1_ratio: %g" % (penalty, f, best_alpha,
+                                               best_l1_ratio)),
+                      display_mode="yz", cut_coords=[20, -2])
+
 plt.show()
