0ad1e9a67b5bc31b0f470c9f766c0f6edd3d5b16
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2014-12-18 22:04:50 +0100

    ENH: using sk.cv.train_test_split in oasis

--- examples/plot_oasis_vbm_space_net.py
+++ examples/plot_oasis_vbm_space_net.py
@@ -16,18 +16,13 @@ dataset_files = datasets.fetch_oasis_vbm(n_subjects=n_subjects)
 age = dataset_files.ext_vars['age'].astype(float)
 age = np.array(age)
 X = np.array(dataset_files.gray_matter_maps)
-n_subjects_train = (len(X) * 8) / 10
-
-# shuffle data
-perm = np.random.permutation(len(X))
-X = X[perm]
-age = age[perm]
 
 # split data into training set and test set
-X_train = X[:n_subjects_train]
-y_train = age[:n_subjects_train]
-X_test = X[n_subjects_train:]
-y_test = age[n_subjects_train:]
+from sklearn.utils import check_random_state
+from sklearn.cross_validation import train_test_split
+rng = check_random_state(42)
+X_train, X_test, y_train, y_test = train_test_split(X, age, test_size=.8,
+                                                    random_state=rng)
 
 # sort test data for better visualization (trend, etc.)
 perm = np.argsort(y_test)[::-1]
@@ -40,7 +35,8 @@ from nilearn.decoding import SpaceNetRegressor
 import matplotlib.pyplot as plt
 from nilearn.plotting import plot_stat_map
 for penalty in ['tv-l1', 'smooth-lasso'][1:]:
-    decoder = SpaceNetRegressor(memory="cache", penalty=penalty, verbose=2)
+    decoder = SpaceNetRegressor(memory="cache", penalty=penalty, verbose=2,
+                                 n_jobs=24)
     decoder.fit(X_train, y_train)  # fit
     coef_img = decoder.coef_img_
     y_pred = decoder.predict(X_test).ravel()  # predict
