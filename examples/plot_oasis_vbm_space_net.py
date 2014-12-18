9f980a2a3865403cff7ad0fe0fe14a4a3656c105
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-12-18 22:47:58 +0100

    ENH+BF+CONFUSION: (on utterly bad prediction scores on oasis dataset) now using all oasis subjects

--- examples/plot_oasis_vbm_space_net.py
+++ examples/plot_oasis_vbm_space_net.py
@@ -6,8 +6,7 @@ Voxel-Based Morphometry on Oasis dataset with Space-Net prior
 # Authors: DOHMATOB Elvis
 #          FRITSCH Virgile
 
-n_subjects = 100  # more subjects requires more memory
-n_subjects_train = 100
+n_subjects = None  # more subjects requires more memory
 
 
 ### Load Oasis dataset ########################################################
@@ -16,24 +15,33 @@ from nilearn import datasets
 dataset_files = datasets.fetch_oasis_vbm(n_subjects=n_subjects)
 age = dataset_files.ext_vars['age'].astype(float)
 age = np.array(age)
-perm = np.argsort(age)[::-1]
+X = np.array(dataset_files.gray_matter_maps)
+n_subjects_train = (len(X) * 8) / 10
+
+# shuffle data
+perm = np.random.permutation(len(X))
+X = X[perm]
 age = age[perm]
-X = np.array(dataset_files.gray_matter_maps)[perm]
+
+# split data into training set and test set
 X_train = X[:n_subjects_train]
 y_train = age[:n_subjects_train]
-# X_test = X[n_subjects_train:]
-# y_test = age[n_subjects_train:]
-X_test = X_train.copy()
-y_test = y_train.copy()
+X_test = X[n_subjects_train:]
+y_test = age[n_subjects_train:]
+
+# sort test data for better visualization (trend, etc.)
+perm = np.argsort(y_test)[::-1]
+y_test = y_test[perm]
+X_test = X_test[perm]
 
 
 ### Fit and predict ###########################################################
 from nilearn.decoding import SpaceNetRegressor
 import matplotlib.pyplot as plt
 from nilearn.plotting import plot_stat_map
-for penalty in ['tv-l1', 'smooth-lasso']:
+for penalty in ['tv-l1', 'smooth-lasso'][1:]:
     decoder = SpaceNetRegressor(memory="cache", penalty=penalty, verbose=2,
-                                n_jobs=20, standardize=False)
+                                n_jobs=24)
     decoder.fit(X_train, y_train)  # fit
     coef_img = decoder.coef_img_
     y_pred = decoder.predict(X_test).ravel()  # predict
@@ -50,11 +58,11 @@ for penalty in ['tv-l1', 'smooth-lasso']:
     linewidth = 3
     ax1 = plt.subplot('211')
     ax1.plot(y_test, label="True age", linewidth=linewidth)
-    ax1.plot(y_pred, '--', c="g", label="Fitted age", linewidth=linewidth)
+    ax1.plot(y_pred, '--', c="g", label="Predicted age", linewidth=linewidth)
     ax1.set_ylabel("age")
     plt.legend(loc="best")
     ax2 = plt.subplot("212")
-    ax2.plot(y_test - y_pred, label="True age - fitted age",
+    ax2.plot(y_test - y_pred, label="True age - predicted age",
              linewidth=linewidth)
     ax2.set_xlabel("subject")
     plt.legend(loc="best")
