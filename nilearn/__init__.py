bc0ec14690c40010bb90793b67d23e7ede9c1b48
Loïc Estève <loic.esteve@ymail.com>
2015-01-23 13:53:27 +0100

    Cosmetic changes to __init__.py
    * the comment about checking dependencies before importing gzip is not relevant any more since we are not checking that we can import gzip in _check_module_dependencies
    * moved gzip import at the top of the file

--- nilearn/__init__.py
+++ nilearn/__init__.py
@@ -9,18 +9,15 @@ visualization tools.
 See http://nilearn.github.com for complete documentation.
 """
 
+import gzip
+
 from .version import _check_module_dependencies, __version__
 
 _check_module_dependencies()
 
-
 # Monkey-patch gzip to have faster reads on large gzip files
-#   Note: import must occur AFTER dependency checks occur above.
-import gzip
-
 if hasattr(gzip.GzipFile, 'max_read_chunk'):
     gzip.GzipFile.max_read_chunk = 100 * 1024 * 1024  # 100Mb
-
 
 # Boolean controling whether the joblib caches should be
 # flushed if the version of certain modules changes (eg nibabel, as it
