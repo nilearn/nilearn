6409eb49b2fea74ddec1f68b3b49aa28409b6a40
Loïc Estève <loic.esteve@ymail.com>
2015-01-23 15:12:58 +0100

    Python 2.6 fix

--- nilearn/tests/test_common_checks.py
+++ nilearn/tests/test_common_checks.py
@@ -6,8 +6,7 @@ modules whose name starts with an underscore.
 
 """
 #Author: Virgile Fritsch, <virgile.fritsch@inria.fr>, Jun. 2014
-from nose.tools import (assert_equal, assert_less, assert_greater_equal,
-                        assert_raises)
+from nose.tools import assert_true, assert_equal, assert_raises
 
 from nilearn._utils.common_checks import check_n_jobs
 
@@ -23,8 +22,8 @@ def test_check_n_jobs():
     # Too many CPUs case
     n_jobs_large = 100000000
     n_jobs_safe = check_n_jobs(n_jobs_large)
-    assert_less(n_jobs_safe, n_jobs_large)
-    assert_greater_equal(n_jobs_safe, 1)
+    assert_true(n_jobs_safe < n_jobs_large)
+    assert_true(n_jobs_safe >= 1)
 
     # Too many CPUs ignored
     n_jobs_not_enough = -100000000  # all CPUs except 100000000
