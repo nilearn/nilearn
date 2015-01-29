eb2eaedbe3576fe80ca38bf62b5d8cbfe27aed43
Loïc Estève <loic.esteve@ymail.com>
2015-01-29 15:27:09 +0100

    TRAVIS cosmetic tweaks to show-packages-versions.py

--- continuous_integration/show-python-packages-versions.py
+++ continuous_integration/show-python-packages-versions.py
@@ -7,19 +7,20 @@ DEPENDENCIES = ['numpy', 'scipy', 'sklearn', 'matplotlib', 'nibabel']
 def print_package_version(package_name, indent='  '):
     try:
         package = __import__(package_name)
-        version = package.__version__
+        version = getattr(package, '__version__', None)
+        package_file = getattr(package, '__file__', )
+        provenance_info = '{0} from {1}'.format(version, package_file)
     except ImportError:
-        version = 'not installed'
-    except AttributeError:
-        version = None
+        provenance_info = 'not installed'
 
-    print('{0}{1}: {2}'.format(indent, package_name, version))
+    print('{0}{1}: {2}'.format(indent, package_name, provenance_info))
 
 if __name__ == '__main__':
-    print('=' * 80)
-    print('Python', sys.version, '\n')
+    print('=' * 120)
+    print('Python', sys.version)
+    print('from: ', sys.executable, '\n')
 
     print('Dependencies versions')
     for package_name in DEPENDENCIES:
         print_package_version(package_name)
-    print('=' * 80)
+    print('=' * 120)
