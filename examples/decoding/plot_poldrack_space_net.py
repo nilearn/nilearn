98c59811ac8ac88362b6118794b598ed92682eb7
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2015-02-05 13:39:02 +0100

    - ENH: removed zombie doc for inexistend normalize param - ENH: simplified space nets by removing superfluous for loops

--- examples/decoding/plot_poldrack_space_net.py
+++ examples/decoding/plot_poldrack_space_net.py
@@ -12,23 +12,31 @@ data = fetch_mixed_gambles(n_subjects=16, make_Xy=True)
 X, y, mask_img = data.X, data.y, data.mask_img
 
 
-### Fit and predict ##########################################################
+### Fit Smooth-Lasso ##########################################################
+penalty = "smooth-lasso"
 from nilearn.decoding import SpaceNetRegressor
-penalties = ["smooth-lasso", "tv-l1"]
-decoders = {}
-for penalty in penalties:
-    decoder = SpaceNetRegressor(mask=mask_img, penalty=penalty,
-                                eps=1e-1,  # prefer large alphas
-                                memory="cache", verbose=2)
-    decoder.fit(X, y)  # fit
-    decoders[penalty] = decoder
+decoder = SpaceNetRegressor(mask=mask_img, penalty=penalty,
+                            eps=1e-1,  # prefer large alphas
+                            memory="cache", verbose=2)
+decoder.fit(X, y)  # fit
 
-
-### Visualization #############################################################
+### Visualize Smooth-Lasso weights
 import matplotlib.pyplot as plt
 from nilearn.plotting import plot_stat_map
 from nilearn.image import mean_img
-for penalty, decoder in decoders.iteritems():
-    plot_stat_map(mean_img(decoder.coef_img_), title=penalty,
-                  display_mode="yz", cut_coords=[20, -2])
+plot_stat_map(mean_img(decoder.coef_img_), title=penalty, display_mode="yz",
+              cut_coords=[20, -2])
+
+
+### Fit TV-L1 #################################################################
+penalty = "tv-l1"
+decoder = SpaceNetRegressor(mask=mask_img, penalty=penalty,
+                            eps=1e-1,  # prefer large alphas
+                            memory="cache", verbose=2)
+decoder.fit(X, y)  # fit
+
+### Visualize TV-L1 weights
+plot_stat_map(mean_img(decoder.coef_img_), title=penalty, display_mode="yz",
+              cut_coords=[20, -2])
+
 plt.show()
