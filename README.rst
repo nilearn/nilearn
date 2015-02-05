8c800db935169895d9ee89525831f5b6fad2fb85
Konstantin Shmelkov <konstantin.shmelkov@polytechnique.edu>
2015-02-05 17:25:24 +0100

    another version try

--- README.rst
+++ README.rst
@@ -29,7 +29,7 @@ The required dependencies to use the software are:
 * Python >= 2.6,
 * setuptools
 * Numpy >= 1.6
-* SciPy >= 0.11
+* SciPy >= 0.9
 * Scikit-learn >= 0.12.1
 * Nibabel >= 1.1.0.
 This configuration corresponds to versions about the end of 2012.
--- nilearn/version.py
+++ nilearn/version.py
@@ -19,7 +19,7 @@ REQUIRED_MODULE_METADATA = (
         'required_at_installation': True,
         'install_info': _NILEARN_INSTALL_MSG}),
     ('scipy', {
-        'min_version': '0.11.0',
+        'min_version': '0.9.0',
         'required_at_installation': True,
         'install_info': _NILEARN_INSTALL_MSG}),
     ('sklearn', {
