541d2d0635cc091087636baa6a84959a24997719
Loïc Estève <loic.esteve@ymail.com>
2015-02-06 11:00:13 +0100

    DOC: mention that scikit-learn 0.14.1 is needed for some examples

--- doc/sphinxext/gen_rst.py
+++ doc/sphinxext/gen_rst.py
@@ -452,8 +452,14 @@ Examples
 
 .. warning::
 
-   If you want to run the examples you need to make sure you have installed
-   nilearn by following the instructions :ref:`there <installation>`.
+   If you want to run the examples you need to make sure you have
+   installed nilearn by following the instructions :ref:`there
+   <installation>`.
+
+.. note::
+
+    A few examples may not run with scikit-learn versions older than
+    0.14.1.
 
 
 """)
