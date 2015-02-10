a993c818398cf044187037de85750278ae66b95d
Loïc Estève <loic.esteve@ymail.com>
2015-02-10 09:31:56 +0100

    DOC follow same naming conventions as in other examples

--- examples/manipulating_visualizing/plot_atlas.py
+++ examples/manipulating_visualizing/plot_atlas.py
@@ -6,11 +6,12 @@ Plot the regions of a reference atlas.
 """
 
 import matplotlib.pyplot as plt
-from nilearn.datasets import fetch_yeo_2011_atlas
+from nilearn import datasets
 from nilearn import plotting
 
-atlas = fetch_yeo_2011_atlas()
-print atlas.description
+yeo_2011_atlas_dataset = datasets.fetch_yeo_2011_atlas()
+print yeo_2011_atlas_dataset.description
 
-plotting.plot_roi(atlas.thick_17, title="yeo 2011 atlas")
+plotting.plot_roi(yeo_2011_atlas_dataset.thick_17,
+                  title="yeo 2011 atlas")
 plt.show()
