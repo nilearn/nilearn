9e27961a5c6118f963c08558287d8b8cf3ace9d3
Ben Cipollini <bcipolli@ucsd.edu>
2015-02-23 14:05:47 -0800

    Avoid exploding memory on transpose error.

--- nilearn/masking.py
+++ nilearn/masking.py
@@ -666,6 +666,14 @@ def _unmask_nd(X, mask, order="C"):
         raise ValueError("mask must be a boolean array")
     if X.ndim != 2:
         raise ValueError("X must be a 2-dimensional array")
+    n_features = mask.sum()
+    if X.shape[-1] != n_features:
+        # Handle (potential) transpose as a special case, as this
+        #   mistake can lead to system-fatal memory allocation below.
+        if X.shape[0] == n_features:
+            X = X.T
+        else:
+            raise ValueError('X must be of shape (samples, %d).' % n_features)
 
     data = np.zeros(mask.shape + (X.shape[0],), dtype=X.dtype, order=order)
     data[mask, :] = X.T
