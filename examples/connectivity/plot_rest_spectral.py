7fbe49c973b21da7d713e31d12d100e74b0779b1
Ben Cipollini <bcipolli@ucsd.edu>
2015-02-22 20:51:35 -0800

    Using smooth_img / plot_roi

--- examples/connectivity/plot_rest_spectral.py
+++ examples/connectivity/plot_rest_spectral.py
@@ -11,19 +11,16 @@ Refernce :
     Constrained Spectral Clustering". Human Brain Mapping 33, no 8 (2012):
     1914-1928. doi:10.1002/hbm.21333.
 """
-
 import numpy as np
 import scipy as sp
-import time
 
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.image import grid_to_graph
 
-from nilearn import datasets, input_data
-from nilearn._utils.cache_mixin import cache
-
 ### Fetch and mask data #######################################################
 print('Loading dataset and masking subject data... ')
+
+from nilearn import datasets, input_data
 
 dataset = datasets.fetch_adhd(n_subjects=1)
 nifti_masker = input_data.NiftiMasker(memory='nilearn_cache', memory_level=1,
@@ -46,10 +43,12 @@ for i, (r, c) in enumerate(zip(rows, cols)):
         values[i] = 0.
         continue
     values[i] = corr
+
 # Keep a number of correlation equal to XX% of the number of voxels
 n_voxels = connectivity.shape[0]
 thr = np.sort(values)[- int(n_voxels * 1.8)]
-print("Voxels: %d; thresshold: %.2f" % (n_voxels, thr))
+print("Voxels: %d; thresshold: %.4f" % (n_voxels, thr))
+
 rows = rows[np.where(values >= thr)]
 cols = cols[np.where(values >= thr)]
 values = values[np.where(values >= thr)]
@@ -64,9 +63,8 @@ clustering = spectral_clustering(affinity,
 
 ### Plot results #######################################################
 
-from nilearn.image import index_img
-clustering = nifti_masker.inverse_transform(clustering)
+from nilearn.plotting import plot_roi
 
-plt.imshow(clustering.get_data()[..., 25] + 1, cmap='Set1',
-          interpolation='nearest')
+cluster_img = nifti_masker.inverse_transform(clustering)
+plot_roi(cluster_img)
 plt.show()
