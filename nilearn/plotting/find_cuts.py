488c3cb994353e66338d0b5155deb33dc78ccdc0
Alexandre Abraham <abraham.alexandre@gmail.com>
2015-03-17 11:33:50 +0100

    Remove redundant test.

--- nilearn/plotting/find_cuts.py
+++ nilearn/plotting/find_cuts.py
@@ -77,7 +77,7 @@ def find_xyz_cut_coords(img, mask=None, activation_threshold=None):
                                                    80)
     mask = np.abs(my_map) > activation_threshold - 1.e-15
     # mask may be zero everywhere in rare cases
-    if (mask.max() == 0) and (mask.min() == 0):
+    if mask.max() == 0:
         return .5 * np.array(data.shape)
     mask = largest_connected_component(mask)
     slice_x, slice_y, slice_z = ndimage.find_objects(mask)[0]
