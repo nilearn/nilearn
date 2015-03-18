79afb608c3f6a2729e168182bc7881c86ca92312
Loïc Estève <loic.esteve@ymail.com>
2015-03-18 08:55:38 +0100

    Switch version to dev one
    and use a PEP440 compliant one while we are at it

--- nilearn/version.py
+++ nilearn/version.py
@@ -5,7 +5,23 @@ nilearn version, required package versions, and utilities for checking
 # Author: Loïc Estève, Ben Cipollini
 # License: simplified BSD
 
-__version__ = '0.1.2'
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+# X.Y
+# X.Y.Z # For bugfix releases
+#
+# Admissible pre-release markers:
+# X.YaN # Alpha release
+# X.YbN # Beta release
+# X.YrcN # Release Candidate
+# X.Y # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = '0.1.3.dev0'
 
 _NILEARN_INSTALL_MSG = 'See %s for installation information.' % (
     'http://nilearn.github.io/introduction.html#installation')
