5d68e77b21d77d812fe34cad99f32b2547d295d1
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2015-05-04 14:25:05 +0200

    BF: issue #570 (including tests)

--- nilearn/decomposition/canica.py
+++ nilearn/decomposition/canica.py
@@ -165,7 +165,7 @@ class CanICA(MultiPCA, CacheMixin):
         ica_maps_and_sparsities = ((ica_map,
                                     np.sum(np.abs(ica_map), axis=1).max())
                                    for ica_map in ica_maps_gen_)
-        ica_maps, sparsity = min(ica_maps_and_sparsities, key=itemgetter(-1))
+        ica_maps, _ = min(ica_maps_and_sparsities, key=itemgetter(-1))
 
         # Thresholding
         ratio = None
@@ -184,5 +184,10 @@ class CanICA(MultiPCA, CacheMixin):
                 100. - (100. / len(ica_maps)) * ratio)
             ica_maps[abs_ica_maps < threshold] = 0.
         self.components_ = ica_maps
+
+        # BF for issue #570: flip signs of components to have more +ve than -ve
+        for component in self.components_:
+            if (component > 0).sum() < (component < 0).sum():
+                component *= -1
 
         return self
--- nilearn/decomposition/tests/test_canica.py
+++ nilearn/decomposition/tests/test_canica.py
@@ -2,17 +2,18 @@
 
 import numpy as np
 from numpy.testing import assert_array_almost_equal
-from nose.tools import assert_true, assert_raises
+from nose.tools import assert_true, assert_raises, assert_less_equal
 
 import nibabel
 
 from nilearn.decomposition.canica import CanICA
 
 
-def test_canica_square_img():
+def _make_canica_test_data(rng=None, n_subjects=8):
     shape = (20, 20, 1)
     affine = np.eye(4)
-    rng = np.random.RandomState(0)
+    if rng is None:
+        rng = np.random.RandomState(0)
 
     # Create two images with "activated regions"
     component1 = np.zeros(shape)
@@ -36,7 +37,7 @@ def test_canica_square_img():
 
     # Create a "multi-subject" dataset
     data = []
-    for i in range(8):
+    for _ in range(n_subjects):
         this_data = np.dot(rng.normal(size=(40, 4)), components)
         this_data += .01 * rng.normal(size=this_data.shape)
         # Get back into 3D for CanICA
@@ -46,6 +47,12 @@ def test_canica_square_img():
 
     mask_img = nibabel.Nifti1Image(np.ones(shape, dtype=np.int8), affine)
 
+    return data, mask_img, components, rng
+
+
+def test_canica_square_img():
+    data, mask_img, components, rng = _make_canica_test_data()
+ 
     # We do a large number of inits to be sure to find the good match
     canica = CanICA(n_components=4, random_state=rng, mask=mask_img,
                     smoothing_fwhm=0., n_init=50)
@@ -66,6 +73,33 @@ def test_canica_square_img():
 
     # Smoke test to make sure an error is raised when no data is passed.
     assert_raises(TypeError, canica.fit)
+
+
+def test_component_sign():
+    # BF for issue #570
+
+    # make data
+    data, mask_img, components, rng = _make_canica_test_data(n_subjects=3)
+
+    # fill with -1's
+    components[:] = -1
+
+    # have some +1's (so things are not degenerate), but still have more -1's
+    # than +1's
+    for mp in components:
+        mp[rng.randn(*mp.shape) > .8] *= -1
+        plus, minus = (mp > 0).sum(), (mp < 0).sum()
+        assert_less_equal(plus, minus)
+
+    # fit run CanICA at different times of the day
+    canica = CanICA(n_components=4, random_state=rng, mask=mask_img)
+    for _ in xrange(3):
+        canica.fit(data)
+        maps = canica.masker_.inverse_transform(canica.components_).get_data()
+        maps = np.rollaxis(maps, 3, 0)
+        for mp in maps:
+            plus, minus = (mp > 0).sum(), (mp < 0).sum()
+            assert_less_equal(minus, plus)
 
 if __name__ == "__main__":
     test_canica_square_img()
