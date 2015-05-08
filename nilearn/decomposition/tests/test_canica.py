1872ec1a1359fc4486f244b7e6e561cbbba10882
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2015-05-08 14:07:21 +0200

    FIXUPS: test_component_sign in canica (issue #570)

--- nilearn/decomposition/tests/test_canica.py
+++ nilearn/decomposition/tests/test_canica.py
@@ -15,12 +15,22 @@ except ImportError:
             raise AssertionError("%f is not less than %f" % (a, b))
 
 
-def _make_canica_test_data(rng=None, n_subjects=8):
-    shape = (20, 20, 1)
-    affine = np.eye(4)
+def _make_data_from_components(components, affine, shape, rng=None,
+                               n_subjects=8):
+    data = []
     if rng is None:
         rng = np.random.RandomState(0)
+    for _ in range(n_subjects):
+        this_data = np.dot(rng.normal(size=(40, 4)), components)
+        this_data += .01 * rng.normal(size=this_data.shape)
+        # Get back into 3D for CanICA
+        this_data = np.reshape(this_data, (40,) + shape)
+        this_data = np.rollaxis(this_data, 0, 4)
+        data.append(nibabel.Nifti1Image(this_data, affine))
+    return data
 
+
+def _make_canica_components(shape):
     # Create two images with "activated regions"
     component1 = np.zeros(shape)
     component1[:5, :10] = 1
@@ -38,21 +48,21 @@ def _make_canica_test_data(rng=None, n_subjects=8):
     component4[-5:, :10] = 1
     component4[-10:-5, :10] = -1
 
-    components = np.vstack((component1.ravel(), component2.ravel(),
-                            component3.ravel(), component4.ravel()))
+    return np.vstack((component1.ravel(), component2.ravel(),
+                      component3.ravel(), component4.ravel()))
+
+
+def _make_canica_test_data(rng=None, n_subjects=8):
+    if rng is None:
+        rng = np.random.RandomState(0)
+    shape = (20, 20, 1)
+    affine = np.eye(4)
+    components = _make_canica_components(shape)
 
     # Create a "multi-subject" dataset
-    data = []
-    for _ in range(n_subjects):
-        this_data = np.dot(rng.normal(size=(40, 4)), components)
-        this_data += .01 * rng.normal(size=this_data.shape)
-        # Get back into 3D for CanICA
-        this_data = np.reshape(this_data, (40,) + shape)
-        this_data = np.rollaxis(this_data, 0, 4)
-        data.append(nibabel.Nifti1Image(this_data, affine))
-
+    data = _make_data_from_components(components, affine, shape, rng=rng,
+                                      n_subjects=n_subjects)
     mask_img = nibabel.Nifti1Image(np.ones(shape, dtype=np.int8), affine)
-
     return data, mask_img, components, rng
 
 
@@ -84,17 +94,23 @@ def test_canica_square_img():
 def test_component_sign():
     # BF for issue #570
 
-    # make data
-    data, mask_img, components, rng = _make_canica_test_data(n_subjects=2)
+    # make data (SVD)
+    rng = np.random.RandomState(0)
+    shape = (20, 10, 1)
+    affine = np.eye(4)
+    components = _make_canica_components(shape)
 
-    # fill with -1's
-    components[:] = -1
-
-    # have some +1's (so things are not degenerate), but still have more -1's
-    # than +1's
+    # make more de-activations than activations
     for mp in components:
-        mp[rng.randn(*mp.shape) > .8] *= -.5
-        assert_less_than(mp.max(), -mp.min())
+        mp[rng.randn(*mp.shape) > .8] *= -1.
+        assert_less_than(mp.max(), -mp.min())  # goal met ?
+    data = _make_data_from_components(components, affine, shape,
+                                      rng=rng, n_subjects=2)
+
+    # synthesize data with given components
+    data = _make_data_from_components(components, affine, shape, rng=rng,
+                                      n_subjects=2)
+    mask_img = nibabel.Nifti1Image(np.ones(shape, dtype=np.int8), affine)
 
     # fit run CanICA at different times of the day
     canica = CanICA(n_components=4, random_state=rng, mask=mask_img)
