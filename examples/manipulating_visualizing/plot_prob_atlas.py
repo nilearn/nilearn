0d1c6ff5ccf4bfac557d508981594a7aef41c2a6
KamalakerDadi <dkamalakarreddy@gmail.com>
2015-06-19 15:27:26 +0200

    Added an example showing how to plot Harvard Oxford Atlas

---
+++ examples/manipulating_visualizing/plot_prob_atlas.py
@@ -0,0 +1,43 @@
+"""
+Plotting 4D Atlas maps or regions onto the anatomical image
+===========================================================
+
+We show here how to plot the atlas maps or statistical maps which are
+4D using a three different display types, for example using
+1. "contours", plots can be seen only in contours
+2. "filled_contours", plots can be seen in contours with fillings
+3. "continuous or overlays", plots can be seen in continuous overlays
+
+You can plot each map with each different color which will chose
+automatically.
+Please see the related documentation for more information.
+"""
+import matplotlib.pyplot as plt
+from nilearn import datasets
+from nilearn import plotting
+
+# Fetch the 4D atlas data or 4D statistical output data
+# Harvard Oxford Atlas
+atlas_filename, labels = datasets.fetch_harvard_oxford('cort-prob-2mm')
+
+# Plotting Harvard Oxford Atlas maps as a contours type
+# using view_type='contours'
+display = plotting.img_plotting.plot_prob_atlas(atlas_filename,
+                                                view_type='contours',
+                                                title='Harvard-Oxford atlas '
+                                                      'maps as contours')
+# Plotting Harvard Oxford Atlas maps as a contours filled regions
+# using view_type='filled_contours'
+display = plotting.img_plotting.plot_prob_atlas(atlas_filename,
+                                                view_type='filled_contours',
+                                                title='Harvard-Oxford atlas '
+                                                      'maps as a '
+                                                      'contours and contour fillings')
+# Plotting Harvard Oxford Atlas maps as a continuous type
+# using view_type='continuous'
+display = plotting.img_plotting.plot_prob_atlas(atlas_filename,
+                                                view_type='continuous',
+                                                title='Harvard-Oxford atlas '
+                                                      'maps as a '
+                                                      'continuous overlays')
+plt.show()
