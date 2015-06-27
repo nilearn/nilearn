2a04f4153bc2a6d85fd72ce375df83ea546fd9b2
banilo <danilobzdok@gmail.com>
2015-06-27 10:56:00 +0200

    new_img_like: niimg compat

--- nilearn/_utils/niimg.py
+++ nilearn/_utils/niimg.py
@@ -111,12 +111,12 @@ def load_niimg(niimg, dtype=None):
     return niimg
 
 
-def new_img_like(ref_img, data, affine, copy_header=False):
+def new_img_like(ref_niimg, data, affine=None, copy_header=False):
     """Create a new image of the same class as the reference image
 
     Parameters
     ----------
-    ref_img: image
+    ref_niimg: image
         Reference image. The new image will be of the same type.
 
     data: numpy array
@@ -135,23 +135,26 @@ def new_img_like(ref_img, data, affine, copy_header=False):
     new_img: image
         An image which has the same type as the reference image.
     """
+    from .niimg_conversions import check_niimg  # avoid circular import
+
+    ref_niimg = check_niimg(ref_niimg)
     if affine is None:
-        affine = ref_img.get_affine()
+        affine = ref_niimg.get_affine()
     if data.dtype == bool:
         default_dtype = np.int8
         if (LooseVersion(nibabel.__version__) >= LooseVersion('1.2.0') and
-                isinstance(ref_img, nibabel.freesurfer.mghformat.MGHImage)):
+                isinstance(ref_niimg, nibabel.freesurfer.mghformat.MGHImage)):
             default_dtype = np.uint8
         data = as_ndarray(data, dtype=default_dtype)
     header = None
     if copy_header:
-        header = copy.copy(ref_img.get_header())
+        header = copy.copy(ref_niimg.get_header())
         header['scl_slope'] = 0.
         header['scl_inter'] = 0.
         header['glmax'] = 0.
         header['cal_max'] = np.max(data) if data.size > 0 else 0.
         header['cal_max'] = np.min(data) if data.size > 0 else 0.
-    return ref_img.__class__(data, affine, header=header)
+    return ref_niimg.__class__(data, affine, header=header)
 
 
 def copy_img(img):
