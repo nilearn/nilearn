b4fd01ca09f98499892c10e16af37b09c35b369c
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-07-14 17:16:02 +0200

    refactor demos

--- examples/manipulating_visualizing/plot_demo_glass_brain.py
+++ examples/manipulating_visualizing/plot_demo_glass_brain.py
@@ -20,21 +20,12 @@ localizer_tmap_filename = localizer_dataset.tmaps[1]
 # demo glass brain plotting
 from nilearn import plotting
 
-plotting.plot_glass_brain(localizer_tmap_filename, threshold=0, colorbar=True)
+plotting.plot_glass_brain(localizer_tmap_filename, colorbar=True)
 
 plotting.plot_glass_brain(localizer_tmap_filename, threshold=3, colorbar=True)
 
 plotting.plot_glass_brain(localizer_tmap_filename, title='plot_glass_brain',
                           black_bg=True, display_mode='xz', threshold=3, colorbar=True)
-
-plotting.plot_glass_brain(localizer_tmap_filename, threshold=0, colorbar=True, plot_abs=False)
-
-plotting.plot_glass_brain(localizer_tmap_filename, threshold=3, colorbar=True,
-                          plot_abs=False)
-
-plotting.plot_glass_brain(localizer_tmap_filename, title='plot_glass_brain',
-                          black_bg=True, display_mode='xz', threshold=3, colorbar=True,
-                          plot_abs=False)
 
 import matplotlib.pyplot as plt
 plt.show()
