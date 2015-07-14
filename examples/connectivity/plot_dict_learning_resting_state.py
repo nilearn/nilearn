27d2724e28113ba5951302cf875bd4327d3180dd
Arthur Mensch <arthur.mensch@gmail.com>
2015-07-14 15:57:50 +0200

    WIP

--- examples/connectivity/plot_dict_learning_resting_state.py
+++ examples/connectivity/plot_dict_learning_resting_state.py
@@ -19,7 +19,7 @@ Pre-prints for paper is available on hal
 from nilearn import datasets
 # For linear assignment (should be moved in non user space...)
 
-adhd_dataset = datasets.fetch_adhd(n_subjects=20, data_dir='/media/data/neuro')
+adhd_dataset = datasets.fetch_adhd(n_subjects=20)
 func_filenames = adhd_dataset.func  # list of 4D nifti files for each subject
 
 # print basic information on the dataset
@@ -33,26 +33,23 @@ from nilearn.decomposition.canica import CanICA
 n_components = 10
 
 dict_learning = DictLearning(n_components=n_components, smoothing_fwhm=6.,
-                             memory="/media/data/nilearn_cache", memory_level=5, verbose=2, random_state=0,
+                             memory="nilearn_cache", memory_level=5, verbose=2, random_state=0,
                              n_jobs=1, alpha=6, n_iter=1000)
-canica = CanICA(n_components=n_components, smoothing_fwhm=6.,
-                memory="/media/data/nilearn_cache", memory_level=5, verbose=2, random_state=0,
-                n_jobs=1, n_init=1, threshold=3.)
+canica = CanICA(n_components=n_components, smoothing_fwhm=6., memory_level=5, verbose=2, random_state=0,
+                memory="nilearn_cache", n_jobs=1, n_init=1, threshold=3.)
 
 estimators = [canica, dict_learning]
 
+components_imgs = []
 
 for estimator in estimators:
+    print('[Example] Learning maps using %s model' % type(estimator).__name__)
     estimator.fit(func_filenames)
-
-print('[Example] Dumping results')
-
-components_imgs = []
-# Retrieve learned spatial maps in brain space
-for i, estimator in enumerate(estimators):
+    print('[Example] Dumping results')
     components_img = estimator.masker_.inverse_transform(estimator.components_)
     components_img.to_filename('%s_resting_state.nii.gz' % type(estimator).__name__)
     components_imgs.append(components_img)
+
 
 ### Visualize the results #####################################################
 # Show some interesting components
@@ -60,6 +57,7 @@ import matplotlib.pyplot as plt
 from nilearn.plotting import plot_stat_map, find_xyz_cut_coords
 from nilearn.image import index_img
 
+print('[Example] Displaying')
 for i in range(n_components):
     if i % 2 == 0:
         fig, axes = plt.subplots(nrows=3)
