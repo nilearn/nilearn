a0e99e161a2d4f4070a56afe22b9bf391fe8cecb
juhuntenburg <ju.huntenburg@gmail.com>
2015-07-15 10:56:59 +0200

    improving descriptions

--- examples/connectivity/plot_atlas_comparison.py
+++ examples/connectivity/plot_atlas_comparison.py
@@ -1,6 +1,6 @@
 """
-Extracting signals from different atlases and compare
-======================================================
+Extracting resting-state signals from different atlases for comparison
+======================================================================
 
 In this example :class:`nilearn.input_data.NiftiLabelsMasker` is used to
 extract time series from nifti objects using different parcellation atlases.
@@ -20,12 +20,23 @@ destrieux = datasets.fetch_atlas_destrieux_2009()
 yeo = datasets.fetch_atlas_yeo_2011()
 harvard_oxford = datasets.fetch_atlas_harvard_oxford('cort-maxprob-thr25-2mm')
 
-atlases = {'Destrieux Atlas': destrieux['maps'],
-           'Yeo Atlas 17 thick': yeo['thick_17'],
-           'Harvard Oxford > 25%': harvard_oxford['maps']}
+print('Destrieux atlas nifti image (3D) is located at: %s' % destrieux['maps'])
+print('Yeo atlas nifti image (3D) with 17 parcels and liberal mask is located '
+      'at: %s' % yeo['thick_17'])
+print('Harvard Oxford atlas nifti image (3D) thresholded at .25 is located '
+      'at: %s' % harvard_oxford['maps'])
+
+atlases = {'Destrieux Atlas (struct)': destrieux['maps'],
+           'Yeo Atlas 17 thick (func)': yeo['thick_17'],
+           'Harvard Oxford > 25% (struct)': harvard_oxford['maps']}
 
 # load functional data
 data = datasets.fetch_adhd(n_subjects=10)
+
+print('Functional nifti images (4D, one per subject) are located at : %r'
+      % data['func'])
+print('Counfound csv files (one per subject) are located at : %r'
+      % data['confounds'])
 
 # loop over atlases
 for name, atlas in sorted(atlases.items()):
@@ -39,7 +50,7 @@ for name, atlas in sorted(atlases.items()):
     for func, confounds in zip(data.func, data.confounds):
         time_series.append(masker.fit_transform(func, confounds=confounds))
 
-    time_series = np.concatenate((time_series))
+    time_series = np.concatenate(time_series)
 
     # calculate correlation matrix and display
     correlation_matrix = np.corrcoef(time_series.T)
