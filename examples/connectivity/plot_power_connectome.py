80cf426f5a62f823270060e3718b06872320f1eb
amadeuskanaan <amadeus.kanan@gmail.com>
2015-07-16 11:40:15 +0200

    pep8  checker, description of example

--- examples/connectivity/plot_power_connectome.py
+++ examples/connectivity/plot_power_connectome.py
@@ -1,46 +1,61 @@
+"""
+Extracting signals and plotting a connectome for the Power-264 seed-region atlas
+=============================================================
+
+This example shows how to extract signals from spherical seed-regions based
+on the Power-264 atlas (Power, 2011) and estimating a connectome using sparse
+inverse covariance.
+
+Power, Jonathan D., et al. "Functional network organization of the
+human brain." Neuron 72.4 (2011): 665-678.
+
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from nilearn import datasets
 
 
-#fetch power atlas coords
+# fetch power atlas coords
 power = datasets.fetch_atlas_power_2011()
 power_coords = power.rois[['x', 'y', 'z']]
 
 #fetch dataset
-
 adhd = datasets.fetch_adhd(n_subjects=1)
 
 # Preprocess image and extract signals from Power-264
 from nilearn import input_data
-masker = input_data.NiftiSpheresMasker(seeds = power_coords,
-                                       radius = 5.,
-                                       standardize = True,
-                                       smoothing_fwhm = 4,
-                                       detrend = True,
-                                       low_pass = 0.1,
-                                       high_pass = 0.01,
-                                       t_r = 2.5,
-                                      verbose = 5)
 
-timeseries = masker.fit_transform(adhd.func[0], confounds = adhd.confounds[0])
+masker = input_data.NiftiSpheresMasker(seeds=power_coords,
+                                       radius=5.,
+                                       standardize=True,
+                                       smoothing_fwhm=4,
+                                       detrend=True,
+                                       low_pass=0.1,
+                                       high_pass=0.01,
+                                       t_r=2.5,
+                                       verbose=5)
+
+timeseries = masker.fit_transform(adhd.func[0], confounds=adhd.confounds[0])
 
 # calculate connectivity and plot Power-264 correlation matrix
 connectivity = np.arctanh(np.corrcoef(timeseries.T))
-plt.imshow(connectivity, vmin = -1., vmax = 1.)
+plt.imshow(connectivity, vmin=-1., vmax=1.)
 plt.colorbar()
 plt.title('Power 264 Connectivity')
 
 # Plot the connectome
 from nilearn import plotting
+
 plotting.plot_connectome(connectivity,
                          power_coords,
-                         edge_threshold ='99.8%',
-                         node_size = 20)
+                         edge_threshold='99.8%',
+                         node_size=20)
 
-#############################################################################
+
 # Compute the sparse inverse covariance
 from sklearn.covariance import GraphLassoCV
+
 estimator = GraphLassoCV()
 estimator.fit(timeseries)
 
@@ -54,13 +69,14 @@ plt.title('Covariance')
 plotting.plot_connectome(estimator.covariance_,
                          power_coords,
                          title='Covariance',
-                         edge_threshold ='99.8%',
-                         node_size = 20)
+                         edge_threshold='99.8%',
+                         node_size=20)
 
 # Display the sparse inverse covariance
 plt.figure(figsize=(5, 5))
 plt.imshow(-estimator.precision_, interpolation="nearest",
            vmax=1, vmin=-1, cmap=plt.cm.RdBu_r)
+
 # And display the labels
 plt.title('Sparse inverse covariance')
 
@@ -68,6 +84,5 @@ plt.title('Sparse inverse covariance')
 plotting.plot_connectome(-estimator.precision_, power_coords,
                          title='Sparse inverse covariance',
                          edge_threshold="99.8%",
-                         node_size = 20)
-
+                         node_size=20)
 plt.show()
