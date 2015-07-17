4a95b0b929d0b692280675bf0c557a421b8d6efb
amadeuskanaan <amadeus.kanan@gmail.com>
2015-07-17 14:51:50 +0200

    plots connectomes instead of mats with computed parcel cut coords

--- examples/connectivity/plot_atlas_comparison.py
+++ examples/connectivity/plot_atlas_comparison.py
@@ -9,11 +9,11 @@ The time series of all subjects of the ADHD Dataset are concatenated to create
 parcel-wise correlation matrices for each atlas.
 
 """
-
-from nilearn import datasets
-from nilearn.input_data import NiftiLabelsMasker
 import numpy as np
 from matplotlib import pyplot as plt
+from nilearn import datasets
+from nilearn.input_data import NiftiLabelsMasker
+from nilearn.plotting import plot_connectome, find_parcellation_cut_coords
 
 # load atlases
 destrieux = datasets.fetch_atlas_destrieux_2009()
@@ -26,9 +26,9 @@ print('Yeo atlas nifti image (3D) with 17 parcels and liberal mask is located '
 print('Harvard Oxford atlas nifti image (3D) thresholded at .25 is located '
       'at: %s' % harvard_oxford['maps'])
 
-atlases = {'Destrieux Atlas (struct)': destrieux['maps'],
-           'Yeo Atlas 17 thick (func)': yeo['thick_17'],
-           'Harvard Oxford > 25% (struct)': harvard_oxford['maps']}
+atlases_and_thresholds = {'Destrieux Atlas (struct)': [destrieux['maps'], '97%'],
+                          'Yeo Atlas 17 thick (func)': [yeo['thick_17'], '70%'],
+                          'Harvard Oxford > 25% (struct)': [harvard_oxford['maps'], '90%']}
 
 # load functional data
 data = datasets.fetch_adhd(n_subjects=10)
@@ -39,7 +39,7 @@ print('Counfound csv files (one per subject) are located at : %r'
       % data['confounds'])
 
 # loop over atlases
-for name, atlas in sorted(atlases.items()):
+for name, (atlas, threshold) in sorted(atlases_and_thresholds.items()):
     # create masker to extract functional data within atlas parcels
     masker = NiftiLabelsMasker(labels_img=atlas,
                                standardize=True,
@@ -55,8 +55,13 @@ for name, atlas in sorted(atlases.items()):
     # calculate correlation matrix and display
     correlation_matrix = np.corrcoef(time_series.T)
 
-    plt.figure(figsize=(5, 5))
-    plt.suptitle(name, size=14)
-    plt.imshow(correlation_matrix, interpolation="nearest")
+    # grab center coordiantes for atlas labels
+    coordinates = find_parcellation_cut_coords(atlas)
+
+    #plot connectome
+    plot_connectome(correlation_matrix,
+                    coordinates,
+                    edge_threshold=threshold,
+                    title=name)
 
 plt.show()
