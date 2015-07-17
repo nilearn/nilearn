4bf8d754e166d97e541e011941ab6c0256ec1a21
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2015-07-17 17:47:11 +0200

    BF: broken backport

--- nilearn/_utils/fixes/__init__.py
+++ nilearn/_utils/fixes/__init__.py
@@ -1,6 +1,13 @@
 from distutils.version import LooseVersion
 import sklearn
 
+if (LooseVersion(sklearn.__version__) < LooseVersion('0.15') or
+      sklearn.__version__ == '0.15-git'):
+    from .sklearn_f_regression_nosparse import (
+        f_regression_nosparse as f_regression)
+else:
+    from sklearn.feature_selection import f_regression
+
 # atleast2d_or_csr
 try:
     from sklearn.utils import atleast2d_or_csr
