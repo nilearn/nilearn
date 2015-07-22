26b45754898348454979d609adf39dd4cebb1c62
KamalakerDadi <dkamalakarreddy@gmail.com>
2015-07-22 13:54:53 +0200

    Simple code for a Voxel Ratio Region Extraction

--- nilearn/region_extraction/SimpleThresholdExtractor.py
+++ nilearn/region_extraction/SimpleThresholdExtractor.py
@@ -1,6 +1,5 @@
-
 """
-Simple Threshold Extractor
+Voxel_ratio_extractor
 """
 
 import nibabel
@@ -9,57 +8,71 @@ import numpy as np
 from scipy.ndimage import label
 from scipy.stats import scoreatpercentile
 
-from sklearn.base import BaseEstimator
-
 from nilearn import plotting
 from nilearn._utils import check_niimg
-from nilearn.image import iter_img
+from nilearn._utils.niimg_conversions import concat_niimgs
+from nilearn.image import iter_img, new_img_like
 
 
-def extract_connected_components(img, min_size):
+def extract_seperate_regions(img, img_data, affine, min_size):
     """ Extract the components which have largest
         connections given a thresholded image of the
         components
 
-    Parameters
-    ----------
-    img: 3D/4D Nifti like object
-        a thresholded image of the components
+        Parameters
+        ----------
+        img: numpy array of 3D Nifti like object
+            contains a thresholded image of the components
 
-    Returns
-    -------
-    the extracted largest connected components of each thresholded
-    map
+        Returns
+        -------
+        the seperated or segmented regions extracted from
+        the input image which contains a set of connected
+        maps
 
     """
-    components, labels = label(img)
+    all_regions_img = []
+    # n_components and n_labels before removing
+    # spurious regions
+    components, labels = label(img_data)
     label_count = np.bincount(components.ravel())
     label_count[0] = 0
     for label_assign, count in enumerate(label_count):
         if count < min_size:
-            img[components == label_assign] = 0
+            img_data[components == label_assign] = 0
 
-    return img
+    # Extract n_regions and n_labels survived after
+    # minimizing the unwanted or spurious regions
+    n_components, n_regions = label(img_data)
+    for n in range(n_regions):
+        region = (n_components == n) * img_data
+        region_img = new_img_like(img, region, affine)
+        all_regions_img.append(region_img)
+        regions_4dfile = concat_niimgs(all_regions_img)
+
+    return regions_4dfile
 
 
-class SimpleThresholdExtractor(BaseEstimator):
-    """ Finds the maximum value of the maps and picks only the
-        voxels which have value more than half of the maximum value.
+class VoxelRatioExtractor():
+    """ Finds threshold automatically by choosing the
+        number of voxels which more intense voxels within
+        the brain volume.
 
         Parameters
         ----------
         n_regions: int
-            number of regions to extract from a set of brain maps
+            number of regions to extract from a set
+            of brain maps
 
         min_size: int, optional
-            size of the voxels for the components extraction
+            size of the voxels for the regions extraction
 
         Returns
         -------
         segmented regions of the brain maps
     """
     def __init__(self, n_regions=None, min_size=20,
-                 ratio=1., threshold=None):
+                 ratio=1., threshold='auto'):
         self.n_regions = n_regions
         self.min_size = min_size
         self.ratio = ratio
@@ -76,7 +89,6 @@ class SimpleThresholdExtractor(BaseEstimator):
         -------
         regions of the largest connected components
         """
-        print self
         min_size = self.min_size
         maps_img = check_niimg(maps_img)
         maps_data = maps_img.get_data()
@@ -98,42 +110,19 @@ class SimpleThresholdExtractor(BaseEstimator):
                              str(self.threshold))
 
         maps_affine = maps_img.get_affine()
-        regions_img = []
+        all_imgs_4d = []
+        index = []
 
-        for img in iter_img(maps_img):
+        for i, img in enumerate(iter_img(maps_img)):
             img_data = img.get_data()
 
             img_data[img_data < threshold] = 0.
-            region = extract_connected_components(img_data, min_size)
-            regions_img.append(region)
+            imgs_4dfile = extract_seperate_regions(img, img_data,
+                                                   maps_affine, min_size)
 
-        self.regions_ = regions_img
+            all_imgs_4d.append(imgs_4dfile)
+            all_regions_extracted = concat_niimgs(all_imgs_4d)
+            index.extend([i] * all_regions_extracted.shape[3])
 
-
-class VoxelRatioExtractor(SimpleThresholdExtractor):
-    """ Finds threshold automatically and picks only voxels
-        which are in the brain volume by multiplying the ratio
-        with the number of voxels in the maps
-
-    """
-    def __init__(self, n_regions=None, min_size=20,
-                 ratio=1., threshold='auto'):
-
-        self.n_regions = n_regions
-        self.ratio = ratio
-        self.threshold = threshold
-        self.min_size = min_size
-
-    def transform(self, maps_img):
-        """ Extract the regions from the maps
-
-        Parameters
-        ----------
-        maps_img: Niimg like object or path to the filename
-
-        Returns
-        -------
-        regions of the largest connected components
-        """
-        print self
-        return super(VoxelRatioExtractor, self).transform(maps_img)
+        self.index_ = index
+        self.regions_ = all_regions_extracted
