dd60137f3741079dfb47effe6d8ed4b611055602
bthirion <bertrand.thirion@inria.fr>
2015-08-17 17:06:40 +0200

    Added an example using FIAC data

---
+++ examples/plot_fiac_analysis.py
@@ -0,0 +1,99 @@
+_doc = """
+GLM fitting in fMRI
+===================
+
+Full step-by-step example of fitting a GLM to experimental data and visualizing
+the results. This is done on two runs of one subject of the FIAC dataset.
+For details on the data, please see:
+ 
+Dehaene-Lambertz G, Dehaene S, Anton JL, Campagne A, Ciuciu P, Dehaene
+G, Denghien I, Jobert A, LeBihan D, Sigman M, Pallier C, Poline
+JB. Functional segregation of cortical language areas by sentence
+repetition. Hum Brain Mapp. 2006: 27:360--371.
+http://www.pubmedcentral.nih.gov/articlerender.fcgi?artid=2653076#R11
+
+
+More specifically:
+
+1. A sequence of fMRI volumes are loaded
+2. A design matrix describing all the effects related to the data is computed
+3. a mask of the useful brain volume is computed
+4. A GLM is applied to the dataset (effect/covariance,
+   then contrast estimation)
+"""
+
+print _doc
+
+from os import mkdir, path, getcwd
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from nilearn.plotting import plot_stat_map
+import nibabel as nib
+
+from nistats.glm import FMRILinearModel
+from nistats.design_matrix import make_design_matrix
+from nistats import datasets
+
+
+# write directory
+write_dir = path.join(getcwd(), 'results')
+if not path.exists(write_dir):
+    mkdir(write_dir)
+
+# Data and analysis parameters
+data = datasets.fetch_fiac_first_level()
+fmri_files = [data['func1'], data['func2']]
+design_files = [data['dmtx_ses1'], data['dmtx_ses2']]
+
+# Load all the data into a common GLM
+multi_session_model = FMRILinearModel(fmri_files, design_files, data['mask'])
+
+# GLM fitting
+multi_session_model.fit(do_scaling=True, model='ar1')
+
+
+def make_fiac_contrasts(n_columns):
+    """ Specify some contrasts for the FIAC experiment"""
+    contrast = {}
+    # the design matrices of both runs comprise 13 columns
+    # the first 5 columns of the design matrices correspond to the following
+    # conditions: ['SSt-SSp', 'SSt-DSp', 'DSt-SSp', 'DSt-DSp', 'FirstSt']
+
+    def _pad_vector(contrast_, n_columns):
+        return np.hstack((contrast_, np.zeros(n_columns - len(contrast_))))
+
+    contrast['SStSSp_minus_DStDSp'] = _pad_vector([1, 0, 0, -1], n_columns)
+    contrast['DStDSp_minus_SStSSp'] = - contrast['SStSSp_minus_DStDSp']
+    contrast['DSt_minus_SSt'] = _pad_vector([- 1, - 1, 1, 1], n_columns)
+    contrast['DSp_minus_SSp'] = _pad_vector([- 1, 1, - 1, 1], n_columns)
+    contrast['DSt_minus_SSt_for_DSp'] = _pad_vector([0, - 1, 0, 1], n_columns)
+    contrast['DSp_minus_SSp_for_DSt'] = _pad_vector([0, 0, - 1, 1], n_columns)
+    contrast['Deactivation'] = _pad_vector([- 1, - 1, - 1, - 1, 4], n_columns)
+    contrast['Effects_of_interest'] = np.eye(n_columns)[:5]
+    return contrast
+
+# compute fixed effects of the two runs and compute related images
+n_columns = np.load(design_files[0])['X'].shape[1]
+contrasts = make_fiac_contrasts(n_columns)
+
+print('Computing contrasts...')
+mean_img = multi_session_model.means[0]  # for display
+for index, (contrast_id, contrast_val) in enumerate(contrasts.items()):
+    print('  Contrast % 2i out of %i: %s' % (
+        index + 1, len(contrasts), contrast_id))
+    z_image_path = path.join(write_dir, '%s_z_map.nii' % contrast_id)
+    z_map, = multi_session_model.contrast(
+        [contrast_val] * 2, con_id=contrast_id, output_z=True)
+    nib.save(z_map, z_image_path)
+
+    # make a snapshot of the contrast activation
+    if contrast_id == 'Effects_of_interest':
+        vmax = max(- z_map.get_data().min(), z_map.get_data().max())
+        vmin = - vmax
+        plot_stat_map(z_map, bg_img=mean_img, threshold=2.5)
+        plt.savefig(path.join(write_dir, '%s_z_map.png' % contrast_id))
+
+print('All the  results were witten in %s' % write_dir)
+plt.show()
