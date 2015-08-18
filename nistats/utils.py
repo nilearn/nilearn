47d1fd1fa235d0e0ed976ecbdf290b841580bf1b
bthirion <bertrand.thirion@inria.fr>
2015-08-18 13:28:10 +0200

    Removed unsued open4csv

--- nistats/utils.py
+++ nistats/utils.py
@@ -241,29 +241,4 @@ def pos_recipr(X):
     X = np.asarray(X)
     return np.where(X <= 0, 0, 1. / X)
 
-
-def open4csv(fname, mode):
-    """ Open filename `fname` for CSV IO in read or write `mode`
-
-    Parameters
-    ----------
-    fname : str
-        filename to open
-    mode : {'r', 'w'}
-        Mode to open file.  Don't specify binary or text modes; we need to
-        chose these according to python version.
-
-    Returns
-    -------
-    fobj : file object
-        open file object; needs to be closed by the caller
-    """
-    if mode not in ('r', 'w'):
-        raise ValueError('Only "r" and "w" allowed for mode')
-    if not py3: # Files for csv reading and writing should be binary mode
-        return open(fname, mode + 'b')
-    return open(fname, mode, newline='')
-
-
 _basestring = str if py3 else basestring
-
