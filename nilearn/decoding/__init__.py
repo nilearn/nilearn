fdede2f7b4e71b7b81ff933c6805ccc3fea7cbd1
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2015-09-11 08:59:41 +0200

    BF: fixing non py3 compat import

--- nilearn/decoding/__init__.py
+++ nilearn/decoding/__init__.py
@@ -6,7 +6,7 @@ from .searchlight import SearchLight
 from .space_net import SpaceNetClassifier, SpaceNetRegressor
 from .searchlight import search_light
 from .fista import mfista
-from space_net_solvers import tvl1_solver
+from .space_net_solvers import tvl1_solver
 
 __all__ = ["SearchLight", "SpaceNetRegressor", "SpaceNetClassifier",
            "search_light", "mfista", "tvl1_solver"]
