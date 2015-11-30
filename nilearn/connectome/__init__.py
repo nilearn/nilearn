c6fac6e436906a57be67331602ad7732faa69e54
Gael Varoquaux <gael.varoquaux@normalesup.org>
2015-11-30 07:57:09 +0100

    BUG: avoid import failure
    I am not sure why the import is failing on travis. I cannot reproduce
    locally.

--- nilearn/connectome/__init__.py
+++ nilearn/connectome/__init__.py
@@ -6,7 +6,7 @@ of Gaussian graphical models.
 
 from .connectivity_matrices import sym_to_vec, ConnectivityMeasure
 
-from .group_sparse_covariance_ import (GroupSparseCovariance,
+from .group_sparse_cov import (GroupSparseCovariance,
     GroupSparseCovarianceCV, group_sparse_covariance)
 
 __all__ = ['sym_to_vec', 'ConnectivityMeasure',
