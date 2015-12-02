b840139eae196353d9245f51af0ecb8e4e8ea560
ainafp <ainafp@gmail.com>
2015-12-02 16:14:25 +0100

    Added tests for copy_img and new_image_like, to check that there are no side effects when copying an image

--- nilearn/tests/test_niimg.py
+++ nilearn/tests/test_niimg.py
@@ -1,7 +1,13 @@
 import os
+import joblib
+import numpy as np
 
-from nilearn._utils.testing import assert_raises_regex
+from nibabel import Nifti1Image
+from nilearn.image import new_img_like
 from nilearn._utils import niimg
+from nilearn._utils.testing import assert_raises_regex
+from nose.tools import assert_equal
+
 
 currdir = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,3 +15,23 @@ currdir = os.path.dirname(os.path.abspath(__file__))
 def test_copy_img():
     assert_raises_regex(ValueError, "Input value is not an image",
                         niimg.copy_img, 3)
+
+
+def test_copy_img_side_effect():
+    img1 = Nifti1Image(np.ones((2, 2, 2, 2)), affine=np.eye(4))
+    hash1 = joblib.hash(img1)
+    img2 = niimg.copy_img(img1)
+    hash2 = joblib.hash(img1)
+    assert_equal(hash1, hash2)
+
+
+def test_new_img_like_side_effect():
+    img1 = Nifti1Image(np.ones((2, 2, 2, 2)), affine=np.eye(4))
+    hash1 = joblib.hash(img1)
+    img2 = new_img_like(img1, np.ones((2, 2, 2, 2)), img1.get_affine().copy(), copy_header=True)
+    hash2 = joblib.hash(img1)
+    assert_equal(hash1, hash2)
+
+
+
+
