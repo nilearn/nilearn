ff6dc7d1925fdc38ef50a3191a15cd4011a82709
Gael Varoquaux <gael.varoquaux@normalesup.org>
2015-12-03 11:16:31 +0100

    DOC: add a comment on dim in plot_roi

--- examples/manipulating_visualizing/plot_dim_plotting.py
+++ examples/manipulating_visualizing/plot_dim_plotting.py
@@ -7,6 +7,9 @@ The `dim` argument controls the contrast of the background.
 *dim* modifies the contrast of this image: dim=0 leaves the image
 unchanged, negative values of *dim* enhance it, and positive values
 decrease it (dim the background).
+
+This *dim* argument may also be useful for the plot_roi function used to
+display ROIs on top of a background image.
 """
 
 # Retrieve the data: the localizer dataset with contrast maps
@@ -34,7 +37,7 @@ plotting.plot_stat_map(localizer_tmap_filename,
                        bg_img=localizer_anat_filename,
                        cut_coords=(36, -27, 66),
                        threshold=3, title="dim=0",
-                       dim=.0)
+                       dim=0)
 
 plotting.plot_stat_map(localizer_tmap_filename,
                        bg_img=localizer_anat_filename,
@@ -45,7 +48,7 @@ plotting.plot_stat_map(localizer_tmap_filename,
 plotting.plot_stat_map(localizer_tmap_filename,
                        bg_img=localizer_anat_filename,
                        cut_coords=(36, -27, 66),
-                       threshold=3, title="dim=1.",
-                       dim=1.)
+                       threshold=3, title="dim=1",
+                       dim=1)
 
 plotting.show()
