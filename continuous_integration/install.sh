674c5a501d917765faec4450172f1de1efb749d4
Loïc Estève <loic.esteve@ymail.com>
2016-01-28 10:39:50 +0100

    Fix typo

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -27,7 +27,7 @@ create_new_venv() {
 
 print_conda_requirements() {
     # Echo a conda requirement string for example
-    # "pip nose python='.7.3 scikit-learn=*". It has a hardcoded
+    # "pip nose python='2.7.3 scikit-learn=*". It has a hardcoded
     # list of possible packages to install and looks at _VERSION
     # environment variables to know whether to install a given package and
     # if yes which version to install. For example:
