398ab45ba1a611ace5c603f04ee4fa8ef1ff115c
Salma <salmabougacha@hotmail.com>
2016-01-29 18:49:22 +0100

    test connectivity measure dimension error

--- nilearn/connectome/connectivity_matrices.py
+++ nilearn/connectome/connectivity_matrices.py
@@ -6,6 +6,7 @@ from scipy import linalg
 
 from sklearn.base import BaseEstimator, TransformerMixin, clone
 from sklearn.covariance import LedoitWolf
+from .. import signal
 from .._utils.extmath import is_spd
 
 
@@ -287,7 +288,7 @@ class ConnectivityMeasure(BaseEstimator, TransformerMixin):
     in post-stroke patients using group-level covariance modeling, MICCAI 2010.
     """
 
-    def __init__(self, cov_estimator=LedoitWolf(),
+    def __init__(self, cov_estimator=LedoitWolf(store_precision=False),
                  kind='covariance'):
         self.cov_estimator = cov_estimator
         self.kind = kind
@@ -298,7 +299,7 @@ class ConnectivityMeasure(BaseEstimator, TransformerMixin):
 
         Parameters
         ----------
-        X : list of numpy.ndarray, shapes (n_samples, n_features)
+        X : array-like, shape (n_subjects, n_samples, n_features)
             The input subjects time series.
 
         Returns
@@ -307,6 +308,9 @@ class ConnectivityMeasure(BaseEstimator, TransformerMixin):
             The object itself. Useful for chaining operations.
         """
         self.cov_estimator_ = clone(self.cov_estimator)
+        if np.ndim(X) != 3:
+            raise ValueError("Expects a 3D array-like data, got array with"
+                             " {} dimensions".format(np.ndim(X)))
 
         if self.kind == 'tangent':
             covariances = [self.cov_estimator_.fit(x).covariance_ for x in X]
@@ -330,25 +334,29 @@ class ConnectivityMeasure(BaseEstimator, TransformerMixin):
         output : numpy.ndarray, shape (n_samples, n_features, n_features)
              The transformed connectivity matrices.
         """
-        covariances = [self.cov_estimator_.fit(x).covariance_ for x in X]
-        covariances = np.array(covariances)
-        if self.kind == 'covariance':
-            connectivities = covariances
-        elif self.kind == 'tangent':
-            connectivities = [_map_eigenvalues(np.log, self.whitening_.dot(
-                                               cov).dot(self.whitening_))
-                              for cov in covariances]
-        elif self.kind == 'precision':
-            connectivities = [linalg.inv(cov) for cov in covariances]
-        elif self.kind == 'partial correlation':
-            connectivities = [_prec_to_partial(linalg.inv(cov))
-                              for cov in covariances]
-        elif self.kind == 'correlation':
-            connectivities = [_cov_to_corr(cov) for cov in covariances]
+        if self.kind == 'correlation':
+            covariances_std = [self.cov_estimator_.fit(
+                signal._standardize(x, detrend=False, normalize=True)
+                ).covariance_ for x in X]
+            connectivities = [_cov_to_corr(cov) for cov in covariances_std]
         else:
-            raise ValueError('Allowed connectivity kinds are "correlation", '
-                             '"partial correlation", "tangent", '
-                             '"covariance" and "precision", got kind '
-                             '"{}"'.format(self.kind))
+            covariances = [self.cov_estimator_.fit(x).covariance_ for x in X]
+            if self.kind == 'covariance':
+                connectivities = covariances
+            elif self.kind == 'tangent':
+                connectivities = [_map_eigenvalues(np.log, self.whitening_.dot(
+                                                   cov).dot(self.whitening_))
+                                  for cov in covariances]
+            elif self.kind == 'precision':
+                connectivities = [linalg.inv(cov) for cov in covariances]
+            elif self.kind == 'partial correlation':
+                connectivities = [_prec_to_partial(linalg.inv(cov))
+                                  for cov in covariances]
+            else:
+                raise ValueError('Allowed connectivity kinds are '
+                                 '"correlation", '
+                                 '"partial correlation", "tangent", '
+                                 '"covariance" and "precision", got kind '
+                                 '"{}"'.format(self.kind))
 
         return np.array(connectivities)
