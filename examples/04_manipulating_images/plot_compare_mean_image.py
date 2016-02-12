3f1f415af14ad407fd8477a36464fccb6c0092ba
Gael Varoquaux <gael.varoquaux@normalesup.org>
2016-02-12 22:34:48 +0100

    DOC: cosmit in example
    plot_stat_maps make a more pleasing plot here
    
    imports close to where they are use makes examples easier to read for
    beginners.

--- examples/04_manipulating_images/plot_compare_mean_image.py
+++ examples/04_manipulating_images/plot_compare_mean_image.py
@@ -8,23 +8,27 @@ We compare the means of 2 resting state 4D images. The mean of the images
 could have been computed with nilearn :func:`nilearn.image.mean_img` function.
 """
 
-from nilearn import datasets, plotting, image
 
 ###############################################################################
 # Fetching 2 subject resting state functionnal MRI from datasets.
+from nilearn import datasets
 dataset = datasets.fetch_adhd(n_subjects=2)
+
 
 ###############################################################################
 # Print basic information on the adhd subjects resting state datasets.
 print('Subject 1 resting state dataset at: %s' % dataset.func[0])
 print('Subject 2 resting state dataset at: %s' % dataset.func[1])
 
+
 ###############################################################################
 # Comparing the means of the 2 resting state datasets.
+from nilearn import plotting, image
+
 result_img = image.math_img("np.mean(img1, axis=-1) - np.mean(img2, axis=-1)",
                             img1=dataset.func[0],
                             img2=dataset.func[1])
 
-plotting.plot_epi(result_img,
-                  title="Comparing means of 2 resting state 4D images.")
+plotting.plot_stat_map(result_img,
+                       title="Comparing means of 2 resting state 4D images.")
 plotting.show()
