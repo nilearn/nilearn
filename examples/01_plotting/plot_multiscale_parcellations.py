92d193a01108004fe7f4190786728d426761b65f
KamalakerDadi <dkamalakarreddy@gmail.com>
2016-02-14 21:55:30 +0100

    pep8 corrections

--- examples/01_plotting/plot_multiscale_parcellations.py
+++ examples/01_plotting/plot_multiscale_parcellations.py
@@ -11,7 +11,7 @@ details about different versions and different networks, please refer to its
 documentation.
 """
 
-################################################################################
+###############################################################################
 # Fetching multiscale group brain parcellations
 # Import datasets module and use fetch_atlas_basc_multiscale function
 from nilearn import datasets
@@ -23,10 +23,9 @@ networks_64 = parcellations['scale064']
 networks_197 = parcellations['scale197']
 networks_444 = parcellations['scale444']
 
-################################################################################
+###############################################################################
 # Visualizing brain parcellations
 # Import plotting module and use plot_roi function, since the maps are in 3D
-import matplotlib.pyplot as plt
 from nilearn import plotting
 
 # The coordinates of all plots are selected automatically by itself
