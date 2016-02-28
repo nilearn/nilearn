f9235a5b68b324eb93bfceefc68ab01529706c4e
mjboos <moritz.boos@gmail.com>
2016-02-28 18:57:25 +0100

    integrated comments

--- examples/02_decoding/plot_miyawaki_encoding.py
+++ examples/02_decoding/plot_miyawaki_encoding.py
@@ -33,14 +33,8 @@ approach for the same dataset.
 ##############################################################################
 # Loading the data
 # ----------------
-# Some general imports:
-
-import numpy as np
-import pylab as plt
-
-
-##############################################################################
 # Now we can load the data set:
+
 from nilearn.datasets import fetch_miyawaki2008
 
 dataset = fetch_miyawaki2008()
@@ -57,6 +51,7 @@ y_random_filenames = dataset.label[12:]
 # We can use :func:`nilearn.input_data.MultiNiftiMasker` to load the fMRI
 # data, clean and mask it.
 
+import numpy as np
 from nilearn.input_data import MultiNiftiMasker
 
 masker = MultiNiftiMasker(mask_img=dataset.mask, detrend=True,
@@ -76,6 +71,7 @@ for y in y_random_filenames:
 ##############################################################################
 # Let's take a look at some of these binary images:
 
+import pylab as plt
 plt.figure(figsize=(8, 4))
 plt.subplot(1, 2, 1)
 plt.imshow(y_train[0][124], interpolation='nearest', cmap='gray')
@@ -124,24 +120,21 @@ from sklearn.cross_validation import KFold
 # we hold out each fold of the data for testing, then fit a ridge regression
 # to the remaining 9/10 of the data, using y_train as predictors
 # and X_train as targets, and create predictions for the held-out 10th.
+from sklearn.metrics import r2_score
 
 estimator = Ridge(alpha=100.)
 cv = KFold(len(y_train), 10)
 
-predictions = []
+scores = []
 for train, test in cv:
-    predictions.append(Ridge(alpha=100.).fit(
+    # we train the Ridge estimator on the training set
+    # and predict the fMRI activity for the test set
+    predictions = Ridge(alpha=100.).fit(
     y_train.reshape(-1, 100)[train], X_train[train]).predict(
-        y_train.reshape(-1, 100)[test]))
-
-##############################################################################
-# To have a measure for the quality of our encoding model, we estimate how
-# much variance the encoding model explains in each voxel.
-
-from sklearn.metrics import r2_score
-
-scores = [r2_score(X_train[test], pred, multioutput = 'raw_values')
-         for pred, (train, test) in zip(predictions, cv)]
+        y_train.reshape(-1, 100)[test])
+    # we compute how much variance our encoding model explains in each voxel
+    scores.append(r2_score(X_train[test], predictions,
+                           multioutput='raw_values'))
 
 ##############################################################################
 # Mapping the encoding scores on the brain
@@ -156,28 +149,27 @@ cut_score = np.array(scores).mean(axis=0)
 cut_score[cut_score < 0] = 0
 
 # bring the scores into the shape of the background brain
-score_map = new_img_like(dataset.background,
+score_map_img = new_img_like(dataset.background,
                          unmask(cut_score, dataset.mask).get_data())
 
-thresholded_score_map = threshold_img(score_map, threshold = 1e-6)
+thresholded_score_map_img = threshold_img(score_map_img, threshold=1e-6)
 
 ##############################################################################
 # Plotting the statistical map on a background brain, we mark four voxels
 # which we will inspect more closely later on.
-
 from nilearn.plotting import plot_stat_map
 from nilearn.image.resampling import coord_transform
 
 def index_to_xy_coord(x, y, z=10):
     '''Transforms data index to coordinates of the background + offset'''
     coords = coord_transform(x, y, z,
-                             affine=thresholded_score_map.get_affine())
+                             affine=thresholded_score_map_img.get_affine())
     return np.array(coords)[np.newaxis, :] + np.array([0, 1, 0])
 
 
 xy_indices_of_special_voxels = [(30, 10), (32, 10), (31, 9), (31, 10)]
 
-display = plot_stat_map(thresholded_score_map, bg_img=dataset.background,
+display = plot_stat_map(thresholded_score_map_img, bg_img=dataset.background,
                         cut_coords=[-8], display_mode='z', aspect=1.25,
                         title='Explained variance per voxel')
 
@@ -185,16 +177,15 @@ display = plot_stat_map(thresholded_score_map, bg_img=dataset.background,
 
 for i, (x, y) in enumerate(xy_indices_of_special_voxels):
     display.add_markers(index_to_xy_coord(x, y),
-                        edgecolor=['r', 'b', 'magenta', 'g'][i],
-                        marker_size=150, marker = 's',
-                        facecolor = 'none', lw = 4.5)
+                        edgecolor=['b', 'r', 'magenta', 'g'][i],
+                        marker_size=150, marker='s',
+                        facecolor='none', lw=4.5)
 
 
 # re-set figure size after construction so colorbar gets rescaled too
 fig = plt.gcf()
 fig.set_size_inches(12, 12)
 
-plt.show()
 
 ##############################################################################
 # Estimating receptive fields
@@ -222,6 +213,9 @@ from matplotlib import gridspec
 from matplotlib.patches import Rectangle
 
 fig = plt.figure(figsize=(12, 12))
+fig.suptitle('Receptive fields of the marked voxels', fontsize=25)
+
+# GridSpec allows us to do subplots with more control of the spacing
 gs1 = gridspec.GridSpec(2, 3)
 
 # we fit the Lasso for each of the three voxels of the upper row
@@ -232,17 +226,17 @@ for i, index in enumerate([1780, 1951, 2131]):
     # add a black background
     ax.imshow(np.zeros_like(rf), vmin=0., vmax=1., cmap='gray')
     ax_im = ax.imshow(np.ma.masked_less(rf, 0.1), interpolation="nearest",
-                      cmap=['Reds', 'Greens', 'Blues'][i], vmin=0., vmax=0.75)
+                      cmap=['Blues', 'Greens', 'Reds'][i], vmin=0., vmax=0.75)
     # add the marked pixel
     ax.add_patch(Rectangle(
         (marked_pixel[1] - .5, marked_pixel[0] - .5), 1, 1,
-        facecolor = 'none', edgecolor = 'r', lw = 4))
+        facecolor='none', edgecolor='r', lw=4))
     plt.axis('off')
     plt.colorbar(ax_im, ax=ax)
 
 # and then for the voxel at the bottom
 
-gs1.update(left=0., right=1., wspace=0.1, bottom = 0.3)
+gs1.update(left=0., right=1., wspace=0.1, bottom=0.3)
 ax = plt.subplot(gs1[1, 1])
 # we reshape the coefficients into the form of the original images
 rf = lasso.fit(y_train, X_train[:, 1935]).coef_.reshape((10, 10))
@@ -253,11 +247,13 @@ ax_im = ax.imshow(np.ma.masked_less(rf, 0.1), interpolation="nearest",
 # add the marked pixel
 ax.add_patch(Rectangle(
     (marked_pixel[1] - .5, marked_pixel[0] - .5), 1, 1,
-    facecolor = 'none', edgecolor = 'r', lw = 4))
+    facecolor='none', edgecolor='r', lw=4))
 plt.axis('off')
 plt.colorbar(ax_im, ax=ax)
 
 ##############################################################################
 # The receptive fields of the four voxels are not only close to each other,
 # the relative location of the pixel each voxel is most sensitive to
-# roughly maps to the relative location of the voxels to each other.
+# roughly maps to the relative location of the voxels to each other. 
+# We can see a relationship between some voxel's receptive field and
+# its location in the brain.
