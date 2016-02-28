528f0b13b4ac4565280d20f73eb58c36929ef705
juhuntenburg <ju.huntenburg@gmail.com>
2016-02-28 12:26:01 +0100

    remove non-ascii character

--- nilearn/plotting/surf_plotting.py
+++ nilearn/plotting/surf_plotting.py
@@ -46,7 +46,7 @@ def check_surf_mesh(surf_mesh):
             coords, faces = nibabel.freesurfer.io.read_geometry(surf_mesh)
         elif surf_mesh.endswith('gii'):
             coords, faces = gifti.read(surf_mesh).getArraysFromIntent(nibabel.nifti1.intent_codes['NIFTI_INTENT_POINTSET'])[0].data, \
-                            gifti.read(surf_mesh).getArraysFromIntent(nibabel.nifti1.intent_codes['NIFTI_INTENT_TRIANGLEÞ'])[0].data
+                            gifti.read(surf_mesh).getArraysFromIntent(nibabel.nifti1.intent_codes['NIFTI_INTENT_TRIANGLE'])[0].data
         else:
             raise ValueError('Format of mesh file not recognized.')
 
