fix: warn 3D normalization only if normalize==True

nilearn/signal.py
@@ -47,12 +47,13 @@ def _standardize(signals, detrend=False, normalize=True):
         signals = _detrend(signals, inplace=False)
     else:
         signals = signals.copy()
-    if signals.shape[0] == 1:
-        warnings.warn('Standardization of 3D signal has been requested but '
-            'would lead to zero values. Skipping.')
-        return signals
 
     if normalize:
+        if signals.shape[0] == 1:
+            warnings.warn('Standardization of 3D signal has been requested but '
+                'would lead to zero values. Skipping.')
+            return signals
+
         if not detrend:
             # remove mean if not already detrended
             signals = signals - signals.mean(axis=0)