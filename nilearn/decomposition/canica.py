4679e924b9c3dd6a085bd1ce0f83177b4781b133
Alexandre M. S <alexsavio@users.noreply.github.com>
2016-04-13 09:05:47 +0200

    Remove data from CanICA init docstring (#1080)

--- nilearn/decomposition/canica.py
+++ nilearn/decomposition/canica.py
@@ -27,11 +27,6 @@ class CanICA(MultiPCA):
         it will be computed automatically by a MultiNiftiMasker with default
         parameters.
 
-    data: array-like, shape = [[n_samples, n_features], ...]
-        Training vector, where n_samples is the number of samples,
-        n_features is the number of features. There is one vector per
-        subject.
-
     n_components: int
         Number of components to extract
 
