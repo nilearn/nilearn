5c2156f5d3064e779625c466351845f74c56b9e3
KamalakerDadi <dkamalakarreddy@gmail.com>
2016-04-17 10:44:06 +0200

    Minor improvements in plot_demo_plotting example - More integration of plotting tutorial into example plotting functions

--- examples/01_plotting/plot_demo_plotting.py
+++ examples/01_plotting/plot_demo_plotting.py
@@ -1,19 +1,22 @@
 """
-Plotting in nilearn
+Plotting tools in nilearn
 ==========================
 
-Nilearn comes with a set of plotting function for Nifti-like images,
-see :ref:`plotting` for more details.
+Nilearn comes with a set of plotting functions for an easy visualization of
+Nifti-like images types such as statistical maps overlayed on anatomical
+images or overlayed on glass brain image, anatomical images, functional/EPI
+images, region specific mask images.
+
+See :ref:`plotting` for more details.
 """
 
-# Import plotting and image processing tools
-from nilearn import plotting, image
-
 ###############################################################################
-# Retrieve the data: haxby dataset to have EPI images and masks, and
-# localizer dataset to have contrast maps
+# First, we retrieve the data from nilearn: haxby dataset to have EPI images
+# and masks, and localizer dataset to have contrast maps
 
+# Import datasets module
 from nilearn import datasets
+
 haxby_dataset = datasets.fetch_haxby(n_subjects=1)
 
 # print basic information on the dataset
@@ -35,28 +38,50 @@ localizer_anat_filename = localizer_dataset.anats[1]
 localizer_tmap_filename = localizer_dataset.tmaps[1]
 
 ###############################################################################
-# Plotting statistical maps
+# Plotting statistical maps using function `plot_stat_map`
+
+# Import plotting module
+from nilearn import plotting
+
+# Visualizing t-map image on subject specific anatomical image with manual
+# positioning of coordinates using cut_coords given as a list
 plotting.plot_stat_map(localizer_tmap_filename, bg_img=localizer_anat_filename,
                        threshold=3, title="plot_stat_map",
-                       cut_coords=(36, -27, 66))
+                       cut_coords=[36, -27, 66])
 
 ###############################################################################
-# Plotting glass brain
+# Plotting statistical maps on glass brain image using function `plot_glass_brain`
+
+# Now, t-map image is overlayed on glass brain image as fixed background
 plotting.plot_glass_brain(localizer_tmap_filename, title='plot_glass_brain',
                           threshold=3)
 
 ###############################################################################
-# Plotting anatomical maps
+# Plotting anatomical images using function `plot_anat`
+
+# Visualizing anatomical image of haxby dataset
 plotting.plot_anat(haxby_anat_filename, title="plot_anat")
 
 ###############################################################################
-# Plotting ROIs (here the mask)
+# Plotting ROIs (here the mask) using function `plot_roi`
+
+# Visualizing ventral temporal region image from haxby dataset overlayed on
+# subject specific anatomical image with coordinates positioned automatically on
+# region of interest (roi)
 plotting.plot_roi(haxby_mask_filename, bg_img=haxby_anat_filename,
                   title="plot_roi")
 
 ###############################################################################
-# Plotting EPI haxby
+# Plotting EPI image using function `plot_epi`
+
+# Import image processing tool
+from nilearn import image
+
+# compute mean of the functional image from haxby dataset in t dimension.
+# Basically reducing the functional image from 4D to 3D
 mean_haxby_img = image.mean_img(haxby_func_filename)
+
+# Visualizing mean image (3D)
 plotting.plot_epi(mean_haxby_img, title="plot_epi")
 
 plotting.show()
