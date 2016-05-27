b03a0a2642c4fabb5ef9eb2912624829b3677f9c
bthirion <bertrand.thirion@inria.fr>
2016-05-27 15:02:18 +0200

    took this opportunity to enforce pep8 in the edge_detect module

--- nilearn/plotting/edge_detect.py
+++ nilearn/plotting/edge_detect.py
@@ -19,11 +19,11 @@ def _orientation_kernel(t):
     """ 
     sin = np.sin
     pi  = np.pi
-    t = pi*t
-    arr =  np.array([[sin(t),        sin(t+.5*pi),  sin(t+pi)    ],
-                     [sin(t+1.5*pi), 0,             sin(t+1.5*pi)],
-                     [sin(t+pi),     sin(t+.5*pi),  sin(t)       ]])
-    return np.round(.5*((1+arr))**2).astype(np.bool)
+    t = pi * t
+    arr = np.array([[sin(t), sin(t + .5 * pi), sin(t + pi)],
+                    [sin(t + 1.5 * pi), 0, sin(t + 1.5 * pi)],
+                    [sin(t + pi), sin(t + .5 * pi), sin(t)]])
+    return np.round(.5 * ((1 + arr)) ** 2).astype(np.bool)
 
 
 def _edge_detect(image, high_threshold=.75, low_threshold=.4):
@@ -70,7 +70,7 @@ def _edge_detect(image, high_threshold=.75, low_threshold=.4):
     img /= img.max()
     grad_x = ndimage.sobel(img, mode='constant', axis=0)
     grad_y = ndimage.sobel(img, mode='constant', axis=1)
-    grad_mag = np.sqrt(grad_x**2 + grad_y**2)
+    grad_mag = np.sqrt(grad_x ** 2 + grad_y ** 2)
     grad_angle = np.arctan2(grad_y, grad_x)
     # Scale the angles in the range [0, 2]
     grad_angle = (grad_angle + np.pi) / np.pi
@@ -79,10 +79,10 @@ def _edge_detect(image, high_threshold=.75, low_threshold=.4):
     thinner = np.zeros(grad_mag.shape, dtype=np.bool)
     for angle in np.arange(0, 2, .25):
         thinner = thinner | (
-                (grad_mag > .85*ndimage.maximum_filter(grad_mag,
-                                    footprint=_orientation_kernel(angle))) 
+                (grad_mag > .85 * ndimage.maximum_filter(
+                    grad_mag, footprint=_orientation_kernel(angle)))
                 & (((grad_angle - angle) % 2) < .75)
-               )
+                )
     # Remove the edges next to the side of the image: they are not reliable
     thinner[0]     = 0
     thinner[-1]    = 0
@@ -93,10 +93,12 @@ def _edge_detect(image, high_threshold=.75, low_threshold=.4):
     # Hysteresis thresholding: find seeds above a high threshold, then
     # expand out until we go below the low threshold
     grad_values = thinned_grad[thinner]
-    high = thinned_grad > fast_abs_percentile(grad_values, 100*high_threshold)
-    low = thinned_grad >  fast_abs_percentile(grad_values, 100*low_threshold)
-    edge_mask = ndimage.binary_dilation(high, structure=np.ones((3, 3)), 
-                                        iterations=-1, mask=low)
+    high = thinned_grad > fast_abs_percentile(grad_values,
+                                              100 * high_threshold)
+    low = thinned_grad > fast_abs_percentile(grad_values,
+                                             100 * low_threshold)
+    edge_mask = ndimage.binary_dilation(
+        high, structure=np.ones((3, 3)), iterations=-1, mask=low)
     return grad_mag, edge_mask
 
 
@@ -118,8 +120,6 @@ def _edge_map(image):
     edge_mask = _edge_detect(image)[-1]
     edge_mask = edge_mask.astype(np.float)
     edge_mask = -np.sqrt(ndimage.distance_transform_cdt(edge_mask))
-    edge_mask[edge_mask != 0] -= -.05+edge_mask.min()
+    edge_mask[edge_mask != 0] -= -.05 + edge_mask.min()
     edge_mask = np.ma.masked_less(edge_mask, .01)
     return edge_mask
-
-
