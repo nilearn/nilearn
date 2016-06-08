f24f700113e5ee673b408a7897a0828d6212ace5
ahoyosid <ahoyosidrobo@gmail.com>
2016-06-08 13:02:28 +0200

    changing tutple coords to list

--- examples/02_decoding/plot_haxby_decoder.py
+++ examples/02_decoding/plot_haxby_decoder.py
@@ -10,21 +10,22 @@ reproducing the Haxby 2001 study on a face vs house discrimination task.
 # Load Haxby dataset
 import numpy as np
 import nibabel
-from nilearn import datasets
 from nilearn.image import new_img_like
+from nilearn.image import mean_img
+from nilearn.datasets import fetch_haxby
+from nilearn.decoding import Decoder
+from nilearn.plotting import plot_stat_map, show
+
 
 # Load the Haxby dataset
-from nilearn.datasets import fetch_haxby
 data_files = fetch_haxby(n_subjects=1)
 
 func_filenames = data_files.func[0]
 labels_filenames = data_files.session_target[0]
 
-from nilearn.image import mean_img
 background_img = mean_img(func_filenames)
 
 # Load Target labels
-import numpy as np
 labels = np.recfromcsv(labels_filenames, delimiter=" ")
 
 # Restrict to face and house conditions
@@ -36,7 +37,6 @@ condition_mask_train = np.logical_and(condition_mask, labels['chunks'] <= 6)
 condition_mask_test = np.logical_and(condition_mask, labels['chunks'] > 6)
 
 ### Prediction with Decoder ###################################################
-from nilearn.decoding import Decoder
 decoder = Decoder(estimator='svc_l2', screening_percentile=20, cv=3,
                   mask_strategy='epi', standardize=True, smoothing_fwhm=4,
                   n_jobs=1)
@@ -52,8 +52,7 @@ print("=== DECODER ===")
 print("Prediction accuracy: %f" % prediction_accuracy)
 print("")
 
-from nilearn.plotting import plot_stat_map, show
-plot_stat_map(weight_img, background_img, cut_coords=(-34, -16),
+plot_stat_map(weight_img, background_img, cut_coords=[-34, -16],
               display_mode="yz")
 
 show()
