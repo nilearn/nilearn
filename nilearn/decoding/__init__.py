9d1da81542e8dacacb0714cd04d4ee619df25aa9
ahoyosid <ahoyosidrobo@gmail.com>
2016-06-08 12:46:25 +0200

    removing merging conflict, ini file

--- nilearn/decoding/__init__.py
+++ nilearn/decoding/__init__.py
@@ -1,13 +1,11 @@
 """
 Decoding tools and algorithms.
 """
-from .space_net import SpaceNetClassifier, SpaceNetRegressor
-<<<<<<< HEAD
+
+
 from .searchlight import SearchLight
+from .space_net import SpaceNetClassifier, SpaceNetRegressor
 from .decoder import Decoder
 
-__all__ = ['SearchLight', 'Decoder']
-=======
 
-__all__ = ['SearchLight', 'SpaceNetClassifier', 'SpaceNetRegressor']
->>>>>>> e8e0790447f74266a2c05738eb216283bb391b55
+__all__ = ['SearchLight', 'SpaceNetClassifier', 'SpaceNetRegressor', 'Decoder']
