075aca48a9ed332ff1fa68b6547f173c90eed0c3
Gael Varoquaux <gael.varoquaux@normalesup.org>
2016-06-09 17:43:19 +0200

    DOC: split an example in notebook-style

--- examples/01_plotting/plot_demo_glass_brain_extensive.py
+++ examples/01_plotting/plot_demo_glass_brain_extensive.py
@@ -3,7 +3,8 @@ Glass brain plotting in nilearn (all options)
 =============================================
 
 This example goes through different options of the :func:`nilearn.plotting.plot_glass_brain` function
-(including plotting negative values). 
+(including plotting negative values).
+
 See :ref:`plotting` for more plotting functionalities.
 """
 
@@ -19,32 +20,49 @@ localizer_dataset = datasets.fetch_localizer_contrasts(
 localizer_tmap_filename = localizer_dataset.tmaps[1]
 
 ###############################################################################
-# demo glass brain plotting.
+# Demo glass brain plotting.
 from nilearn import plotting
 
 # Whole brain sagittal cuts
 plotting.plot_glass_brain(localizer_tmap_filename, threshold=3)
 
+
+###############################################################################
+# With a colorbar
 plotting.plot_glass_brain(localizer_tmap_filename, threshold=3, colorbar=True)
 
+
+###############################################################################
+# Black background, and only the (x, z) cuts
 plotting.plot_glass_brain(localizer_tmap_filename, title='plot_glass_brain',
                           black_bg=True, display_mode='xz', threshold=3)
 
+
+###############################################################################
+# Plotting the sign of the activation
 plotting.plot_glass_brain(localizer_tmap_filename, threshold=0, colorbar=True,
                           plot_abs=False)
 
+
+###############################################################################
+# The sign of the activation and a colorbar
 plotting.plot_glass_brain(localizer_tmap_filename, threshold=3,
                           colorbar=True, plot_abs=False)
 
-###############################################################################
-# Hemispheric sagittal cuts
 
-title = 'plot_glass_brain with display_mode="lzr"'
-plotting.plot_glass_brain(localizer_tmap_filename, title=title,
+###############################################################################
+# Different projections for the left and right hemispheres
+# ---------------------------------------------------------
+#
+# Hemispheric sagittal cuts
+plotting.plot_glass_brain(localizer_tmap_filename,
+                          title='plot_glass_brain with display_mode="lzr"',
                           black_bg=True, display_mode='lzr', threshold=3)
 
+###############################################################################
 title = 'plot_glass_brain with display_mode="lyrz"'
 plotting.plot_glass_brain(localizer_tmap_filename, threshold=0, colorbar=True,
-                          title=title, plot_abs=False, display_mode='lyrz')
+                          title='plot_glass_brain with display_mode="lyzr"',
+                          plot_abs=False, display_mode='lyrz')
 
 plotting.show()
