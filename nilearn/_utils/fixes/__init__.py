fd730e88953e8ea5e845d55fc39f4c9930143c3f
ahoyosid <ahoyosidrobo@gmail.com>
2016-07-08 00:58:34 +0200

    removing some files after supporting >= 0.14

--- nilearn/_utils/fixes/__init__.py
+++ nilearn/_utils/fixes/__init__.py
@@ -34,22 +34,6 @@ except ImportError:
 
 
 try:
-    from sklearn.utils.multiclass import type_of_target
-except ImportError:
-    # scikit-learn < 0.14
-    from .scikit_learn_utils import type_of_target
-
-
-try:
-    from sklearn.metrics.scorer import check_scoring
-    from sklearn.metrics.scorer import make_scorer
-except ImportError:
-    # scikit-learn < 0.14
-    from .scikit_learn_scorer import check_scoring
-    from .scikit_learn_scorer import make_scorer
-
-
-try:
     from sklearn.utils import check_X_y
     from sklearn.utils import check_is_fitted
 except ImportError:
@@ -58,13 +42,5 @@ except ImportError:
     from .scikit_learn_validation import check_is_fitted
 
 
-try:
-    from sklearn.grid_search import ParameterGrid
-except ImportError:
-    # scikit-learn < 0.14
-    from .scikit_learn_gridsearch import ParameterGrid
-
-
 __all__ = ['f_regression', 'atleast2d_or_csr', 'roc_auc_score',
-           'check_X_y', 'check_is_fitted', 'check_scoring',
-           'check_cv', 'make_scorer']
+           'check_X_y', 'check_is_fitted', 'check_cv']
