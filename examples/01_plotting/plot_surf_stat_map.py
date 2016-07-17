120a7cfa0e94e93de7b5023656d2e8fa6ec81ebb
juhuntenburg <ju.huntenburg@gmail.com>
2016-07-17 22:37:06 +0200

    extending plotting exaple to create seed-based connectivity map

--- examples/01_plotting/plot_surf_stat_map.py
+++ examples/01_plotting/plot_surf_stat_map.py
@@ -1,58 +1,129 @@
 """
-Demonstrate basic surface plotting with plot_surf_stat_map
-==========================================================
+Demonstrate basic loading and plotting of cortical surface data
+on the example of seed-based resting state functional connectivity.
+===================================================================
+The dataset that is a subset of the enhanced NKI Rockland sample
+(http://fcon_1000.projects.nitrc.org/indi/enhanced/, Nooner et al, 2012)
 
+Resting state fMRI scans (TR=645ms) of 102 subjects were preprocessed
+(https://github.com/fliem/nki_nilearn) and projected onto the Freesurfer
+fsaverage5 template (Dale et al, 1999, Fischl et al, 1999). For this example
+we use the time series of a single subject's left hemisphere.
+
+The Destrieux parcellation (Destrieux et al, 2010) in fsaverage5 space as
+distributed with Freesurfer is used to select a seed region in the posterior
+cingulate cortex.
+
+Functional connectivity of the seed region to all other cortical nodes in the
+same hemisphere is calculated using Pearson product-moment correlation
+coefficient.
+
+The :func:`nilearn.plotting.surf_plotting.plot_surf_stat_map` function is used
+to plot the resulting statistical map on the (inflated) pial surface.
 
 See :ref:`plotting` for more details.
+
+References
+----------
+
+Nooner et al, (2012). The NKI-Rockland Sample: A model for accelerating the
+pace of discovery science in psychiatry. Frontiers in neuroscience 6, 152.
+URL http://dx.doi.org/10.3389/fnins.2012.00152
+
+Dale et al, (1999). Cortical surface-based analysis.I. Segmentation and
+surface reconstruction. Neuroimage 9.
+URL http://dx.doi.org/10.1006/nimg.1998.0395
+
+Fischl et al, (1999). Cortical surface-based analysis. II: Inflation,
+flattening, and a surface-based coordinate system. Neuroimage 9.
+http://dx.doi.org/10.1006/nimg.1998.0396
+
+Destrieux et al, (2010). Automatic parcellation of human cortical gyri and
+sulci using standard anatomical nomenclature. NeuroImage, 53, 1.
+URL http://dx.doi.org/10.1016/j.neuroimage.2010.06.010.
 """
 
 ###############################################################################
 from nilearn import plotting
 from nilearn import datasets
+from scipy import stats
+import nibabel as nb
+import numpy as np
 
 ###############################################################################
 # Retrieve the data
 nki_dataset = datasets.fetch_nki_enhanced_surface(n_subjects=1)
 
+# Fsaverage5 left hemisphere surface mesh files
 fsaverage5_pial = nki_dataset['fsaverage5_pial_left'][0]
 fsaverage5_inflated = nki_dataset['fsaverage5_infl_left'][0]
 sulcal_depth_map = nki_dataset['fsaverage5_sulc_left'][0]
-stat_map = nki_dataset['func_left'][0]
+
+# Destrieux parcellation left hemisphere in fsaverage5 space
+# TO DO: include in nki_dataset for fetching
+destrieux = '/home/julia/nilearn_testing/lh.aparc.a2009s.annot'
+
+# NKI resting state data set of one subject left hemisphere in fsaverage5 space
+resting_state = nki_dataset['func_left'][0]
 
 ###############################################################################
-# display unthresholded stat map in lateral and medial view
+# Load resting state time series and parcellation
+timeseries = plotting.surf_plotting.check_surf_data(resting_state)
+parcellation = nb.freesurfer.read_annot(destrieux)
+
+# Extract seed region: dorsal posterior cingulate gyrus
+region = 'G_cingul-Post-dorsal'
+labels = np.where(parcellation[0] == parcellation[2].index(region))
+labels_map = np.zeros(timeseries.shape[0])
+labels_map[labels] = 1
+
+# Extract time series from seed region
+seed_timeseries = np.mean(timeseries[labels], axis=0)
+
+# Calculate Pearson product-moment correlation coefficient between seed
+# time series and timeseries of all cortical nodes of the hemisphere
+stat_map = np.zeros(timeseries.shape[0])
+for i in range(timeseries.shape[0]):
+    stat_map[i] = stats.pearsonr(seed_timeseries, timeseries[i])[0]
+
+# Re-mask previously masked nodes (medial wall)
+stat_map[np.where(np.mean(timeseries, axis=1) == 0)] = 0
+
+###############################################################################
+# Display unthresholded stat map in lateral and medial view
 plotting.plot_surf_stat_map(fsaverage5_pial, 'left', stat_map=stat_map)
-
-# display thresholded stat map without background map
 plotting.plot_surf_stat_map(fsaverage5_pial, 'left', stat_map=stat_map,
-                            threshold=.5)
-
-# display thresholded stat map with background map lateral and medial
-plotting.plot_surf_stat_map(fsaverage5_pial, 'left', stat_map=stat_map,
-                            bg_map=sulcal_depth_map, threshold=.5)
-plotting.plot_surf_stat_map(fsaverage5_pial, 'left', stat_map=stat_map,
-                            bg_map=sulcal_depth_map, threshold=.5,
                             view='medial')
 
-# display thresholded stat map with background map with background on statmap
+# Display background values through stat map
+plotting.plot_surf_stat_map(fsaverage5_pial, 'left', stat_map=stat_map,
+                            bg_map=sulcal_depth_map, bg_on_stat=True)
+
+# Display background values through stat map but less dark
 plotting.plot_surf_stat_map(fsaverage5_pial, 'left', stat_map=stat_map,
                             bg_map=sulcal_depth_map, bg_on_stat=True,
-                            threshold=.5)
+                            darkness=.5)
 
-# display thresholded stat map with background map, background on stat_map
-# with less dark background
+# Display thresholded stat map with background map
 plotting.plot_surf_stat_map(fsaverage5_pial, 'left', stat_map=stat_map,
                             bg_map=sulcal_depth_map, bg_on_stat=True,
-                            darkness=0.5, threshold=.5)
+                            threshold=.6)
+plotting.plot_surf_stat_map(fsaverage5_pial, 'left', stat_map=stat_map,
+                            bg_map=sulcal_depth_map, bg_on_stat=True,
+                            threshold=.6, view='medial')
 
-# display thresholded stat map without background map on inflated surface
+# Display stat map with on inflated surface
 plotting.plot_surf_stat_map(fsaverage5_inflated, 'left', stat_map=stat_map,
-                            bg_map=sulcal_depth_map, threshold=.5)
+                            bg_map=sulcal_depth_map, bg_on_stat=True,
+                            threshold=.6)
+plotting.plot_surf_stat_map(fsaverage5_inflated, 'left', stat_map=stat_map,
+                            bg_map=sulcal_depth_map, bg_on_stat=True,
+                            view='medial', threshold=.6)
 
 # changing the colormap and alpha
 plotting.plot_surf_stat_map(fsaverage5_pial, 'left', stat_map=stat_map,
-                            bg_map=sulcal_depth_map, cmap='Spectral',
-                            threshold=.5, alpha=0.7)
+                            bg_map=sulcal_depth_map, bg_on_stat=True,
+                            cmap='Spectral', threshold=.6, alpha=.7)
 
 # saving plots to file
 plotting.plot_surf_stat_map(fsaverage5_pial, 'left', stat_map=stat_map,
