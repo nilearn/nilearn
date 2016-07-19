6e38eda6c3ba6e600a3cd4c779f7735c70576bb5
ahoyosid <ahoyosidrobo@gmail.com>
2016-07-19 06:05:04 +0200

    removing extra spaces

--- nilearn/_utils/fixes/__init__.py
+++ nilearn/_utils/fixes/__init__.py
@@ -9,14 +9,12 @@ if (LooseVersion(sklearn.__version__) < LooseVersion('0.15') or
 else:
     from sklearn.feature_selection import f_regression
 
-
 try:
     # scikit-learn >= 0.16
     from sklearn.cross_validation import check_cv
 except ImportError:
     # scikit-learn < 0.16
     from sklearn.cross_validation import _check_cv as check_cv
-
 
 # atleast2d_or_csr
 try:
@@ -25,13 +23,11 @@ except ImportError:
     # Changed in 0.15
     from sklearn.utils import check_array as atleast2d_or_csr
 
-
 # roc_auc_score
 try:
     from sklearn.metrics import roc_auc_score
 except ImportError:
     from sklearn.metrics import auc as roc_auc_score
-
 
 try:
     from sklearn.utils import check_X_y
@@ -40,7 +36,6 @@ except ImportError:
     # scikit-learn < 0.16
     from .sklearn_validation import check_X_y
     from .sklearn_validation import check_is_fitted
-
 
 __all__ = ['f_regression', 'atleast2d_or_csr', 'roc_auc_score',
            'check_X_y', 'check_is_fitted', 'check_cv']
