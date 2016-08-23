ee8acb84b2763c1b754a1c9b26a0df8e044b28ed
Salma <salmabougacha@hotmail.com>
2016-08-23 14:14:12 +0200

    better narration, 8mm spheres

--- examples/03_connectivity/plot_adhd_spheres.py
+++ examples/03_connectivity/plot_adhd_spheres.py
@@ -5,12 +5,12 @@ Extracting brain signal from spheres
 This example shows how to extract brain signals from spheres described by the
 coordinates of their center in MNI space and a given radius in millimeters.
 In particular, it creates 4 ROIs part of the default mode network (DMN) as
-10-mm-radius spheres centered at coordinates from [1] and estimates partial
+8-mm-radius spheres centered at coordinates from [1] and estimates partial
 correlation strength between them. The example concludes with a more advanced
 part dedicated to spheres radius choice.
 
 
-** References **
+**References**
 
 [1] Vincent JL, Kahn I, Snyder AZ, Raichle ME, Buckner RL (2008) Evidence
 for a frontoparietal control system revealed by intrinsic functional con-
@@ -49,43 +49,62 @@ labels = ['PCC', 'MPFC', 'lAG', 'rAG']
 # It is advised to mask the spheres with grey matter. We don't have subject's
 # grey matter mask, we resort to a less precise group-level one.
 gm_mask_img = datasets.fetch_icbm152_brain_gm_mask()
+print(type(gm_mask_img))
+
+###############################################################################
+# This is a Nifti image, characterized by a data array, an affine and a header.
 
 ###############################################################################
 # Creating the ROIs
-#------------------
+# -----------------
 #
-# We compute voxels indices within the grey mask
-# data in the gray mask image is stored in a 3D array
-import numpy as np
+# Data in the gray mask image is stored in a 3D array
 
 gm_mask_array = gm_mask_img.get_data()
-i, j, k = np.where(gm_mask_array != 0)
+print('GM image data of shape {0}.'.format(gm_mask_array.shape))
 
-
+###############################################################################
+# We identify indices of GM voxels and compute their MNI coordinates
+import numpy as np
 from nilearn.image.resampling import coord_transform
-gm_mask_mni_coords = np.array(coord_transform(i, j, k, gm_mask_img.affine)).T
+i, j, k = np.where(gm_mask_array != 0)
+gm_voxels_coords = np.array(coord_transform(i, j, k, gm_mask_img.affine)).T
+
+print('Computed MNI coordinates of {0} GM voxels.'.format(
+    gm_voxels_coords.shape[0]))
 
 ###############################################################################
-# We use the obtained coordinates to identify voxels lying inside the seeds.
-pcc_10mm_mask = np.linalg.norm(gm_mask_mni_coords - pcc_coords, axis=1) < 10.
-mpfc_10mm_mask = np.linalg.norm(gm_mask_mni_coords - mpfc_coords, axis=1) < 10.
-lag_10mm_mask = np.linalg.norm(gm_mask_mni_coords - lag_coords, axis=1) < 10.
-rag_10mm_mask = np.linalg.norm(gm_mask_mni_coords - rag_coords, axis=1) < 10.
+# We use the obtained coordinates to label GM voxels less than 8mm from each
+# ROI center
+gm_voxels_in_pcc = np.linalg.norm(gm_voxels_coords - pcc_coords, axis=1) < 8.
+gm_voxels_in_mpfc = np.linalg.norm(gm_voxels_coords - mpfc_coords, axis=1) < 8.
+gm_voxels_in_lag = np.linalg.norm(gm_voxels_coords - lag_coords, axis=1) < 8.
+gm_voxels_in_rag = np.linalg.norm(gm_voxels_coords - rag_coords, axis=1) < 8.
 
-dmn_10mm_mask = pcc_10mm_mask + mpfc_10mm_mask + lag_10mm_mask + rag_10mm_mask
+###############################################################################
+# and combine the labels to mark GM voxels part of the whole DMN.
+gm_voxels_in_dmn = \
+    gm_voxels_in_pcc + gm_voxels_in_mpfc + gm_voxels_in_lag + gm_voxels_in_rag
 
+print('Labeled {0} from the {1} GM voxels as part of DMN.'.format(
+    gm_voxels_in_dmn.sum(), gm_voxels_in_dmn.shape[0]))
+###############################################################################
+# Finally, we need to go back from the mask array to the mask image.
 from nilearn import masking
-dmn_10mm_mask_img = masking.unmask(dmn_10mm_mask, gm_mask_img)
 
+dmn_mask_img = masking.unmask(gm_voxels_in_dmn, gm_mask_img)
+print(type(dmn_mask_img))
+
+###############################################################################
+# We can visualize the ROIs.
 from nilearn import plotting
-plotting.plot_roi(dmn_10mm_mask_img, cut_coords=[0, -68, 28])
+
+plotting.plot_roi(dmn_mask_img, cut_coords=[0, -68, 28],
+                  title="DMN spheres intersected with GM")
 
 ###############################################################################
-# We can visualize 
-
-###############################################################################
-# Computing average signals on 10mm radius spheres
-# ------------------------------------------------
+# Computing average signals on the spheres
+# ----------------------------------------
 #
 # We make use of a spheric ROIs dedicated object, the
 # `:class:nilearn.input_data.NiftiSpheresMasker`. We define it by specifying
@@ -94,26 +113,26 @@ plotting.plot_roi(dmn_10mm_mask_img, cut_coords=[0, -68, 28])
 # filtering choices.
 from nilearn import input_data
 
-dmn_10mm_masker = input_data.NiftiSpheresMasker(
-    dmn_coords, radius=10., mask_img=gm_mask_img,
+dmn_masker = input_data.NiftiSpheresMasker(
+    dmn_coords, radius=8., mask_img=gm_mask_img,
     low_pass=0.1, t_r=2.5, high_pass=None, detrend=True, standardize=True,
     memory='nilearn_cache', memory_level=1, verbose=1)
 
 ###############################################################################
 # Time-series are computed once functional and confounds filenames given.
-dmn_10mm_average_time_series = dmn_10mm_masker.fit_transform(
+dmn_time_series = dmn_masker.fit_transform(
     func_filename, confounds=[confound_filename])
 
-print('Saved seeds time-series in array of shape {0}'.format(
-    dmn_10mm_average_time_series.shape))
+print('Saved ROIs time-series in array of shape {0}'.format(
+    dmn_time_series.shape))
 
 ###############################################################################
 # We display the time series and check visually their synchronization.
 import matplotlib.pyplot as plt
 
 plt.figure()
-for time_serie, label in zip(dmn_10mm_average_time_series.T, labels):
-    plt.plot(time_serie, label=label, lw=3.)
+for time_serie, label in zip(dmn_time_series.T, labels):
+    plt.plot(time_serie, label=label)
 
 plt.title('Default Mode Network Average Time Series')
 plt.xlabel('Scan number')
@@ -135,41 +154,40 @@ estimator = LedoitWolf()
 
 ###############################################################################
 # We just need to fit the estimator with the DMN timeseries.
-estimator.fit(dmn_10mm_average_time_series)
+estimator.fit(dmn_time_series)
 
 # negated precision coefficients are proportional to partial correlations.
-connectivity_matrix = -estimator.precision_  # no it's not connectivity matrix
+negated_precision_matrix = -estimator.precision_
 
 ###############################################################################
-# We can check that we got a square (n_spheres, n_spheres) connectivity matrix.
-print('connectivity matrix has shape {0}'.format(
-    connectivity_matrix.shape))
+# We can check that we got a square (n_spheres, n_spheres) matrix.
+print('Precision matrix has shape {0}'.format(negated_precision_matrix.shape))
 
 ###############################################################################
 # Visualizing the connections
 # ---------------------------
 
-##########################################################################
-# We display the connectivity graph with hemispheric projections using
-# the connectome dedicated function `nilearn.plotting.plot_connectome`.
-# Connectivity values are reflected by edges colors.
-from nilearn import plotting
-
-title = "Connectivity projected on hemispheres"
-plotting.plot_connectome(connectivity_matrix, dmn_coords, title=title,
-                         display_mode='lyrz')
-
-
-title = 'DMN 10mm seed-to-voxel correlation maps within-seeds'
+###############################################################################
+# We visualize the connectivity graph using the connectome dedicated function
+# `nilearn.plotting.plot_connectome`. Connectivity values are reflected by
+# edges colors.
 figure = plt.figure(figsize=(6, 6))
 display = plotting.plot_connectome(
-    connectivity_matrix, dmn_coords, node_size=0,
-    title=title, display_mode='z', figure=figure, edge_vmax=.6)
+    negated_precision_matrix, dmn_coords, figure=figure,
+    title='DMN connectivity', display_mode='z', node_size=0)
 
-display.add_overlay(dmn_10mm_mask_img)#, levels=[0, 1])
-plt.text(pcc_coords[0] + 10., pcc_coords[1] + 10., 'PCC')
-plt.text(mpfc_coords[0] + 10., mpfc_coords[1] + 10., 'MPFC')
-plt.text(lag_coords[0] + 10., lag_coords[1] + 10., 'lAG')
-plt.text(rag_coords[0] + 10., rag_coords[1] + 10., 'rAG')
+# We overlay the graph by ROIs contours, and label them.
+display.add_contours(dmn_mask_img, levels=[0])
+plt.text(pcc_coords[0] + 8., pcc_coords[1] + 8., 'PCC')
+plt.text(mpfc_coords[0] + 8., mpfc_coords[1] + 8., 'MPFC')
+plt.text(lag_coords[0] + 8., lag_coords[1] + 8., 'lAG')
+plt.text(rag_coords[0] + 8., rag_coords[1] + 8., 'rAG')
+
+###############################################################################
+# We can also display more synthetic connectome with hemispheric projections.
+plotting.plot_connectome(
+    negated_precision_matrix, dmn_coords,
+    title='Connectivity projected on hemispheres', display_mode='lyrz')
+# Notice MPFC (0, 51, -7) is included in both hemispheres since x == 0.
 
 plotting.show()
