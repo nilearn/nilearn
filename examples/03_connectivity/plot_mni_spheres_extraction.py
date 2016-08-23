cdcd8b152fb527da942287886ccb32da2bdc296b
Salma <salmabougacha@hotmail.com>
2016-08-23 14:57:06 +0200

    narration cosmit

--- examples/03_connectivity/plot_mni_spheres_extraction.py
+++ examples/03_connectivity/plot_mni_spheres_extraction.py
@@ -3,18 +3,17 @@ Extracting brain signal from spheres
 ====================================
 
 This example shows how to extract brain signals from spheres described by the
-coordinates of their center in MNI space and a given radius in millimeters.
-In particular, it creates 4 ROIs part of the default mode network (DMN) as
-8-mm-radius spheres centered at coordinates from [1] and estimates partial
-correlation strength between them. The example concludes with a more advanced
-part dedicated to spheres radius choice.
+coordinates of their **center in MNI space** and a given
+**radius in millimeters**.
 
+In particular, it creates 4 ROIs part of the default mode network (DMN) as
+8-mm-radius spheres centered at coordinates published in [1] and estimates
+**direct connectivity** between them.
 
 **References**
 
-[1] Vincent JL, Kahn I, Snyder AZ, Raichle ME, Buckner RL (2008) Evidence
-for a frontoparietal control system revealed by intrinsic functional con-
-nectivity. J Neurophysiol 100:3328-3342.
+[1] Vincent J.L. et al.,  "Evidence for a frontoparietal control system
+revealed by intrinsic functional connectivity", J Neurophysiol 100 (2008).
 
 """
 
@@ -46,8 +45,8 @@ dmn_coords = [pcc_coords, mpfc_coords, lag_coords, rag_coords]
 labels = ['PCC', 'MPFC', 'lAG', 'rAG']
 
 ###############################################################################
-# It is advised to mask the spheres with grey matter. We don't have subject's
-# grey matter mask, we resort to a less precise group-level one.
+# It is advised to **mask the spheres with grey matter**. We don't have
+# subject's grey matter mask, we resort to a less precise group-level one.
 gm_mask_img = datasets.fetch_icbm152_brain_gm_mask()
 print(type(gm_mask_img))
 
@@ -58,13 +57,13 @@ print(type(gm_mask_img))
 # Creating the ROIs
 # -----------------
 #
-# Data in the gray mask image is stored in a 3D array
+# Data in the gray mask image is stored in a 3D array.
 
 gm_mask_array = gm_mask_img.get_data()
 print('GM image data of shape {0}.'.format(gm_mask_array.shape))
 
 ###############################################################################
-# We identify indices of GM voxels and compute their MNI coordinates
+# We identify indices of GM voxels and compute their MNI coordinates.
 import numpy as np
 from nilearn.image.resampling import coord_transform
 i, j, k = np.where(gm_mask_array != 0)
@@ -74,22 +73,23 @@ print('Computed MNI coordinates of {0} GM voxels.'.format(
     gm_voxels_coords.shape[0]))
 
 ###############################################################################
-# We use the obtained coordinates to label GM voxels less than 8mm from each
-# ROI center
+# We use the obtained coordinates to mask GM voxels more than 8mm from each
+# ROI center,
 gm_voxels_in_pcc = np.linalg.norm(gm_voxels_coords - pcc_coords, axis=1) < 8.
 gm_voxels_in_mpfc = np.linalg.norm(gm_voxels_coords - mpfc_coords, axis=1) < 8.
 gm_voxels_in_lag = np.linalg.norm(gm_voxels_coords - lag_coords, axis=1) < 8.
 gm_voxels_in_rag = np.linalg.norm(gm_voxels_coords - rag_coords, axis=1) < 8.
 
+print(gm_voxels_in_pcc)
 ###############################################################################
-# and combine the labels to mark GM voxels part of the whole DMN.
+# and combine the masks to mark GM voxels part of the whole DMN.
 gm_voxels_in_dmn = \
     gm_voxels_in_pcc + gm_voxels_in_mpfc + gm_voxels_in_lag + gm_voxels_in_rag
 
-print('Labeled {0} from the {1} GM voxels as part of DMN.'.format(
+print('{0} voxels from the {1} GM voxels are part of our DMN.'.format(
     gm_voxels_in_dmn.sum(), gm_voxels_in_dmn.shape[0]))
 ###############################################################################
-# Finally, we need to go back from the mask array to the mask image.
+# Finally, we need to go back from the mask array to the ROIs mask image.
 from nilearn import masking
 
 dmn_mask_img = masking.unmask(gm_voxels_in_dmn, gm_mask_img)
@@ -108,7 +108,7 @@ plotting.plot_roi(dmn_mask_img, cut_coords=[0, -68, 28],
 #
 # We make use of a spheric ROIs dedicated object, the
 # `:class:nilearn.input_data.NiftiSpheresMasker`. We define it by specifying
-# the spheres centers and radius, as well as optional arguments:
+# the **spheres centers and radius**, as well as optional arguments:
 # The grey matter mask image, the detrending, signal normalization and
 # filtering choices.
 from nilearn import input_data
@@ -123,8 +123,8 @@ dmn_masker = input_data.NiftiSpheresMasker(
 dmn_time_series = dmn_masker.fit_transform(
     func_filename, confounds=[confound_filename])
 
-print('Saved ROIs time-series in array of shape {0}'.format(
-    dmn_time_series.shape))
+print('Computed ROIs average signals: {0} timepoints for {1} ROIs.'.format(
+    dmn_time_series.shape[0], dmn_time_series.shape[1]))
 
 ###############################################################################
 # We display the time series and check visually their synchronization.
@@ -142,12 +142,14 @@ plt.tight_layout()
 
 
 ###############################################################################
-# Estimating connectivity
-# -----------------------
+# Estimating direct connectivity
+# ------------------------------
 #
-# Direct connections between ROIs are revealed by the signals precision
-# (ie inverse covariance) matrix. We compute this matrix using the Ledoit-Wolf
-# estimator, well suited to small number of ROIs /large number of scans cases.
+# **Direct connections** between ROIs are revealed by the ROI-to-ROI
+# **partial correlation coefficients**. A non-normalized version of these
+# coefficients can be obtained from the inverse covariance or precision matrix
+# of the signals. We estimate that matrix with **Ledoit-Wolf** estimator,
+# well suited to **small number of ROIs /large number of scans** cases.
 from sklearn.covariance import LedoitWolf
 
 estimator = LedoitWolf()
@@ -160,7 +162,7 @@ estimator.fit(dmn_time_series)
 negated_precision_matrix = -estimator.precision_
 
 ###############################################################################
-# We can check that we got a square (n_spheres, n_spheres) matrix.
+# We can check that we got a square `n_spheres` by `n_spheres` matrix.
 print('Precision matrix has shape {0}'.format(negated_precision_matrix.shape))
 
 ###############################################################################
