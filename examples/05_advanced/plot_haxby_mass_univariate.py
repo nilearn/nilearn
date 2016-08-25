eaabc6629e3f07a054a1ced74ed2caad2e4922aa
Gael Varoquaux <gael.varoquaux@normalesup.org>
2016-08-25 13:01:31 +0200

    ENH: plot_haxby_mass_univariate on subject 2

--- examples/05_advanced/plot_haxby_mass_univariate.py
+++ examples/05_advanced/plot_haxby_mass_univariate.py
@@ -32,7 +32,7 @@ References
 ##############################################################################
 # Load Haxby dataset
 from nilearn import datasets
-haxby_dataset = datasets.fetch_haxby(subjects=[1])
+haxby_dataset = datasets.fetch_haxby(subjects=[2])
 
 # print basic information on the dataset
 print('Mask nifti image (3D) is located at: %s' % haxby_dataset.mask)
@@ -43,6 +43,7 @@ print('Functional nifti image (4D) is located at: %s' % haxby_dataset.func[0])
 mask_filename = haxby_dataset.mask
 from nilearn.input_data import NiftiMasker
 nifti_masker = NiftiMasker(
+    smoothing_fwhm=8,
     mask_img=mask_filename,
     memory='nilearn_cache', memory_level=1)  # cache options
 func_filename = haxby_dataset.func[0]
@@ -122,32 +123,19 @@ from nilearn.plotting import plot_stat_map, show
 from nilearn import image
 mean_fmri_img = image.mean_img(func_filename)
 
-# Various plotting parameters
-z_slice = -17  # plotted slice
-from nilearn.image.resampling import coord_transform
-affine = signed_neg_log_pvals_unmasked.affine
-from scipy import linalg
-_, _, k_slice = coord_transform(0, 0, z_slice,
-                                linalg.inv(affine))
-k_slice = np.round(k_slice)
-
 threshold = -np.log10(0.1)  # 10% corrected
 
 vmax = min(signed_neg_log_pvals.max(),
            neg_log_pvals_bonferroni.max())
 
 # Plot thresholded p-values map corresponding to F-scores
-fig = plt.figure(figsize=(4, 5.5), facecolor='k')
-
 display = plot_stat_map(neg_log_pvals_bonferroni_unmasked, mean_fmri_img,
                         threshold=threshold, cmap=plt.cm.RdBu_r,
-                        display_mode='z', cut_coords=[z_slice],
-                        figure=fig, vmax=vmax)
+                        display_mode='z', cut_coords=[-1, ],
+                        vmax=vmax)
 
 neg_log_pvals_bonferroni_data = neg_log_pvals_bonferroni_unmasked.get_data()
-neg_log_pvals_bonferroni_slice_data = \
-    neg_log_pvals_bonferroni_data[..., k_slice]
-n_detections = (neg_log_pvals_bonferroni_slice_data > threshold).sum()
+n_detections = (neg_log_pvals_bonferroni_data > threshold).sum()
 title = ('Negative $\log_{10}$ p-values'
          '\n(Parametric two-sided F-test'
          '\n+ Bonferroni correction)'
@@ -156,17 +144,12 @@ title = ('Negative $\log_{10}$ p-values'
 display.title(title, y=1.1)
 
 # Plot permutation p-values map
-fig = plt.figure(figsize=(4, 5.5), facecolor='k')
-
 display = plot_stat_map(signed_neg_log_pvals_unmasked, mean_fmri_img,
                         threshold=threshold, cmap=plt.cm.RdBu_r,
-                        display_mode='z', cut_coords=[z_slice],
-                        figure=fig, vmax=vmax)
+                        display_mode='z', cut_coords=[-1, ],
+                        vmax=vmax)
 
-signed_neg_log_pvals_data = signed_neg_log_pvals_unmasked.get_data()
-signed_neg_log_pvals_slice_data = \
-    signed_neg_log_pvals_data[..., k_slice, 0]
-n_detections = (np.abs(signed_neg_log_pvals_slice_data) > threshold).sum()
+n_detections = (np.abs(signed_neg_log_pvals) > threshold).sum()
 title = ('Negative $\log_{10}$ p-values'
          '\n(Non-parametric two-sided test'
          '\n+ max-type correction)'
