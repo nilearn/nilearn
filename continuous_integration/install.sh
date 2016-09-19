97ed168f4f4db8029d4e609bad1b910a59067b40
Martin <mperezguevara@gmail.com>
2016-09-19 17:04:50 +0200

    changes in install.sh to make cicle ci work. Conditional travis commands.

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -52,9 +52,12 @@ print_conda_requirements() {
 }
 
 create_new_conda_env() {
-    # Deactivate the travis-provided virtual environment and setup a
-    # conda-based environment instead
-    deactivate
+    # Skip Travis related code on circle ci.
+    if [ -z $CIRCLECI ]; then
+        # Deactivate the travis-provided virtual environment and setup a
+        # conda-based environment instead
+        deactivate
+    fi
 
     # Use the miniconda installer for faster download / install of conda
     # itself
@@ -62,7 +65,8 @@ create_new_conda_env() {
         -O miniconda.sh
     chmod +x miniconda.sh && ./miniconda.sh -b
     export PATH=/home/travis/miniconda2/bin:$PATH
-    conda update --yes conda
+    echo $PATH
+    conda update --quiet --yes conda
 
     # Configure the conda environment and put it in the path using the
     # provided versions
@@ -73,8 +77,10 @@ create_new_conda_env() {
 
     if [[ "$INSTALL_MKL" == "true" ]]; then
         # Make sure that MKL is used
-        conda install --yes mkl
-    else
+        conda install --quiet --yes mkl
+    elif [[ -z $CIRCLECI ]]; then
+        # Travis doesn't use MKL but circle ci does for speeding up examples
+        # generation in the html documentation.
         # Make sure that MKL is not used
         conda remove --yes --features mkl || echo "MKL not installed"
     fi
@@ -82,6 +88,7 @@ create_new_conda_env() {
 
 if [[ "$DISTRIB" == "neurodebian" ]]; then
     create_new_venv
+    pip install nose-timer
     bash <(wget -q -O- http://neuro.debian.net/_files/neurodebian-travis.sh)
     sudo apt-get install -qq python-scipy python-nose python-nibabel python-sklearn python-pandas python-numpy
     pip install numpy==1.7.1
@@ -90,6 +97,7 @@ if [[ "$DISTRIB" == "neurodebian" ]]; then
 
 elif [[ "$DISTRIB" == "conda" ]]; then
     create_new_conda_env
+    pip install nose-timer
     # Note: nibabel is in setup.py install_requires so nibabel will
     # always be installed eventually. Defining NIBABEL_VERSION is only
     # useful if you happen to want a specific nibabel version rather
@@ -111,6 +119,8 @@ else
     echo "Unrecognized distribution ($DISTRIB); cannot setup travis environment."
     exit 1
 fi
+
+pip install psutil memory_profiler
 
 if [[ "$COVERAGE" == "true" ]]; then
     pip install coverage coveralls
