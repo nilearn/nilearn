5a13e4fe77845daccad3f528a99ad124b1c99a7a
Martin <mperezguevara@gmail.com>
2016-09-19 16:51:29 +0200

    added circle ci integration

---
+++ continuous_integration/circle_ci_test_doc.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+# on circle ci, each command run with it's own execution context so we have to
+# activate the conda testenv on a per command basis. That's why we put calls to
+# python (conda) in a dedicated bash script and we activate the conda testenv
+# here.
+source activate testenv
+
+# pipefail is necessary to propagate exit codes
+set -o pipefail && cd doc && make html-strict 2>&1 | tee ~/log.txt
