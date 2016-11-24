a97b6dee98f56c99fd88b6142223a548d06a778c
darya <daria.chizhik@gmail.com>
2016-11-24 11:01:28 -0500

    Make changes according to comments

--- examples/04_manipulating_images/plot_extract_regions_labels_image.py
+++ examples/04_manipulating_images/plot_extract_regions_labels_image.py
@@ -1,12 +1,13 @@
 """
 Regions labeling and extraction using Yeo atlas
-=============================================================
+===============================================
 
-This example shows how to use :class:nilearn.regions.extract_regions_labels_img
+This example shows how to use
+:class:`nilearn.regions.extract_regions_labels_img`
 to assigns each separated region of the atlas a unique label.
 
-We use the Yeo atlas as an example for labeling regiones,
-:func:nilearn.datasets.fetch_atlas_yeo_2011
+We use the Yeo atlas as an example for labeling regions,
+:func:`nilearn.datasets.fetch_atlas_yeo_2011`
 
 """
 
@@ -25,10 +26,11 @@ region_labels = extract_regions_labels_img(atlas_yeo,
                                            connect_diag=True)
 
 ################################################################################
-# Counting regions and potting them
-import nibabel as nb
+# Counting regions and plotting them
+
+from nilearn.image import load_img
 import numpy as np
-atlas_yeo_img = nb.load(atlas_yeo)
+atlas_yeo_img = load_img(atlas_yeo)
 atlas_yeo_data = atlas_yeo_img.get_data()
 region_labels_data = region_labels.get_data()
 
@@ -37,23 +39,21 @@ print("The labeled regions of Yeo atlas: ", np.unique(region_labels_data))
 
 # Let's plot them
 # The original Yeo atlas has 7 regions, that is indicated in the colorbar.
-# The colorbar also shows the corespondence to the color and the cluster
+# The colorbar also shows the correspondence to the color and the cluster
 from nilearn import plotting
 
 # the original Yeo atlases
 pl_yeo = plotting.plot_roi(atlas_yeo, title='Original Yeo atlas',
-                           cut_coords=(8,-4,9),
+                           cut_coords=(8, -4, 9),
                            cmap = 'gist_rainbow', colorbar=True)
-plotting.show()
-
 # labeled Yeo atlas
 # Note: the same cluster in original and labeled atlas could have different
 # color, so, you can not compare it.
-# On the plost we observe the region that belonged to both hemispheres in
-# original Yeo atlas and had the one color is splitted i more regios with
+# On the plots we observe the region that belonged to both hemispheres in
+# original Yeo atlas and had the one color is slitted in more regions with
 # different colors.
 pl_yeo_labeled = plotting.plot_roi(region_labels, title='Labeled Yeo atlas',
-                                   cut_coords=(8,-4,9), cmap = 'gist_rainbow',
+                                   cut_coords=(8, -4, 9), cmap='gist_rainbow',
                                    colorbar=True)
 plotting.show()
 
@@ -84,8 +84,7 @@ region_labels_cd_false = extract_regions_labels_img(
 region_labels_cd_false_data = region_labels_cd_false.get_data()
 
 val_cd_false, size_cd_false = np.unique(region_labels_cd_false_data,
-                                         return_counts=True)
-
+                                        return_counts=True)
 
 # Measuring the size of each region of labeled atlas
 # printing sorted dictionary of the region and its size
@@ -104,15 +103,14 @@ print("The size of the labeled regions with connect_diag=False: ",
 # min_size parameter helps us to filter these small regions
 
 region_labels_min = extract_regions_labels_img(atlas_yeo, min_size=100,
-                                                  connect_diag=False)
+                                               connect_diag=False)
 
 region_labels_min_data = region_labels_min.get_data()
 
 val_min, size_min = np.unique(region_labels_min_data, return_counts=True)
 
 d_min = dict(zip(val_min[1:], size_min[1:]))
-d_min_sorted = sorted(d_min.items(), key=operator.itemgetter(1),
-                           reverse=True)
+d_min_sorted = sorted(d_min.items(), key=operator.itemgetter(1), reverse=True)
 
 print("The size of the labeled regions with connect_diag=False and min_size",
       d_min_sorted)
