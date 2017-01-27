275d58448c024eda5d4c430262434158d26f94f2
ahoyosid <ahoyosidrobo@gmail.com>
2017-01-27 14:17:01 +0100

    increasing coverage

--- nilearn/decoding/tests/test_space_net.py
+++ nilearn/decoding/tests/test_space_net.py
@@ -110,14 +110,20 @@ def test_params_correctly_propagated_in_constructors():
 
 def test_screening_space_net():
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> increasing coverage
     for verbose in [0, 2]:
         screening_percentile = assert_warns(UserWarning,
                                             _adjust_screening_percentile, 10,
                                             mask, verbose)
+<<<<<<< HEAD
 =======
     screening_percentile = assert_warns(UserWarning,
                                         _adjust_screening_percentile, 10, mask)
 >>>>>>> addressing checking masker comments
+=======
+>>>>>>> increasing coverage
     # We gave here a very small mask, judging by standards of brain size
     # thus the screening_percentile_ corrected for brain size should
     # be 100%
