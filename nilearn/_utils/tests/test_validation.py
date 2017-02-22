bc5fafa2118f5e69f74e837106ec2637c404a184
ahoyosid <ahoyosidrobo@gmail.com>
2017-02-22 10:53:48 +0100

    cleaning the code

--- nilearn/_utils/tests/test_validation.py
+++ nilearn/_utils/tests/test_validation.py
@@ -8,34 +8,11 @@ from sklearn.base import BaseEstimator
 
 from nilearn.input_data import NiftiMasker
 from nilearn._utils.testing import assert_warns
-from nilearn._utils.validation import check_masker, check_feature_screening
 from nilearn._utils.param_validation import (MNI152_BRAIN_VOLUME,
                                              _get_mask_volume)
 
 mni152_brain_mask = (
     "/usr/share/fsl/data/standard/MNI152_T1_1mm_brain_mask.nii.gz")
-
-
-def test_feature_screening():
-    for is_classif in [True, False]:
-        for screening_percentile in [100, None, 20, 101, -1, 10]:
-
-            if screening_percentile == 100 or screening_percentile is None:
-                assert_equal(check_feature_screening(
-                    screening_percentile, MNI152_BRAIN_VOLUME, is_classif),
-                    None)
-            elif screening_percentile == 101 or screening_percentile == -1:
-                assert_raises(ValueError, check_feature_screening,
-                              screening_percentile, MNI152_BRAIN_VOLUME,
-                              is_classif)
-            elif screening_percentile == 20:
-                assert_true(isinstance(check_feature_screening(
-                    screening_percentile, MNI152_BRAIN_VOLUME, is_classif),
-                    BaseEstimator))
-            else:
-                assert_warns(UserWarning, check_feature_screening,
-                             screening_percentile, MNI152_BRAIN_VOLUME * 2,
-                             is_classif)
 
 
 def test_get_mask_volume():
