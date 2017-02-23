3233092d5ee400637fe2a74629b19d04e187b897
ahoyosid <ahoyosidrobo@gmail.com>
2017-02-23 13:00:54 +0100

    removing the function indexable

--- nilearn/_utils/fixes/sklearn_validation.py
+++ nilearn/_utils/fixes/sklearn_validation.py
@@ -399,27 +399,3 @@ def check_is_fitted(estimator, attributes, msg=None, all_or_any=all):
 
     if not all_or_any([hasattr(estimator, attr) for attr in attributes]):
         raise NotFittedError(msg % {'name': type(estimator).__name__})
-
-
-def indexable(*iterables):
-    """Make arrays indexable for cross-validation.
-    Checks consistent length, passes through None, and ensures that everything
-    can be indexed by converting sparse matrices to csr and converting
-    non-interable objects to arrays.
-    Parameters
-    ----------
-    *iterables : lists, dataframes, arrays, sparse matrices
-        List of objects to ensure sliceability.
-    """
-    result = []
-    for X in iterables:
-        if sp.issparse(X):
-            result.append(X.tocsr())
-        elif hasattr(X, "__getitem__") or hasattr(X, "iloc"):
-            result.append(X)
-        elif X is None:
-            result.append(X)
-        else:
-            result.append(np.array(X))
-    check_consistent_length(*result)
-    return result
