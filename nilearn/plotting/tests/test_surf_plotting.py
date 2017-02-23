1de8103e14f8adf54d5a0cae2bf0cffc2fab0796
Gael Varoquaux <gael.varoquaux@normalesup.org>
2017-02-23 11:01:54 +0100

    FIX: bugware to avoid a bug in old nibabel versions

--- nilearn/plotting/tests/test_surf_plotting.py
+++ nilearn/plotting/tests/test_surf_plotting.py
@@ -4,7 +4,7 @@ from nibabel import gifti
 import tempfile
 import os
 import matplotlib.pyplot as plt
-from nose.tools import assert_true, assert_false
+from distutils.version import LooseVersion
 from numpy.testing import assert_array_equal, assert_array_almost_equal, \
                           assert_equal
 from nilearn._utils.testing import assert_raises_regex
@@ -25,19 +25,25 @@ def _generate_surf():
 
 def test_load_surf_data_array():
     # test loading and squeezing data from numpy array
-    data_flat = np.zeros((20,))
+    data_flat = np.zeros((20, ))
     data_squeeze = np.zeros((20, 1, 3))
-    assert_array_equal(load_surf_data(data_flat), np.zeros((20,)))
+    assert_array_equal(load_surf_data(data_flat), np.zeros((20, )))
     assert_array_equal(load_surf_data(data_squeeze), np.zeros((20, 3)))
 
 
 def test_load_surf_data_file_nii_gii():
     # test loading of fake data from gifti file
     filename_gii = tempfile.mktemp(suffix='.gii')
-    darray = gifti.GiftiDataArray(data=np.zeros((20,)))
+    if LooseVersion(nb.__version__) > LooseVersion('1.2.0'):
+        darray = gifti.GiftiDataArray(data=np.zeros((20, )))
+    else:
+        # Avoid a bug in nibabel 1.2.0 where GiftiDataArray were not
+        # initialized properly:
+        darray = gifti.GiftiDataArray.from_array(np.zeros((20, )),
+                                                 intent='t test')
     gii = gifti.GiftiImage(darrays=[darray])
     gifti.write(gii, filename_gii)
-    assert_array_equal(load_surf_data(filename_gii), np.zeros((20,)))
+    assert_array_equal(load_surf_data(filename_gii), np.zeros((20, )))
     os.remove(filename_gii)
 
     # test loading of data from empty gifti file
@@ -52,26 +58,26 @@ def test_load_surf_data_file_nii_gii():
     # test loading of fake data from nifti file
     filename_nii = tempfile.mktemp(suffix='.nii')
     filename_niigz = tempfile.mktemp(suffix='.nii.gz')
-    nii = nb.Nifti1Image(np.zeros((20,)), affine=None)
+    nii = nb.Nifti1Image(np.zeros((20, )), affine=None)
     nb.save(nii, filename_nii)
     nb.save(nii, filename_niigz)
-    assert_array_equal(load_surf_data(filename_nii), np.zeros((20,)))
-    assert_array_equal(load_surf_data(filename_niigz), np.zeros((20,)))
+    assert_array_equal(load_surf_data(filename_nii), np.zeros((20, )))
+    assert_array_equal(load_surf_data(filename_niigz), np.zeros((20, )))
     os.remove(filename_nii)
     os.remove(filename_niigz)
 
 
 def test_load_surf_data_file_freesurfer():
     # test loading of fake data from sulc and thickness files
-    data = np.zeros((20,))
+    data = np.zeros((20, ))
     filename_sulc = tempfile.mktemp(suffix='.sulc')
     nb.freesurfer.io.write_morph_data(filename_sulc, data)
-    assert_array_equal(load_surf_data(filename_sulc), np.zeros((20,)))
+    assert_array_equal(load_surf_data(filename_sulc), np.zeros((20, )))
     os.remove(filename_sulc)
 
     filename_thick = tempfile.mktemp(suffix='.thickness')
     nb.freesurfer.io.write_morph_data(filename_thick, data)
-    assert_array_equal(load_surf_data(filename_thick), np.zeros((20,)))
+    assert_array_equal(load_surf_data(filename_thick), np.zeros((20, )))
     os.remove(filename_thick)
 
     # test loading of data from real label and annot files
@@ -80,7 +86,7 @@ def test_load_surf_data_file_freesurfer():
     label = load_surf_data(os.path.join(datadir, 'test.label'))
     assert_array_equal(label[:5], label_start)
     assert_array_equal(label[-5:], label_end)
-    assert_equal(label.shape, (10,))
+    assert_equal(label.shape, (10, ))
     del label, label_start, label_end
 
     annot_start = np.array([24, 29, 28, 27, 24, 31, 11, 25, 0, 12])
@@ -88,13 +94,13 @@ def test_load_surf_data_file_freesurfer():
     annot = load_surf_data(os.path.join(datadir, 'test.annot'))
     assert_array_equal(annot[:10], annot_start)
     assert_array_equal(annot[-10:], annot_end)
-    assert_equal(annot.shape, (10242,))
+    assert_equal(annot.shape, (10242, ))
     del annot, annot_start, annot_end
 
 
 def test_load_surf_data_file_error():
     # test if files with unexpected suffixes raise errors
-    data = np.zeros((20,))
+    data = np.zeros((20, ))
     wrong_suff = ['.vtk', '.obj', '.mnc', '.txt']
     for suff in wrong_suff:
         filename_wrong = tempfile.mktemp(suffix=suff)
@@ -187,7 +193,7 @@ def test_load_surf_mesh_file_error():
 def test_plot_surf():
     mesh = _generate_surf()
     rng = np.random.RandomState(0)
-    bg = rng.randn(mesh[0].shape[0],)
+    bg = rng.randn(mesh[0].shape[0], )
 
     # Plot mesh only
     plot_surf(mesh)
@@ -209,7 +215,6 @@ def test_plot_surf():
 def test_plot_surf_error():
     mesh = _generate_surf()
     rng = np.random.RandomState(0)
-    bg = rng.randn(mesh[0].shape[0],)
 
     # Wrong inputs for view or hemi
     assert_raises_regex(ValueError, 'view must be one of',
@@ -221,13 +226,13 @@ def test_plot_surf_error():
     assert_raises_regex(ValueError,
                         'bg_map does not have the same number of vertices',
                         plot_surf, mesh,
-                        bg_map=rng.randn(mesh[0].shape[0]-1,))
+                        bg_map=rng.randn(mesh[0].shape[0]-1, ))
 
     # Wrong size of surface data
     assert_raises_regex(ValueError,
                         'surf_map does not have the same number of vertices',
                         plot_surf, mesh,
-                        surf_map=rng.randn(mesh[0].shape[0]+1,))
+                        surf_map=rng.randn(mesh[0].shape[0]+1, ))
 
     assert_raises_regex(ValueError,
                         'surf_map can only have one dimension', plot_surf,
@@ -237,8 +242,8 @@ def test_plot_surf_error():
 def test_plot_surf_stat_map():
     mesh = _generate_surf()
     rng = np.random.RandomState(0)
-    bg = rng.randn(mesh[0].shape[0],)
-    data = 10*rng.randn(mesh[0].shape[0],)
+    bg = rng.randn(mesh[0].shape[0], )
+    data = 10*rng.randn(mesh[0].shape[0], )
 
     # Plot mesh with stat map
     plot_surf_stat_map(mesh, stat_map=data)
@@ -267,8 +272,7 @@ def test_plot_surf_stat_map():
 def test_plot_surf_stat_map_error():
     mesh = _generate_surf()
     rng = np.random.RandomState(0)
-    bg = rng.randn(mesh[0].shape[0],)
-    data = 10*rng.randn(mesh[0].shape[0],)
+    data = 10 * rng.randn(mesh[0].shape[0], )
 
     # Try to input vmin
     assert_raises_regex(ValueError,
