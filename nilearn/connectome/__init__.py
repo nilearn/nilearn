e57279cb4f1eaf8cf3a7a99aa0e843bf09050fcf
salma <salmabougacha@hotmail.com>
2017-03-04 15:52:36 +0100

    fix indentations in __init__

--- nilearn/connectome/__init__.py
+++ nilearn/connectome/__init__.py
@@ -5,14 +5,13 @@ of Gaussian graphical models.
 """
 
 from .connectivity_matrices import (sym_matrix_to_vec, vec_to_sym_matrix,
-				    sym_to_vec,
-                                    ConnectivityMeasure, cov_to_corr,
-                                    prec_to_partial)
+                                    sym_to_vec, ConnectivityMeasure,
+                                    cov_to_corr, prec_to_partial)
 
 from .group_sparse_cov import (GroupSparseCovariance,
     GroupSparseCovarianceCV, group_sparse_covariance)
 
 __all__ = ['sym_matrix_to_vec', 'vec_to_sym_matrix', 'sym_to_vec',
-	   'ConnectivityMeasure', 'cov_to_corr', 'prec_to_partial',
+           'ConnectivityMeasure', 'cov_to_corr', 'prec_to_partial',
            'GroupSparseCovariance', 'GroupSparseCovarianceCV',
            'group_sparse_covariance']
