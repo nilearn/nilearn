27243ee461a3345091c3828681488c4d303a276e
Sylvain LANNUZEL <sylvain@dhcp-147-251.via.ecp.fr>
2017-04-05 17:17:47 +0200

    error fix

--- nilearn/decomposition/tests/test_canica.py
+++ nilearn/decomposition/tests/test_canica.py
@@ -140,4 +140,4 @@ def test_component_sign():
 def test_threshold_bound():
     # Smoke test to make sure an error is raised when threshold
     # is higher than number of components
-    assert_raises(ValueError, canICA, n_components=4, threshold=5.)
+    assert_raises(ValueError, CanICA, n_components=4, threshold=5.)
