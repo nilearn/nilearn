0df53dc99ca9ee59de9c61fc47637e9413115742
ahoyosid <ahoyosidrobo@gmail.com>
2017-04-11 15:51:04 +0200

    improving tests

--- nilearn/decoding/tests/test_decoder.py
+++ nilearn/decoding/tests/test_decoder.py
@@ -5,25 +5,17 @@ Test the decoder module
 # Author: Andres Hoyos-Idrobo
 # License: simplified BSD
 
-from nose.tools import (assert_equal, assert_true, assert_false,
-                        assert_raises)
-import warnings
-import os
+from nose.tools import assert_equal, assert_true, assert_raises
 import numpy as np
-import nibabel
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC, SVR
 from sklearn.linear_model import (LogisticRegression, RidgeClassifier,
                                   Ridge)
-from sklearn.base import BaseEstimator
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
-
-from nilearn.input_data import NiftiMasker
 from nilearn.decoding.tests.test_same_api import to_niimgs
-# from nilearn.image import index_img
-from nilearn._utils.testing import assert_warns
-from nilearn.decoding.decoder import (Decoder, DecoderRegressor,
-                                      _check_param_grid)
+from nilearn.decoding.decoder import Decoder, DecoderRegressor, BaseDecoder
+from nilearn.decoding.decoder import _check_param_grid
 
 
 # Crete a test dataset
@@ -35,7 +27,6 @@ y_classif = np.hstack([[-1] * 50, [1] * 50])
 y_classif_str = np.hstack([['face'] * 50, ['house'] * 50])
 y_multiclass = np.hstack([[0] * 35, [1] * 30, [2] * 35])
 
-
 # Test estimators
 # Regression
 ridge = Ridge()
@@ -45,6 +36,7 @@ svc = LinearSVC()
 logistic_l1 = LogisticRegression(penalty='l1')
 logistic_l2 = LogisticRegression(penalty='l2')
 ridge_classifier = RidgeClassifier()
+random_forest = RandomForestClassifier()
 
 
 def test_check_param_grid():
@@ -56,7 +48,6 @@ def test_check_param_grid():
                    'logistic_l1': (logistic_l1, 'C'),
                    'logistic_l2': (logistic_l2, 'C'),
                    'ridge_classifier': (ridge_classifier, 'alpha')}
-
     # Regression
     for _, (regressor, param) in regressors.items():
         param_grid = _check_param_grid(regressor, X, y_regression, None)
@@ -65,6 +56,11 @@ def test_check_param_grid():
     for _, (classifier, param) in classifiers.items():
         param_grid = _check_param_grid(classifier, X, y_classif, None)
         assert_equal(list(param_grid.keys())[0], param)
+
+    # Using a non-linear estimator to raise the error
+    for estimator in ['log_l1', random_forest]:
+        assert_raises(ValueError, _check_param_grid, estimator, X,
+                      y_classif, None)
 
 
 def test_checking_inputs_length():
@@ -93,3 +89,12 @@ def test_decoder_prediction():
         y_pred = model.predict(X)
         assert_true(accuracy_score(y, y_pred) > 0.95)
 
+
+def test_check_estimator():
+    iris = load_iris()
+    X, y = iris.data, iris.target
+    y = 2 * (y > 0) - 1
+    X_, mask = to_niimgs(X, (2, 2, 2))
+
+    for estimator in ['log_l1', 'log_l2', 'ridgo']:
+        assert_raises(ValueError, BaseDecoder(estimator=estimator).fit, X_, y)
