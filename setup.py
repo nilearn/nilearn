740983c28fb7877692b0cc66c3c8c6a55cd743b0
bthirion <bertrand.thirion@inria.fr>
2017-05-03 20:09:26 +0200

    Dropping support for Python 2.6 due to nilearn

--- setup.py
+++ setup.py
@@ -77,7 +77,6 @@ if __name__ == "__main__":
               'Operating System :: Unix',
               'Operating System :: MacOS',
               'Programming Language :: Python :: 2',
-              'Programming Language :: Python :: 2.6',
               'Programming Language :: Python :: 2.7',
               'Programming Language :: Python :: 3.3',
               'Programming Language :: Python :: 3.4',
