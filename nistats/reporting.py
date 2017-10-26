3b4f0fe091423120616c7df59f003607610c0ef2
Martin Perez-Guevara <mperezguevara@gmail.com>
2017-10-26 11:56:02 -0700

    complete bids openneuro example

--- nistats/reporting.py
+++ nistats/reporting.py
@@ -4,9 +4,15 @@ import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 from nilearn.image.image import check_niimg_3d
+from patsy import DesignInfo
+from scipy.ndimage import label, binary_dilation, center_of_mass
+from nilearn.image.resampling import coord_transform
+import pandas as pd
+import matplotlib 
+matplotlib.rc('xtick', labelsize=20) 
 
 
-def compare_niimgs(ref_imgs, src_imgs, plot_hist=True, log=True,
+def compare_niimgs(ref_imgs, src_imgs, masker, plot_hist=True, log=True,
                    ref_label="image set 1", src_label="image set 2",
                    output_dir=None):
     """Compares two lists of images.
@@ -41,20 +47,19 @@ def compare_niimgs(ref_imgs, src_imgs, plot_hist=True, log=True,
     corrs = []
     for i, (ref_img, src_img) in enumerate(zip(ref_imgs, src_imgs)):
         _, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
-        ref_img = check_niimg_3d(ref_img).get_data()
-        src_img = check_niimg_3d(src_img).get_data()
-        if ref_img.shape != src_img.shape:
+        ref_data = masker.transform(ref_img).ravel()
+        src_data = masker.transform(src_img).ravel()
+        if ref_data.shape != src_data.shape:
             warnings.warn("Images are not shape-compatible")
             return
 
-        ref_img = ref_img.ravel()
-        src_img = src_img.ravel()
-        corr = stats.pearsonr(ref_img, src_img)[0]
+        corr = stats.pearsonr(ref_data, src_data)[0]
         corrs.append(corr)
 
         if plot_hist:
-            ax1.scatter(ref_img, src_img, label="Pearsonr: %.2f" % corr, c="g",
-                        alpha=.6)
+            ax1.scatter(
+                ref_data, src_data, label="Pearsonr: %.2f" % corr, c="g",
+                alpha=.6)
             x = np.linspace(*ax1.get_xlim(), num=100)
             ax1.plot(x, x, linestyle="--", c="k")
             ax1.grid("on")
@@ -62,9 +67,9 @@ def compare_niimgs(ref_imgs, src_imgs, plot_hist=True, log=True,
             ax1.set_ylabel(src_label)
             ax1.legend(loc="best")
 
-            ax2.hist(ref_img, alpha=.6, bins=128, log=log, label=ref_label)
-            ax2.hist(src_img, alpha=.6, bins=128, log=log, label=src_label)
-            ax2.set_title("histogram of values")
+            ax2.hist(ref_data, alpha=.6, bins=128, log=log, label=ref_label)
+            ax2.hist(src_data, alpha=.6, bins=128, log=log, label=src_label)
+            ax2.set_title("Histogram of imgs values")
             ax2.grid("on")
             ax2.legend(loc="best")
 
@@ -76,3 +81,94 @@ def compare_niimgs(ref_imgs, src_imgs, plot_hist=True, log=True,
         plt.tight_layout()
 
     return corrs
+
+
+def plot_contrast_matrix(contrast_def, design_matrix, colorbar=False, ax=None):
+    design_column_names = design_matrix.columns.tolist()
+    if isinstance(contrast_def, str):
+        di = DesignInfo(design_column_names)
+        contrast_def = di.linear_constraint(contrast_def).coefs
+
+    if ax is None:
+        plt.figure(figsize=(8, 4))
+        ax = plt.gca()
+
+    maxval = np.max(np.abs(contrast_def))
+
+    con_mx = np.asmatrix(contrast_def)
+    mat = ax.matshow(con_mx, aspect='equal', extent=[0, con_mx.shape[1],
+                     0, con_mx.shape[0]], cmap='gray', vmin=-maxval,
+                     vmax=maxval)
+    ax.set_label('conditions')
+    ax.set_ylabel('')
+    ax.set_yticklabels(['' for x in ax.get_yticklabels()])
+
+    # Shift ticks to be at 0.5, 1.5, etc
+    ax.xaxis.set(ticks=np.arange(1.0, len(design_column_names) + 1.0),
+                 ticklabels=design_column_names)
+    ax.set_xticklabels(design_column_names, rotation=90, ha='right')
+
+    if colorbar:
+        plt.colorbar(mat, fraction=0.025, pad=0.04)
+
+    plt.tight_layout()
+
+    return ax
+
+
+def get_clusters_table(stat_img, stat_threshold, cluster_threshold):
+    stat_map = stat_img.get_data()
+
+    # Extract connected components above threshold
+    label_map, n_labels = label(stat_map > stat_threshold)
+
+    # labels = label_map[search_mask.get_data() > 0]
+    for label_ in range(1, n_labels + 1):
+        if np.sum(label_map == label_) < cluster_threshold:
+            stat_map[label_map == label_] = 0
+
+    label_map, n_labels = label(stat_map > stat_threshold)
+    label_map = np.ravel(label_map)
+    stat_map = np.ravel(stat_map)
+
+    peaks = []
+    max_stat = []
+    clusters_size = []
+    # centers = []
+    coords = []
+    for label_ in range(1, n_labels + 1):
+        cluster = stat_map.copy()
+        cluster[label_map != label_] = 0
+
+        peak = np.unravel_index(np.argmax(cluster),
+                                stat_img.get_data().shape)
+        peaks.append(peak)
+
+        max_stat.append(np.max(cluster))
+
+        clusters_size.append(np.sum(label_map == label_))
+
+        # center = center_of_mass(cluster)
+        # centers.append([round(x) for x in center[:3]])
+
+        x_map, y_map, z_map = peak
+        mni_coords = np.asarray(
+            coord_transform(x_map, y_map, z_map, stat_img.affine)).tolist()
+        mni_coords = [round(x) for x in mni_coords]
+        coords.append(mni_coords)
+
+    vx, vy, vz = zip(*peaks)
+    x, y, z = zip(*coords)
+
+    columns = ['Vx', 'Vy', 'Vz', 'X', 'Y', 'Z', 'Peak stat', 'Cluster size']
+    clusters_table = pd.DataFrame(
+        list(zip(vx, vy, vz, x, y, z, max_stat, clusters_size)),
+        columns=columns)
+    # clusters_table.index.name = 'Cluster'
+
+    # d = dict(selector="th",
+    #     props=[('text-align', 'center')])
+
+    # clusters_table.style.set_properties(**{'width':'10em', 'text-align':'left'}).set_table_styles([d])
+
+    return clusters_table
