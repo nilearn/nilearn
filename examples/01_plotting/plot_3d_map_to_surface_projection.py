c50a5c3fd26f43eb11b53877b542f59e7c4f54a2
Jerome Dockes <jerome@dockes.org>
2017-11-14 21:22:10 +0100

    use localizer_button_task rather than a brainpedia image for surf projection example

--- examples/01_plotting/plot_3d_map_to_surface_projection.py
+++ examples/01_plotting/plot_3d_map_to_surface_projection.py
@@ -2,8 +2,7 @@
 Making a surface plot of a 3D statistical map
 =============================================
 
-Download a statistical map from Neurovault and a cortical mesh (fsaverage),
-project the 3D data on the mesh and display a surface plot.
+project a 3D statistical map onto a cortical mesh and display a surface plot.
 
 """
 
@@ -14,8 +13,8 @@ from nilearn import plotting
 # Get a statistical map
 # ---------------------
 
-brainpedia = datasets.fetch_neurovault_ids(image_ids=(32015,))
-image = brainpedia.images[0]
+localizer_dataset = datasets.fetch_localizer_button_task()
+localizer_tmap = localizer_dataset.tmaps[0]
 
 ##############################################################################
 # Get a cortical mesh
@@ -27,12 +26,16 @@ fsaverage = datasets.fetch_surf_fsaverage5()
 # Sample the 3D data around each node of the mesh
 # -----------------------------------------------
 
-texture = plotting.niimg_to_surf_data(image, fsaverage.pial_left)
+texture = plotting.niimg_to_surf_data(localizer_tmap, fsaverage.pial_right)
 
 ##############################################################################
 # Plot the result
 # ---------------
 
-plotting.plot_surf_stat_map(fsaverage.infl_left, texture, cmap='bwr')
+plotting.plot_surf_stat_map(fsaverage.infl_right, texture, hemi='right',
+                            title='Surface right hemisphere')
+
+plotting.plot_glass_brain(localizer_tmap, display_mode='r', plot_abs=False,
+                          title='Glass brain')
 
 plotting.show()
