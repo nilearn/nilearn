d3d69c2ed88ebfbd5e23419a46c9b1cf67cf06f1
Jerome Dockes <jerome@dockes.org>
2017-11-16 19:36:39 +0100

    improve surf projection example description + move imports

--- examples/01_plotting/plot_3d_map_to_surface_projection.py
+++ examples/01_plotting/plot_3d_map_to_surface_projection.py
@@ -2,15 +2,17 @@
 Making a surface plot of a 3D statistical map
 =============================================
 
-project a 3D statistical map onto a cortical mesh and display a surface plot.
+project a 3D statistical map onto a cortical mesh using
+:func:nilearn.surface.vol_to_surf. Display a surface plot of the projected map
+using :func:nilearn.plotting.plot_surf_stat_map.
 
 """
-
-from nilearn import datasets, surface, plotting
 
 ##############################################################################
 # Get a statistical map
 # ---------------------
+
+from nilearn import datasets
 
 localizer_dataset = datasets.fetch_localizer_button_task()
 localizer_tmap = localizer_dataset.tmaps[0]
@@ -25,11 +27,15 @@ fsaverage = datasets.fetch_surf_fsaverage5()
 # Sample the 3D data around each node of the mesh
 # -----------------------------------------------
 
+from nilearn import surface
+
 texture = surface.vol_to_surf(localizer_tmap, fsaverage.pial_right)
 
 ##############################################################################
 # Plot the result
 # ---------------
+
+from nilearn import plotting
 
 plotting.plot_surf_stat_map(fsaverage.infl_right, texture, hemi='right',
                             title='Surface right hemisphere',
