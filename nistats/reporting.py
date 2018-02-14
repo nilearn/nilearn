b8e2fb7e87347e5ac84b93154754099d95708de4
Taylor Salo <tsalo006@fiu.edu>
2018-02-14 11:06:49 -0500

    Move get_clusters_table from thresholding to reporting.
    - Don’t report peaks in matrix space.
    - Report cluster size in mm3 instead of voxels.
    - Report top 3 subpeaks more than 8mm separated when available.
    - Add argument for voxel-connectivity for cluster definition.

--- nistats/reporting.py
+++ nistats/reporting.py
@@ -6,14 +6,144 @@ Author: Martin Perez-Guevara, Elvis Dohmatob, 2017
 
 import os
 import warnings
+from string import ascii_lowercase
+
 import numpy as np
+import pandas as pd
 from scipy import stats
 import nilearn.plotting  # overrides the backend on headless servers
+from nilearn.image.resampling import coord_transform
 import matplotlib
 import matplotlib.pyplot as plt
+import scipy.ndimage.measurements as meas
+from skimage.feature import peak_local_max
 from patsy import DesignInfo
+
 from .design_matrix import check_design_matrix
 matplotlib.rc('xtick', labelsize=20)
+
+
+def _get_conn(conn='corners'):
+    if conn == 'corners':
+        return np.ones((3, 3, 3), int)
+    elif conn == 'edges':
+        mat = np.ones((3, 3, 3), int)  # 18 connectivity
+        for i in [0, -1]:
+            for j in [0, -1]:
+                for k in [0, -1]:
+                    mat[i, j, k] = 0
+        return mat
+    elif conn == 'faces':
+        mat = np.zeros((3, 3, 3), int)
+        mat[1, 1, :] = 1
+        mat[1, :, 1] = 1
+        mat[:, 1, 1] = 1
+        return mat
+    else:
+        raise Exception('Connectivity pattern "{0}" unknown.'.format(conn))
+
+
+def get_clusters_table(stat_img, stat_threshold, cluster_threshold=None,
+                       connectivity='corners'):
+    """Creates pandas dataframe with img cluster statistics.
+
+    Parameters
+    ----------
+    stat_img : Niimg-like object,
+       statistical image (presumably in z scale)
+
+    stat_threshold: float, optional
+        cluster forming threshold (either a p-value or z-scale value)
+
+    cluster_threshold : int, optional
+        cluster size threshold
+
+    Returns
+    -------
+    Pandas dataframe with img clusters
+    """
+    cols = ['Cluster ID', 'X', 'Y', 'Z', 'Peak Stat', 'Cluster Size (mm3)']
+    stat_map = stat_img.get_data()
+    conn_mat = _get_conn(connectivity)
+    voxel_size = np.prod(stat_img.get_header().get_zooms())
+
+    # Binarize
+    binarized = stat_map > stat_threshold
+    binarized = binarized.astype(int)
+
+    # If the stat threshold is too high simply return an empty dataframe
+    if np.sum(binarized) == 0:
+        warnings.warn('Attention: No clusters with stat higher than %f' %
+                      stat_threshold)
+        return pd.DataFrame(columns=cols)
+
+    # Extract connected components above cluster size threshold
+    label_map = meas.label(binarized, conn_mat)[0]
+    clust_ids = sorted(list(np.unique(label_map)[1:]))
+    for c_val in clust_ids:
+        if cluster_threshold is not None and np.sum(label_map == c_val) < cluster_threshold:
+            stat_map[label_map == c_val] = 0
+            binarized[label_map == c_val] = 0
+
+    # If the cluster threshold is too high simply return an empty dataframe
+    # this checks for stats higher than threshold after small clusters
+    # were removed from stat_map
+    if np.sum(stat_map > stat_threshold) == 0:
+        warnings.warn('Attention: No clusters with more than %d voxels' %
+                      cluster_threshold)
+        return pd.DataFrame(columns=cols)
+
+    # Now re-label and create table
+    label_map = meas.label(binarized, conn_mat)[0]
+    clust_ids = sorted(list(np.unique(label_map)[1:]))
+    peak_vals = np.array([np.max(stat_map * (label_map == c)) for c in clust_ids])
+    clust_order = (-peak_vals).argsort()  # Sort by descending max value
+    clust_ids = [clust_ids[c] for c in clust_order]
+
+    rows = []
+    for c_id, c_val in enumerate(clust_ids):
+        cluster_mask = label_map == c_val
+        masked_data = stat_map * cluster_mask
+
+        # k
+        cluster_size_vox = np.sum(cluster_mask)
+        cluster_size_mm = int(cluster_size_vox * voxel_size)
+
+        # xyz and val
+        def _get_val(row, input_arr):
+            """Small function for extracting values from array based on index.
+            """
+            i, j, k = row
+            return input_arr[i, j, k]
+
+        subpeak_dist = int(np.round(8. / np.cbrt(voxel_size)))  # 8mm dist
+        subpeak_ijk = peak_local_max(masked_data, min_distance=subpeak_dist, num_peaks=4)
+        subpeak_vals = np.apply_along_axis(arr=subpeak_ijk, axis=1,
+                                           func1d=_get_val,
+                                           input_arr=masked_data)
+        order = (-subpeak_vals).argsort()
+        subpeak_ijk = subpeak_ijk[order, :]
+        subpeak_xyz = np.asarray(
+            coord_transform(
+                subpeak_ijk[:, 0], subpeak_ijk[:, 1], subpeak_ijk[:, 2],
+                stat_img.affine)).tolist()
+        subpeak_xyz = np.array(subpeak_xyz).T
+        subpeak_vals = subpeak_vals[order]
+
+        for subpeak in range(len(subpeak_vals)):
+            if subpeak == 0:
+                row = [c_id+1,
+                       subpeak_xyz[subpeak, 0], subpeak_xyz[subpeak, 1], subpeak_xyz[subpeak, 2],
+                       subpeak_vals[subpeak], cluster_size_mm]
+            else:
+                sp_id = '{0}{1}'.format(c_id+1, ascii_lowercase[subpeak-1])
+                row = [sp_id,
+                       subpeak_xyz[subpeak, 0], subpeak_xyz[subpeak, 1], subpeak_xyz[subpeak, 2],
+                       subpeak_vals[subpeak], '']
+            rows += [row]
+    df = pd.DataFrame(columns=cols, data=rows)
+    df.set_index('Cluster ID', inplace=True)
+    return df
 
 
 def compare_niimgs(ref_imgs, src_imgs, masker, plot_hist=True, log=True,
