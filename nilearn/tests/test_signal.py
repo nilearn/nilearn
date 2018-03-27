6a153f87b3c83460f0c6a7675e9c3caab908cb50
Pradeep Reddy Raamana <raamana@gmail.com>
2018-03-27 17:05:43 -0400

    PEP compliance

--- nilearn/tests/test_signal.py
+++ nilearn/tests/test_signal.py
@@ -143,7 +143,7 @@ def test_butterworth():
                                 low_pass=low_pass, high_pass=high_pass,
                                 copy=True)
     np.testing.assert_almost_equal(data, data_original)
-    np.testing.assert_(id(out1)!=id(data_original))
+    np.testing.assert_(id(out1) != id(data_original))
 
     # check that multiple- and single-timeseries filtering do the same thing.
     np.testing.assert_almost_equal(out1[:, 0], out_single)
@@ -291,24 +291,24 @@ def test_clean_t_r():
     """Different TRs must produce different results after filtering"""
 
     # n_features  Must be higher than 500
-    for n_samples, n_features in zip(( 34,  42, 100),
+    for n_samples, n_features in zip((34, 42, 100),
                                      (501, 647, 786)):
         x_orig = generate_signals_plus_trends(n_features=n_features, n_samples=n_samples)
 
-        random_tr_list1 = np.round(np.random.rand(10)*10, decimals=2)
-        random_tr_list2 = np.round(np.random.rand(10)*10, decimals=2)
+        random_tr_list1 = np.round(np.random.rand(10) * 10, decimals=2)
+        random_tr_list2 = np.round(np.random.rand(10) * 10, decimals=2)
         for tr1, tr2 in zip(random_tr_list1, random_tr_list2):
 
-            low_pass_freq_list  = tr1* np.array([1.0 / 100, 1.0 / 110, 1.0 / 128])
-            high_pass_freq_list = tr1* np.array([1.0 / 210, 1.0 / 190, 1.0 / 175])
+            low_pass_freq_list = tr1 * np.array([1.0 / 100, 1.0 / 110, 1.0 / 128])
+            high_pass_freq_list = tr1 * np.array([1.0 / 210, 1.0 / 190, 1.0 / 175])
 
             for low_cutoff, high_cutoff in zip(low_pass_freq_list,
                                                high_pass_freq_list):
 
                 det_one_tr = nisignal.clean(x_orig, t_r=tr1, low_pass=low_cutoff,
                                             high_pass=high_cutoff)
-                det_diff_tr  = nisignal.clean(x_orig, t_r=tr2, low_pass=low_cutoff,
-                                            high_pass=high_cutoff)
+                det_diff_tr = nisignal.clean(x_orig, t_r=tr2, low_pass=low_cutoff,
+                                             high_pass=high_cutoff)
 
                 if not np.isclose(tr1, tr2, atol=0.3):
                     msg = 'results do not differ for different TRs: {} and {} ' \
