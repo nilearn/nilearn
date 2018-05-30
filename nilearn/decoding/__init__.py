54834bbc4d40ecaa8f70c80389d48c385a39b5e7
Sylvain Takerkart <sylvain.takerkart@univ-amu.fr>
2018-05-30 14:18:53 +0200

    Updated decoding/__init__.py

--- nilearn/decoding/__init__.py
+++ nilearn/decoding/__init__.py
@@ -2,8 +2,8 @@
 Decoding tools and algorithms.
 """
 
-
 from nilearn.decoding.searchlight import SearchLight
+from nilearn.decoding.surf_searchlight import SurfSearchLight
 from nilearn.decoding.space_net import SpaceNetClassifier, SpaceNetRegressor
 from nilearn.decoding.decoder import (Decoder, DecoderRegressor,
                                       fREMClassifier, fREMRegressor)
