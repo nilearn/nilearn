082ac24f7faa0d7c063d686ee0e0d7c3b544d6b6
Sylvain Takerkart <sylvain.takerkart@univ-amu.fr>
2018-05-30 14:18:53 +0200

    Updated decoding/__init__.py

--- nilearn/decoding/__init__.py
+++ nilearn/decoding/__init__.py
@@ -3,6 +3,7 @@ Decoding tools and algorithms.
 """
 
 from .searchlight import SearchLight
+from .surf_searchlight import SurfSearchLight
 from .space_net import SpaceNetClassifier, SpaceNetRegressor
 
-__all__ = ['SearchLight', 'SpaceNetClassifier', 'SpaceNetRegressor']
+__all__ = ['SurfSearchLight', 'SearchLight', 'SpaceNetClassifier', 'SpaceNetRegressor']
