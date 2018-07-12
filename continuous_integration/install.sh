c7caad87aea0d6fae9f1d311be32f9f3e78965de
kchawla-pi <kc.insight.pi@gmail.com>
2018-07-12 16:00:33 +0200

    Moved wget statement from install.sh to config.yml

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -61,9 +61,9 @@ create_new_conda_env() {
 
     # Use the miniconda installer for faster download / install of conda
     # itself
-    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
-        -O ~/miniconda.sh
-    chmod +x ~/miniconda.sh && ~/miniconda.sh -b
+#    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
+#        -O ~/miniconda.sh
+#    chmod +x ~/miniconda.sh && ~/miniconda.sh -b
     export PATH=$HOME/miniconda2/bin:$PATH
     echo $PATH
     conda update --quiet --yes conda
