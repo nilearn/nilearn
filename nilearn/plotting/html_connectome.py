e69e40cecd6a1da0a832bf3458780b6a98f0a7b2
Jerome Dockes <jerome@dockes.org>
2018-07-12 10:01:09 +0200

    refactoring

--- nilearn/plotting/html_connectome.py
+++ nilearn/plotting/html_connectome.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy import sparse
 from nilearn import datasets
 
-from .html_surface import (add_js_lib, HTMLDocument, to_plotly,
+from .html_surface import (add_js_lib, SurfaceView, to_plotly,
                            _encode, colorscale, cm)
 
 
@@ -36,7 +36,6 @@ def _get_connectome(adjacency_matrix, coords, threshold=None,
     adjacency_matrix = adjacency_matrix.copy()
     colors, vmin, vmax, cmap, norm, abs_threshold = colorscale(
         cmap, adjacency_matrix.ravel(), threshold=threshold)
-    colors = json.loads(colors)
     connectome['colorscale'] = colors
     connectome['cmin'], connectome['cmax'] = float(vmin), float(vmax)
     if threshold is not None:
@@ -47,7 +46,7 @@ def _get_connectome(adjacency_matrix, coords, threshold=None,
     padded = np.zeros(len(d) * 3, dtype='<f4')
     padded[::3] = d
     padded[1::3] = d
-    connectome["_a_c"] = _encode(padded)
+    connectome["_c_c"] = _encode(padded)
     c = coords[idx]
     x, y, z = c.T
     for coord, cname in [(x, "x"), (y, "y"), (z, "z")]:
@@ -55,13 +54,13 @@ def _get_connectome(adjacency_matrix, coords, threshold=None,
         padded_coord[::3] = coord[::2]
         padded_coord[1::3] = coord[1::2]
         coord = _encode(padded_coord)
-        connectome["_a_{}".format(cname)] = coord
+        connectome["_c_{}".format(cname)] = coord
     return connectome
 
 
 def view_connectome(adjacency_matrix, coords, threshold=None, embed_js=True,
                     cmap=cm.cold_hot):
-    mesh = datasets.fetch_surf_fsaverage5()
+    mesh = datasets.fetch_surf_fsaverage()
     mesh_info = {}
     mesh_info["connectome"] = _get_connectome(
         adjacency_matrix, coords, threshold=threshold, cmap=cmap)
@@ -69,6 +68,6 @@ def view_connectome(adjacency_matrix, coords, threshold=None, embed_js=True,
         mesh_info[hemi] = to_plotly(mesh[hemi])
     as_json = json.dumps(mesh_info)
     as_html = _get_html_template().replace(
-        'INSERT_STAT_MAP_JSON_HERE', as_json)
+        'INSERT_CONNECTOME_JSON_HERE', as_json)
     as_html = add_js_lib(as_html, embed_js=embed_js)
-    return HTMLDocument(as_html)
+    return SurfaceView(as_html)
