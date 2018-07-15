e237b13bcfcf17236ed8c0685338ccc87e28e437
Jerome Dockes <jerome@dockes.org>
2018-07-15 21:36:45 +0200

    add encode and view stat map

---
+++ nilearn/plotting/html_stat_map.py
@@ -0,0 +1,30 @@
+import os
+import tempfile
+import base64
+
+from nilearn import image, datasets
+
+from .html_surface import _get_html_template
+
+
+def _encode_nii(img):
+    fd, temp = tempfile.mkstemp(suffix='.nii')
+    os.close(fd)
+    try:
+        img.to_filename(temp)
+        with open(temp, 'rb') as f:
+            binary = f.read()
+            encoded = base64.b64encode(binary).decode('utf-8')
+            return encoded
+    finally:
+        os.remove(temp)
+
+
+def view_stat_map(stat_map_img):
+    mni = datasets.load_mni152_template()
+    stat_map_img = image.resample_to_img(stat_map_img, mni)
+    html = _get_html_template('stat_map_template.html')
+    html = html.replace('INSERT_STAT_MAP_DATA_HERE', _encode_nii(stat_map_img))
+    html = html.replace('INSERT_MNI_DATA_HERE', _encode_nii(mni))
+    with open('/tmp/stat_map.html', 'wb') as f:
+        f.write(html.encode('utf-8'))
