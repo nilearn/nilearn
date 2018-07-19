35b565a2717391c49e3e241f5479d7868c16f0bb
SylvainLan <sylvain.lannuzel@student.ecp.fr>
2018-07-19 09:01:42 +0200

    add test for compute_multi_gray_matter_mask

--- nilearn/tests/test_masking.py
+++ nilearn/tests/test_masking.py
@@ -14,6 +14,7 @@ from nibabel import Nifti1Image
 from nilearn import masking
 from nilearn.masking import (compute_epi_mask, compute_multi_epi_mask,
                              compute_background_mask, compute_gray_matter_mask,
+                             compute_multi_gray_matter_mask,
                              unmask, _unmask_3d, _unmask_4d, intersect_masks,
                              MaskWarning)
 from nilearn._utils.testing import (write_tmp_imgs, assert_raises_regex)
@@ -112,6 +113,15 @@ def test_compute_gray_matter_mask():
         compute_gray_matter_mask(image, threshold=1)
     assert_equal(len(w), 1)
     assert_true(isinstance(w[0].message, masking.MaskWarning))
+
+    # Check that masks obtained from same FOV are the same
+    img1 = Nifti1Image(np.full((9, 9, 9), np.random.rand()), np.eye(4))
+    img2 = Nifti1Image(np.full((9, 9, 9), np.random.rand()), np.eye(4))
+
+    mask_img1 = compute_gray_matter_mask(img1)
+    mask_img2 = compute_gray_matter_mask(img2)
+    np.testing.assert_array_equal(mask_img1.get_data(),
+                                  mask_img2.get_data())
 
 
 def test_apply_mask():
@@ -399,6 +409,15 @@ def test_compute_multi_epi_mask():
                                       target_affine=np.eye(4),
                                       target_shape=(4, 4, 1))
     assert_array_equal(mask_ab, mask_ab_.get_data())
+
+
+def test_compute_multi_gray_matter_mask():
+    assert_raises(TypeError, compute_multi_gray_matter_mask, [])
+
+    # Check error raised if images with different shapes are given as input
+    imgs = [Nifti1Image(np.ones((9, 9, 9)), np.eye(4)),
+            Nifti1Image(np.ones((9, 9, 8)), np.eye(4))]
+    assert_raises(ValueError, compute_multi_gray_matter_mask, imgs)
 
 
 def test_error_shape(random_state=42, shape=(3, 5, 7, 11)):
