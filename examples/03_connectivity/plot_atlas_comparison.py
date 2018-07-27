c5dd2c7259b2a51a02af6c587f66c331bcdc9a0e
KamalakerDadi <dkamalakarreddy@gmail.com>
2018-07-27 11:08:08 +0200

    Update an example: written more focus on cut coords extraction

--- examples/03_connectivity/plot_atlas_comparison.py
+++ examples/03_connectivity/plot_atlas_comparison.py
@@ -1,8 +1,16 @@
 """
-Extracting resting-state signals from different atlases for comparison
-======================================================================
+Comparing connectomes on different reference atlases
+====================================================
 
-In this example :class:`nilearn.input_data.NiftiLabelsMasker` is used to
+In this example, an important last part is to turn a parcellation into
+connectome for visualization. This requires choosing centers for each parcel
+or network, via :func:`nilearn.plotting.find_parcellation_cut_coords` for
+parcellation based on labels and
+:func:`nilearn.plotting.find_probabilistic_atlas_cut_coords` for
+parcellation based on probabilistic values.
+
+As an intermediary step, we make use of
+:class:`nilearn.input_data.NiftiLabelsMasker` to
 extract time series from nifti objects using different parcellation atlases.
 
 The time series of all subjects of the ADHD Dataset are concatenated to create
@@ -11,14 +19,14 @@ parcel-wise correlation matrices for each atlas.
 # author: Amadeus Kanaan
 
 """
+# General imports
 import numpy as np
-from nilearn import datasets
-from nilearn.input_data import NiftiLabelsMasker
-from nilearn import plotting
 
 ####################################################################
 # Load atlases
 # -------------
+from nilearn import datasets
+
 destrieux = datasets.fetch_atlas_destrieux_2009()
 yeo = datasets.fetch_atlas_yeo_2011()
 harvard_oxford = datasets.fetch_atlas_harvard_oxford('cort-maxprob-thr25-2mm')
@@ -28,11 +36,10 @@ print('Yeo atlas nifti image (3D) with 17 parcels and liberal mask is located '
       'at: %s' % yeo['thick_17'])
 print('Harvard Oxford atlas nifti image (3D) thresholded at .25 is located '
       'at: %s' % harvard_oxford['maps'])
-# Store atlases and thresholds in a dictionary. Atlases are for extracting
-# coordinates and threshold for displaying the strength of the connectivity.
-atlases_and_thresholds = {'Destrieux Atlas (struct)': [destrieux['maps'], '97%'],
-                          'Yeo Atlas 17 thick (func)': [yeo['thick_17'], '70%'],
-                          'Harvard Oxford > 25% (struct)': [harvard_oxford['maps'], '90%']}
+# Store atlases in a dictionary. Atlases are for extracting coordinates.
+atlases = {'Destrieux Atlas (struct)': destrieux['maps'],
+           'Yeo Atlas 17 thick (func)': yeo['thick_17'],
+           'Harvard Oxford > 25% (struct)': harvard_oxford['maps']}
 
 #########################################################################
 # Load functional data
@@ -47,7 +54,10 @@ print('Counfound csv files (one per subject) are located at : %r'
 ##########################################################################
 # Iterate over fetched atlases to extract coordinates
 # ---------------------------------------------------
-for name, (atlas, threshold) in sorted(atlases_and_thresholds.items()):
+from nilearn.input_data import NiftiLabelsMasker
+from nilearn import plotting
+
+for name, atlas in sorted(atlases.items()):
     # create masker to extract functional data within atlas parcels
     masker = NiftiLabelsMasker(labels_img=atlas,
                                standardize=True,
@@ -66,8 +76,8 @@ for name, (atlas, threshold) in sorted(atlases_and_thresholds.items()):
     # grab center coordinates for atlas labels
     coordinates = plotting.find_parcellation_cut_coords(atlas)
 
-    # plot connectome
+    # plot connectome with 80% edge strength in the connectivity
     plotting.plot_connectome(correlation_matrix, coordinates,
-                             edge_threshold=threshold, title=name)
+                             edge_threshold="80%", title=name)
 
 plotting.show()
