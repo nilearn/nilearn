2092077330069158703433c9dd6f2ab2b01d6801
SylvainLan <sylvain.lannuzel@student.ecp.fr>
2018-08-02 13:01:09 +0200

    ENH add dtype for MultiNiftiMasker

--- nilearn/input_data/multi_nifti_masker.py
+++ nilearn/input_data/multi_nifti_masker.py
@@ -269,8 +269,7 @@ class MultiNiftiMasker(NiftiMasker, CacheMixin):
                                        target_fov=target_fov,
                                        memory=self.memory,
                                        memory_level=self.memory_level,
-                                       verbose=self.verbose,
-                                       dtype=self.dtype)
+                                       verbose=self.verbose)
 
         if confounds is None:
             confounds = itertools.repeat(None, len(imgs_list))
@@ -293,6 +292,7 @@ class MultiNiftiMasker(NiftiMasker, CacheMixin):
                           verbose=self.verbose,
                           confounds=cfs,
                           copy=copy,
+                          dtype=self.dtype
                           )
             for imgs, cfs in izip(niimg_iter, confounds))
         return data
