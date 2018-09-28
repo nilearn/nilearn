06f2723d4f8ecf2de40804762e91e782c1ee251d
rschmaelzle <schmaelz@msu.edu>
2018-09-27 21:26:38 -0400

    Fix paradigm key checks for duration, trial_type, modulation

--- nistats/experimental_paradigm.py
+++ nistats/experimental_paradigm.py
@@ -48,18 +48,16 @@ def check_paradigm(paradigm):
     """
     if 'onset' not in paradigm.keys():
         raise ValueError('The provided paradigm has no onset key')
+    if 'duration' not in paradigm.keys():
+        raise ValueError('The provided paradigm has no duration key')
 
     onset = np.array(paradigm['onset'])
+    duration = np.array(paradigm['duration']).astype(np.float)
     n_events = len(onset)
     trial_type = np.repeat('dummy', n_events)
-    duration = np.zeros(n_events)
     modulation = np.ones(n_events)
-    if 'trial_type' in paradigm.keys():
-        warnings.warn("'trial_type' key found in the given paradigm.")
-        trial_type = np.array(paradigm['trial_type'])
-    if 'duration' in paradigm.keys():
-        warnings.warn("'duration' key found in the given paradigm.")
-        duration = np.array(paradigm['duration']).astype(np.float)
+    if 'trial_type' not in paradigm.keys():
+        warnings.warn("'trial_type' key not found in the given paradigm.")
     if 'modulation' in paradigm.keys():
         warnings.warn("'modulation' key found in the given paradigm.")
         modulation = np.array(paradigm['modulation']).astype(np.float)
