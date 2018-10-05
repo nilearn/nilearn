b1b859af8437282d897358dee561c6502bf0376f
Kshitij Chawla <kc.insight.pi@gmail.com>
2018-10-05 15:01:43 +0200

    Example uses pd.read_table() for .tsv, as has correct default args; much cleaner

--- examples/02_first_level_models/plot_localizer_surface_analysis.py
+++ examples/02_first_level_models/plot_localizer_surface_analysis.py
@@ -29,7 +29,7 @@ slice_time_ref = 0.5
 from nistats.datasets import fetch_localizer_first_level
 data = fetch_localizer_first_level()
 paradigm_file = data.paradigm
-paradigm = pd.read_csv(paradigm_file, sep='\t', index_col=None)
+paradigm = pd.read_table(paradigm_file)
 fmri_img = data.epi_img
 
 #########################################################################
