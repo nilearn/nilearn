4dc07ce2fd5a29dedf91d979da693f189cc04903
bthirion <bertrand.thirion@inria.fr>
2018-10-07 22:34:49 +0200

    fixed failing test

--- nistats/tests/test_datasets.py
+++ nistats/tests/test_datasets.py
@@ -162,13 +162,13 @@ def test_make_bids_compliant_localizer_first_level_paradigm_file():
     
     def _expected_output_data_from_test_file():
         file_data = [
-            ['calculvideo', 0.0],
-            ['calculvideo', 2.400000095],
-            ['damier_H', 8.699999809],
-            ['clicDaudio', 11.39999961],
+            ['calculvideo', 0.0, 1.0],
+            ['calculvideo', 2.400000095, 1.0],
+            ['damier_H', 8.699999809, 1.0],
+            ['clicDaudio', 11.39999961, 1.0],
             ]
         file_data = pd.DataFrame(file_data)
-        file_data.columns = ['trial_type', 'onset']
+        file_data.columns = ['trial_type', 'onset', 'duration']
         return file_data
     
     def run_test():
