e2d00acaebdd15f7465923d620b6b920a0ffe0c6
bthirion <bertrand.thirion@inria.fr>
2018-10-09 00:36:19 +0200

    Fixed erroneous tests

--- nistats/tests/test_first_level_model.py
+++ nistats/tests/test_first_level_model.py
@@ -232,7 +232,7 @@ def test_first_level_model_design_creation():
         FUNCFILE = FUNCFILE[0]
         func_img = load(FUNCFILE)
         # basic test based on basic_paradigm and glover hrf
-        t_r = 1.0
+        t_r = 10.0
         slice_time_ref = 0.
         events = basic_paradigm()
         model = FirstLevelModel(t_r, slice_time_ref, mask=mask,
@@ -259,7 +259,7 @@ def test_first_level_model_glm_computation():
         FUNCFILE = FUNCFILE[0]
         func_img = load(FUNCFILE)
         # basic test based on basic_paradigm and glover hrf
-        t_r = 1.0
+        t_r = 10.0
         slice_time_ref = 0.
         events = basic_paradigm()
         # ols case
@@ -284,7 +284,7 @@ def test_first_level_model_contrast_computation():
         FUNCFILE = FUNCFILE[0]
         func_img = load(FUNCFILE)
         # basic test based on basic_paradigm and glover hrf
-        t_r = 1.0
+        t_r = 10.0
         slice_time_ref = 0.
         events = basic_paradigm()
         # ols case
