ac3e033a0517074fa29728553575a99e9ecf1f27
kchawla-pi <kc.insight.pi@gmail.com>
2018-10-11 12:05:09 +0200

    Removed the non-functioning test of nilearn import python2 warning
     - Removed __main__().

--- nilearn/tests/test_py2_deprecation_warning.py
+++ nilearn/tests/test_py2_deprecation_warning.py
@@ -10,18 +10,3 @@ def test_py2_deprecation_warning():
     with warnings.catch_warnings(record=True) as raised_warnings:
         _py2_deprecation_warning()
         assert_true(raised_warnings[0].category is DeprecationWarning)
-
-
-# def test_py2_warning_at_nilearn_import():
-#     with warnings.catch_warnings(record=True) as raised_warnings:
-#         warnings.simplefilter('always')
-#         from nilearn.plotting import view_stat_map
-#         if LooseVersion(sys.version) < LooseVersion('3'):
-#             assert_true(raised_warnings[0].category is DeprecationWarning)
-#         else:
-#             assert raised_warnings is None
-
-
-if __name__ == '__main__':
-    test_py2_deprecation_warning()
-    # test_py2_warning_at_nilearn_import()
