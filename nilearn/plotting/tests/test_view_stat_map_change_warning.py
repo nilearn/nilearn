cdd4f5ef9001f673af46d9f58b8e9580c3d8492a
kchawla-pi <kc.insight.pi@gmail.com>
2018-10-13 11:47:11 +0200

    Installed nilearn source in current dir to fix import failure in TravisCI
     -  Reformatted & reordered imports for cleanliness.

--- nilearn/plotting/tests/test_view_stat_map_change_warning.py
+++ nilearn/plotting/tests/test_view_stat_map_change_warning.py
@@ -1,8 +1,8 @@
 import warnings
 
-from nilearn._utils.testing import generate_fake_fmri
 from nilearn.plotting.html_stat_map import (_view_stat_map_change_warning,
                                             view_stat_map)
+from nilearn._utils.testing import generate_fake_fmri
 
 
 def test_view_stat_map_change_warning():
@@ -16,11 +16,6 @@ def test_view_stat_map_raises_change_warning():
     with warnings.catch_warnings(record=True) as raised_warnings:
         view_stat_map(stat_map_img=fake_4D_mri[0])
         assert raised_warnings[0].category is FutureWarning
-        
-        
-if __name__ == '__main__':
-    test_view_stat_map_change_warning()
-    test_view_stat_map_raises_change_warning()
 
 
 
