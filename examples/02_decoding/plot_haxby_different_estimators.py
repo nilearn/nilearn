43e6ecce6bcf1b2092f6c0c856c4f75018a2a35f
kchawla-pi <kc.insight.pi@gmail.com>
2018-10-13 15:21:31 +0200

    Fixed FutureWarning from LogicsticReggression about default solver changing
     - Explicitly specified linera_model.LogisticRegression(solver='liblinear') .

--- examples/02_decoding/plot_haxby_different_estimators.py
+++ examples/02_decoding/plot_haxby_different_estimators.py
@@ -71,14 +71,18 @@ svm_cv = GridSearchCV(SVC(C=1., kernel="linear"),
                       param_grid={'C': [.1, .5, 1., 5., 10., 50., 100.]},
                       scoring='f1', n_jobs=1, cv=3, iid=False)
 
-logistic_cv = GridSearchCV(LogisticRegression(C=1., penalty="l1"),
-                           param_grid={'C': [.1, .5, 1., 5., 10., 50., 100.]},
-                           scoring='f1', cv=3, iid=False)
-logistic_l2_cv = GridSearchCV(LogisticRegression(C=1., penalty="l2"),
-                              param_grid={
-                                  'C': [.1, .5, 1., 5., 10., 50., 100.]
-                              },
-                              scoring='f1', cv=3, iid=False)
+logistic_cv = GridSearchCV(
+        LogisticRegression(C=1., penalty="l1", solver='liblinear'),
+        param_grid={'C': [.1, .5, 1., 5., 10., 50., 100.]},
+        scoring='f1', cv=3, iid=False,
+        )
+logistic_l2_cv = GridSearchCV(
+        LogisticRegression(C=1., penalty="l2", solver='liblinear'),
+        param_grid={
+            'C': [.1, .5, 1., 5., 10., 50., 100.]
+            },
+        scoring='f1', cv=3, iid=False,
+        )
 
 # The ridge classifier has a specific 'CV' object that can set it's
 # parameters faster than using a GridSearchCV
