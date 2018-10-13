1f1640a19dffe33918e5d33203b5214f4e5b4c1e
kchawla-pi <kc.insight.pi@gmail.com>
2018-10-13 18:57:25 +0200

    Cleaner python version check in the test

--- nilearn/tests/test_init.py
+++ nilearn/tests/test_init.py
@@ -1,14 +1,12 @@
 import sys
 import warnings
 
-from distutils.version import LooseVersion
-
 from nose.tools import assert_true
 from nilearn import _py2_deprecation_warning
 
 
 def test_py2_deprecation_warning():
-    if LooseVersion(sys.version) < LooseVersion('3'):
+    if sys.version_info.major == 2:
         with warnings.catch_warnings(record=True) as raised_warnings:
             _py2_deprecation_warning()
             assert_true(raised_warnings[0].category is DeprecationWarning)
