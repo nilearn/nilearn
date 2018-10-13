37ae36fe9699d6090daea23f21200644a664cb9d
kchawla-pi <kc.insight.pi@gmail.com>
2018-10-13 17:22:23 +0200

    Using stdli's distutils for python version checks
     - Less code coupling

--- nilearn/tests/test_py2_deprecation_warning.py
+++ nilearn/tests/test_py2_deprecation_warning.py
@@ -1,12 +1,14 @@
+import sys
 import warnings
 
+from distutils.version import LooseVersion
+
 from nose.tools import assert_true
-from sklearn.externals import six
 from nilearn import _py2_deprecation_warning
 
 
 def test_py2_deprecation_warning():
-    if six.PY2:
+    if LooseVersion(sys.version) < LooseVersion('3'):
         with warnings.catch_warnings(record=True) as raised_warnings:
             _py2_deprecation_warning()
             assert_true(raised_warnings[0].category is DeprecationWarning)
