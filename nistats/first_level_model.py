6f4d80d52ab40be221f665dd620e2c1e22d31808
btnguyen <tuan-binh.nguyen@inria.fr>
2018-10-16 17:08:21 +0200

    remove unused import

--- nistats/first_level_model.py
+++ nistats/first_level_model.py
@@ -20,7 +20,7 @@ import json
 
 import numpy as np
 import pandas as pd
-from nibabel import Nifti1Image, AnalyzeImage
+from nibabel import Nifti1Image
 
 from sklearn.base import BaseEstimator, TransformerMixin, clone
 from sklearn.externals.joblib import Memory
