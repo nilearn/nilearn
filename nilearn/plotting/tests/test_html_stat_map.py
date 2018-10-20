fa5c4c1d23c002760188221457ee746c91537710
pbellec <pierre.bellec@gmail.com>
2018-10-19 22:51:12 -0400

    Fixed flake8 warnings.

--- nilearn/plotting/tests/test_html_stat_map.py
+++ nilearn/plotting/tests/test_html_stat_map.py
@@ -1,10 +1,10 @@
 import warnings
 
-from nose.tools import assert_equal
 import numpy as np
 
 from nilearn import datasets, image
 from nilearn.plotting import html_stat_map
+
 
 def _check_html(html):
     """ Check the presence of some expected code in the html
@@ -19,11 +19,12 @@ def _assert_warnings_in(list1, list2):
     """
     diff = list(list1 - list2)
     try:
-        assert(len(diff)==0)
+        assert(len(diff) == 0)
     except AssertionError as e:
         e.args += ('The following warnings were not expected',
                    diff)
         raise
+
 
 def test_view_stat_map():
     mni = datasets.load_mni152_template()
@@ -41,10 +42,10 @@ def test_view_stat_map():
         html = html_stat_map.view_stat_map(img, threshold=2., vmax=4.)
         _check_html(html)
         html = html_stat_map.view_stat_map(img, symmetric_cmap=False)
-        #img_4d = image.new_img_like(img, img.get_data()[:, :, :, np.newaxis])
-        #assert len(img_4d.shape) == 4
-        #html = html_stat_map.view_stat_map(img_4d, threshold=2., vmax=4.)
-        #_check_html(html)
+        # img_4d = image.new_img_like(img, img.get_data()[:, :, :, np.newaxis])
+        # assert len(img_4d.shape) == 4
+        # html = html_stat_map.view_stat_map(img_4d, threshold=2., vmax=4.)
+        # _check_html(html)
     warning_categories = set(warning_.category for warning_ in w)
     expected_categories = set([FutureWarning, UserWarning,
                                DeprecationWarning])
