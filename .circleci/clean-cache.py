73db9b75c1dd4403787e382faa949d8c1be13f89
kchawla-pi <kc.insight.pi@gmail.com>
2018-12-03 13:28:45 +0100

    Cleaned up some python code, fixed the incorrect circleci log upload

--- .circleci/clean-cache.py
+++ .circleci/clean-cache.py
@@ -3,11 +3,14 @@
 import os
 from datetime import datetime as dt
 
+def update_cache_timestamp():
+    timestamp_dirpath = os.path.dirname(__file__)
+    timestamp_filename = 'manual-cache-timestamp'
+    timestamp_filepath = os.path.join(timestamp_dirpath, timestamp_filename)
+    utc_now_timestamp = dt.utcnow()
+    with open(timestamp_filepath, 'w') as write_obj:
+        write_obj.write(str(utc_now_timestamp))
 
-'nilearn/.circleci' in __file__
-timestamp_dirpath = os.path.dirname(__file__)
-timestamp_filename = 'manual-cache-timestamp'
-timestamp_filepath = os.path.join(timestamp_dirpath, timestamp_filename)
-utc_now_timestamp = dt.utcnow()
-with open(timestamp_filepath, 'w') as write_obj:
-    write_obj.write(str(utc_now_timestamp))
+
+if __name__ == '__main__':
+    update_cache_timestamp()
