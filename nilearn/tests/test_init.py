18b3a934ab89c85b076ba5205a6beea8d061e524
kchawla-pi <kc.insight.pi@gmail.com>
2018-12-06 15:57:05 +0100

    Added tests for DeprecationWarnings for Python 3.4; increased CircleCI timeout
     - Increased CircleCI timeout to 7hrs due to recent increase in build time.

--- nilearn/tests/test_init.py
+++ nilearn/tests/test_init.py
@@ -2,11 +2,18 @@ import sys
 import warnings
 
 from nose.tools import assert_true
-from nilearn import _py2_deprecation_warning
+from nilearn import _py2_deprecation_warning, _py34_deprecation_warning
 
 
 def test_py2_deprecation_warning():
     if sys.version_info.major == 2:
         with warnings.catch_warnings(record=True) as raised_warnings:
             _py2_deprecation_warning()
+            assert_true(raised_warnings[0].category is DeprecationWarning)
+
+
+def test_py34_deprecation_warning():
+    if sys.version_info.major == 3 and sys.version_info.minor == 4:
+        with warnings.catch_warnings(record=True) as raised_warnings:
+            _py34_deprecation_warning()
             assert_true(raised_warnings[0].category is DeprecationWarning)
