735487eb1250b29a90df37c4d30bb12e40d8f69c
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-01-17 19:30:36 +0100

    Addressed design deficiencies in python deprecation functions & their tests
     - Simplifies testing the 2 deprecation functions.
     - Reduced redundant code.
     - Sets the stage for solving the glitch where importing Nistats in Py2, 3.4
     overrides the warnings filter of subsequent warnings & testing it.

--- nistats/tests/test_init.py
+++ nistats/tests/test_init.py
@@ -2,18 +2,37 @@ import sys
 import warnings
 
 from nose.tools import assert_true
-from nistats import _py2_deprecation_warning, _py34_deprecation_warning
 
 
 def test_py2_deprecation_warning():
-    if sys.version_info.major == 2:
-        with warnings.catch_warnings(record=True) as raised_warnings:
-            _py2_deprecation_warning()
-            assert_true(raised_warnings[0].category is DeprecationWarning)
+    from nistats import _py2_deprecation_warning
+    with warnings.catch_warnings(record=True) as raised_warnings:
+        _py2_deprecation_warning()
+    assert_true(raised_warnings[0].category is DeprecationWarning)
 
 
 def test_py34_deprecation_warning():
-    if sys.version_info.major == 3 and sys.version_info.minor == 4:
-        with warnings.catch_warnings(record=True) as raised_warnings:
-            _py34_deprecation_warning()
-            assert_true(raised_warnings[0].category is DeprecationWarning)
+    from nistats import _py34_deprecation_warning
+    with warnings.catch_warnings(record=True) as raised_warnings:
+        _py34_deprecation_warning()
+    assert_true(raised_warnings[0].category is DeprecationWarning)
+
+
+def test_python_deprecation_warnings():
+    from nistats import _python_deprecation_warnings
+    with warnings.catch_warnings(record=True) as raised_warnings:
+        _python_deprecation_warnings()
+    if sys.version_info.major == 2:
+        assert_true(raised_warnings[0].category is DeprecationWarning)
+        assert_true(
+                str(raised_warnings[0].message).startswith(
+                        'Python2 support is deprecated')
+                )
+    elif sys.version_info.major == 3 and sys.version_info.minor == 4:
+        assert_true(raised_warnings[0].category is DeprecationWarning)
+        assert_true(
+                str(raised_warnings[0].message).startswith(
+                        'Python 3.4 support is deprecated')
+                )
+    else:
+        assert_true(len(raised_warnings) == 0)
