5cb52e1f9f86e10fc8f77045d01021b81bb2b8a3
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-01-17 17:44:55 +0100

    Addressed design deficiencies in python deprecaton functions & their tests
     - Simplifies testing the 2 deprecation functions.
     - Reduces redundant code.
     - Sets the stage for solving the glitch where importing Nilearn overrides
     the warnings filter of subsequent warnings & testing it.

--- nilearn/tests/test_init.py
+++ nilearn/tests/test_init.py
@@ -1,4 +1,3 @@
-import sys
 import warnings
 
 from nose.tools import assert_true
@@ -6,14 +5,12 @@ from nilearn import _py2_deprecation_warning, _py34_deprecation_warning
 
 
 def test_py2_deprecation_warning():
-    if sys.version_info.major == 2:
-        with warnings.catch_warnings(record=True) as raised_warnings:
-            _py2_deprecation_warning()
-            assert_true(raised_warnings[0].category is DeprecationWarning)
+    with warnings.catch_warnings(record=True) as raised_warnings:
+        _py2_deprecation_warning()
+        assert_true(raised_warnings[0].category is DeprecationWarning)
 
 
 def test_py34_deprecation_warning():
-    if sys.version_info.major == 3 and sys.version_info.minor == 4:
-        with warnings.catch_warnings(record=True) as raised_warnings:
-            _py34_deprecation_warning()
-            assert_true(raised_warnings[0].category is DeprecationWarning)
+    with warnings.catch_warnings(record=True) as raised_warnings:
+        _py34_deprecation_warning()
+        assert_true(raised_warnings[0].category is DeprecationWarning)
