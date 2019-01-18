5461274621d6c294f9b88dab205790a4d80eda81
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-01-18 14:31:22 +0100

    Fixed, simplified tests for python deprecation warnings
    - Warnings filter is now message specific.
    - Nistats import time warnings are silenced so as not to interfere with tests.

--- nistats/tests/test_init.py
+++ nistats/tests/test_init.py
@@ -4,22 +4,34 @@ import warnings
 from nose.tools import assert_true
 
 
-def test_py2_deprecation_warning():
+# import time warnings don't interfere with warning's tests
+with warnings.catch_warnings(record=True):
+    from nistats import _py34_deprecation_warning
     from nistats import _py2_deprecation_warning
+    from nistats import _python_deprecation_warnings
+
+
+def test_py2_deprecation_warning():
     with warnings.catch_warnings(record=True) as raised_warnings:
-        _py2_deprecation_warning()
+            _py2_deprecation_warning()
     assert_true(raised_warnings[0].category is DeprecationWarning)
+    assert_true(
+            str(raised_warnings[0].message).startswith(
+                    'Python2 support is deprecated')
+            )
 
 
 def test_py34_deprecation_warning():
-    from nistats import _py34_deprecation_warning
     with warnings.catch_warnings(record=True) as raised_warnings:
         _py34_deprecation_warning()
     assert_true(raised_warnings[0].category is DeprecationWarning)
+    assert_true(
+            str(raised_warnings[0].message).startswith(
+            'Python 3.4 support is deprecated')
+            )
 
 
 def test_python_deprecation_warnings():
-    from nistats import _python_deprecation_warnings
     with warnings.catch_warnings(record=True) as raised_warnings:
         _python_deprecation_warnings()
     if sys.version_info.major == 2:
@@ -36,3 +48,18 @@ def test_python_deprecation_warnings():
                 )
     else:
         assert_true(len(raised_warnings) == 0)
+
+
+def test_warnings_filter_scope():
+    """
+    Tests that warnings generated at Nistats import in Python 2, 3.4 envs
+    do not change the warnings filter for subsequent warnings.
+    """
+    with warnings.catch_warnings(record=True) as raised_warnings:
+        warnings.warn('Dummy warning 1')  # Will be raised.
+        warnings.filterwarnings("ignore", message="Dummy warning")
+        warnings.warn('Dummy warning 2')  # Will not be raised.
+        import nistats  # Irrespective of warning raised in py2, py3.4 ...
+        warnings.warn('Dummy warning 3')  # ...this should not be raised.
+    assert str(raised_warnings[0].message) == 'Dummy warning 1'
+    assert str(raised_warnings[-1].message) != 'Dummy warning 3'
