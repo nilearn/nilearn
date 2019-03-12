3516ba3162d2a123d1c7e110294ad76911f41e6b
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-03-12 14:05:16 +0100

    Sorted raised warnings in test to make the comparision order predictable

--- nilearn/tests/test_helpers.py
+++ nilearn/tests/test_helpers.py
@@ -55,6 +55,9 @@ def test_replace_parameters():
                                       )
     
     assert actual_output == expected_output
+
+    expected_warnings.sort()
+    raised_warnings.sort(key=lambda mem: str(mem.message))
     for raised_warning_, expected_warning_ in zip(raised_warnings,
                                                   expected_warnings):
         assert str(raised_warning_.message) == expected_warning_
@@ -100,6 +103,8 @@ def test_future_warn_deprecated_params():
                 lib_name='somelib',
                 kwargs=mock_input,
                 )
+    expected_warnings.sort()
+    raised_warnings.sort(key=lambda mem: str(mem.message))
     for raised_warning_, expected_warning_ in zip(raised_warnings,
                                                   expected_warnings
                                                   ):
