51f076c3faa4210d5f8ad766df2df72cfda4cbd9
chrplr <christophe@pallier.org>
2019-04-16 13:36:46 +0200

    cosmetic changes to fir example

--- examples/02_first_level_models/plot_fir_model.py
+++ examples/02_first_level_models/plot_fir_model.py
@@ -5,17 +5,18 @@ FIR models are used to estimate the hemodyamic response non-parametrically.
 The example below shows that they're good to do statistical inference
 even on fast event-related fMRI datasets.
 
-Specifically, the so-called 'localizer' dataset proposed by Pinel is
-downloaded, then analysed with a FIR model with 3 lags.
-4 main contrasts are estimated.
+Here, we demonstrate the use of a FIR model with 3 lags, computing 4 contrasts from a single subject dataset from the "Neurospin Localizer". It is a fast event related design: During 5 minutes, 80 events of the following types are presented : ['calculaudio', 'calculvideo'', 'clicDvideo', 'clicGaudio', 'clicGvideo', 'damier_H', 'damier_V', 'phraseaudio', 'phrasevideo']
 
 """
 
 #########################################################################
 # Grab localizer data
 
+import numpy as np
 import pandas as pd
 from nistats import datasets
+from nilearn import plotting
+import matplotlib.pyplot as plt
 
 data = datasets.fetch_localizer_first_level()
 fmri_img = data.epi_img
@@ -26,65 +27,63 @@ events = pd.read_table(events_file)
 #########################################################################
 # Automate contrast computation
 
-import numpy as np
+# import numpy as np
 
-def make_localizer_contrasts(design_matrix):
-    """ returns a dictionary of four contrasts, given the design matrix"""
+# def make_localizer_contrasts(design_matrix):
+#     """ returns a dictionary of four contrasts, given the design matrix"""
 
-    # first generate canonical contrasts 
-    contrast_matrix = np.eye(design_matrix.shape[1])
-    contrasts = dict([(column, contrast_matrix[i])
-                      for i, column in enumerate(design_matrix.columns)])
+#     # first generate canonical contrasts 
+#     contrast_matrix = np.eye(design_matrix.shape[1])
+#     contrasts = dict([(column, contrast_matrix[i])
+#                       for i, column in enumerate(design_matrix.columns)])
 
-    # Add more complex contrasts
-    contrasts['audio'] = (contrasts['clicDaudio']
-                          + contrasts['clicGaudio']
-                          + contrasts['calculaudio']
-                          + contrasts['phraseaudio']
-                          )
-    contrasts['video'] = (contrasts['clicDvideo']
-                          + contrasts['clicGvideo']
-                          + contrasts['calculvideo']
-                          + contrasts['phrasevideo']
-                          )
-    contrasts['computation'] = contrasts['calculaudio'] + contrasts['calculvideo']
-    contrasts['sentences'] = contrasts['phraseaudio'] + contrasts['phrasevideo']
+#     # Add more complex contrasts
+#     contrasts['audio'] = (contrasts['clicDaudio']
+#                           + contrasts['clicGaudio']
+#                           + contrasts['calculaudio']
+#                           + contrasts['phraseaudio']
+#                           )
+#     contrasts['video'] = (contrasts['clicDvideo']
+#                           + contrasts['clicGvideo']
+#                           + contrasts['calculvideo']
+#                           + contrasts['phrasevideo']
+#                           )
+#     contrasts['computation'] = contrasts['calculaudio'] + contrasts['calculvideo']
+#     contrasts['sentences'] = contrasts['phraseaudio'] + contrasts['phrasevideo']
 
-    # Short dictionary of more relevant contrasts
-    contrasts = {
-        'left-right': (contrasts['clicGaudio']
-                       + contrasts['clicGvideo']
-                       - contrasts['clicDaudio']
-                       - contrasts['clicDvideo']
-                       ),
-        'H-V': contrasts['damier_H'] - contrasts['damier_V'],
-        'audio-video': contrasts['audio'] - contrasts['video'],
-        'computation-sentences': (contrasts['computation'] -
-                                  contrasts['sentences']
-                                  ),
-    }
-    return contrasts
+#     # Short dictionary of more relevant contrasts
+#     contrasts = {
+#         'left-right': (contrasts['clicGaudio']
+#                        + contrasts['clicGvideo']
+#                        - contrasts['clicDaudio']
+#                        - contrasts['clicDvideo']
+#                        ),
+#         'H-V': contrasts['damier_H'] - contrasts['damier_V'],
+#         'audio-video': contrasts['audio'] - contrasts['video'],
+#         'computation-sentences': (contrasts['computation'] -
+#                                   contrasts['sentences']
+#                                   ),
+#     }
+#     return contrasts
 
-#########################################################################
-# Result plotting function
+# #########################################################################
+# # Result plotting function
 
-from nilearn import plotting
-import matplotlib.pyplot as plt
 
-def plot_contrast(first_level_model):
-    """ Given a first model, specify, enstimate and plot the main contrasts"""
-    design_matrix = first_level_model.design_matrices_[0]
-    # Call the contrast specification within the function
-    contrasts = make_localizer_contrasts(design_matrix)
-    fig = plt.figure(figsize=(11, 3))
-    # compute the per-contrast z-map
-    for index, (contrast_id, contrast_val) in enumerate(contrasts.items()):
-        ax = plt.subplot(1, len(contrasts), 1 + index)
-        z_map = first_level_model.compute_contrast(
-            contrast_val, output_type='z_score')
-        plotting.plot_stat_map(
-            z_map, display_mode='z', threshold=3.0, title=contrast_id, axes=ax,
-            cut_coords=1)
+# def plot_contrast(first_level_model):
+#     """ Given a first model, specify, estimate and plot the main contrasts"""
+#     design_matrix = first_level_model.design_matrices_[0]
+#     # Call the contrast specification within the function
+#     contrasts = make_localizer_contrasts(design_matrix)
+#     fig = plt.figure(figsize=(11, 3))
+#     # compute the per-contrast z-map
+#     for index, (contrast_id, contrast_val) in enumerate(contrasts.items()):
+#         ax = plt.subplot(1, len(contrasts), 1 + index)
+#         z_map = first_level_model.compute_contrast(
+#             contrast_val, output_type='z_score')
+#         plotting.plot_stat_map(
+#             z_map, display_mode='z', threshold=3.0, title=contrast_id, axes=ax,
+#             cut_coords=1)
 
 
 #########################################################################
@@ -96,7 +95,7 @@ def plot_contrast(first_level_model):
 from nistats.first_level_model import FirstLevelModel
 from nistats.reporting import plot_design_matrix, plot_contrast_matrix
 
-first_level_model = FirstLevelModel(t_r, hrf_model='fir', fir_delays=[1, 2, 3])
+first_level_model = FirstLevelModel(t_r, hrf_model='fir', fir_delays=[1, 2, 3]) 
 first_level_model = first_level_model.fit(fmri_img, events=events)
 design_matrix = first_level_model.design_matrices_[0]
 plot_design_matrix(design_matrix)
@@ -127,14 +126,14 @@ contrasts = {
                    - contrasts["clicDaudio"] - contrasts["clicDvideo"]),
     "H-V": contrasts["damier_H"] - contrasts["damier_V"],
     "audio-video": contrasts["audio"] - contrasts["video"],
-    "computation-sentences": (contrasts["computation"] -
-                              contrasts["sentences"]),
+    "sentences-computation": (contrasts["sentences"]-contrasts["computation"])
     }
+
 
 plot_contrast_matrix(contrasts['left-right'], design_matrix)
 
 #########################################################################
-# Take a breathe.
+# Take a breath.
 #
 # We can now  proceed by estimating the contrasts and displaying them.
 
