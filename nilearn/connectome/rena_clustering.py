f10f5e7878e121bcf684070d40ebbed5898bbb05
jerome-alexis_chevalier <jerome-alexis.chevalier@inria.fr>
2019-04-16 14:32:25 +0200

    removing _get_max_connectivity function since scipy version is at least 0.17 (>0.14)

--- nilearn/connectome/rena_clustering.py
+++ nilearn/connectome/rena_clustering.py
@@ -4,9 +4,6 @@
 # Author: Andres Hoyos idrobo, Gael Varoquaux, Jonas Kahn and  Bertrand Thirion
 # License: simplified BSD
 
-from distutils.version import LooseVersion
-import scipy
-
 import numpy as np
 import warnings
 from scipy.sparse import csgraph, coo_matrix, dia_matrix
@@ -185,7 +182,7 @@ def _nn_connectivity(connectivity, threshold=1e-7):
         (1. / connectivity.data, connectivity.nonzero()),
         (n_features, n_features)).tocsr()
 
-    max_connectivity = _get_max_connectivity(connectivity_)
+    max_connectivity = connectivity.max(axis=0).toarray()[0]
     inv_max = dia_matrix((1. / max_connectivity, 0),
                          shape=(n_features, n_features))
 
@@ -596,27 +593,3 @@ class ReNA(BaseEstimator, ClusterMixin, TransformerMixin):
         X_inv = Xred[..., inverse]
 
         return self.masker_.inverse_transform(X_inv)
-
-
-def _get_max_connectivity(connectivity):
-    """This function calculate of the maximum on the axis=0.
-    It is applied to the connectivity matrix.
-    This matrix is sparse and symetric.
-    """
-    if LooseVersion(scipy.__version__) >= LooseVersion('0.14'):
-        max_connectivity = connectivity.max(axis=0).toarray()[0]
-
-    if LooseVersion(scipy.__version__) < LooseVersion('0.10'):
-        N = connectivity.shape[0]
-        max_connectivity = np.zeros((N))
-        for i in range(N):
-            row = connectivity.getrow(i)
-            ind = row.nonzero()
-            max_connectivity[i] = np.max(row[ind])
-    else:
-        N = connectivity.shape[0]
-        max_connectivity = np.zeros((N))
-        for i in range(N):
-            max_connectivity[i] = connectivity.getrow(i).max()
-
-    return max_connectivity
