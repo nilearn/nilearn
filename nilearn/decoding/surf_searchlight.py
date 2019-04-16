629c516b974cdd882d91ea23fa7ef9c831432670
Sylvain Takerkart <sylvain.takerkart@univ-amu.fr>
2019-04-16 14:09:13 +0200

    1. Refactored surf_searchlight.py to use low-level functions from searchlight.py. 2. Added test for high-level SurfSearchLight object

--- nilearn/decoding/surf_searchlight.py
+++ nilearn/decoding/surf_searchlight.py
@@ -17,183 +17,18 @@ from distutils.version import LooseVersion
 
 import numpy as np
 
-from sklearn.externals.joblib import Parallel, delayed, cpu_count
 from sklearn import svm
 from sklearn.base import BaseEstimator
 
 
 from .. import surf_masking
 from ..input_data.gifti_spheres_masker import _apply_surfmask_and_get_affinity
-
 from .._utils.compat import _basestring
-from sklearn.model_selection import cross_val_score
+from .searchlight import search_light
+
 
 ESTIMATOR_CATALOG = dict(svc=svm.LinearSVC, svr=svm.SVR)
 
-
-def surf_search_light(X, y, estimator, A, groups=None, scoring=None,
-                      cv=None, n_jobs=-1, verbose=0):
-    """Function for computing a search_light on the cortical surface
-
-    Parameters
-    ----------
-    X : array-like of shape at least 2D
-        data to fit.
-
-    y : array-like
-        target variable to predict.
-
-    estimator : estimator object implementing 'fit'
-        object to use to fit the data
-
-    A : scipy sparse matrix.
-        adjacency matrix. Defines for each sample the neigbhoring samples
-        following a given structure of the data.
-
-    groups : array-like, optional
-        group label for each sample for cross validation. default None
-        NOTE: will have no effect for scikit learn < 0.18
-
-    scoring : string or callable, optional
-        The scoring strategy to use. See the scikit-learn documentation
-        for possible values.
-        If callable, it taks as arguments the fitted estimator, the
-        test data (X_test) and the test target (y_test) if y is
-        not None.
-
-    cv : cross-validation generator, optional
-        A cross-validation generator. If None, a 3-fold cross
-        validation is used or 3-fold stratified cross-validation
-        when y is supplied.
-
-    n_jobs : int, optional
-        The number of CPUs to use to do the computation. -1 means
-        'all CPUs'.
-
-    verbose : int, optional
-        The verbosity level. Defaut is 0
-
-    Returns
-    -------
-    scores : array-like of shape (number of rows in A)
-        search_light scores
-    """
-    group_iter = GroupIterator(A.shape[0], n_jobs)
-    scores = Parallel(n_jobs=n_jobs, verbose=verbose)(
-        delayed(_group_iter_surf_search_light)(
-            A.rows[list_i],
-            estimator, X, y, groups, scoring, cv,
-            thread_id + 1, A.shape[0], verbose)
-        for thread_id, list_i in enumerate(group_iter))
-    return np.concatenate(scores)
-
-
-class GroupIterator(object):
-    """Group iterator
-
-    Provides group of features for surf_search_light loop
-    that may be used with Parallel.
-
-    Parameters
-    ----------
-    n_features : int
-        Total number of features
-
-    n_jobs : int, optional
-        The number of CPUs to use to do the computation. -1 means
-        'all CPUs'. Defaut is 1
-    """
-    def __init__(self, n_features, n_jobs=1):
-        self.n_features = n_features
-        if n_jobs == -1:
-            n_jobs = cpu_count()
-        self.n_jobs = n_jobs
-
-    def __iter__(self):
-        split = np.array_split(np.arange(self.n_features), self.n_jobs)
-        for list_i in split:
-            yield list_i
-
-
-def _group_iter_surf_search_light(list_rows, estimator, X, y, groups,
-                                  scoring, cv, thread_id, total, verbose=0):
-    """Function for grouped iterations of surf_search_light
-
-    Parameters
-    -----------
-    list_rows : array of arrays of int
-        adjacency rows. For a voxel with index i in X, list_rows[i] is the list
-        of neighboring voxels indices (in X).
-
-    estimator : estimator object implementing 'fit'
-        object to use to fit the data
-
-    X : array-like of shape at least 2D
-        data to fit.
-
-    y : array-like
-        target variable to predict.
-
-    groups : array-like, optional
-        group label for each sample for cross validation.
-
-    scoring : string or callable, optional
-        Scoring strategy to use. See the scikit-learn documentation.
-        If callable, takes as arguments the fitted estimator, the
-        test data (X_test) and the test target (y_test) if y is
-        not None.
-
-    cv : cross-validation generator, optional
-        A cross-validation generator. If None, a 3-fold cross validation is
-        used or 3-fold stratified cross-validation when y is supplied.
-
-    thread_id : int
-        process id, used for display.
-
-    total : int
-        Total number of voxels, used for display
-
-    verbose : int, optional
-        The verbosity level. Defaut is 0
-
-    Returns
-    -------
-    par_scores : numpy.ndarray
-        score for each voxel. dtype: float64.
-    """
-    par_scores = np.zeros(len(list_rows))
-    t0 = time.time()
-    for i, row in enumerate(list_rows):
-        kwargs = dict()
-        kwargs['scoring'] = scoring
-        kwargs['groups'] = groups
-        # if not LooseVersion(sklearn.__version__) < LooseVersion('0.15'):
-        #     kwargs['scoring'] = scoring
-        # elif scoring is not None:
-        #     warnings.warn('Scikit-learn version is too old. '
-        #                   'scoring argument ignored', stacklevel=2)
-        par_scores[i] = np.mean(cross_val_score(estimator, X[:, row],
-                                                y, cv=cv, n_jobs=1,
-                                                **kwargs))
-        if verbose > 0:
-            # One can't print less than each 10 iterations
-            step = 11 - min(verbose, 10)
-            if (i % step == 0):
-                # If there is only one job, progress information is fixed
-                if total == len(list_rows):
-                    crlf = "\r"
-                else:
-                    crlf = "\n"
-                percent = float(i) / len(list_rows)
-                percent = round(percent * 100, 2)
-                dt = time.time() - t0
-                # We use a max to avoid a division by zero
-                remaining = (100. - percent) / max(0.01, percent) * dt
-                sys.stderr.write(
-                    "Job #%d, processed %d/%d voxels "
-                    "(%0.2f%%, %i seconds remaining)%s"
-                    % (thread_id, i, len(list_rows), percent, remaining, crlf))
-    return par_scores
 
 
 ##############################################################################
@@ -310,9 +145,9 @@ class SurfSearchLight(BaseEstimator):
 
         # scores is an 1D array of CV scores with length equals to the number
         # of voxels in processing mask (columns in process_mask)
-        scores = surf_search_light(X, y, estimator, A, groups,
-                                   self.scoring, self.cv, self.n_jobs,
-                                   self.verbose)
+        scores = search_light(X, y, estimator, A, groups,
+                              self.scoring, self.cv, self.n_jobs,
+                              self.verbose)
                                    
         print(scores.shape)
                                    
