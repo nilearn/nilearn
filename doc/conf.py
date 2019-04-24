3571fb36d29b38677a0f64b4825907554f430fcd
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-04-24 21:44:03 +0200

    Removed 1 ur style python2 only string encoding

--- doc/conf.py
+++ doc/conf.py
@@ -227,12 +227,10 @@ htmlhelp_basename = 'PythonScientic'
 # (source start file, target name, title, author, documentclass
 # [howto/manual]).
 latex_documents = [
-  ('index', 'nilearn.tex', u'NeuroImaging with scikit-learn',
-   ur"""Gaël Varoquaux and Alexandre Abraham"""
-   + r"\\\relax ~\\\relax http://nilearn.github.io",
-   'manual'),
-
-]
+  ('index', 'nilearn.tex', 'NeuroImaging with scikit-learn',
+   'Gaël Varoquaux and Alexandre Abraham'
+   + r"\\\relax ~\\\relax http://nilearn.github.io", 'manual'),
+    ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
