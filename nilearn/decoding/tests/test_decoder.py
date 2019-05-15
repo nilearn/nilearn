75c3c996825de671a70c1781d15936d1959fefca
bnguyen <tuan-binh.nguyen@inria.fr>
2019-05-15 17:13:10 +0200

    Add binary classification case & usage of predict_proba

--- nilearn/decoding/tests/test_decoder.py
+++ nilearn/decoding/tests/test_decoder.py
@@ -129,7 +129,43 @@ def test_parallel_fit():
             assert_equal(a, b)
 
 
-def test_decoder_classification():
+def test_decoder_binary_classification():
+    X, y = make_classification(n_samples=200, n_features=125, scale=3.0,
+                               n_informative=5, n_classes=2, random_state=42)
+    X, mask = to_niimgs(X, [5, 5, 5])
+
+    # check classification with masker object
+    model = Decoder(mask=NiftiMasker())
+    model.fit(X, y)
+    y_pred = model.predict(X)
+    assert_true(accuracy_score(y, y_pred) > 0.95)
+
+    # decoder object use predict_proba for scoring with logistic model
+    model = Decoder(estimator='logistic_l2', mask=mask)
+    model.fit(X, y)
+    y_pred = model.predict(X)
+    assert_true(accuracy_score(y, y_pred) > 0.95)
+
+    # check different screening_percentile value
+    for screening_percentile in [100, 20]:
+        model = Decoder(mask=mask, screening_percentile=screening_percentile)
+        model.fit(X, y)
+        y_pred = model.predict(X)
+        assert_true(accuracy_score(y, y_pred) > 0.95)
+
+    # check cross-validation scheme and fit attribute with groups enabled
+    for cv in [KFold(n_splits=5), LeaveOneGroupOut()]:
+        model = Decoder(estimator='svc', mask=mask,
+                        standardize=True, cv=cv)
+        if isinstance(cv, LeaveOneGroupOut):
+            groups = rand.binomial(2, 0.3, size=len(y))
+        else:
+            groups = None
+        model.fit(X, y, groups=groups)
+        assert_true(accuracy_score(y, y_pred) > 0.9)
+
+
+def test_decoder_multiclass_classification():
     X, y = make_classification(n_samples=200, n_features=125, scale=3.0,
                                n_informative=5, n_classes=4, random_state=42)
     X, mask = to_niimgs(X, [5, 5, 5])
