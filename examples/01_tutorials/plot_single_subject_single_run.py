61a86db07429739e0c8e7ee0110dbc7369fe7aa3
Ana Luisa Pinho <ana.pinho@inria.fr>
2019-05-22 17:46:20 +0200

    FIXUP: examples

--- examples/01_tutorials/plot_single_subject_single_run.py
+++ examples/01_tutorials/plot_single_subject_single_run.py
@@ -90,13 +90,13 @@ from nistats.first_level_model import FirstLevelModel
 # * standardize=False means that we do not want to rescale the time series to mean 0, variance 1
 # * hrf_model='spm' means that we rely on the SPM "canonical hrf" model (without time or dispersion derivatives)
 # * drift_model='cosine' means that we model the signal drifts as slow oscillating time functions
-# * period_cut=160(s) defines the cutoff frequency (its inverse actually).
+# * high_pass=1/160(Hz) defines the cutoff frequency.
 fmri_glm = FirstLevelModel(t_r=7,
                            noise_model='ar1',
                            standardize=False,
                            hrf_model='spm',
                            drift_model='cosine',
-                           period_cut=160)
+                           high_pass=1./160)
 
 ###############################################################################
 # Now that we have specified the model, we can run it on the fMRI image
