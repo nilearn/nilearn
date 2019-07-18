35baca8324f26bb93c7ebdc3a6c89e78e4e0a413
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-07-18 10:50:01 +0200

    Added nose-exclude to CI setup, attempt 6

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -75,7 +75,7 @@ create_new_conda_env() {
     echo "conda requirements string: $REQUIREMENTS"
     conda create -n testenv --quiet --yes $REQUIREMENTS
     source activate testenv
-    conda install nose-exclude -c conda-forge
+    conda install nose-exclude -c conda-forge --yes
 
     if [[ "$INSTALL_MKL" == "true" ]]; then
         # Make sure that MKL is used
