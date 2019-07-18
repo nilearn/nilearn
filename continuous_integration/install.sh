da87bb5ebb28943df7c931f146e455c46b212002
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-07-18 16:46:09 +0200

    Added pytest for installation in travis

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -75,6 +75,7 @@ create_new_conda_env() {
     echo "conda requirements string: $REQUIREMENTS"
     conda create -n testenv --quiet --yes $REQUIREMENTS
     source activate testenv
+    conda install pytest --yes
 
     if [[ "$INSTALL_MKL" == "true" ]]; then
         # Make sure that MKL is used
