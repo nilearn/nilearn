bb800db32b74987479be7556194fcb5a1a7a5301
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-07-19 14:05:36 +0200

    Moved conftest.py into Nilearn package & fixed flake8 error in it

--- conftest.py
+++
@@ -1,4 +0,0 @@
-try:
-    import matplotlib
-except ImportError:
-    collect_ignore = ["nilearn/plotting"]
---
+++ nilearn/conftest.py
@@ -0,0 +1,6 @@
+try:
+    import matplotlib
+except ImportError:
+    collect_ignore = ['plotting']
+else:
+    matplotlib
