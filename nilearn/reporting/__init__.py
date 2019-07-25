4aa81d33cec67b63767e6190174736073e4c78b0
Elizabeth DuPre <emd222@cornell.edu>
2019-07-25 11:19:25 -0400

    Move HTMLDocument to its own module

--- nilearn/reporting/__init__.py
+++ nilearn/reporting/__init__.py
@@ -2,8 +2,9 @@
 Reporting code for nilearn
 """
 
-from .html_report import (ReportMixin, HTMLDocument, HTMLReport,
-                          set_max_img_views_before_warning)
+from .html_report import (ReportMixin, HTMLReport)
+
+from .html_doc import (HTMLDocument, set_max_img_views_before_warning)
 
 __all__ = ['ReportMixin', 'HTMLDocument', 'HTMLReport',
            'set_max_img_views_before_warning']
