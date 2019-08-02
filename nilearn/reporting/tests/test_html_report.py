2ced8f58714bec5f187320d89a9be1ef25a82df6
Elizabeth DuPre <emd222@cornell.edu>
2019-08-02 13:18:55 -0400

    Retrigger Travis

--- nilearn/reporting/tests/test_html_report.py
+++ nilearn/reporting/tests/test_html_report.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from nibabel import Nifti1Image
 from nilearn import input_data
@@ -21,18 +22,19 @@ def test_3d_reports():
     data = np.zeros((9, 9, 9))
     data[3:-3, 3:-3, 3:-3] = 10
     data_img_3d = Nifti1Image(data, np.eye(4))
+
+    # Dummy mask
+    mask = np.zeros((9, 9, 9))
+    mask[4:-4, 4:-4, 4:-4] = True
+    mask_img_3d = Nifti1Image(data, np.eye(4))
     
-    # check 3d datasets
+    # test .fit method
     mask = input_data.NiftiMasker()
     mask.fit(data_img_3d)
     html = mask.generate_report()
     _check_html(html)
 
-    # Check providing mask to init
-    mask = np.zeros((9, 9, 9))
-    mask[4:-4, 4:-4, 4:-4] = True
-    mask_img_3d = Nifti1Image(data, np.eye(4))
-
+    # check providing mask to init
     masker = input_data.NiftiMasker(mask_img=mask_img_3d)
     masker.fit(data_img_3d)
     html = masker.generate_report()
@@ -40,17 +42,17 @@ def test_3d_reports():
 
 
 def test_4d_reports():
-    # Dummy mask
-    mask = np.zeros((10, 10, 10), dtype=int)
-    mask[3:7, 3:7, 3:7] = 1
-    mask_img = Nifti1Image(mask, np.eye(4))
-
     # Dummy 4D data
     data = np.zeros((10, 10, 10, 3), dtype=int)
     data[..., 0] = 1
     data[..., 1] = 2
     data[..., 2] = 3
     data_img_4d = Nifti1Image(data, np.eye(4))
+
+    # Dummy mask
+    mask = np.zeros((10, 10, 10), dtype=int)
+    mask[3:7, 3:7, 3:7] = 1
+    mask_img = Nifti1Image(mask, np.eye(4))
 
     # test .fit method
     mask = input_data.NiftiMasker(mask_strategy='epi')
@@ -81,6 +83,7 @@ def test_empty_report():
 
 
 def test_overlaid_report():
+    pytest.importorskip('matplotlib')
     empty_svg = ("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAA" +
                  "AAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=")
 
