bd146a7925756626437ec77fd1ca73f51f8253f0
Elizabeth DuPre <emd222@cornell.edu>
2019-08-27 16:17:40 -0400

    Patch _ReportScraper

--- nilearn/reporting/sphinx_report.py
+++ nilearn/reporting/sphinx_report.py
@@ -1,7 +1,10 @@
 # Scraper for sphinx-gallery
 # Copied from https://github.com/mne-tools/mne-python/
 
+import os.path as op
 from shutil import copyfile
+from nilearn.reporting import HTMLReport
+
 
 _SCRAPER_TEXT = '''
 .. only:: builder_html
@@ -29,7 +32,7 @@ class _ReportScraper(object):
 
     def __call__(self, block, block_vars, gallery_conf):
         for report in block_vars['example_globals'].values():
-            if (isinstance(report, Report) and hasattr(report, 'fname') and
+            if (isinstance(report, HTMLReport) and hasattr(report, 'fname') and
                     report.fname.endswith('.html') and
                     gallery_conf['builder_name'] == 'html'):
                 # Thumbnail
