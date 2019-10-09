0331070e36194a2c9b50ea4f683a72dfd6e79cca
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-10-09 10:37:53 +0200

    Fixing malfunctioning allowed-failure section in Travis
     - Deliberate Flake8 violation for testing.

--- nilearn/signal.py
+++ nilearn/signal.py
@@ -593,10 +593,11 @@ def clean(signals, sessions=None, detrend=True, standardize='zscore',
     if detrend and (standardize == 'psc'):
         # If the signal is detrended, we have to know the original mean
         # signal to calculate the psc.
-        signals = _standardize(signals + mean_signals, standardize=standardize,
+        signals = _standardize(signals + mean_signals, standardize=standardize, # temp comment to test flake8 errors
                                detrend=False)
     else:
         signals = _standardize(signals, standardize=standardize,
                                detrend=False)
 
     return signals
+
