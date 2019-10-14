f1a1cc201e2bf5f7cfa1faaefc23364de2f0cde7
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-10-14 17:30:39 +0200

    Removed sub-example due to unfit for lasso dataset - unstable float values

--- examples/03_connectivity/plot_sphere_based_connectome.py
+++ examples/03_connectivity/plot_sphere_based_connectome.py
@@ -332,55 +332,6 @@ print('Dosenbach networks names are {0}'.format(np.unique(dosenbach.networks)))
 
 plotting.show()
 
-
-###############################################################################
-# Connectome extracted from Seitzman's atlas
-# -----------------------------------------------------
-# We repeat the same steps for Seitzman's atlas.
-seitzman = datasets.fetch_coords_seitzman_2018()
-
-coords = np.vstack((
-    seitzman.rois['x'],
-    seitzman.rois['y'],
-    seitzman.rois['z'],
-)).T
-
-###############################################################################
-# Before calculating the connectivity matrix, let's look at the distribution
-# of the regions of the default mode network.
-dmn_rois = seitzman.networks == "DefaultMode"
-dmn_coords = coords[dmn_rois]
-zero_matrix = np.zeros((len(dmn_coords), len(dmn_coords)))
-plotting.plot_connectome(zero_matrix, dmn_coords,
-                         title='Seitzman default mode network',
-                         node_color='darkred', node_size=20)
-
-###############################################################################
-# Now let's calculate connectivity for the Seitzman atlas.
-spheres_masker = input_data.NiftiSpheresMasker(
-    seeds=coords, smoothing_fwhm=6, radius=4,
-    detrend=True, standardize=True, low_pass=0.1, high_pass=0.01, t_r=2,
-    allow_overlap=True)
-
-timeseries = spheres_masker.fit_transform(func_filename,
-                                          confounds=confounds_filename)
-
-covariance_estimator = GraphicalLassoCV()
-covariance_estimator.fit(timeseries)
-matrix = covariance_estimator.covariance_
-
-plotting.plot_matrix(matrix, vmin=-1., vmax=1., colorbar=True,
-                     title='Seitzman correlation matrix')
-
-plotting.plot_connectome(matrix, coords, title='Seitzman correlation graph',
-                         edge_threshold="99.7%", node_size=20, colorbar=True)
-
-
-###############################################################################
-# We can easily identify the networks from the matrix blocks.
-print('Seitzman networks names are {0}'.format(np.unique(seitzman.networks)))
-plotting.show()
-
 ###############################################################################
 # .. seealso::
 #
