a8824fa91c1f9c457da14a133175dfec1f7fb470
jeromedockes <jerome@dockes.org>
2019-10-23 22:47:07 +0200

    Remove file local variables (for emacs, vim etc)(#400)

--- nistats/experimental_paradigm.py
+++ nistats/experimental_paradigm.py
@@ -1,5 +1,3 @@
-# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
-# vi: set ft=python sts=4 ts=4 sw=4 et:
 """
 An experimental protocol is handled as a pandas DataFrame
 that includes an 'onset' field.
--- nistats/first_level_model.py
+++ nistats/first_level_model.py
@@ -1,5 +1,3 @@
-# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
-# vi: set ft=python sts=4 ts=4 sw=4 et:
 """
 This module presents an interface to use the glm implemented in
 nistats.regression.
