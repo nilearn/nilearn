da84a26931d717afbe2f594661e83f8de86d40e3
Elizabeth DuPre <emd222@cornell.edu>
2019-12-03 04:26:36 -0500

    [doc] Iimprove documentation re: using index_img with slice() (#2189)
    * [doc] add description of slice constructor to index_img doc
    
    * [doc] add example using slice constructor to plot_3d_and_4d
    
    * fix: decrease verbosity of dataset fetcher in docstring
    
    * fix: trigger doctest ellipsis for dataset path
    
    * fix: skip doctest for added example
    
    * fix: patch whitespace for more consistent literal block quoting
    
    * doc: resume executing doctest, correctly use ellipsis
    
    * fix: actually write a correct doctest
    
    * enh: iterate on index_img in tutorial
    
    * doc: update index_img docstring to only use shipped templates
    
    * doc: iterate on wording in plot_3d_4d_niimg example
    
    * doc: remove extra import in index_img docstring

--- nilearn/image/image.py
+++ nilearn/image/image.py
@@ -627,6 +627,17 @@ def index_img(imgs, index):
      >>> single_mni_image = index_img(joint_mni_image, 1)
      >>> print(single_mni_image.shape)
      (91, 109, 91)
+
+    We can also select multiple frames using the `slice` constructor::
+
+     >>> five_mni_images = concat_imgs([datasets.load_mni152_template()] * 5)
+     >>> print(five_mni_images.shape)
+     (91, 109, 91, 5)
+    
+     >>> first_three_images = index_img(five_mni_images,
+     ...                                slice(0, 3))
+     >>> print(first_three_images.shape)
+     (91, 109, 91, 3)
     """
     imgs = check_niimg_4d(imgs)
     # duck-type for pandas arrays, and select the 'values' attr
