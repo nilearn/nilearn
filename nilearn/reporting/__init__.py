c785c7a4ca5b7bf31f374fd95be3bb7576bb89e1
Elizabeth DuPre <emd222@cornell.edu>
2019-12-06 11:15:16 -0500

    fix: remove now duplicated report scraper

--- nilearn/reporting/__init__.py
+++ nilearn/reporting/__init__.py
@@ -6,7 +6,5 @@ from .html_report import (ReportMixin, HTMLReport)
 
 from .html_document import (HTMLDocument, set_max_img_views_before_warning)
 
-from .sphinx_report import (_ReportScraper)
-
 __all__ = ['ReportMixin', 'HTMLDocument', 'HTMLReport',
-           'set_max_img_views_before_warning', '_ReportScraper']
+           'set_max_img_views_before_warning']
