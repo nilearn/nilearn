bd30a10ea98847093ee9418582c6c2c47db66c85
Jerome Dockes <jerome@dockes.org>
2019-12-15 19:49:42 +0100

    plot titles in html page titles

--- nilearn/plotting/tests/test_html_stat_map.py
+++ nilearn/plotting/tests/test_html_stat_map.py
@@ -14,12 +14,14 @@ from ..js_plotting_utils import colorscale
 from ..._utils.compat import _basestring
 
 
-def _check_html(html_view):
+def _check_html(html_view, title=None):
     """ Check the presence of some expected code in the html viewer
     """
     assert isinstance(html_view, html_stat_map.StatMapView)
     assert "var brain =" in str(html_view)
     assert "overlayImg" in str(html_view)
+    if title is not None:
+        assert "<title>{}</title>".format(title) in str(html_view)
 
 
 def _simulate_img(affine=np.eye(4)):
@@ -305,9 +307,10 @@ def test_view_img():
         # Create a fake functional image by resample the template
         img = image.resample_img(mni, target_affine=3 * np.eye(3))
         html_view = html_stat_map.view_img(img)
-        _check_html(html_view)
-        html_view = html_stat_map.view_img(img, threshold='95%')
-        _check_html(html_view)
+        _check_html(html_view, title="Slice viewer")
+        html_view = html_stat_map.view_img(img, threshold='95%',
+                                           title="SOME_TITLE")
+        _check_html(html_view, title="SOME_TITLE")
         html_view = html_stat_map.view_img(img, bg_img=mni)
         _check_html(html_view)
         html_view = html_stat_map.view_img(img, bg_img=None)
