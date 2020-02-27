1dffac7377a98843d6730d30edce4293790f0c78
Gael Varoquaux <gael.varoquaux@normalesup.org>
2020-02-27 13:38:32 -0500

    BUG: fix non-imported VisibleDeprecationWarning

--- nilearn/datasets/func.py
+++ nilearn/datasets/func.py
@@ -245,7 +245,7 @@ def fetch_nyu_rest(n_subjects=None, sessions=[1], data_dir=None, resume=True,
     """
     warnings.warn("fetch_nyu_rest has been deprecated and will "
                   "be removed in the 0.8.x release.",
-                  VisibleDeprecationWarning, stacklevel=2)
+                  np.VisibleDeprecationWarning, stacklevel=2)
 
     fa1 = 'http://www.nitrc.org/frs/download.php/1071/NYU_TRT_session1a.tar.gz'
     fb1 = 'http://www.nitrc.org/frs/download.php/1072/NYU_TRT_session1b.tar.gz'
