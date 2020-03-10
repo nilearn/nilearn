5082119bf70968432a3c4b095ac2a1a200fe0ba1
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2020-03-10 16:16:34 +0100

    Adding command for easier diagnosisUsing a python script to build examples

---
+++ .circleci/build_changed_examples.py
@@ -0,0 +1,27 @@
+import os
+import shlex
+import subprocess
+
+from pprint import pprint
+
+
+# Assuming current working directory is nilearn/docs .
+git_diff_examples_cmd = "git diff master --name-only ../examples/**/*.py"
+git_diff_examples_shell = subprocess.Popen(shlex.split(git_diff_examples_cmd),
+                                           stderr=subprocess.STDOUT,
+                                           stdout=subprocess.PIPE,
+                                           )
+output, errors = git_diff_examples_shell.communicate()
+changed_examples = output.decode(encoding='utf8').split('\n')[:-1]
+pprint(changed_examples)
+
+# venv_activate_shell = subprocess.Popen(shlex.split("conda activate nilearn-py37-latest"), shell=True)
+# print(venv_activate_shell.communicate())
+
+sphinx_cmd = ("python -m sphinx -W -D sphinx_gallery_conf.filename_pattern={} "
+              "-b html -d _build/doctrees . _build/html")
+for changed_example_ in changed_examples:
+    example_shell = subprocess.Popen(shlex.split(sphinx_cmd.format(changed_example_)), shell=True)
+    print(example_shell.communicate())
+
+
