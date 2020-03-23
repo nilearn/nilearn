f96b6f2b07a23fbfd172cf71a9effd9250bf85c4
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2020-03-23 14:08:26 +0100

    Adding & fetching upstream master for diff

--- .circleci/build_changed_examples.py
+++ .circleci/build_changed_examples.py
@@ -6,7 +6,15 @@ from pprint import pprint
 
 
 # Assuming current working directory is nilearn/docs .
-git_diff_examples_cmd = "git diff master --name-only ../examples/**/*.py"
+git_add_upstream_cmd = 'git remote add upstream https://github.com/nilearn/nilearn'
+git_diff_examples_shell = subprocess.Popen(shlex.split(git_add_upstream_cmd),
+                                           stderr=subprocess.STDOUT,
+                                           stdout=subprocess.PIPE,
+                                           )
+output, errors = git_diff_examples_shell.communicate()
+
+
+git_diff_examples_cmd = 'git diff upstream/master --name-only ../examples/**/*.py'
 git_diff_examples_shell = subprocess.Popen(shlex.split(git_diff_examples_cmd),
                                            stderr=subprocess.STDOUT,
                                            stdout=subprocess.PIPE,
@@ -19,8 +27,8 @@ pprint(changed_examples)
 # venv_activate_shell = subprocess.Popen(shlex.split("conda activate nilearn-py37-latest"), shell=True)
 # print(venv_activate_shell.communicate())
 
-sphinx_cmd = ("python -m sphinx -W -D sphinx_gallery_conf.filename_pattern={} "
-              "-b html -d _build/doctrees . _build/html")
+sphinx_cmd = ('python -m sphinx -W -D sphinx_gallery_conf.filename_pattern={} '
+              '-b html -d _build/doctrees . _build/html')
 for changed_example_ in changed_examples:
     example_shell = subprocess.Popen(shlex.split(sphinx_cmd.format(changed_example_)), shell=True)
     print(example_shell.communicate())
