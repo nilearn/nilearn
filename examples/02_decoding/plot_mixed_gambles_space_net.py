b67cbb88f9fb35bfafa11c4961d770a6fe69e389
BAZEILLE Thomas <thomas.bazeille@inria.fr>
2020-04-20 11:17:37 +0200

    "B.T. review"

--- examples/02_decoding/plot_mixed_gambles_space_net.py
+++ examples/02_decoding/plot_mixed_gambles_space_net.py
@@ -48,7 +48,7 @@ plot_stat_map(fREM.coef_img_['beta'], title="fREM", display_mode="yz",
 # We can observe that the coefficients map learnt by fREM is structured,
 # due to the spatial regularity imposed by working on clusters and model
 # ensembling. Although these maps have been thresholded for display,
-# they are not sparse (i.e. almost all voxels have non-zero coefficients).#
+# they are not sparse (i.e. almost all voxels have non-zero coefficients).
 # See also this `other example<sphx_glr_auto_examples_02_decoding_plot_haxby_frem.py>`
 # using fREM, and related :ref:`section of user guide <frem>`.
 #
