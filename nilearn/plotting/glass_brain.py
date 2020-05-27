905e52f3706fe8bf0fac975fa7a6c2fa6eb30b2c
SungHo Lee <shlee@unc.edu>
2020-05-26 21:29:20 -0400

    Revert

--- nilearn/plotting/glass_brain.py
+++ nilearn/plotting/glass_brain.py
@@ -101,7 +101,6 @@ def _get_json_and_transform(direction):
         'l': [0.38, 0, 0, 0.38, -108, -70],
         'r': [0.38, 0, 0, 0.38, -108, -70]}
 
-
     dirname = os.path.dirname(os.path.abspath(__file__))
     dirname = os.path.join(dirname, 'glass_brain_files')
     direction_to_filename = dict([
