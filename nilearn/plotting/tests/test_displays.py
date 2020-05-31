48b4068a9f6b1a8fe6f4d392382c71461e3ea606
SungHo Lee <shlee@unc.edu>
2020-05-31 17:40:29 -0400

    Separate decimal position annotation to have cut_coord value

--- nilearn/plotting/tests/test_displays.py
+++ nilearn/plotting/tests/test_displays.py
@@ -137,8 +137,11 @@ def test_annotations():
                          scale_size=2.5,
                          scale_units='cm',
                          scale_loc=3)
+
+
+def test_position_annotation_with_decimals():
     # Test of decimals position annotation with precision of 2
-    orthoslicer.cut_coords = (0, 0, 0)
+    orthoslicer = OrthoSlicer(cut_coords=(0, 0, 0))
     orthoslicer.annotate(positions=True, decimals=2)
     orthoslicer.close()
 
