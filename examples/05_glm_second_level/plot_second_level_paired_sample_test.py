6c0478eb3cb9f4a9f4bff7842804a928eb4d04e8
swaythe <swaythe@gmail.com>
2020-09-18 18:24:34 +0200

    mod paired test eg

--- examples/05_glm_second_level/plot_second_level_paired_sample_test.py
+++ examples/05_glm_second_level/plot_second_level_paired_sample_test.py
@@ -68,7 +68,7 @@ plot_design_matrix(design_matrix)
 
 ############################################################################
 # We formally specify the analysis model and fit it.
-from nilearn.glm.second_level_model import SecondLevelModel
+from nilearn.glm.second_level import SecondLevelModel
 second_level_model = SecondLevelModel().fit(
     second_level_input, design_matrix=design_matrix)
 
