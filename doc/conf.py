6c738cccb59a62535778c5d88915b41784583088
Gael Varoquaux <gael.varoquaux@normalesup.org>
2020-11-12 08:51:04 +0100

    BUILD: make sphinx-gallery more robust
    On my computer, these changes are needed so that sphinx-gallery does not
    error saying that the matplotlib backend is tkagg

--- doc/conf.py
+++ doc/conf.py
@@ -45,14 +45,13 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+              'sphinx_gallery.gen_gallery',
+              'sphinx.ext.autodoc',
               'sphinx.ext.autosummary',
-              ('sphinx.ext.imgmath'  # only available for sphinx >= 1.4
-                  if sphinx.version_info[:2] >= (1, 4)
-                  else 'sphinx.ext.pngmath'),
+              'sphinx.ext.imgmath',
               'sphinx.ext.intersphinx',
               'numpydoc',
-              'sphinx_gallery.gen_gallery',
               ]
 
 autosummary_generate = True
