c6afa7212983556bfb93a7085094570772817e77
NicolasGensollen <nicolas.gensollen@gmail.com>
2020-12-21 15:10:55 +0100

    display python version (debug macos failures...)

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-python -m pip install --progress-bar off --upgrade setuptools wheel
+python -m pip install --progress-bar off --upgrade pip setuptools wheel
 if [ ! -z "$MIN_REQUIREMENTS" ]; then
     pip install --progress-bar off --upgrade -r requirements-min.txt
 else
