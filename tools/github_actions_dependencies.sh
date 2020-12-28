2a4111f87ea7458c4e5aec9db814135bc3cf5451
Gensollen <nicolas.gensollen@gmail.com>
2020-12-28 18:42:06 +0100

    [MRG] Move tests to Github Actions (#2633)
    * Move tests to Github Actions
    
    * Debug windows on latest job
    
    * conda -> pip for min dep
    
    * Fix
    
    * Force test pass...
    
    * Min req only for ubuntu, drop 3.5 for others
    
    * Drop windows testing on Github actions
    
    * enh with jerome suggestions
    
    * display python version (debug macos failures...)
    
    * Fix Github actions on MacOS
    
    * Delete .travis.yml
    
    * Drop matplotlib-dev testing

---
+++ tools/github_actions_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+python -m pip install --progress-bar off --upgrade pip setuptools wheel
+if [ ! -z "$MIN_REQUIREMENTS" ]; then
+    pip install --progress-bar off --upgrade -r requirements-min.txt
+else
+    pip install --progress-bar off --upgrade -r requirements-dev.txt
+fi
+
