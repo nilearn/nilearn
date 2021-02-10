4665ac8e15df33f98c6dbb4f740252c39374b79d
Robert Luke <748691+rob-luke@users.noreply.github.com>
2021-02-11 07:44:16 +1100

    Update nilearn/glm/first_level/first_level.py
    Co-authored-by: Alexandre Gramfort <alexandre.gramfort@m4x.org>

--- nilearn/glm/first_level/first_level.py
+++ nilearn/glm/first_level/first_level.py
@@ -116,7 +116,7 @@ def run_glm(Y, X, noise_model='ar1', bins=100, n_jobs=1, verbose=0):
 
     bins : int, optional
         Maximum number of discrete bins for the AR coef histogram.
-        If an auto regressive model with order greater than one is specified
+        If an autoregressive model with order greater than one is specified
         then adaptive quantification is performed and the coefficients
         will be clustered via K-means with `bins` number of clusters.
         Default=100.
