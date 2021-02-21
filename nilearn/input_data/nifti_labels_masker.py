f9cc35beb61ed7ceb3b898762f2cfc80df0b2b31
Ahmad <ahmad.chamma723@gmail.com>
2021-02-22 00:00:40 +0100

    Solving "NiftiMapsMasker and NiftiLabelsMasker cannot transform 3D images"

--- nilearn/input_data/nifti_labels_masker.py
+++ nilearn/input_data/nifti_labels_masker.py
@@ -184,19 +184,16 @@ class NiftiLabelsMasker(BaseMasker, CacheMixin):
         All parameters are unused, they are for scikit-learn compatibility.
 
         """
-
         logger.log("loading data from %s" %
                    _utils._repr_niimgs(self.labels_img,
                                        shorten=(not self.verbose)),
                    verbose=self.verbose)
-
         self.labels_img_ = _utils.check_niimg_3d(self.labels_img)
         if self.mask_img is not None:
             logger.log("loading data from %s" %
                        _utils._repr_niimgs(self.mask_img,
                                            shorten=(not self.verbose)),
                        verbose=self.verbose)
-
             self.mask_img_ = _utils.check_niimg_3d(self.mask_img)
         else:
             self.mask_img_ = None
