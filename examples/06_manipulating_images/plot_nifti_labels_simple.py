a701b9cd535d17b48dbfde9be311a6342a8ab40a
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-03-11 17:17:56 +0100

    Improve example

--- examples/06_manipulating_images/plot_nifti_labels_simple.py
+++ examples/06_manipulating_images/plot_nifti_labels_simple.py
@@ -1,13 +1,18 @@
 """
-Simple example of NiftiLabelsMasker use
-=======================================
+Extracting signals from brain regions using the NiftiLabelsMasker
+=================================================================
 
-Here is a simple example of automatic signal extraction using the nifti
-labels masker.
+This simple example shows how to extract signals from functional
+fmri data and brain regions defined through an atlas.
+More precisely, this example shows how to use the NiftiLabelsMasker
+object to perform this operation in just a few lines of code.
 """
 
 ###########################################################################
 # Retrieve the brain development functional dataset
+#
+# We start by fetching the brain development functional dataset
+# and we restrict the example to one subject only.
 
 from nilearn import datasets
 dataset = datasets.fetch_development_fmri(n_subjects=1)
@@ -18,6 +23,8 @@ print('First functional nifti image (4D) is at: %s' % func_filename)
 
 ###########################################################################
 # Load an atlas
+#
+# We then load the Harvard-Oxford atlas to define the brain regions
 atlas = datasets.fetch_atlas_harvard_oxford('cort-maxprob-thr25-2mm')
 
 # The first label correspond to the background
@@ -26,11 +33,13 @@ print('The atlas contains {} non-overlapping regions'.format(
 
 ###########################################################################
 # Instantiate the mask and visualize atlas
+#
 from nilearn.input_data import NiftiLabelsMasker
 
 # Instantiate the masker with label image and label values
 masker = NiftiLabelsMasker(atlas.maps,
-                           labels=atlas.labels)
+                           labels=atlas.labels,
+                           standardize=True)
 
 # Visualize the atlas
 # Note that we need to call fit prior to generating the mask
@@ -54,8 +63,24 @@ report = masker.generate_report()
 report
 
 ###########################################################################
-# Preprocess the data with the NiftiLablesMasker
-fmri_masked = masker.transform(func_filename)
-# fmri_masked is now a 2D matrix, (n_time_points x n_regions)
-fmri_masked.shape
+# Process the data with the NiftiLablesMasker
+#
+# In order to extract the signals, we need to call transform on the
+# functional data
+signals = masker.transform(func_filename)
+# signals is a 2D matrix, (n_time_points x n_regions)
+signals.shape
 
+###########################################################################
+# Plot the signals
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(15, 5))
+ax = fig.add_subplot(111)
+for label_idx in range(3):
+    ax.plot(signals[:, label_idx],
+            linewidth=2,
+            label=atlas.labels[label_idx+1]) # 0 is background
+ax.legend(loc=2)
+ax.set_title("Signals for first 3 regions")
+plt.show()
