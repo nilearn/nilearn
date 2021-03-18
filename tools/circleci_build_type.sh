badb1f51d5bc8bfb9deda55e6dd34ea00edc6941
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-03-18 12:25:46 +0100

    First attempt - simplify workflow and adapt sphinx build type

---
+++ tools/circleci_build_type.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -ef
+
+if [ "CIRCLE_BRANCH" == "master" ] || [[ $(cat gitlog.txt) == *"[circle full]"* ]]; then
+    echo "Doing a full build";
+    echo html-strict > build.txt;
+else
+    echo "Doing a partial build";
+    FILENAMES=$(git diff --name-only $(git merge-base $CIRCLE_BRANCH upstream/master) $CIRCLE_BRANCH);
+    echo FILENAMES="$FILENAMES";
+    for FILENAME in $FILENAMES; do
+        if [[ `expr match $FILENAME "\(examples\)/.*plot_.*\.py"` ]]; then
+            echo "Checking example $FILENAME ...";
+            PATTERN=`basename $FILENAME`"\\|"$PATTERN;
+        fi;
+    done;
+    echo PATTERN="$PATTERN";
+    if [[ $PATTERN ]]; then
+        PATTERN="\(${PATTERN::-2}\)";
+        echo html-pattern > build.txt;
+    else
+        echo html-noplot > build.txt;
+    fi;
+fi;
+echo "$PATTERN" > pattern.txt;
+
