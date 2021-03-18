6c2324795282f4cf8572becafed0b02ccb1eb8d3
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-03-18 12:45:49 +0100

    Fix CI

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -3,5 +3,5 @@
 sudo -E apt-get -yq update
 sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install dvipng texlive-latex-base texlive-latex-extra
 python -m pip install --user --upgrade --progress-bar off pip setuptools
-python -m pip install --user --upgrade --progress-bar off -r requirements.txt -r requirements-dev.txt -r requirements-build-docs.txt
+python -m pip install --user --upgrade --progress-bar off -r requirements-dev.txt -r requirements-build-docs.txt
 python -m pip install --user -e .
