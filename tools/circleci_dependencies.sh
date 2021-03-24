3c9778c806ab5adcd9092d6766823267181b5b4b
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-03-24 10:46:46 +0100

    Try with conda...

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -2,6 +2,11 @@
 
 sudo -E apt-get -yq update
 sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install dvipng texlive-latex-base texlive-latex-extra
+conda init bash
+echo "conda version = $(conda --version)"
+conda create -n testenv
+conda install -n testenv -yq python=3.8 numpy scipy scikit-learn matplotlib pandas lxml mkl sphinx numpydoc pillow pandas
+conda install -n testenv -yq nibabel sphinx-gallery junit-xml -c conda-forge
+source activate testenv
 python -m pip install --user --upgrade --progress-bar off pip setuptools
-python -m pip install --user --upgrade --progress-bar off -r requirements-dev.txt -r requirements-build-docs.txt
 python -m pip install --user -e .
