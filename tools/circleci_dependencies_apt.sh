1b3c080bef3761a439c87d8d74c676816be22797
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-04-01 11:47:47 +0200

    remove useless options from apt-get

--- tools/circleci_dependencies_apt.sh
+++ tools/circleci_dependencies_apt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ef
 
 sudo -E apt-get -yq update
-sudo -E apt-get -yq --no-install-suggests --no-install-recommends --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages install dvipng texlive-latex-base texlive-latex-extra
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends install dvipng texlive-latex-base texlive-latex-extra
