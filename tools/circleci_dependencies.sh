21b25b6ed615d1c527947866b57471e931a7d6bc
Elizabeth DuPre <emd222@cornell.edu>
2021-05-12 13:51:35 -0400

    Add copybutton to circle_deps

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -4,7 +4,7 @@ conda init bash
 echo "conda version = $(conda --version)"
 conda create -n testenv
 conda install -n testenv -yq python=3.8 numpy scipy scikit-learn matplotlib pandas lxml mkl sphinx numpydoc pillow pandas
-conda install -n testenv -yq nibabel sphinx-gallery sphinxcontrib-bibtex junit-xml -c conda-forge
+conda install -n testenv -yq nibabel sphinx-gallery sphinxcontrib-bibtex sphinx-copybutton junit-xml -c conda-forge
 source activate testenv
 python -m pip install --user --upgrade --progress-bar off pip setuptools
 python -m pip install .
