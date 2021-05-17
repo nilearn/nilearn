b692248a3b0b88f7825bab96b2868498ee3e67af
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-05-17 15:51:14 +0200

    Pin sphinx version on circleCI

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -3,7 +3,7 @@
 conda init bash
 echo "conda version = $(conda --version)"
 conda create -n testenv
-conda install -n testenv -yq python=3.8 numpy scipy scikit-learn matplotlib pandas lxml mkl sphinx numpydoc pillow pandas
+conda install -n testenv -yq python=3.8 numpy scipy scikit-learn matplotlib pandas lxml mkl sphinx==3.5.4 numpydoc pillow pandas
 conda install -n testenv -yq nibabel sphinx-gallery sphinxcontrib-bibtex junit-xml -c conda-forge
 source activate testenv
 python -m pip install --user --upgrade --progress-bar off pip setuptools
