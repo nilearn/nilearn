42c091e92cd3e95fa1b3559f33970e2466d96937
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-05-20 14:05:25 +0200

    mv flake8 script in build_tools

--- build_tools/github/test.sh
+++ build_tools/github/test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
 
+if [[ -n "$FLAKE8" ]]; then
+    echo "Running flake8 diff script...";
+    source build_tools/flake8_diff.sh
+fi
+
 python -m pytest --pyargs nilearn --cov=nilearn
