0294fe02015a660c808db44be2e71448a9b6de3a
JLefortBesnard <jlefortbesnard@tuta.io>
2021-06-08 22:00:54 +0200

    add missing semicolons + update whats_new.rst [circle full]

--- nilearn/signal.py
+++ nilearn/signal.py
@@ -370,7 +370,7 @@ def high_variance_confounds(series, n_confounds=5, percentile=2.,
     Notes
     -----
     This method is related to what has been published in the literature
-    as 'CompCor' footcite:`BEHZADI200790`.
+    as 'CompCor' :footcite:`BEHZADI200790`.
 
     The implemented algorithm does the following:
 
@@ -437,7 +437,7 @@ def clean(signals, runs=None, detrend=True, standardize='zscore',
 
     Filtering is only meaningful on evenly-sampled signals.
 
-    According to footcite:`Lindquist407676`, removal of confounds will be done
+    According to :footcite:`Lindquist407676`, removal of confounds will be done
     orthogonally to temporal filters (low- and/or high-pass filters), if both
     are specified.
 
@@ -504,10 +504,10 @@ def clean(signals, runs=None, detrend=True, standardize='zscore',
     Notes
     -----
     Confounds removal is based on a projection on the orthogonal
-    of the signal space. See footcite:`Friston1994`.
+    of the signal space. See :footcite:`Friston1994`.
 
     Orthogonalization between temporal filters and confound removal is based on
-    suggestions in footcite:`Lindquist407676`.
+    suggestions in :footcite:`Lindquist407676`.
 
     References
     ----------
