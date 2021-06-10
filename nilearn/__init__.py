aa3a46ac40418d1c2789a7315bfb06cfd0901ad8
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-06-10 10:05:52 +0200

    Deprecate Nibabel 2.x

--- nilearn/__init__.py
+++ nilearn/__init__.py
@@ -34,6 +34,8 @@ signal                  --- Set of preprocessing functions for time series
 
 import gzip
 import os
+import pkg_resources
+import warnings
 
 from distutils.version import LooseVersion
 
@@ -45,7 +47,32 @@ from .version import _check_module_dependencies, __version__
 # see also https://github.com/scikit-learn/scikit-learn/pull/15020
 os.environ.setdefault("KMP_INIT_AT_FORK", "FALSE")
 
+
+def _nibabel2_deprecation_warning():
+    msg = ('Support for Nibabel 2.x is deprecated and will stop '
+           'in release 0.9.0. Please consider upgrading to '
+           'Nibabel 3.x.')
+    warnings.filterwarnings('once', message=msg)
+    warnings.warn(message=msg,
+                  category=FutureWarning,
+                  stacklevel=3)
+
+
+def _nibabel_deprecation_warnings():
+    """Give a deprecation warning is the version of
+    Nibabel is < 3.0.0.
+    """
+    # Nibabel should be installed or we would
+    # have had an error when calling
+    # _check_module_dependencies
+    dist = pkg_resources.get_distribution('nibabel')
+    nib_version = LooseVersion(dist.version)
+    if nib_version < '3.0':
+        _nibabel2_deprecation_warning()
+
+
 _check_module_dependencies()
+_nibabel_deprecation_warnings()
 
 # Temporary work around to address formatting issues in doc tests
 # with NumPy 1.14. NumPy had made more consistent str/repr formatting
