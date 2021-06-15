7e67dd7ff97cf8098c0531724a6e5a7e2be676bd
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-06-15 17:36:22 +0200

    Add more parameters to standards

--- nilearn/_utils/docs.py
+++ nilearn/_utils/docs.py
@@ -18,8 +18,7 @@ docdict = dict()
 # Verbose
 docdict['verbose'] = """
 verbose : int, optional
-    Verbosity level (0 means no message).
-    Default=1."""
+    Verbosity level (0 means no message)."""
 
 # Resume
 docdict['resume'] = """
@@ -39,7 +38,73 @@ url : string, optional
     URL of file to download.
     Override download URL. Used for test only (or if you
     setup a mirror of the data).
-    Default: None."""
+    Default=None."""
+
+# Smoothing_fwhm
+docdict['smoothing_fwhm'] = """
+smoothing_fwhm : float, optional.
+    If smoothing_fwhm is not None, it gives the size in millimeters of the
+    spatial smoothing to apply to the signal.
+    Default=None."""
+
+# Standardize
+docdict['standardize'] = """
+standardize : bool, optional.
+    If standardize is True, the time-series are centered and normed:
+    their variance is put to 1 in the time dimension.
+    Default=True."""
+
+# Target_affine
+docdict['target_affine'] = """
+target_affine: 3x3 or 4x4 matrix, optional.
+    This parameter is passed to image.resample_img. Please see the
+    related documentation for details.
+    Default=None."""
+
+# Target_shape
+docdict['target_shape'] = """
+target_shape: 3-tuple of int, optional.
+    This parameter is passed to image.resample_img. Please see the
+    related documentation for details.
+    Default=None."""
+
+# Low_pass
+docdict['low_pass'] = """
+low_pass: float, optional
+    Low cutoff frequency in Hertz.
+    Default=None."""
+
+# High pass
+docdict['high_pass'] = """
+high_pass: float, optional
+    High cutoff frequency in Hertz.
+    Default=None."""
+
+# t_r
+docdict['t_r'] = """
+t_r: float, optional
+    Repetition time, in second (sampling period). Set to None if not.
+    Default=None."""
+
+# Memory
+docdict['memory'] = """
+memory : instance of joblib.Memory or str
+    Used to cache the masking process.
+    By default, no caching is done. If a str is given, it is the
+    path to the caching directory."""
+
+# Memory_level
+docdict['memory_level'] = """
+memory_level: int, optional.
+    Rough estimator of the amount of memory used by caching. Higher value
+    means more memory for caching.
+    Default=0."""
+
+# n_jobs
+docdict['n_jobs'] = """
+n_jobs : int, optional.
+    The number of CPUs to use to do the computation. -1 means 'all CPUs'.."""
+
 
 docdict_indented = {}
 
