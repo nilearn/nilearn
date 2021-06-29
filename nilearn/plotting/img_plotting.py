d9290149e2a0875ee8727802a9ce026ff14e7d39
Gensollen <nicolas.gensollen@gmail.com>
2021-06-29 15:32:43 +0200

    [circle full] Fix thresholding (#2892)

--- nilearn/plotting/img_plotting.py
+++ nilearn/plotting/img_plotting.py
@@ -402,7 +402,8 @@ class _MNI152Template(SpatialImage):
             anat_img = reorder_img(anat_img)
             data = get_data(anat_img)
             data = data.astype(np.float64)
-            anat_mask = ndimage.morphology.binary_fill_holes(data > 0)
+            anat_mask = ndimage.morphology.binary_fill_holes(
+                data > np.finfo(float).eps)
             data = np.ma.masked_array(data, np.logical_not(anat_mask))
             self._affine = anat_img.affine
             self.data = data
