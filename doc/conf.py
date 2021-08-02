c30d623165722f9e8420014e42f0ef463be58eb6
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-08-02 15:51:35 +0200

    [circle full] fix numpy link

--- doc/conf.py
+++ doc/conf.py
@@ -307,7 +307,7 @@ _python_doc_base = 'https://docs.python.org/3.8'
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'python': (_python_doc_base, None),
-    'numpy': ('https://docs.scipy.org/doc/numpy', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
     'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
     'matplotlib': ('https://matplotlib.org/', None),
     'sklearn': ('https://scikit-learn.org/stable/', None),
