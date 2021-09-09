fbae11ed953c92cf4755bebdb2c4376be307b288
Taylor Salo <tsalo006@fiu.edu>
2021-09-09 12:53:57 -0400

    Install nilearn requirements in install.sh.

--- build_tools/github/install.sh
+++ build_tools/github/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 
 python setup.py build
-python setup.py install
+if [ ! -z "$MIN_REQUIREMENTS" ]; then
+    pip install --progress-bar off --upgrade -r -e .
+else
+    pip install --progress-bar off --upgrade -r -e .[dev]
+fi
