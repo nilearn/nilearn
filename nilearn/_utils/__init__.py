07bf8d6c13bdc853e64186265c70f82a5eb5e948
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-09-27 17:24:23 +0200

    Add possibility to filter by modules

--- nilearn/_utils/__init__.py
+++ nilearn/_utils/__init__.py
@@ -29,10 +29,15 @@ __all__ = ['check_niimg', 'check_niimg_3d', 'concat_niimgs', 'check_niimg_4d',
            ]
 
 
-def all_modules(modules_to_ignore=None):
+def all_modules(modules_to_ignore=None, modules_to_consider=None):
     """Get a list of all modules from nilearn.
 
     This function returns a list of all modules from Nilearn.
+
+    .. note::
+
+        ``modules_to_ignore`` and ``modules_to_consider`` cannot
+        be specified simultaneously.
 
     Parameters
     ----------
@@ -40,11 +45,22 @@ def all_modules(modules_to_ignore=None):
         List of modules to exclude from the listing.
         Default=None.
 
+    modules_to_consider : List/Set of :obj:`str` or None
+        List of modules to include in the listing.
+        Default=None.
+
     Returns
     -------
     all_modules : List of :obj:`str`
         List of modules.
     """
+    if(
+        modules_to_ignore is not None and modules_to_consider is not None
+    ):
+        raise ValueError(
+            "`modules_to_ignore` and `modules_to_consider` "
+            "cannot be both specified."
+        )
     if modules_to_ignore is None:
         modules_to_ignore = {
             "data",
@@ -59,19 +75,31 @@ def all_modules(modules_to_ignore=None):
             path=[root], prefix="nilearn."
         ):
             mod_parts = modname.split(".")
-            if(
-                any(part in modules_to_ignore for part in mod_parts)
-                or "._" in modname
-            ):
-                continue
-            all_modules.append(modname)
+            if modules_to_consider is not None:
+                if mod_parts[-2] in modules_to_consider:
+                    all_modules.append(modname)
+                    continue
+            else:
+                if(
+                    any(part in modules_to_ignore for part in mod_parts)
+                    or "._" in modname
+                ):
+                    continue
+                all_modules.append(modname)
     return all_modules
 
 
-def all_functions(return_private=False, modules_to_ignore=None):
+def all_functions(return_private=False,
+                  modules_to_ignore=None,
+                  modules_to_consider=None):
     """Get a list of all functions from nilearn.
 
     This function returns a list of all functions defined in Nilearn.
+
+    .. note::
+
+        ``modules_to_ignore`` and ``modules_to_consider`` cannot
+        be specified simultaneously.
 
     Parameters
     ----------
@@ -83,6 +111,10 @@ def all_functions(return_private=False, modules_to_ignore=None):
         List of modules to exclude from the listing.
         Default=None.
 
+    modules_to_consider : List/Set of :obj:`str` or None
+        List of modules to consider for the listing.
+        Default=None.
+
     Returns
     -------
     all_functions : List of Tuples (:obj:`str`, callable)
@@ -91,7 +123,11 @@ def all_functions(return_private=False, modules_to_ignore=None):
         and the second element is the function itself.
     """
     all_functions = []
-    for modname in all_modules(modules_to_ignore=modules_to_ignore):
+    modules = all_modules(
+        modules_to_ignore=modules_to_ignore,
+        modules_to_consider=modules_to_consider,
+    )
+    for modname in modules:
         module = import_module(modname)
         functions = [
             (name, func) for name, func in inspect.getmembers(
@@ -108,10 +144,17 @@ def all_functions(return_private=False, modules_to_ignore=None):
     return all_functions
 
 
-def all_classes(return_private=False, modules_to_ignore=None):
+def all_classes(return_private=False,
+                modules_to_ignore=None,
+                modules_to_consider=None):
     """Get a list of all classes from nilearn.
 
     This function returns a list of all classes defined in Nilearn.
+
+    .. note::
+
+        ``modules_to_ignore`` and ``modules_to_consider`` cannot
+        be specified simultaneously.
 
     Parameters
     ----------
@@ -123,6 +166,10 @@ def all_classes(return_private=False, modules_to_ignore=None):
         List of modules to exclude from the listing.
         Default=None.
 
+    modules_to_consider : List/Set of :obj:`str` or None
+        List of modules to consider for the listing.
+        Default=None.
+
     Returns
     -------
     all_classes : List of Tuples (:obj:`str`, callable)
@@ -131,7 +178,11 @@ def all_classes(return_private=False, modules_to_ignore=None):
         and the second element is the class itself.
     """
     all_classes = []
-    for modname in all_modules(modules_to_ignore=modules_to_ignore):
+    modules = all_modules(
+        modules_to_ignore=modules_to_ignore,
+        modules_to_consider=modules_to_consider,
+    )
+    for modname in modules:
         module = import_module(modname)
         classes = [
             (name, cls) for name, cls in inspect.getmembers(
