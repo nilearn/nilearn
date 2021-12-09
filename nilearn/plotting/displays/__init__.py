9758d7382c8ff488da1b350c5be7dddbc3c78089
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-12-09 12:09:40 +0100

    relative -> absolute

--- nilearn/plotting/displays/__init__.py
+++ nilearn/plotting/displays/__init__.py
@@ -5,23 +5,24 @@ These objects are returned by plotting functions
 from the :mod:`~nilearn.plotting` module.
 """
 
-from .axes import BaseAxes, CutAxes, GlassBrainAxes
-
-from .projectors import (
+from nilearn.plotting.displays.axes import (
+    BaseAxes, CutAxes, GlassBrainAxes
+)
+from nilearn.plotting.displays.projectors import (
     OrthoProjector, XZProjector, YZProjector,
     YXProjector, XProjector, YProjector, ZProjector,
     LZRYProjector, LYRZProjector, LYRProjector,
     LZRProjector, LRProjector, LProjector, RProjector,
     get_projector,
 )
-
-from .slicers import (
+from nilearn.plotting.displays.slicers import (
     OrthoSlicer, TiledSlicer, MosaicSlicer, BaseSlicer,
     XZSlicer, YZSlicer, YXSlicer, XSlicer, YSlicer,
     ZSlicer, get_slicer,
 )
-
-from .figures import SurfaceFigure, PlotlySurfaceFigure
+from nilearn.plotting.displays.figures import (
+    SurfaceFigure, PlotlySurfaceFigure
+)
 
 __all__ = ["BaseAxes", "CutAxes", "GlassBrainAxes",
            "OrthoProjector", "XZProjector", "YZProjector",
