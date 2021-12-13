9fbb5991d2430fc87b3c4c5d95ac3b6a1ffe7389
Taylor Salo <tsalo006@fiu.edu>
2021-12-13 14:35:52 -0500

    Update install.sh

--- build_tools/github/install.sh
+++ build_tools/github/install.sh
@@ -4,7 +4,7 @@ python setup.py build
 if [ ! -z "$MIN_REQUIREMENTS" ]; then
     # Install the oldest supported versions of all required dependencies
     # See setup.cfg for dependency group options
-    pip install --progress-bar off --upgrade -e .[min]
+    pip install --progress-bar off --upgrade -e .[min,test]
 else
     # Install the newest supported versions of required and testing-related dependencies
     pip install --progress-bar off --upgrade -e .[test]
