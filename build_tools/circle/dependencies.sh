4839feefbd52df03d8fdbe39c895aa080ccdac99
Taylor Salo <tsalo006@fiu.edu>
2021-12-17 12:11:11 -0500

    Update dependencies.sh

--- build_tools/circle/dependencies.sh
+++ build_tools/circle/dependencies.sh
@@ -7,5 +7,6 @@ conda install -n testenv -yq python=3.8
 source activate testenv
 python -m pip install --user --upgrade --progress-bar off pip setuptools
 # Install the local version of the library, along with both standard and testing-related dependencies
+# The `doc` dependency group is included because the build_docs job uses this script.
 # See setup.cfg for dependency group options
-python -m pip install .[plotting,plotly,test]
+python -m pip install .[plotting,plotly,test,doc]
