4e9ef3b4d2980d8c31d14a828c3387a97b17e523
bthirion <bertrand.thirion@inria.fr>
2022-01-24 10:32:38 +0100

    [FIX] fixed code + added test (#3137)
    * fixed code + added test
    
    * pep8
    
    * Added to whatsnew
    
    * string fix

--- nilearn/glm/tests/test_thresholding.py
+++ nilearn/glm/tests/test_thresholding.py
@@ -24,6 +24,11 @@ def test_fdr():
         fdr_threshold(x, -.1)
     with pytest.raises(ValueError):
         fdr_threshold(x, 1.5)
+    # addresses #2879
+    n = 10
+    pvals = np.linspace(1 / n, 1, n)
+    pvals[0] = 0.007
+    assert np.isfinite(fdr_threshold(norm.isf(pvals), .1))
 
 
 def test_threshold_stats_img():
