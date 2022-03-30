5dcf57cc7e56c38db0210757a82efb18fd3c6b66
Raphael Meudec <raphael.meudec@gmail.com>
2022-03-30 19:01:51 +0200

    Update nilearn/reporting/_get_clusters_table.py
    Co-authored-by: Taylor Salo <tsalo006@fiu.edu>

--- nilearn/reporting/_get_clusters_table.py
+++ nilearn/reporting/_get_clusters_table.py
@@ -155,8 +155,8 @@ def get_clusters_table(stat_img, stat_threshold, cluster_threshold=None,
         Minimum distance between subpeaks in mm. Default=8mm.
 
         .. note::
-            If two different clusters are closer than `min_distance`, it can
-            result in peaks closer than `min_distance`.
+            If two different clusters are closer than ``min_distance``, it can
+            result in peaks closer than ``min_distance``.
 
     Returns
     -------
