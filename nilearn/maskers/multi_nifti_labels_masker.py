4407382046789c5242bc74ab2b98906c2f709a99
ymzayek <yasmin.mzayek@inria.fr>
2022-04-27 13:34:00 +0200

    added parameters corresponding to current version of NiftiLabelsMasker

--- nilearn/maskers/multi_nifti_labels_masker.py
+++ nilearn/maskers/multi_nifti_labels_masker.py
@@ -24,6 +24,12 @@ class MultiNiftiLabelsMasker(NiftiLabelsMasker, CacheMixin):
         See http://nilearn.github.io/manipulating_images/input_output.html
         Region definitions, as one image of labels.
 
+    labels : list of str, optional
+        Full labels corresponding to the labels image. This is used
+        to improve reporting quality if provided.
+        Warning: The labels must be consistent with the label
+        values provided through `labels_img`.
+
     background_label: number, optional
         Label used in labels_img to represent background.
 
@@ -107,14 +113,15 @@ class MultiNiftiLabelsMasker(NiftiLabelsMasker, CacheMixin):
 
     """
 
-    def __init__(self, labels_img, background_label=0, mask_img=None,
-                 smoothing_fwhm=None, standardize=False,
-                 standardize_confounds=True, detrend=False, low_pass=None,
-                 high_pass=None, t_r=None, dtype=None,
-                 resampling_target="data",
+    def __init__(self, labels_img, labels=None, background_label=0,
+                 mask_img=None, smoothing_fwhm=None, standardize=False,
+                 standardize_confounds=True, high_variance_confounds=False,
+                 detrend=False, low_pass=None, high_pass=None, t_r=None,
+                 dtype=None, resampling_target="data",
                  memory=Memory(cachedir=None, verbose=0), memory_level=1,
-                 n_jobs=1, verbose=0, strategy="mean"):
+                 n_jobs=1, verbose=0, strategy="mean", reports=True):
         self.labels_img = labels_img
+        self.labels = labels
         self.background_label = background_label
         self.mask_img = mask_img
 
@@ -124,6 +131,7 @@ class MultiNiftiLabelsMasker(NiftiLabelsMasker, CacheMixin):
         # Parameters for clean()
         self.standardize = standardize
         self.standardize_confounds = standardize_confounds
+        self.high_variance_confounds = high_variance_confounds
         self.detrend = detrend
         self.low_pass = low_pass
         self.high_pass = high_pass
@@ -138,6 +146,12 @@ class MultiNiftiLabelsMasker(NiftiLabelsMasker, CacheMixin):
         self.memory_level = memory_level
         self.n_jobs = n_jobs
         self.verbose = verbose
+        self.reports = reports
+        self._report_content = dict()
+        self._report_content['description'] = (
+            'This reports shows the regions '
+            'defined by the labels of the mask.')
+        self._report_content['warning_message'] = None
 
         available_reduction_strategies = {'mean', 'median', 'sum',
                                           'minimum', 'maximum',
@@ -154,7 +168,8 @@ class MultiNiftiLabelsMasker(NiftiLabelsMasker, CacheMixin):
             raise ValueError("invalid value for 'resampling_target' "
                              "parameter: {}".format(resampling_target))
 
-    def transform_imgs(self, imgs_list, confounds=None, n_jobs=1):
+    def transform_imgs(self, imgs_list, confounds=None, n_jobs=1,
+                       sample_mask=None):
         """Extract signals from a list of 4D niimgs.
 
         Parameters
@@ -167,6 +182,14 @@ class MultiNiftiLabelsMasker(NiftiLabelsMasker, CacheMixin):
             This parameter is passed to signal.clean. Please see the related
             documentation for details.
             shape: list of (number of scans, number of confounds)
+
+        sample_mask : Any type compatible with numpy-array indexing, optional
+            shape: (number of scans - number of volumes removed, )
+            Masks the niimgs along time/fourth dimension to perform scrubbing
+            (remove volumes with high motion) and/or non-steady-state volumes.
+            This parameter is passed to signal.clean.
+
+                .. versionadded:: 0.8.0
 
         Returns
         -------
@@ -197,11 +220,12 @@ class MultiNiftiLabelsMasker(NiftiLabelsMasker, CacheMixin):
 
         region_signals = Parallel(n_jobs=n_jobs)(
                                  delayed(func)
-                                 (imgs=imgs, confounds=cfs)
+                                 (imgs=imgs, confounds=cfs,
+                                  sample_mask=sample_mask)
                                  for imgs, cfs in zip(niimg_iter, confounds))
         return region_signals
 
-    def transform(self, imgs, confounds=None):
+    def transform(self, imgs, confounds=None, sample_mask=None):
         """ Apply mask, spatial and temporal preprocessing
 
         Parameters
@@ -214,6 +238,14 @@ class MultiNiftiLabelsMasker(NiftiLabelsMasker, CacheMixin):
             This parameter is passed to signal.clean. Please see the
             corresponding documentation for details.
 
+        sample_mask : Any type compatible with numpy-array indexing, optional
+            shape: (number of scans - number of volumes removed, )
+            Masks the niimgs along time/fourth dimension to perform scrubbing
+            (remove volumes with high motion) and/or non-steady-state volumes.
+            This parameter is passed to signal.clean.
+
+                .. versionadded:: 0.8.0
+
         Returns
         -------
         data: {list of numpy arrays}
@@ -225,4 +257,5 @@ class MultiNiftiLabelsMasker(NiftiLabelsMasker, CacheMixin):
         if (not hasattr(imgs, '__iter__')
                 or isinstance(imgs, str)):
             return self.transform_single_imgs(imgs)
-        return self.transform_imgs(imgs, confounds, n_jobs=self.n_jobs)
+        return self.transform_imgs(imgs, confounds, n_jobs=self.n_jobs,
+                                   sample_mask=sample_mask)
