225d0446d9080e37c6b47ff676c0ef0ab5a91673
ymzayek <yasmin.mzayek@inria.fr>
2022-04-29 11:59:06 +0200

    added script for multi_nifit_maps_masker

--- nilearn/maskers/__init__.py
+++ nilearn/maskers/__init__.py
@@ -8,9 +8,10 @@ from .multi_nifti_masker import MultiNiftiMasker
 from .nifti_labels_masker import NiftiLabelsMasker
 from .multi_nifti_labels_masker import MultiNiftiLabelsMasker
 from .nifti_maps_masker import NiftiMapsMasker
+from .multi_nifti_maps_masker import MultiNiftiMapsMasker
 from .nifti_spheres_masker import NiftiSpheresMasker
 
 
 __all__ = ['BaseMasker', 'NiftiMasker', 'MultiNiftiMasker',
            'NiftiLabelsMasker', 'MultiNiftiLabelsMasker', 'NiftiMapsMasker',
-           'NiftiSpheresMasker']
+           'MultiNiftiMapsMasker', 'NiftiSpheresMasker']
