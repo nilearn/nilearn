2448eb1371d6289fe55b836f18be0339924882c7
ymzayek <yasmin.mzayek@inria.fr>
2022-10-14 17:40:27 +0200

    Use full path to testenv

--- build_tools/github/build_dependencies.sh
+++ build_tools/github/build_dependencies.sh
@@ -4,7 +4,7 @@ conda init bash
 echo "conda version = $(conda --version)"
 conda create -n testenv
 conda install -n testenv -yq python=3.8
-conda activate testenv
+source activate /usr/share/miniconda/envs/testenv
 python -m pip install --user --upgrade --progress-bar off pip setuptools
 # Install the local version of the library, along with both standard and testing-related dependencies
 # The `doc` dependency group is included because the build_docs job uses this script.
