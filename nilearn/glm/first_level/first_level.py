605fe72afd55e64d865d18d7ba9bcd67d625fd89
Kevin Sitek <sitek.kevin@gmail.com>
2022-11-03 14:58:36 -0400

    Update nilearn/glm/first_level/first_level.py
    Co-authored-by: Yasmin <63292494+ymzayek@users.noreply.github.com>

--- nilearn/glm/first_level/first_level.py
+++ nilearn/glm/first_level/first_level.py
@@ -835,7 +835,6 @@ def first_level_from_bids(dataset_path, task_label, space_label=None,
     sub_labels : list of str, optional
         Specifies the subset of subject labels to model.
         If 'None', will model all subjects in the dataset.
-        
         .. versionadded:: 0.9.3.dev
 
     img_filters : list of tuples (str, str), optional
