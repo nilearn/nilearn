fae77699ac3bf6695f15c086b750a2ccc4e3e0b1
ymzayek <yasmin.mzayek@inria.fr>
2023-02-03 09:55:26 +0100

    Add note

--- build_tools/github/build_docs_dependencies.sh
+++ build_tools/github/build_docs_dependencies.sh
@@ -3,6 +3,7 @@
 conda init bash
 echo "conda version = $(conda --version)"
 conda create -n testenv
+# Specify Python 3.9 version because pip defaults to installing PyPy3.9 otherwise
 conda install -n testenv -yq python=3.9.15
 source activate testenv
 python -m pip install --user --upgrade --progress-bar off pip setuptools
