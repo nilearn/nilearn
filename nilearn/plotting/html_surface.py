f65c43e278794d5f494ed506b2aa6d268d9f497a
mehrdadbn9 <80095851+mehrdadbn9@users.noreply.github.com>
2023-02-15 19:23:27 +0330

    Update html_surface.py

--- nilearn/plotting/html_surface.py
+++ nilearn/plotting/html_surface.py
@@ -38,9 +38,10 @@ def _get_vertexcolor(surf_map, cmap, norm,
     return to_color_strings(vertexcolor)
 
 
-def _one_mesh_info(surf_map, surf_mesh, threshold=None, cmap=cm.cold_hot,
+def one_mesh_info(surf_map, surf_mesh, threshold=None, cmap=cm.cold_hot,
                   black_bg=False, bg_map=None, symmetric_cmap=True,
                   vmax=None, vmin=None):
+    
     """Prepare info for plotting one surface map on a single mesh.
 
     This computes the dictionary that gets inserted in the web page,
@@ -48,6 +49,7 @@ def _one_mesh_info(surf_map, surf_mesh, threshold=None, cmap=cm.cold_hot,
     background color.
 
     """
+    warnings.warn(category=DeprecationWarning, message="insert message here")
     info = {}
     colors = colorscale(
         cmap, surf_map, threshold, symmetric_cmap=symmetric_cmap,
@@ -60,7 +62,9 @@ def _one_mesh_info(surf_map, surf_mesh, threshold=None, cmap=cm.cold_hot,
     info['black_bg'] = black_bg
     info['full_brain_mesh'] = False
     info['colorscale'] = colors['colors']
-    return info
+    return _one_mesh_info(surf_map, surf_mesh, threshold=threshold,
+                          cmap=cmap, black_bg=black_bg, bg_map=bg_map,
+                          symmetric_cmap=symmetric_cmap, vmax=vmax, vmin=vmin)
 
 
 def _check_mesh(mesh):
@@ -78,7 +82,7 @@ def _check_mesh(mesh):
     return mesh
 
 
-def _full_brain_info(volume_img, mesh='fsaverage5', threshold=None,
+def full_brain_info(volume_img, mesh='fsaverage5', threshold=None,
                     cmap=cm.cold_hot, black_bg=False, symmetric_cmap=True,
                     vmax=None, vmin=None, vol_to_surf_kwargs={}):
     """Project 3D map on cortex; prepare info to plot both hemispheres.
@@ -88,6 +92,7 @@ def _full_brain_info(volume_img, mesh='fsaverage5', threshold=None,
     background color.
 
     """
+    warnings.warn(category=DeprecationWarning, message="insert message here")
     info = {}
     mesh = surface.surface._check_mesh(mesh)
     surface_maps = {
@@ -114,7 +119,9 @@ def _full_brain_info(volume_img, mesh='fsaverage5', threshold=None,
     info['black_bg'] = black_bg
     info['full_brain_mesh'] = True
     info['colorscale'] = colors['colors']
-    return info
+    return _full_brain_info(volume_img, mesh='fsaverage5', threshold=threshold,
+                    cmap=cmap, black_bg=black_bg, symmetric_cmap=symmetric_cmap,
+                    vmax=vmax, vmin=vmin, vol_to_surf_kwargs=vol_to_surf_kwargs)
 
 
 def _fill_html_template(info, embed_js=True):
