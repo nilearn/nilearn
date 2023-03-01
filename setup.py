4e23bd3bcacc8e3def4d68100730a73128c4b4b5
Remi Gau <remi_gau@hotmail.com>
2023-03-01 11:30:32 +0100

    add python 3.11

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
         ],
         packages=find_packages(),
         package_data={
