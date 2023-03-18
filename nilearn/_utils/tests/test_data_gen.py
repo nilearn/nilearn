0a065812c0cefe3602c46cf25bb14f654b8ca6d9
Remi Gau <remi_gau@hotmail.com>
2023-03-18 20:20:50 +0100

    fix

--- nilearn/_utils/tests/test_data_gen.py
+++ nilearn/_utils/tests/test_data_gen.py
@@ -7,6 +7,8 @@ from nilearn._utils.data_gen import add_metadata_to_bids_derivatives
 
 def test_add_metadata_to_bids_derivatives(tmp_path):
     # bare bone smoke test
+    target_dir = tmp_path / 'derivatives' / 'sub-01' / 'ses-01' / 'func'
+    target_dir.mkdir(parents=True)
     json_file = add_metadata_to_bids_derivatives(bids_path=tmp_path,
                                                   metadata={"foo": "bar"})
     assert json_file.exists()
