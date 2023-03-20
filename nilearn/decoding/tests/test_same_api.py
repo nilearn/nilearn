d87fe36925ebcba5b5a3f8b31a8a904b7369c794
Remi Gau <remi_gau@hotmail.com>
2023-03-20 15:13:46 +0100

    parametrize test same api

--- nilearn/decoding/tests/test_same_api.py
+++ nilearn/decoding/tests/test_same_api.py
@@ -5,6 +5,7 @@ for computing image gradient, loss functions, etc.
 
 import nibabel
 import numpy as np
+import pytest
 from nilearn.decoding.objective_functions import (
     _logistic_loss_lipschitz_constant,
     _squared_loss,
@@ -106,7 +107,10 @@ def test_lipschitz_constant_loss_logreg():
     assert a == b
 
 
-def test_graph_net_and_tvl1_same_for_pure_l1(max_iter=100, decimal=2):
+@pytest.mark.parametrize("standardize", [True, False])
+def test_graph_net_and_tvl1_same_for_pure_l1(
+    standardize, max_iter=100, decimal=2
+):
     ###############################################################
     # graph_net_solver and tvl1_solver should give same results
     # when l1_ratio = 1.
@@ -120,42 +124,56 @@ def test_graph_net_and_tvl1_same_for_pure_l1(max_iter=100, decimal=2):
 
     # results should be exactly the same for pure lasso
     a = tvl1_solver(
-        unmasked_X, y, alpha, 1.0, mask, loss="mse", max_iter=max_iter
+        unmasked_X,
+        y,
+        alpha,
+        1.0,
+        mask,
+        loss="mse",
+        max_iter=max_iter,
+        verbose=0,
     )[0]
     b = _graph_net_squared_loss(
-        unmasked_X, y, alpha, 1.0, max_iter=max_iter, mask=mask
+        unmasked_X, y, alpha, 1.0, max_iter=max_iter, mask=mask, verbose=0
     )[0]
+
+    # Should be exactly the same (except for numerical errors).
+    # However because of the TV-L1 prox approx, results might be 'slightly'
+    # different.
+    np.testing.assert_array_almost_equal(a, b, decimal=decimal)
 
     mask = nibabel.Nifti1Image(mask.astype(np.float64), np.eye(4))
     X = nibabel.Nifti1Image(X.astype(np.float64), np.eye(4))
-    for standardize in [True, False]:
-        sl = BaseSpaceNet(
-            alphas=alpha,
-            l1_ratios=1.0,
-            mask=mask,
-            penalty="graph-net",
-            max_iter=max_iter,
-            standardize=standardize,
-        ).fit(X, y)
-        tvl1 = BaseSpaceNet(
-            alphas=alpha,
-            l1_ratios=1.0,
-            mask=mask,
-            penalty="tv-l1",
-            max_iter=max_iter,
-            standardize=standardize,
-        ).fit(X, y)
 
-        # Should be exactly the same (except for numerical errors).
-        # However because of the TV-L1 prox approx, results might be 'slightly'
-        # different.
-        np.testing.assert_array_almost_equal(a, b, decimal=decimal)
-        np.testing.assert_array_almost_equal(
-            sl.coef_, tvl1.coef_, decimal=decimal
-        )
+    sl = BaseSpaceNet(
+        alphas=alpha,
+        l1_ratios=1.0,
+        mask=mask,
+        penalty="graph-net",
+        max_iter=max_iter,
+        standardize=standardize,
+        verbose=0,
+    ).fit(X, y)
+    tvl1 = BaseSpaceNet(
+        alphas=alpha,
+        l1_ratios=1.0,
+        mask=mask,
+        penalty="tv-l1",
+        max_iter=max_iter,
+        standardize=standardize,
+        verbose=0,
+    ).fit(X, y)
+
+    # Should be exactly the same (except for numerical errors).
+    # However because of the TV-L1 prox approx, results might be 'slightly'
+    # different.
+    np.testing.assert_array_almost_equal(sl.coef_, tvl1.coef_, decimal=decimal)
 
 
-def test_graph_net_and_tvl1_same_for_pure_l1_logistic(max_iter=20, decimal=2):
+@pytest.mark.parametrize("standardize", [True, False])
+def test_graph_net_and_tvl1_same_for_pure_l1_logistic(
+    standardize, max_iter=20, decimal=2
+):
     ###############################################################
     # graph_net_solver and tvl1_solver should give same results
     # when l1_ratio = 1.
@@ -169,36 +187,52 @@ def test_graph_net_and_tvl1_same_for_pure_l1_logistic(max_iter=20, decimal=2):
     mask = get_data(mask_).astype(bool).ravel()
 
     # results should be exactly the same for pure lasso
-    a = _graph_net_logistic(X, y, alpha, 1.0, mask=mask, max_iter=max_iter)[0]
-    b = tvl1_solver(
-        X, y, alpha, 1.0, loss="logistic", mask=mask, max_iter=max_iter
+    a = _graph_net_logistic(
+        X, y, alpha, 1.0, mask=mask, max_iter=max_iter, verbose=0
     )[0]
-    for standardize in [True, False]:
-        sl = SpaceNetClassifier(
-            alphas=alpha,
-            l1_ratios=1.0,
-            max_iter=max_iter,
-            mask=mask_,
-            penalty="graph-net",
-            standardize=standardize,
-        ).fit(X_, y)
-        tvl1 = SpaceNetClassifier(
-            alphas=alpha,
-            l1_ratios=1.0,
-            max_iter=max_iter,
-            mask=mask_,
-            penalty="tv-l1",
-            standardize=standardize,
-        ).fit(X_, y)
+    b = tvl1_solver(
+        X,
+        y,
+        alpha,
+        1.0,
+        loss="logistic",
+        mask=mask,
+        max_iter=max_iter,
+        verbose=0,
+    )[0]
 
     # should be exactly the same (except for numerical errors)
     np.testing.assert_array_almost_equal(a, b, decimal=decimal)
+
+    sl = SpaceNetClassifier(
+        alphas=alpha,
+        l1_ratios=1.0,
+        max_iter=max_iter,
+        mask=mask_,
+        penalty="graph-net",
+        standardize=standardize,
+        verbose=0,
+    ).fit(X_, y)
+    tvl1 = SpaceNetClassifier(
+        alphas=alpha,
+        l1_ratios=1.0,
+        max_iter=max_iter,
+        mask=mask_,
+        penalty="tv-l1",
+        standardize=standardize,
+        verbose=0,
+    ).fit(X_, y)
+
+    # should be exactly the same (except for numerical errors)
     np.testing.assert_array_almost_equal(
         sl.coef_[0], tvl1.coef_[0], decimal=decimal
     )
 
 
-def test_graph_net_and_tv_same_for_pure_l1_another_test(decimal=1):
+@pytest.mark.parametrize("standardize", [True, False])
+def test_graph_net_and_tv_same_for_pure_l1_another_test(
+    standardize, decimal=1
+):
     ###############################################################
     # graph_net_solver and tvl1_solver should give same results
     # when l1_ratio = 1.
@@ -211,39 +245,40 @@ def test_graph_net_and_tv_same_for_pure_l1_another_test(decimal=1):
     l1_ratio = 1.0
     max_iter = 20
 
-    for standardize in [True, False]:
-        sl = BaseSpaceNet(
-            alphas=alpha,
-            l1_ratios=l1_ratio,
-            penalty="graph-net",
-            max_iter=max_iter,
-            mask=mask,
-            is_classif=False,
-            standardize=standardize,
-            verbose=0,
-        ).fit(X, y)
-        tvl1 = BaseSpaceNet(
-            alphas=alpha,
-            l1_ratios=l1_ratio,
-            penalty="tv-l1",
-            max_iter=max_iter,
-            mask=mask,
-            is_classif=False,
-            standardize=standardize,
-            verbose=0,
-        ).fit(X, y)
+    sl = BaseSpaceNet(
+        alphas=alpha,
+        l1_ratios=l1_ratio,
+        penalty="graph-net",
+        max_iter=max_iter,
+        mask=mask,
+        is_classif=False,
+        standardize=standardize,
+        verbose=0,
+    ).fit(X, y)
+    tvl1 = BaseSpaceNet(
+        alphas=alpha,
+        l1_ratios=l1_ratio,
+        penalty="tv-l1",
+        max_iter=max_iter,
+        mask=mask,
+        is_classif=False,
+        standardize=standardize,
+        verbose=0,
+    ).fit(X, y)
 
     # should be exactly the same (except for numerical errors)
     np.testing.assert_array_almost_equal(sl.coef_, tvl1.coef_, decimal=decimal)
 
 
-def test_coef_shape():
+@pytest.mark.parametrize("penalty", ["graph-net", "tv-l1"])
+@pytest.mark.parametrize("cls", [SpaceNetRegressor, SpaceNetClassifier])
+def test_coef_shape(penalty, cls):
     iris = load_iris()
     X, y = iris.data, iris.target
     X, mask = to_niimgs(X, (2, 2, 2))
-    for penalty in ["graph-net", "tv-l1"]:
-        for cls in [SpaceNetRegressor, SpaceNetClassifier]:
-            model = cls(
-                mask=mask, max_iter=3, penalty=penalty, alphas=1.0
-            ).fit(X, y)
-            assert model.coef_.ndim == 2
+
+    model = cls(
+        mask=mask, max_iter=3, penalty=penalty, alphas=1.0, verbose=0
+    ).fit(X, y)
+
+    assert model.coef_.ndim == 2
