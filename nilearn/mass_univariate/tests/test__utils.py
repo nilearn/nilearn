d51ea09ba1a1a2a7c52030f0d59e13a7945f2a71
Remi Gau <remi_gau@hotmail.com>
2023-03-20 19:31:52 +0100

    refactor test

--- nilearn/mass_univariate/tests/test__utils.py
+++ nilearn/mass_univariate/tests/test__utils.py
@@ -128,19 +128,18 @@ def test_null_to_p_array():
     nulldist = np.random.normal(size=N)
     t = np.sort(np.random.normal(size=N))
     p = np.sort(_utils._null_to_p(t, nulldist))
+
     assert p.shape == (N,)
     assert (p < 1).all()
     assert (p > 0).all()
+
     # Resulting distribution should be roughly uniform
     assert np.abs(p.mean() - 0.5) < 0.02
     assert np.abs(p.var() - 1 / 12) < 0.02
 
 
-def test_calculate_cluster_measures():
-    """Test _calculate_cluster_measures."""
-    threshold = 0.001
-    bin_struct = generate_binary_structure(3, 1)
-
+@pytest.fixture
+def test_arr4d():
     test_arr4d = np.zeros((10, 10, 10, 1))
     test_arr4d[:2, :2, :2, 0] = 5  # 8-voxel cluster, high intensity
     test_arr4d[7:, 7:, 7:, 0] = 1  # 27-voxel cluster, low intensity
@@ -148,69 +147,74 @@ def test_calculate_cluster_measures():
     test_arr4d[6, 6, 8, 0] = 1  # edge touching second cluster
     test_arr4d[3:5, 3:5, 3:5, 0] = -10  # negative cluster, very high intensity
     test_arr4d[5:6, 3:5, 3:5, 0] = 1  # cluster touching negative one
+    return test_arr4d
 
-    # One-sided test where largest cluster doesn't have the highest mass
-    true_size = 27
-    true_mass = 39.992  # (8 vox * 5 intensity) - (8 vox * 0.001 thresh)
+
+@pytest.mark.parametrize(
+    "bin_struct, two_sided_test, true_size, true_mass",
+    [
+        (
+            generate_binary_structure(3, 1),
+            False,
+            27,
+            39.992,
+        ),  # One-sided test: largest cluster doesn't have the highest mass
+        (
+            generate_binary_structure(3, 1),
+            True,
+            27,
+            79.992,
+        ),  # Two-sided test where negative cluster has the higher mass
+        (
+            generate_binary_structure(3, 2),
+            True,
+            28,
+            79.992,
+        ),  # Two-sided test with edge connectivity
+        # should include edge-connected single voxel cluster
+        (
+            generate_binary_structure(3, 3),
+            True,
+            29,
+            79.992,
+        ),  # Two-sided test with corner connectivity
+        # should include corner-connected single voxel cluster
+    ],
+)
+def test_calculate_cluster_measures(
+    test_arr4d, bin_struct, two_sided_test, true_size, true_mass
+):
+    """Test _calculate_cluster_measures.
+
+    true_mass : (8 vox * 5 intensity) - (8 vox * 0.001 thresh)
+    """
+    threshold = 0.001
+
     test_size, test_mass = _utils._calculate_cluster_measures(
         test_arr4d,
         threshold=threshold,
         bin_struct=bin_struct,
-        two_sided_test=False,
+        two_sided_test=two_sided_test,
     )
+
     assert test_size[0] == true_size
     assert test_mass[0] == true_mass
 
-    # Two-sided test where negative cluster has the higher mass
-    true_size = 27
-    true_mass = 79.992  # (8 vox * 5 intensity) - (8 vox * 0.001 thresh)
-    test_size, test_mass = _utils._calculate_cluster_measures(
-        test_arr4d,
-        threshold=threshold,
-        bin_struct=bin_struct,
-        two_sided_test=True,
-    )
-    assert test_size[0] == true_size
-    assert test_mass[0] == true_mass
 
-    # Two-sided test with edge connectivity
-    bin_struct = generate_binary_structure(3, 2)
-
-    true_size = 28  # should include edge-connected single voxel cluster
-    true_mass = 79.992  # (8 vox * 5 intensity) - (8 vox * 0.001 thresh)
-    test_size, test_mass = _utils._calculate_cluster_measures(
-        test_arr4d,
-        threshold=threshold,
-        bin_struct=bin_struct,
-        two_sided_test=True,
-    )
-    assert test_size[0] == true_size
-    assert test_mass[0] == true_mass
-
-    # Two-sided test with corner connectivity
-    bin_struct = generate_binary_structure(3, 3)
-
-    true_size = 29  # should include corner-connected single voxel cluster
-    true_mass = 79.992  # (8 vox * 5 intensity) - (8 vox * 0.001 thresh)
-    test_size, test_mass = _utils._calculate_cluster_measures(
-        test_arr4d,
-        threshold=threshold,
-        bin_struct=bin_struct,
-        two_sided_test=True,
-    )
-    assert test_size[0] == true_size
-    assert test_mass[0] == true_mass
-
-    # Test on empty array
+def test_calculate_cluster_measures_on_empty_array():
     test_arr4d = np.zeros((10, 10, 10, 1))
+    threshold = 0.001
+    bin_struct = generate_binary_structure(3, 1)
+
+    test_size, test_mass = _utils._calculate_cluster_measures(
+        test_arr4d,
+        threshold=threshold,
+        bin_struct=bin_struct,
+        two_sided_test=True,
+    )
+
     true_size = 0
     true_mass = 0
-    test_size, test_mass = _utils._calculate_cluster_measures(
-        test_arr4d,
-        threshold=threshold,
-        bin_struct=bin_struct,
-        two_sided_test=True,
-    )
     assert test_size[0] == true_size
     assert test_mass[0] == true_mass
 
