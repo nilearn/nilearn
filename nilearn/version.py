4b4c170e800682f6488b2bd53710b53b190111be
Remi Gau <remi_gau@hotmail.com>
2023-03-23 14:42:26 +0100

    autofix

--- nilearn/version.py
+++ nilearn/version.py
@@ -1,6 +1,5 @@
-"""
-nilearn version, required package versions, and utilities for checking
-"""
+"""nilearn version, required package versions, and utilities for checking."""
+
 # Author: Loic Esteve, Ben Cipollini
 # License: simplified BSD
 
@@ -20,81 +19,99 @@ nilearn version, required package versions, and utilities for checking
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
-__version__ = '0.10.1.dev'
+__version__ = "0.10.1.dev"
 
-_NILEARN_INSTALL_MSG = 'See %s for installation information.' % (
-    'https://nilearn.github.io/stable/introduction.html#installation')
+_NILEARN_INSTALL_MSG = "See %s for installation information." % (
+    "https://nilearn.github.io/stable/introduction.html#installation"
+)
 
 import operator
 
 # This is a tuple to preserve order, so that dependencies are checked
 #   in some meaningful order (more => less 'core').
 REQUIRED_MODULE_METADATA = (
-    ('numpy', {
-        'min_version': '1.19',
-        'required_at_installation': True,
-        'install_info': _NILEARN_INSTALL_MSG}),
-    ('scipy', {
-        'min_version': '1.6',
-        'required_at_installation': True,
-        'install_info': _NILEARN_INSTALL_MSG}),
-    ('sklearn', {
-        'min_version': '1.0.0',
-        'required_at_installation': True,
-        'install_info': _NILEARN_INSTALL_MSG}),
-    ('joblib', {
-        'min_version': '1.0.0',
-        'required_at_installation': True,
-        'install_info': _NILEARN_INSTALL_MSG}),
-    ('nibabel', {
-        'min_version': '3.2',
-        'required_at_installation': False}),
-    ('pandas', {
-        'min_version': '1.1.5',
-        'required_at_installation': True,
-        'install_info': _NILEARN_INSTALL_MSG}),
-    ("requests", {
-        "min_version": "2.25",
-        "required_at_installation": False
-    })
+    (
+        "numpy",
+        {
+            "min_version": "1.19",
+            "required_at_installation": True,
+            "install_info": _NILEARN_INSTALL_MSG,
+        },
+    ),
+    (
+        "scipy",
+        {
+            "min_version": "1.6",
+            "required_at_installation": True,
+            "install_info": _NILEARN_INSTALL_MSG,
+        },
+    ),
+    (
+        "sklearn",
+        {
+            "min_version": "1.0.0",
+            "required_at_installation": True,
+            "install_info": _NILEARN_INSTALL_MSG,
+        },
+    ),
+    (
+        "joblib",
+        {
+            "min_version": "1.0.0",
+            "required_at_installation": True,
+            "install_info": _NILEARN_INSTALL_MSG,
+        },
+    ),
+    ("nibabel", {"min_version": "3.2", "required_at_installation": False}),
+    (
+        "pandas",
+        {
+            "min_version": "1.1.5",
+            "required_at_installation": True,
+            "install_info": _NILEARN_INSTALL_MSG,
+        },
+    ),
+    ("requests", {"min_version": "2.25", "required_at_installation": False}),
 )
 
-OPTIONAL_MATPLOTLIB_MIN_VERSION = '3.3.0'
+OPTIONAL_MATPLOTLIB_MIN_VERSION = "3.3.0"
 
 
 def _import_module_with_version_check(
-        module_name,
-        minimum_version,
-        install_info=None):
+    module_name, minimum_version, install_info=None
+):
     """Check that module is installed with a recent enough version."""
     try:
         module = __import__(module_name)
     except ImportError as exc:
-        user_friendly_info = ('Module "{0}" could not be found. {1}').format(
+        user_friendly_info = ('Module "{}" could not be found. {}').format(
             module_name,
-            install_info or 'Please install it properly to use nilearn.')
+            install_info or "Please install it properly to use nilearn.",
+        )
         exc.args += (user_friendly_info,)
         # Necessary for Python 3 because the repr/str of ImportError
         # objects was changed in Python 3
-        if hasattr(exc, 'msg'):
-            exc.msg += '. ' + user_friendly_info
+        if hasattr(exc, "msg"):
+            exc.msg += ". " + user_friendly_info
         raise
 
     # Avoid choking on modules with no __version__ attribute
-    module_version = getattr(module, '__version__', '0.0.0')
+    module_version = getattr(module, "__version__", "0.0.0")
 
-    version_too_old = (
-        not _compare_version(module_version, '>=', minimum_version)
+    version_too_old = not _compare_version(
+        module_version, ">=", minimum_version
     )
 
     if version_too_old:
         message = (
-            'A {module_name} version of at least {minimum_version} '
-            'is required to use nilearn. {module_version} was found. '
-            'Please upgrade {module_name}').format(
-                module_name=module_name,
-                minimum_version=minimum_version,
-                module_version=module_version)
+            "A {module_name} version of at least {minimum_version} "
+            "is required to use nilearn. {module_version} was found. "
+            "Please upgrade {module_name}"
+        ).format(
+            module_name=module_name,
+            minimum_version=minimum_version,
+            module_version=module_version,
+        )
 
         raise ImportError(message)
 
@@ -136,11 +153,10 @@ def _compare_version(version_a, operator, version_b):
 
     """
     from packaging.version import parse
+
     if operator not in VERSION_OPERATORS:
-        raise ValueError(
-            "'_compare_version' received an unexpected "
-            "operator {0}.".format(operator)
-        )
+        error_msg = "'_compare_version' received an unexpected operator "
+        raise ValueError(error_msg + operator + ".")
     return VERSION_OPERATORS[operator](parse(version_a), parse(version_b))
 
 
@@ -157,13 +173,15 @@ def _check_module_dependencies(is_nilearn_installing=False):
     -------
     ImportError
     """
-
-    for (module_name, module_metadata) in REQUIRED_MODULE_METADATA:
-        if not (is_nilearn_installing and
-                not module_metadata['required_at_installation']):
+    for module_name, module_metadata in REQUIRED_MODULE_METADATA:
+        if not (
+            is_nilearn_installing
+            and not module_metadata["required_at_installation"]
+        ):
             # Skip check only when installing and it's a module that
             # will be auto-installed.
             _import_module_with_version_check(
                 module_name=module_name,
-                minimum_version=module_metadata['min_version'],
-                install_info=module_metadata.get('install_info'))
+                minimum_version=module_metadata["min_version"],
+                install_info=module_metadata.get("install_info"),
+            )
