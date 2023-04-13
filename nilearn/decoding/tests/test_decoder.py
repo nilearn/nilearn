ff696d1c98d72def6ef0feb96766fd75924b9712
Michelle Wang <tomichellewang@gmail.com>
2023-04-13 13:18:48 -0400

    fix formatting

--- nilearn/decoding/tests/test_decoder.py
+++ nilearn/decoding/tests/test_decoder.py
@@ -235,8 +235,7 @@ def test_wrap_param_grid_warning(param_grid, need_wrap):
     warning_messages = [str(warning.message) for warning in raised_warnings]
 
     found_warning = any(
-        expected_warning_substring in x
-        for x in warning_messages
+        expected_warning_substring in x for x in warning_messages
     )
 
     if need_wrap:
