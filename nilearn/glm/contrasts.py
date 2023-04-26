77ce231494f070d66cbb07e74bf8772136176909
Remi Gau <remi_gau@hotmail.com>
2023-04-26 12:08:00 +0200

    Apply suggestions from code review

--- nilearn/glm/contrasts.py
+++ nilearn/glm/contrasts.py
@@ -428,7 +428,7 @@ def compute_fixed_effects(contrast_imgs, variance_imgs, mask=None,
         if len(dofs) != n_runs:
             raise ValueError(
                 f"The number of dofs ({len(dofs)}) "
-                f"differs from the number of contrast images ({n_runs}). ")
+                f"differs from the number of contrast images ({n_runs})."
             )
     else:
         dofs = [100] * n_runs
