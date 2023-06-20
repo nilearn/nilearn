9593aeb78ada67d2685f321488ba6ca284c9f381
ymzayek <yasmin.mzayek@inria.fr>
2023-06-20 14:43:32 +0200

    Add init file

---
+++ nilearn/_coordimage/__init__.py
@@ -0,0 +1,30 @@
+"""Experimental module implementing surface objects"""
+
+from .caretspec import (
+    CaretSpecDataFile,
+    CaretSpecFile,
+    CaretSpecParser,
+    SurfaceDataFile,
+)
+from .coordimage import (
+    CoordinateAxis,
+    CoordinateImage,
+    GeometryCollection,
+    Parcel,
+)
+from .pointset import NdGrid, Pointset, TriangularMesh, TriMeshFamily
+
+__all__ = [
+    "CoordinateImage",
+    "CoordinateAxis",
+    "Parcel",
+    "GeometryCollection",
+    "Pointset",
+    "TriangularMesh",
+    "TriMeshFamily",
+    "NdGrid",
+    "CaretSpecDataFile",
+    "SurfaceDataFile",
+    "CaretSpecFile",
+    "CaretSpecParser",
+]
