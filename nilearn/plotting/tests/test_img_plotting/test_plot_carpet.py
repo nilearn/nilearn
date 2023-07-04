6f11115e8cd742494feb2d933a9933c322080dfa
Remi Gau <remi_gau@hotmail.com>
2023-07-04 14:32:16 +0200

    apply black to plotting

--- nilearn/plotting/tests/test_img_plotting/test_plot_carpet.py
+++ nilearn/plotting/tests/test_img_plotting/test_plot_carpet.py
@@ -8,47 +8,54 @@ from nilearn.plotting import plot_carpet
 
 def test_plot_carpet(testdata_4d_for_plotting):
     """Check contents of plot_carpet figure against data in image."""
-    img_4d = testdata_4d_for_plotting['img_4d']
-    img_4d_long = testdata_4d_for_plotting['img_4d_long']
-    mask_img = testdata_4d_for_plotting['img_mask']
-    display = plot_carpet(img_4d, mask_img, detrend=False, title='TEST')
+    img_4d = testdata_4d_for_plotting["img_4d"]
+    img_4d_long = testdata_4d_for_plotting["img_4d_long"]
+    mask_img = testdata_4d_for_plotting["img_mask"]
+    display = plot_carpet(img_4d, mask_img, detrend=False, title="TEST")
     # Next two lines retrieve the numpy array from the plot
     ax = display.axes[0]
     plotted_array = ax.images[0].get_array()
-    assert (
-        plotted_array.shape == (np.prod(img_4d.shape[:-1]), img_4d.shape[-1])
+    assert plotted_array.shape == (
+        np.prod(img_4d.shape[:-1]),
+        img_4d.shape[-1],
     )
     # Make sure that the values in the figure match the values in the image
     np.testing.assert_almost_equal(
-        plotted_array.sum(),
-        img_4d.get_fdata().sum(),
-        decimal=3
+        plotted_array.sum(), img_4d.get_fdata().sum(), decimal=3
     )
     # Save execution time and memory
     plt.close(display)
 
     fig, ax = plt.subplots()
-    display = plot_carpet(img_4d_long, mask_img, t_r=None, detrend=True,
-                          title='TEST', figure=fig, axes=ax)
+    display = plot_carpet(
+        img_4d_long,
+        mask_img,
+        t_r=None,
+        detrend=True,
+        title="TEST",
+        figure=fig,
+        axes=ax,
+    )
     # Next two lines retrieve the numpy array from the plot
     ax = display.axes[0]
     plotted_array = ax.images[0].get_array()
     # Check size
-    n_items = (np.prod(img_4d_long.shape[:-1])
-               * np.ceil(img_4d_long.shape[-1] / 4))
+    n_items = np.prod(img_4d_long.shape[:-1]) * np.ceil(
+        img_4d_long.shape[-1] / 4
+    )
     assert plotted_array.size == n_items
     plt.close(display)
 
 
 def test_plot_carpet_with_atlas(testdata_4d_for_plotting):
     """Test plot_carpet when using an atlas."""
-    img_4d = testdata_4d_for_plotting['img_4d']
-    mask_img = testdata_4d_for_plotting['img_atlas']
-    atlas_labels = testdata_4d_for_plotting['atlas_labels']
+    img_4d = testdata_4d_for_plotting["img_4d"]
+    mask_img = testdata_4d_for_plotting["img_atlas"]
+    atlas_labels = testdata_4d_for_plotting["atlas_labels"]
 
     # Test atlas - labels
     # t_r is set explicitly for this test as well
-    display = plot_carpet(img_4d, mask_img, t_r=2, detrend=False, title='TEST')
+    display = plot_carpet(img_4d, mask_img, t_r=2, detrend=False, title="TEST")
 
     # Check the output
     # Two axes: 1 for colorbar and 1 for imshow
@@ -56,7 +63,7 @@ def test_plot_carpet_with_atlas(testdata_4d_for_plotting):
     # The y-axis label of the imshow should be 'voxels' since atlas labels are
     # unknown
     ax = display.axes[1]
-    assert ax.get_ylabel() == 'voxels'
+    assert ax.get_ylabel() == "voxels"
 
     # Next two lines retrieve the numpy array from the plot
     ax = display.axes[0]
@@ -73,7 +80,7 @@ def test_plot_carpet_with_atlas(testdata_4d_for_plotting):
         mask_img,
         mask_labels=atlas_labels,
         detrend=True,
-        title='TEST',
+        title="TEST",
         figure=fig,
         axes=ax,
     )
--- nilearn/plotting/tests/test_img_plotting/test_plot_roi.py
+++ nilearn/plotting/tests/test_img_plotting/test_plot_roi.py
@@ -15,50 +15,55 @@ def demo_plot_roi(**kwargs):
     data = np.zeros((91, 109, 91))
     # Color a asymmetric rectangle around Broca area.
     x, y, z = -52, 10, 22
-    x_map, y_map, z_map = coord_transform(x, y, z,
-                                          np.linalg.inv(MNI_AFFINE))
-    data[int(x_map) - 5:int(x_map) + 5, int(y_map) - 3:int(y_map) + 3,
-         int(z_map) - 10:int(z_map) + 10] = 1
+    x_map, y_map, z_map = coord_transform(x, y, z, np.linalg.inv(MNI_AFFINE))
+    data[
+        int(x_map) - 5 : int(x_map) + 5,
+        int(y_map) - 3 : int(y_map) + 3,
+        int(z_map) - 10 : int(z_map) + 10,
+    ] = 1
     img = Nifti1Image(data, MNI_AFFINE)
     return plot_roi(img, title="Broca's area", **kwargs)
 
 
-@pytest.mark.parametrize('view_type', ['contours', 'continuous'])
-@pytest.mark.parametrize('black_bg', [True, False])
-@pytest.mark.parametrize('threshold', [.5, .2])
-@pytest.mark.parametrize('alpha', [.7, .1])
-@pytest.mark.parametrize('display_mode,cut_coords', [("ortho", None), ("z", 3),
-                                                     ("x", [2., 10])])
-def test_plot_roi_view_types(view_type, black_bg, threshold, alpha,
-                             display_mode, cut_coords):
+@pytest.mark.parametrize("view_type", ["contours", "continuous"])
+@pytest.mark.parametrize("black_bg", [True, False])
+@pytest.mark.parametrize("threshold", [0.5, 0.2])
+@pytest.mark.parametrize("alpha", [0.7, 0.1])
+@pytest.mark.parametrize(
+    "display_mode,cut_coords", [("ortho", None), ("z", 3), ("x", [2.0, 10])]
+)
+def test_plot_roi_view_types(
+    view_type, black_bg, threshold, alpha, display_mode, cut_coords
+):
     """Smoke-test for plot_roi.
 
     Tests different combinations of parameters `view_type`, `black_bg`,
     `threshold`, and `alpha`.
     """
     kwargs = dict()
-    if view_type == 'contours':
-        kwargs["linewidth"] = 2.
-    demo_plot_roi(view_type=view_type,
-                  black_bg=black_bg,
-                  threshold=threshold,
-                  alpha=alpha,
-                  display_mode=display_mode,
-                  cut_coords=cut_coords,
-                  **kwargs)
+    if view_type == "contours":
+        kwargs["linewidth"] = 2.0
+    demo_plot_roi(
+        view_type=view_type,
+        black_bg=black_bg,
+        threshold=threshold,
+        alpha=alpha,
+        display_mode=display_mode,
+        cut_coords=cut_coords,
+        **kwargs,
+    )
     plt.close()
 
 
 def test_plot_roi_view_type_error():
     """Test error message for invalid view_type."""
-    with pytest.raises(ValueError,
-                       match='Unknown view type:'):
-        demo_plot_roi(view_type='flled')
+    with pytest.raises(ValueError, match="Unknown view type:"):
+        demo_plot_roi(view_type="flled")
 
 
 def test_demo_plot_roi_output_file(tmpdir):
     """Tests plot_roi file saving capabilities."""
-    filename = str(tmpdir.join('test.png'))
-    with open(filename, 'wb') as fp:
+    filename = str(tmpdir.join("test.png"))
+    with open(filename, "wb") as fp:
         out = demo_plot_roi(output_file=fp)
     assert out is None
