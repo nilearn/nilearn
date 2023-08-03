92bba5e239def0ca92a467b64f5979e280933429
Hao-Ting Wang <htwangtw@gmail.com>
2023-08-03 12:13:51 -0400

    Docstring for compcor related private function

--- nilearn/interfaces/fmriprep/load_confounds_compcor.py
+++ nilearn/interfaces/fmriprep/load_confounds_compcor.py
@@ -10,7 +10,24 @@ anat_masker = {"combined": ["combined"],
 
 
 def _find_compcor(confounds_json, compcor, n_compcor):
-    """Build list for the number of compcor components."""
+    """Build list for the number of compcor components.
+
+    Parameters
+    ----------
+    confounds_json : dict
+        Dictionary of confounds from the confounds.json file.
+    compcor : str
+        Compcor strategy to use. Options are "temporal_anat", "temporal",
+        "anat", or "combined".
+    n_compcor : int or str
+        Number of compcor components to retain. If "all", all components
+        are retained.
+
+    Returns
+    -------
+    collector : list
+        List of compcor components to retain.
+    """
     prefix_set, anat_mask = _check_compcor_method(compcor)
 
     collector = []
@@ -38,7 +55,22 @@ def _find_compcor(confounds_json, compcor, n_compcor):
 
 
 def _select_compcor(compcor_cols, n_compcor):
-    """Retain a specified number of compcor components."""
+    """Retain a specified number of compcor components.
+
+    Parameters
+    ----------
+    compcor_cols : list
+        List of compcor components column names filtered by user
+        selected CompCor strategy.
+    n_compcor : int or str
+        Number of compcor components to retain. If "all", all components
+        are retained.
+
+    Returns
+    -------
+    compcor_cols : list
+        List of compcor components column names to retain.
+    """
     # only select if not "all", or less components are requested than there
     # actually is
     if (n_compcor != "all") and (n_compcor < len(compcor_cols)):
@@ -47,7 +79,21 @@ def _select_compcor(compcor_cols, n_compcor):
 
 
 def _check_compcor_method(compcor):
-    """Load compcor options and check if method is acceptable."""
+    """Load compcor options and check if method is acceptable.
+
+    Parameters
+    ----------
+    compcor : str
+        Compcor strategy to use. Options are "temporal_anat", "temporal",
+        "anat", or "combined".
+
+    Returns
+    -------
+    prefix_set : list
+        List of prefixes to use for compcor components.
+    anat_mask : list
+        List of anatomical masks to use for acompcor.
+    """
     compcor_type = compcor.split("_")
     if len(compcor_type) > 1:
         compcor_type = "_".join(compcor_type[:-1])
@@ -63,7 +109,26 @@ def _check_compcor_method(compcor):
 
 
 def _acompcor_mask(confounds_json, anat_mask, compcor_cols_filt, n_compcor):
-    """Filter according to acompcor mask(s) and select top components."""
+    """Filter according to acompcor mask(s) and select top components.
+
+    Parameters
+    ----------
+    confounds_json : dict
+        Dictionary of confounds from the confounds.json file.
+    anat_mask : list
+        List of anatomical masks to use for acompcor.
+    compcor_cols_filt : list
+        List of compcor components column names filtered by user
+        selected CompCor strategy.
+    n_compcor : int or str
+        Number of compcor components to retain. If "all", all components
+        are retained.
+
+    Returns
+    -------
+    collector : list
+        List of compcor components column names to retain.
+    """
     collector = []
     for mask in anat_mask:
         cols = _json_mask(compcor_cols_filt, confounds_json, mask)
@@ -73,7 +138,24 @@ def _acompcor_mask(confounds_json, anat_mask, compcor_cols_filt, n_compcor):
 
 
 def _json_mask(compcor_cols_filt, confounds_json, mask):
-    """Extract anat compcor components from a given mask."""
+    """Extract anat compcor components with a given mask.
+
+    Parameters
+    ----------
+    compcor_cols_filt : list
+        List of compcor components column names filtered by user
+        selected CompCor strategy.
+    confounds_json : dict
+        Dictionary of confounds from the confounds.json file.
+    mask : str
+        Mask to use for acompcor.
+
+    Returns
+    -------
+    compcor_cols_filt : list
+        List of compcor components column names filtered by type of
+        acompcor mask.
+    """
     return [
         compcor_col
         for compcor_col in compcor_cols_filt
@@ -82,7 +164,20 @@ def _json_mask(compcor_cols_filt, confounds_json, mask):
 
 
 def _prefix_confound_filter(prefix, all_compcor_name):
-    """Get confound columns by prefix and acompcor mask."""
+    """Get confound columns by prefix and acompcor mask.
+
+    Parameters
+    ----------
+    prefix : str
+        Prefix to use for compcor components.
+    all_compcor_name : list
+        List of all compcor components column names.
+
+    Returns
+    -------
+    compcor_cols_filt : list
+        List of compcor components column names filtered by prefix.
+    """
     compcor_cols_filt = []
     for nn in range(len(all_compcor_name)):
         nn_str = str(nn).zfill(2)
