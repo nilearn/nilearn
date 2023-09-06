ef4edf297d3c02f4d0313588e10538f9edbacf19
Patrick Sadil <psadil@gmail.com>
2023-09-06 10:09:22 -0400

    Update nilearn/plotting/displays/_figures.py
    Co-authored-by: Remi Gau <remi_gau@hotmail.com>

--- nilearn/plotting/displays/_figures.py
+++ nilearn/plotting/displays/_figures.py
@@ -133,7 +133,7 @@ class PlotlySurfaceFigure(SurfaceFigure):
             Every index needs to correspond to one index in roi_map.
             If None, all regions in roi_map are used.
 
-        labels : list of strings or None, or None, optional
+        labels : list of strings or None, or None,  default=None
             A list of labels for the individual regions of interest. Provide
             None as list entry to skip showing the label of that region. If
             None, no labels are used.
