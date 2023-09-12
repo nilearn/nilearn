35fe115d5b7ce22f8e36f50b660afdca872cce4f
Remi Gau <remi_gau@hotmail.com>
2023-09-11 22:54:16 -0400

    update year in doc

--- doc/conf.py
+++ doc/conf.py
@@ -94,7 +94,7 @@ bibtex_footbibliography_header = ""
 
 # General information about the project.
 project = "Nilearn"
-copyright = "The nilearn developers 2010-2022"
+copyright = "The nilearn developers 2010-2023"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
