58641a166641c926edbba04067fd196f256a480e
Remi Gau <remi_gau@hotmail.com>
2023-09-20 05:31:14 -0400

    improve bash script and run it in CI

--- doc/fill_doc_check.sh
+++ doc/fill_doc_check.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+# This script is used to check
+# if there are any unreplaced arguments from the docstrings
+# in html version of the docs.
+#
+#   Parameters
+#   ----------
+#   %(img)s    < ----- argument to replace by strings defined in nilearn/_utils/docs.py
+#
+# If the output of this script is empty, then there are no unreplaced arguments.
+# If not, then some function is likely missing an @fill_doc decorator.
+#
+# See nilearn/_utils/docs.py.
+
 
 set -e -x -u -o pipefail
 
