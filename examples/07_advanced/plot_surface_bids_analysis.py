edde0295a380c8e5ba7fb8c0e606a44966ca9445
Remi Gau <remi_gau@hotmail.com>
2023-10-07 15:26:39 +0200

    sty

--- examples/07_advanced/plot_surface_bids_analysis.py
+++ examples/07_advanced/plot_surface_bids_analysis.py
@@ -56,8 +56,8 @@ _, models_run_imgs, models_events, models_confounds = \
         img_filters=[('desc', 'preproc')])
 
 #############################################################################
-# We also need to get the :term:`TR` information. For that we use the json sidecar file
-# of the dataset's functional images.
+# We also need to get the :term:`TR` information.
+# For that we use the json sidecar file of the dataset's functional images.
 import json
 import os
 
