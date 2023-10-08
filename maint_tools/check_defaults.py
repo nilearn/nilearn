94af24988f0e4c886ae545348811f43c8a108b2d
Remi Gau <remi_gau@hotmail.com>
2023-10-08 21:30:35 +0200

    create script to update defaults

---
+++ maint_tools/check_defaults.py
@@ -0,0 +1,128 @@
+"""Update default values in docstrings."""
+from __future__ import annotations
+
+import ast
+import itertools
+from pathlib import Path
+
+from docstring_parser import parse
+from docstring_parser.common import DocstringStyle
+from rich import print
+
+SKIP_PRIVATE = False
+
+
+def root_dir() -> Path:
+    """Return path to root directory."""
+    return Path(__file__).parent.parent
+
+
+def check_docstring(docstring: str, file: Path, lineno: int) -> str:
+    """Check docstring content."""
+    docstring = parse(docstring, style=DocstringStyle.NUMPYDOC)
+
+    targets = [
+        "Default=",
+        "Default =",
+        "Default = ",
+        "default=",
+        "Default is ",
+    ]
+    targets += [target.lower() for target in targets]
+
+    for target_str, param in itertools.product(targets, docstring.params):
+        if param.arg_name.startswith("%("):
+            continue
+
+        if (
+            param.description is not None
+            and target_str in param.description
+            and param.default is None
+        ):
+            # extract default value from description
+            default = param.description.split(target_str)[1].split(".")[0]
+
+            type_name = f"{param.type_name}, default={default}"
+
+            with open(file) as f:
+                content = f.readlines()
+
+            with open(file, "w") as f:
+                # skip the line from beginning of file to lineno
+                for i, line in enumerate(content):
+                    if i < lineno:
+                        update_def = False
+                        update_desc = False
+                    elif i == lineno:
+                        update_def = True
+                        update_desc = False
+
+                    if update_def and line.startswith(
+                        f"    {param.arg_name} : {param.type_name}"
+                    ):
+                        f.write(f"    {param.arg_name} : {type_name}\n")
+                        update_def = False
+                        update_desc = True
+
+                    elif update_desc:
+                        if line == f"        {target_str}{default}.\n":
+                            f.write("")
+                            update_desc = False
+                            continue
+                        elif line.endswith(f" {target_str}{default}.\n"):
+                            f.write(
+                                line.replace(f" {target_str}{default}.", "")
+                            )
+                            update_desc = False
+                        else:
+                            f.write(line)
+
+                    else:
+                        f.write(line)
+
+
+def check_functions(body, file):
+    """Check functions of a module or methods of a class."""
+    for node in body:
+        if isinstance(node, ast.FunctionDef):
+            if SKIP_PRIVATE and node.name.startswith("_"):
+                continue
+            print(f"function: '{node.name}' in {file.resolve()}:{node.lineno}")
+            docstring = ast.get_docstring(node)
+            docstring = check_docstring(docstring, file, node.lineno)
+
+
+def main():
+    """Update defaults."""
+    modules = (root_dir() / "nilearn").glob("**/*.py")
+
+    files_to_skip = ["test_", "conftest.py"]
+
+    for file in modules:
+        if (
+            any(file.name.startswith(s) for s in files_to_skip)
+            or file.parent.name == "tests"
+        ):
+            continue
+
+        if SKIP_PRIVATE and (
+            file.name.startswith("_") or file.parent.name.startswith("_")
+        ):
+            continue
+
+        with open(file) as f:
+            module = ast.parse(f.read())
+
+        check_functions(module.body, file)
+
+        class_definitions = [
+            node for node in module.body if isinstance(node, ast.ClassDef)
+        ]
+
+        for class_def in class_definitions:
+            print(f"class: '{class_def.name}'")
+            check_functions(class_def.body, file)
+
+
+if __name__ == "__main__":
+    main()
