e593e67a6610d6cf250eea624f99d60989f8e5f0
Mia Zwally <132242817+MIZwally@users.noreply.github.com>
2023-10-13 11:43:30 -0400

    Moved elif to separate if statement

--- nilearn/image/image.py
+++ nilearn/image/image.py
@@ -1285,15 +1285,15 @@ def clean_img(
     # Put results back into Niimg-like object
     if mask_img is not None:
         imgs_ = masking.unmask(data, mask_img)
-    elif 'sample_mask' in clean_kwargs:
-        sample_shape = imgs_.shape[:3] + clean_kwargs['sample_mask'].shape
-        imgs_ = new_img_like(
-            imgs_, data.T.reshape(sample_shape), copy_header=True
-        )
     else:
         imgs_ = new_img_like(
             imgs_, data.T.reshape(imgs_.shape), copy_header=True
         )
+    if 'sample_mask' in clean_kwargs:
+        sample_shape = imgs_.shape[:3] + clean_kwargs['sample_mask'].shape
+        imgs_ = new_img_like(
+            imgs_, data.T.reshape(sample_shape), copy_header=True
+    )
 
     return imgs_
 
