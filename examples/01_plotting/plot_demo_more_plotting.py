0c15203f4c50cbe2f22f2caacb00687a86fc092e
Remi Gau <remi_gau@hotmail.com>
2024-01-16 20:02:48 +0100

    output 01

--- examples/01_plotting/plot_demo_more_plotting.py
+++ examples/01_plotting/plot_demo_more_plotting.py
@@ -403,11 +403,16 @@ display.annotate(scalebar=True, scale_size=25, scale_units="mm")
 # :func:`nilearn.plotting.plot_stat_map` using the built-in parameter
 # ``output_file``. We provide the filename and the file extension as
 # a string (supported extensions are .png, .pdf, .svg).
+from pathlib import Path
+
+output_dir = Path.cwd() / "results" / "plot_demo_more_plotting"
+output_dir.mkdir(exist_ok=True, parents=True)
+print(f"Output will be saved to: {output_dir}")
 
 plotting.plot_stat_map(
     stat_img,
     title="Using plot_stat_map output_file",
-    output_file="plot_stat_map.png",
+    output_file=output_dir / "plot_stat_map.png",
 )
 
 # %%
@@ -417,7 +422,7 @@ plotting.plot_stat_map(
 
 display = plotting.plot_stat_map(stat_img, title="Using display savefig")
 
-display.savefig("plot_stat_map_from_display.png")
+display.savefig(output_dir / "plot_stat_map_from_display.png")
 
 # In non-interactive settings make sure you close your displays
 display.close()
