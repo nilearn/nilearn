85724d19f4469575303ad551174937de491d7760
Remi Gau <remi_gau@hotmail.com>
2024-01-24 10:08:49 +0100

    fix type for 3.8

--- nilearn/surface/surface_image.py
+++ nilearn/surface/surface_image.py
@@ -1,9 +1,8 @@
 """Surface API."""
-from __future__ import annotations
-
 import abc
 import dataclasses
 import pathlib
+from typing import Dict
 
 import numpy as np
 
@@ -133,10 +132,6 @@ def _check_data_and_mesh_compat(mesh, data):
             )
 
 
-PolyData = dict[str, np.ndarray]
-PolyMesh = dict[str, _Mesh]
-
-
 @dataclasses.dataclass
 class SurfaceImage:
     """Surface image, usually containing meshes & data for both hemispheres.
@@ -156,8 +151,8 @@ class SurfaceImage:
     shape : :obj:`tuple`
     """
 
-    mesh: PolyMesh
-    data: PolyData
+    mesh: Dict[str, _Mesh]
+    data: Dict[str, np.ndarray]
     shape: tuple[int, ...] = dataclasses.field(init=False)
 
     def __post_init__(self):
