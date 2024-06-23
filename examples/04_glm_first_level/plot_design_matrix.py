cbc91daf7498a795a3012f9133418fbb4ffbb015
Remi Gau <remi_gau@hotmail.com>
2024-06-23 20:35:45 +0200

    add regressor correlation matrix

--- examples/04_glm_first_level/plot_design_matrix.py
+++ examples/04_glm_first_level/plot_design_matrix.py
@@ -115,6 +115,92 @@ plt.show()
 
 
 # %%
+# Correlation between regressors
+# ------------------------------
+from matplotlib.colorbar import make_axes
+
+
+def plot_corr_matrix_mpl(
+    des_mat, partial="upper", cmap="bwr", ax=None, fig=None
+):
+
+    if not isinstance(des_mat, pd.DataFrame):
+        raise TypeError(
+            f"'des_mat' must be a andas dataframe instance.\n"
+            f"Got: {type(des_mat)}"
+        )
+
+    ALLOWED_CMAP = ["RdBu_r", "bwr", "seismic_r"]
+    if cmap not in ALLOWED_CMAP:
+        raise ValueError(f"cmap must be one of {ALLOWED_CMAP}")
+
+    ALLOWED_PARTIALS = ["upper", "lower", None]
+    if partial not in ALLOWED_PARTIALS:
+        raise ValueError(f"partial must be one of {ALLOWED_PARTIALS}")
+
+    columns_to_drop = ["intercept", "constant"]
+    columns_to_drop.extend(
+        col for col in des_mat.columns if col.startswith("drift_")
+    )
+    des_mat = des_mat.drop(columns=columns_to_drop, errors="ignore")
+
+    mat = des_mat.corr()
+
+    # For a heatmap mask, 0 = show, 1 = hide
+    if partial:
+        mask = np.ones_like(mat, dtype=bool)
+        if partial == "upper":
+            mask[np.triu_indices_from(mask)] = False
+        elif partial == "lower":
+            mask[np.tril_indices_from(mask)] = False
+        mat[mask] = 0
+
+    # find the second-largest value in each row
+    # to omit values on the diagonal that will always be == 1
+    second_largest = np.partition(mat.to_numpy(), -2, axis=1)[:, -2]
+    vmax = max(abs(mat.min().min()), max(second_largest))
+
+    if ax is None:
+        if fig is None:
+            fig = plt.figure()
+        ax = fig.add_axes(111)
+    elif fig is None:
+        fig = ax.get_figure()
+
+    im = ax.imshow(mat, cmap=cmap, vmax=vmax, vmin=vmax * -1)
+
+    cax, _ = make_axes(
+        ax, location="right", fraction=0.18, shrink=0.75, pad=0.02, aspect=15.0
+    )
+    fig.colorbar(im, cax=cax, spacing="proportional", orientation="vertical")
+
+    col_labels = des_mat.columns
+    ax.set_xticks(
+        np.arange(mat.shape[1]), labels=col_labels, rotation=60, ha="left"
+    )
+    ax.set_yticks(np.arange(mat.shape[0]), labels=col_labels)
+
+    ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
+
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+
+    return ax
+
+
+fig2, (ax1, ax2, ax3) = plt.subplots(figsize=(100, 6), nrows=1, ncols=3)
+plot_corr_matrix_mpl(X1, ax=ax1)
+ax1.set_title("Event-related correlation matrix", fontsize=12)
+plot_corr_matrix_mpl(X2, ax=ax2, partial="lower")
+ax2.set_title("Block correlation matrix", fontsize=12)
+plot_corr_matrix_mpl(X3, ax=ax3, partial=None)
+ax3.set_title("FIR correlation matrix", fontsize=12)
+plt.show()
+
+
+# %%
 # Parametric modulation
 # ---------------------
 # By default, the fMRI GLM will expect that all events
