32639c464b075c38d4b08145e735ca5bb9d20e78
Remi Gau <remi_gau@hotmail.com>
2024-09-03 10:42:00 +0200

    clean example

--- examples/00_tutorials/plot_single_subject_single_run.py
+++ examples/00_tutorials/plot_single_subject_single_run.py
@@ -6,7 +6,7 @@ In this tutorial, we use a General Linear Model (:term:`GLM`) to compare the
 :term:`fMRI` signal during periods of auditory stimulation
 versus periods of rest.
 
-warning::
+.. warning::
 
     The analyse described here is performed in the native space,
     directly on the original :term:`EPI` scans
@@ -29,22 +29,30 @@ from nilearn.datasets import fetch_spm_auditory
 
 subject_data = fetch_spm_auditory()
 
+# %%
+# Inspecting the dataset
+# ----------------------
+
+# %%
 # print dataset descriptions
-print(subject_data.description)
+subject_data.description
 
+# %%
 # print paths of func image
-print(subject_data.func[0])
-
-from nilearn.image import mean_img
+subject_data.func[0]
 
 # %%
 # We can display the mean functional image and the subject's anatomy:
-from nilearn.plotting import plot_anat, plot_img, plot_stat_map
+from nilearn.image import mean_img
+from nilearn.plotting import plot_anat, plot_img, plot_stat_map, show
 
 fmri_img = subject_data.func
 mean_img = mean_img(subject_data.func[0], copy_header=True)
-plot_img(mean_img, colorbar=True, cbar_tick_format="%i")
+plot_img(mean_img, colorbar=True, cbar_tick_format="%i", cmap="gray")
+
 plot_anat(subject_data.anat, colorbar=True, cbar_tick_format="%i")
+
+show()
 
 
 # %%
@@ -60,6 +68,7 @@ import pandas as pd
 events = pd.read_csv(subject_data.events, sep="\t")
 events
 
+
 # %%
 # Performing the :term:`GLM` analysis
 # -----------------------------------
@@ -73,16 +82,17 @@ from nilearn.glm.first_level import FirstLevelModel
 # %%
 # Parameters of the first-level model
 #
-# * t_r=7(s) is the time of repetition of acquisitions
-# * noise_model='ar1' specifies the noise covariance model: a lag-1 dependence
-# * standardize=False means that we do not want
+# * ``t_r=7(s)`` is the time of repetition of acquisitions
+# * ``noise_model='ar1'`` specifies the noise covariance model:
+#   a lag-1 dependence
+# * ``standardize=False`` means that we do not want
 #   to rescale the time series to mean 0, variance 1
-# * hrf_model='spm' means that we rely
+# * ``hrf_model='spm'`` means that we rely
 #   on the :term:`SPM` "canonical hrf" model
 #   (without time or dispersion derivatives)
-# * drift_model='cosine' means that we model the signal drifts
+# * ``drift_model='cosine'`` means that we model the signal drifts
 #   as slow oscillating time functions
-# * high_pass=0.01(Hz) defines the cutoff frequency
+# * ``high_pass=0.01`` (Hz) defines the cutoff frequency
 #   (inverse of the time period).
 fmri_glm = FirstLevelModel(
     t_r=7,
@@ -105,13 +115,11 @@ design_matrix = fmri_glm.design_matrices_[0]
 # %%
 # Formally, we have taken the first design matrix, because the model is
 # implictily meant to for multiple runs.
-import matplotlib.pyplot as plt
-
 from nilearn.plotting import plot_design_matrix
 
 plot_design_matrix(design_matrix)
 
-plt.show()
+show()
 
 # %%
 # Save the design matrix image to disk
@@ -128,11 +136,14 @@ plot_design_matrix(design_matrix, output_file=output_dir / "design_matrix.png")
 # The first column contains the expected response profile of regions which are
 # sensitive to the auditory stimulation.
 # Let's plot this first column
+import matplotlib.pyplot as plt
 
 plt.plot(design_matrix["listening"])
 plt.xlabel("scan")
 plt.title("Expected Auditory Response")
-plt.show()
+
+show()
+
 
 # %%
 # Detecting voxels with significant effects
@@ -160,8 +171,8 @@ activation = np.zeros(nb_regressors)
 activation[0] = 1
 
 # %%
-# Let's look at it: plot the coefficients of the :term:`contrast`, indexed by
-# the names of the columns of the design matrix.
+# Let's look at it: plot the coefficients of the :term:`contrast`,
+# indexed by the names of the columns of the design matrix.
 
 from nilearn.plotting import plot_contrast_matrix
 
@@ -182,30 +193,46 @@ eff_map = fmri_glm.compute_contrast(activation, output_type="effect_size")
 
 z_map = fmri_glm.compute_contrast(activation, output_type="z_score")
 
+
 # %%
 # Plot thresholded z scores map
 # -----------------------------
 #
-# We display it on top of the average
-# functional image of the series (could be the anatomical image of the
-# subject).  We use arbitrarily a threshold of 3.0 in z-scale. We'll
-# see later how to use corrected thresholds. We will show 3
-# axial views, with display_mode='z' and cut_coords=3.
-
+# We display it on top of the average functional image of the series
+# (could be the anatomical image of the subject).
+# We use arbitrarily a threshold of 3.0 in z-scale.
+# We'll see later how to use corrected thresholds.
+# We will show 3 axial views, with display_mode='z' and cut_coords=3.
+plotting_config = {
+    "bg_img": mean_img,
+    "display_mode": "z",
+    "cut_coords": 3,
+    "black_bg": True,
+}
 plot_stat_map(
     z_map,
-    bg_img=mean_img,
-    threshold=3.0,
-    display_mode="z",
-    cut_coords=3,
-    black_bg=True,
-    title="Listening greater than rest (Z>3)",
+    threshold=3,
+    title="listening > rest (|Z|>3)",
+    figure=plt.figure(figsize=(10, 4)),
+    **plotting_config,
 )
-plt.show()
+show()
 
 # %%
-# Statistical significance testing. One should worry about the
-# statistical validity of the procedure: here we used an arbitrary
+# .. note::
+#
+#   Notice how the visualisations above shows both 'activated' voxels
+#   with Z > 3,
+#   as well as 'deactivated' voxels with Z < -3.
+#   In the rest of this example we will show only the activate voxels
+#   by using one-sided tests.
+
+# %%
+# Statistical significance testing
+# --------------------------------
+#
+# One should worry about the statistical validity of the procedure:
+# here we used an arbitrary
 # threshold of 3.0 but the threshold should provide some guarantees on
 # the risk of false detections (aka type-1 errors in statistics).
 # One suggestion is to control the false positive rate
@@ -216,20 +243,24 @@ plt.show()
 from nilearn.glm import threshold_stats_img
 
 clean_map, threshold = threshold_stats_img(
-    z_map, alpha=0.001, height_control="fpr", two_sided=False
+    z_map,
+    alpha=0.001,
+    height_control="fpr",
+    two_sided=False,  # using a one-sided test
 )
-print(f"Uncorrected p<0.001 threshold: {threshold:.3f}")
+# Let's use a sequential colormap as we will only display positive values.
+plotting_config["cmap"] = "black_red"
 plot_stat_map(
     clean_map,
-    bg_img=mean_img,
     threshold=threshold,
-    display_mode="z",
-    cut_coords=3,
-    black_bg=True,
-    title="Listening greater than rest (p<0.001)",
-    cmap="black_red",
+    title=(
+        "listening > rest (Uncorrected p<0.001; "
+        f"threshold: {threshold:.3f})"
+    ),
+    figure=plt.figure(figsize=(10, 4)),
+    **plotting_config,
 )
-plt.show()
+show()
 
 # %%
 # The problem is that with this you expect 0.001 * n_voxels to show up
@@ -241,18 +272,17 @@ plt.show()
 clean_map, threshold = threshold_stats_img(
     z_map, alpha=0.05, height_control="bonferroni", two_sided=False
 )
-print(f"Bonferroni-corrected, p<0.05 threshold: {threshold:.3f}")
 plot_stat_map(
     clean_map,
-    bg_img=mean_img,
     threshold=threshold,
-    display_mode="z",
-    cut_coords=3,
-    black_bg=True,
-    title="Listening greater than rest (p<0.05, corrected)",
-    cmap="black_red",
+    title=(
+        "listening > rest (p<0.05 Bonferroni-corrected, "
+        f"threshold: {threshold:.3f})"
+    ),
+    figure=plt.figure(figsize=(10, 4)),
+    **plotting_config,
 )
-plt.show()
+show()
 
 # %%
 # This is quite conservative indeed!  A popular alternative is to
@@ -263,18 +293,17 @@ plt.show()
 clean_map, threshold = threshold_stats_img(
     z_map, alpha=0.05, height_control="fdr", two_sided=False
 )
-print(f"False Discovery rate = 0.05 threshold: {threshold:.3f}")
 plot_stat_map(
     clean_map,
-    bg_img=mean_img,
     threshold=threshold,
-    display_mode="z",
-    cut_coords=3,
-    black_bg=True,
-    title="Listening greater than rest (fdr=0.05)",
-    cmap="black_red",
+    title=(
+        "listening > rest (p<0.05 FDR-corrected; "
+        f"threshold: {threshold:.3f})"
+    ),
+    figure=plt.figure(figsize=(10, 4)),
+    **plotting_config,
 )
-plt.show()
+show()
 
 # %%
 # Finally people like to discard isolated voxels (aka "small
@@ -292,15 +321,16 @@ clean_map, threshold = threshold_stats_img(
 )
 plot_stat_map(
     clean_map,
-    bg_img=mean_img,
     threshold=threshold,
-    display_mode="z",
-    cut_coords=3,
-    black_bg=True,
-    title="Listening greater than rest (fdr=0.05), clusters > 10 voxels",
-    cmap="black_red",
+    title=(
+        "listening > rest "
+        f"(p<0.05 FDR-corrected; threshold: {threshold:.3f}; "
+        "clusters > 10 voxels)"
+    ),
+    figure=plt.figure(figsize=(10, 4)),
+    **plotting_config,
 )
-plt.show()
+show()
 
 
 # %%
@@ -333,7 +363,7 @@ table.to_csv(output_dir / "table.csv")
 # (possibly two-, three- or higher-dimensional)
 # explains a significant proportion of the signal.
 # Here one might for instance test which voxels are well
-# explained by the combination of more active and less active than rest.
+# explained by the combination of more active or less active than rest.
 
 # %%
 # Specify the :term:`contrast` and compute the corresponding map.
@@ -341,12 +371,14 @@ table.to_csv(output_dir / "table.csv")
 # as for t-contrasts.
 
 z_map = fmri_glm.compute_contrast(
-    activation, output_type="z_score", stat_type="F"
+    activation,
+    output_type="z_score",
+    stat_type="F",  # set stat_type to 'F' to perform an F test
 )
 
 # %%
-# Note that the statistic has been converted to a z-variable, which
-# makes it easier to represent it.
+# Note that the statistic has been converted to a z-variable,
+# which makes it easier to represent it.
 
 clean_map, threshold = threshold_stats_img(
     z_map,
@@ -357,13 +389,9 @@ clean_map, threshold = threshold_stats_img(
 )
 plot_stat_map(
     clean_map,
-    bg_img=mean_img,
     threshold=threshold,
-    display_mode="z",
-    cut_coords=3,
-    black_bg=True,
     title="Effects of interest (fdr=0.05), clusters > 10 voxels",
-    cmap="black_red",
-    symmetric_cbar=False,
+    figure=plt.figure(figsize=(10, 4)),
+    **plotting_config,
 )
-plt.show()
+show()
