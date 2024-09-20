b5fb304095a6a750f4d11cbcfc8f7559739c2f7c
Remi Gau <remi_gau@hotmail.com>
2024-09-20 15:52:24 +0200

    mock data

--- nilearn/datasets/tests/test_atlas.py
+++ nilearn/datasets/tests/test_atlas.py
@@ -5,6 +5,7 @@
 import itertools
 import os
 import re
+import xml.etree.ElementTree as ET
 from pathlib import Path
 
 import numpy as np
@@ -17,7 +18,7 @@ from sklearn.utils import Bunch
 from nilearn._utils import data_gen
 from nilearn._utils.testing import serialize_niimg
 from nilearn.conftest import _rng
-from nilearn.datasets import atlas, fetch_atlas_aal
+from nilearn.datasets import atlas
 from nilearn.datasets._utils import fetch_files
 from nilearn.datasets.tests._testing import dict_to_archive
 from nilearn.image import get_data
@@ -826,14 +827,38 @@ def test_fetch_atlas_schaefer_2018(tmp_path, request_mocker):
         assert np.array_equal(np.unique(img.dataobj), np.arange(n_rois + 1))
 
 
-def test_aal_version_deprecation(request_mocker):
+@pytest.fixture
+def aal_xml():
+    atlas = ET.Element("atlas", version="2.0")
 
-    img_pattern = re.compile("https://www.gin.cnrs.fr/*")
-    request_mocker.url_mapping[img_pattern] = (
-        data_gen.generate_labeled_regions((7, 6, 5), 16)
+    data = ET.SubElement(atlas, "data")
+
+    label1 = ET.SubElement(data, "label")
+    ET.SubElement(label1, "index").text = "2001"
+    ET.SubElement(label1, "name").text = "Precentral_L"
+
+    # Convert the XML tree to a string with proper encoding and declaration
+    return ET.ElementTree(atlas)
+
+
+def test_aal_version_deprecation(
+    tmp_path, shape_3d_default, affine_eye, aal_xml
+):
+    img = data_gen.generate_labeled_regions(
+        shape_3d_default, 15, affine=affine_eye
     )
+    output_path = tmp_path / "aal_SPM12/aal/atlas/AAL.nii"
+    output_path.parent.mkdir(parents=True)
+    img.to_filename(output_path)
+
+    with open(
+        tmp_path / "aal_SPM12" / "aal" / "atlas" / "AAL.xml", "wb"
+    ) as file:
+        aal_xml.write(file, encoding="ISO-8859-1", xml_declaration=True)
 
     with pytest.deprecated_call(
         match=r"Starting in version 0\.13, the default fetched mask"
     ):
-        fetch_atlas_aal()
+        atlas.fetch_atlas_aal(
+            data_dir=tmp_path,
+        )
