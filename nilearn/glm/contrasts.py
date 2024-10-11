cc7bf46094620bd281245e0072db1762adab51ee
bthirion <bertrand.thirion@inria.fr>
2024-10-11 17:38:31 +0200

    ruff compliance

--- nilearn/glm/contrasts.py
+++ nilearn/glm/contrasts.py
@@ -539,8 +539,12 @@ def compute_fixed_effects(
         else:
             masker = NiftiMasker(mask_img=mask).fit()
 
-    variances = np.array([masker.transform(vi).squeeze() for vi in variance_imgs])
-    contrasts = np.array([masker.transform(ci).squeeze() for ci in contrast_imgs])
+    variances = np.array(
+        [masker.transform(vi).squeeze() for vi in variance_imgs]
+        )
+    contrasts = np.array(
+        [masker.transform(ci).squeeze() for ci in contrast_imgs]
+        )
 
     if dofs is not None:
         if len(dofs) != n_runs:
