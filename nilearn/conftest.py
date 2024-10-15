89b91df4a9b6c482ec6f3bc17a6c9fa53d4bfad8
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2024-10-15 09:55:13 +0200

    Test for screening adjustment (#4630)
    * test for screening adjustment
    
    * fix test failure
    
    * review comment
    
    * add docstring
    
    * format docstring
    
    * split the test in two
    
    * move booolean conversion to later step
    
    * fix tests

--- nilearn/conftest.py
+++ nilearn/conftest.py
@@ -21,6 +21,7 @@ from nilearn.datasets.tests._testing import (
 from nilearn.experimental.conftest import (  # noqa: F401
     drop_img_part,
     make_mini_img,
+    make_mini_mask,
     mini_img,
     mini_mask,
     mini_mesh,
