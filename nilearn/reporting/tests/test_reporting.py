c3330ca2630114b093a3862a7f0cb4cd8d41fb4d
Hande Gözükan <hande@ginkgo.work>
2024-10-28 13:51:21 +0100

    remove unnecessary import

--- nilearn/reporting/tests/test_reporting.py
+++ nilearn/reporting/tests/test_reporting.py
@@ -6,7 +6,6 @@ from nilearn._utils.helpers import is_matplotlib_installed
 from nilearn.image import get_data
 
 # Set backend to avoid DISPLAY problems
-from nilearn.plotting import _set_mpl_backend  # noqa
 from nilearn.reporting import get_clusters_table
 from nilearn.reporting.get_clusters_table import (
     _cluster_nearest_neighbor,
