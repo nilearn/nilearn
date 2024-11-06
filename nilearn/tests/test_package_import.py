b967a6aecdbc184fdf58f98bee7eacaae783f871
Hande Gözükan <hande@ginkgo.work>
2024-11-06 15:54:54 +0100

    add test to verify warning from nilearn.reporting pkg when matplotlib is not installed

--- nilearn/tests/test_package_import.py
+++ nilearn/tests/test_package_import.py
@@ -17,3 +17,13 @@ def test_should_raise_warning_if_matplotlib_not_installed():
         ),
     ):
         from nilearn.plotting import cm  # noqa
+
+    with (
+        pytest.raises(
+            ModuleNotFoundError, match="No module named 'matplotlib'"
+        ),
+        pytest.warns(
+            UserWarning, match="Some dependencies of nilearn.plotting"
+        ),
+    ):
+        from nilearn.reporting import make_glm_report  # noqa
