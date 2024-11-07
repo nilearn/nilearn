20752d585cd64e99b9a47163efe6e8bb21ab9a9e
Remi Gau <remi_gau@hotmail.com>
2024-11-07 16:36:42 +0000

    adapt example searchlight

--- examples/02_decoding/plot_haxby_searchlight_surface.py
+++ examples/02_decoding/plot_haxby_searchlight_surface.py
@@ -39,26 +39,17 @@ y, run = y[condition_mask], run[condition_mask]
 
 from sklearn import neighbors
 
-from nilearn import datasets, surface
+from nilearn import datasets
+from nilearn.experimental.surface import (
+    SurfaceImage,
+    load_fsaverage,
+)
 
-fsaverage = datasets.fetch_surf_fsaverage(mesh="fsaverage5")
-hemi = "left"
+fsaverage = load_fsaverage()
+fmri_img_surf = SurfaceImage.from_volume(
+    mesh=fsaverage["pial"], volume_img=fmri_img, radius=5
+)
 
-radius = 5.0
-pial_mesh = fsaverage[f"pial_{hemi}"]
-X = surface.vol_to_surf(fmri_img, pial_mesh, radius=radius).T
-
-# %%
-# To define the :term:`BOLD` responses
-# to be included within each searchlight "sphere"
-# we define an adjacency matrix based on the inflated surface vertices such
-# that nearby surfaces are concatenated within the same searchlight.
-
-infl_mesh = fsaverage[f"infl_{hemi}"]
-coords, _ = surface.load_surf_mesh(infl_mesh)
-radius = 3.0
-nn = neighbors.NearestNeighbors(radius=radius)
-adjacency = nn.fit(coords).radius_neighbors_graph(coords).tolil()
 
 # %%
 # Searchlight computation
@@ -70,27 +61,51 @@ from sklearn.preprocessing import StandardScaler
 
 from nilearn.decoding.searchlight import search_light
 
-# Simple linear estimator preceded by a normalization step
-estimator = make_pipeline(StandardScaler(), RidgeClassifier(alpha=10.0))
+scores = {}
 
-# Define cross-validation scheme
-cv = KFold(n_splits=3, shuffle=False)
+for hemi in ["left", "right"]:
+    # To define the :term:`BOLD responses
+    # to be included within each searchlight "sphere"
+    # we define an adjacency matrix
+    # based on the inflated surface vertices
+    # such that nearby surfaces are concatenated
+    # within the same searchlight.
+    coordinates = fsaverage["inflated"].parts[hemi].coordinates
+    nn = neighbors.NearestNeighbors(radius=3)
+    adjacency = nn.fit(coordinates).radius_neighbors_graph(coordinates).tolil()
 
-# Cross-validated search light
-scores = search_light(X, y, estimator, adjacency, cv=cv, n_jobs=2)
+    # Simple linear estimator preceded by a normalization step
+    estimator = make_pipeline(StandardScaler(), RidgeClassifier(alpha=10.0))
+
+    # Define cross-validation scheme
+    cv = KFold(n_splits=3, shuffle=False)
+
+    X = fmri_img_surf.data.parts["hemi"]
+
+    # Cross-validated search light
+    scores[hemi] = search_light(X, y, estimator, adjacency, cv=cv, n_jobs=2)
+
 # %%
 # Visualization
 # -------------
 from nilearn import plotting
+from nilearn.experimental.surface import load_fsaverage_data
+
+score_img = SurfaceImage(mesh=fsaverage["inflated"], data=scores)
 
 chance = 0.5
+for hemi in ["left", "right"]:
+    score_img.data.parts[hemi] = score_img.data.parts[hemi] - 0.5
+
+fsaverage_data = load_fsaverage_data(mesh_type="inflated", data_type="sulcal")
+
 plotting.plot_surf_stat_map(
-    infl_mesh,
-    scores - chance,
+    stat_map=score_img,
     view="medial",
+    hemi="left",
     colorbar=True,
-    threshold=0.1,
-    bg_map=fsaverage[f"sulc_{hemi}"],
+    threshold=0.6,
+    bg_map=fsaverage_data,
     title="Accuracy map, left hemisphere",
 )
 plotting.show()
