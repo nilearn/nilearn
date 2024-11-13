3dca1f72e81e9f81fed93cf9db3638cd54106b29
Remi Gau <remi_gau@hotmail.com>
2024-11-13 10:25:25 +0100

    remove old code

--- nilearn/reporting/tests/test_html_report.py
+++ nilearn/reporting/tests/test_html_report.py
@@ -11,7 +11,7 @@ from nilearn._utils.data_gen import (
     generate_maps,
     generate_random_img,
 )
-from nilearn.experimental.surface import SurfaceLabelsMasker, SurfaceMasker
+from nilearn.experimental.surface import SurfaceLabelsMasker
 from nilearn.image import get_data, new_img_like
 from nilearn.maskers import (
     MultiNiftiLabelsMasker,
@@ -21,6 +21,7 @@ from nilearn.maskers import (
     NiftiMapsMasker,
     NiftiMasker,
     NiftiSpheresMasker,
+    SurfaceMasker,
 )
 
 # Note: html output by nilearn view_* functions
