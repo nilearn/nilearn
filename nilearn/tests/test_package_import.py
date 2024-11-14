e76593cbc12cde874eb51db5addfa27f4600dfb3
Remi Gau <remi_gau@hotmail.com>
2024-11-14 21:28:58 +0100

    skip tests

--- nilearn/tests/test_package_import.py
+++ nilearn/tests/test_package_import.py
@@ -1,8 +1,29 @@
-import pytest
+import os
 
+import pytest
+from matplotlib import __version__ as mpl_version
+from numpy import __version__ as np_version
+
+from nilearn._utils import compare_version
 from nilearn._utils.helpers import is_matplotlib_installed
 
 
+def on_windows_with_old_mpl_and_new_numpy():
+    return (
+        compare_version(np_version, ">", "1.26.4")
+        and compare_version(mpl_version, "<", "3.8.0")
+        and os.name == "nt"
+    )
+
+
+@pytest.mark.skipif(
+    on_windows_with_old_mpl_and_new_numpy(),
+    reason="Old matplotlib not compatible with numpy 2.0 on windows.",
+)
+@pytest.mark.skipif(
+    is_matplotlib_installed(),
+    reason="This test should run only if matplotlib is not installed.",
+)
 @pytest.mark.skipif(
     is_matplotlib_installed(),
     reason="This test should run only if matplotlib is not installed.",
@@ -19,6 +40,10 @@ def test_import_plotting_should_raise_warning_if_matplotlib_not_installed():
         from nilearn.plotting import cm  # noqa
 
 
+@pytest.mark.skipif(
+    on_windows_with_old_mpl_and_new_numpy(),
+    reason="Old matplotlib not compatible with numpy 2.0 on windows.",
+)
 @pytest.mark.skipif(
     is_matplotlib_installed(),
     reason="This test should run only if matplotlib is not installed.",
