057db7e61a36212f3c9978df93569415383c104d
Remi Gau <remi_gau@hotmail.com>
2024-11-14 14:35:45 +0100

    unpdate init

--- nilearn/experimental/surface/__init__.py
+++ nilearn/experimental/surface/__init__.py
@@ -1,10 +1,5 @@
 """The :mod:`nilearn.experimental.surface` module."""
 
-from nilearn.experimental.surface._datasets import (
-    fetch_nki,
-    load_fsaverage,
-    load_fsaverage_data,
-)
 from nilearn.experimental.surface._surface_image import (
     FileMesh,
     InMemoryMesh,
@@ -27,7 +22,4 @@ __all__ = [
     "SurfaceImage",
     "SurfaceLabelsMasker",
     "SurfaceMasker",
-    "fetch_nki",
-    "load_fsaverage",
-    "load_fsaverage_data",
 ]
