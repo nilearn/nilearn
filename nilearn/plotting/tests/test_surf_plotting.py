2366f5b1ef245da449165e650dd1d11ee339d47d
Remi Gau <remi_gau@hotmail.com>
2024-11-26 17:02:54 +0100

    fix

--- nilearn/plotting/tests/test_surf_plotting.py
+++ nilearn/plotting/tests/test_surf_plotting.py
@@ -225,7 +225,7 @@ def test_check_surface_plotting_inputs_extract_mesh_and_data(
         hemi=hemi,
         bg_map=bg_map,
     )
-    assert_array_equal(out_surf_map, surf_img().data.parts[hemi][:, 0])
+    assert_array_equal(out_surf_map, surf_img().data.parts[hemi].T)
     assert_surf_mesh_equal(out_surf_mesh, surf_img().mesh.parts[hemi])
     assert bg_map == out_bg_map
 
