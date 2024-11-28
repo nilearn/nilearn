849db4522eb2829083fce9b6253f0f6217b9b30a
Remi Gau <remi_gau@hotmail.com>
2024-11-28 22:12:09 +0100

    test

--- examples/04_glm_first_level/plot_design_matrix.py
+++ examples/04_glm_first_level/plot_design_matrix.py
@@ -136,7 +136,8 @@ plt.show()
 # We can plot the correlation between the regressors of our design matrix.
 # This is important to check as highly correlated regressors can affect
 # the effficieny of
-# `your design <https://imaging.mrc-cbu.cam.ac.uk/imaging/DesignEfficiency#Correlation_between_regressors>`_. # noqa: E501
+# `your design <https://imaging.mrc-cbu.cam.ac.uk/imaging/\
+#  DesignEfficiency#Correlation_between_regressors>`_.
 #
 from nilearn.plotting import plot_design_matrix_correlation
 
