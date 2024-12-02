f8a32fe327b627642f1272607724366299222638
Remi Gau <remi_gau@hotmail.com>
2024-12-02 13:30:33 +0100

    [FIX] use linebreaks in doc to avoid noqa (#4815)

--- examples/02_decoding/plot_haxby_anova_svm.py
+++ examples/02_decoding/plot_haxby_anova_svm.py
@@ -81,7 +81,8 @@ y_pred = decoder.predict(func_img)
 # to the cv parameter of decoder.leave-one-session-out.
 # For more details please take a look at:
 # `Measuring prediction scores using cross-validation\
-# <../00_tutorials/plot_decoding_tutorial.html#measuring-prediction-scores-using-cross-validation>`_  # noqa: E501
+# <../00_tutorials/plot_decoding_tutorial.html\
+# #measuring-prediction-scores-using-cross-validation>`_
 from sklearn.model_selection import LeaveOneGroupOut
 
 cv = LeaveOneGroupOut()
