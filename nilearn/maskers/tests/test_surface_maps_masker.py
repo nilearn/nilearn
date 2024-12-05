097da5037a49e5a7448206c8d73bf5fec9e41375
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2024-12-05 17:38:37 +0100

    squeeze until fixtures are fixed

--- nilearn/maskers/tests/test_surface_maps_masker.py
+++ nilearn/maskers/tests/test_surface_maps_masker.py
@@ -7,7 +7,12 @@ def test_surface_maps_masker_fit_transform_shape(
     surf_maps_img, surf_img, surf_mask
 ):
     """Test that the fit_transform method returns the expected shape."""
-    masker = SurfaceMapsMasker(surf_maps_img, surf_mask()).fit()
+    # TODO: remove after #4897 is merged
+    surf_mask = surf_mask()
+    surf_mask.data.parts["left"] = surf_mask.data.parts["left"].squeeze()
+    surf_mask.data.parts["right"] = surf_mask.data.parts["right"].squeeze()
+
+    masker = SurfaceMapsMasker(surf_maps_img, surf_mask).fit()
     region_signals = masker.transform(surf_img(50))
     assert region_signals.shape == (
         surf_img(50).shape[0],
@@ -21,7 +26,12 @@ def test_surface_maps_masker_inverse_transform_shape(
     """Test that inverse_transform returns an image with the same shape as the
     input.
     """
-    masker = SurfaceMapsMasker(surf_maps_img, surf_mask()).fit()
+    # TODO: remove after #4897 is merged
+    surf_mask = surf_mask()
+    surf_mask.data.parts["left"] = surf_mask.data.parts["left"].squeeze()
+    surf_mask.data.parts["right"] = surf_mask.data.parts["right"].squeeze()
+
+    masker = SurfaceMapsMasker(surf_maps_img, surf_mask).fit()
     region_signals = masker.fit_transform(surf_img(50))
     X_inverse_transformed = masker.inverse_transform(region_signals)
     assert X_inverse_transformed.shape == surf_img(50).shape
@@ -29,18 +39,31 @@ def test_surface_maps_masker_inverse_transform_shape(
 
 def test_surface_maps_masker_1d_maps_img(surf_img):
     """Test that an error is raised when maps_img has 1D data."""
+    # TODO: remove after #4897 is merged
+    surf_maps_img_1d = surf_img()
+    surf_maps_img_1d.data.parts["left"] = surf_maps_img_1d.data.parts[
+        "left"
+    ].squeeze()
+    surf_maps_img_1d.data.parts["right"] = surf_maps_img_1d.data.parts[
+        "right"
+    ].squeeze()
+
     with pytest.raises(
         ValueError,
         match="each hemisphere of maps_img should have 2 dimensions",
     ):
-        SurfaceMapsMasker(maps_img=surf_img()).fit()
+        SurfaceMapsMasker(maps_img=surf_maps_img_1d).fit()
 
 
 def test_surface_maps_masker_1d_img(surf_maps_img, surf_img):
     """Test that an error is raised when img has 1D data."""
+    surf_img_1d = surf_img()
+    surf_img_1d.data.parts["left"] = surf_img_1d.data.parts["left"].squeeze()
+    surf_img_1d.data.parts["right"] = surf_img_1d.data.parts["right"].squeeze()
+
     with pytest.raises(
         ValueError,
         match="each hemisphere of img should have 2 dimensions",
     ):
         masker = SurfaceMapsMasker(maps_img=surf_maps_img).fit()
-        masker.transform(surf_img())
+        masker.transform(surf_img_1d)
