2378e39837d9dbe57bbe84d50383156bd1824b06
Hande Gözükan <hande@ginkgo.work>
2024-12-12 17:00:10 +0100

    add test for get_clusters_table import

--- nilearn/tests/test_package_import.py
+++ nilearn/tests/test_package_import.py
@@ -32,6 +32,9 @@ def on_windows_with_old_mpl_and_new_numpy():
     reason="This test should run only if matplotlib is not installed.",
 )
 def test_import_plotting_should_raise_warning_if_matplotlib_not_installed():
+    """Tests if importing nilearn.plotting displays correct warning and raises
+    error when matplotlib is not installed.
+    """
     with (
         pytest.raises(
             ModuleNotFoundError, match="No module named 'matplotlib'"
@@ -52,10 +55,24 @@ def test_import_plotting_should_raise_warning_if_matplotlib_not_installed():
     reason="This test should run only if matplotlib is not installed.",
 )
 def test_import_reporting_should_raise_warning_if_matplotlib_not_installed():
+    """Tests if importing nilearn.reporting.make_glm_report displays correct
+    warning and raises error when matplotlib is not installed.
+    """
     with (
-        pytest.warns(UserWarning, match="nilearn.reporting.glm_reporter and"),
         pytest.raises(
             ImportError, match="cannot import name 'make_glm_report'"
         ),
+        pytest.warns(UserWarning, match="nilearn.reporting.glm_reporter and"),
     ):
         from nilearn.reporting import make_glm_report  # noqa: F401
+
+
+@pytest.mark.skipif(
+    is_matplotlib_installed(),
+    reason="This test should run only if matplotlib is not installed.",
+)
+def test_import_get_clusters_table_when_matplotlib_not_installed():
+    """Tests if nilearn.reporting.get_clusters_table can be imported without
+    problems when matplotlib is not installed.
+    """
+    from nilearn.reporting import get_clusters_table  # noqa: F401
