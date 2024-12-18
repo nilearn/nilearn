3b1345efc4a7fcc1cb09079695863f0befb060f6
Remi Gau <remi_gau@hotmail.com>
2024-12-18 12:55:24 +0100

    ref

--- nilearn/plotting/tests/test_surf_plotting.py
+++ nilearn/plotting/tests/test_surf_plotting.py
@@ -1505,16 +1505,32 @@ def test_plot_img_on_surf_input_as_file(pyplot, img_3d_mni_as_file):
     plot_img_on_surf(stat_map=str(img_3d_mni_as_file))
 
 
-def test_plot_surf_contours(pyplot, in_memory_mesh):
-    # we need a valid parcellation for testing
+@pytest.fixture
+def parcellation(in_memory_mesh):
     parcellation = np.zeros((in_memory_mesh.n_vertices,))
     parcellation[in_memory_mesh.faces[3]] = 1
     parcellation[in_memory_mesh.faces[5]] = 2
+    return parcellation
+
+
+def test_plot_surf_contours(pyplot, in_memory_mesh, parcellation):
     plot_surf_contours(in_memory_mesh, parcellation)
     plot_surf_contours(in_memory_mesh, parcellation, levels=[1, 2])
     plot_surf_contours(
         in_memory_mesh, parcellation, levels=[1, 2], cmap="gist_ncar"
     )
+
+
+def test_plot_surf_contours_legend(pyplot, in_memory_mesh, parcellation):
+    fig = plot_surf_contours(
+        in_memory_mesh,
+        parcellation,
+        legend=True,
+    )
+    assert fig.legends is not None
+
+
+def test_plot_surf_contours_colors(pyplot, in_memory_mesh, parcellation):
     plot_surf_contours(
         in_memory_mesh, parcellation, levels=[1, 2], colors=["r", "g"]
     )
@@ -1522,27 +1538,24 @@ def test_plot_surf_contours(pyplot, in_memory_mesh):
         in_memory_mesh,
         parcellation,
         levels=[1, 2],
-        colors=["r", "g"],
         labels=["1", "2"],
-    )
-    fig = plot_surf_contours(
-        in_memory_mesh,
-        parcellation,
-        levels=[1, 2],
         colors=["r", "g"],
-        labels=["1", "2"],
-        legend=True,
     )
-    assert fig.legends is not None
     plot_surf_contours(
         in_memory_mesh,
         parcellation,
         levels=[1, 2],
         colors=[[0, 0, 0, 1], [1, 1, 1, 1]],
     )
+
+
+def test_plot_surf_contours_fig_axes(pyplot, in_memory_mesh, parcellation):
     fig, axes = plt.subplots(1, 1, subplot_kw={"projection": "3d"})
     plot_surf_contours(in_memory_mesh, parcellation, axes=axes)
     plot_surf_contours(in_memory_mesh, parcellation, figure=fig)
+
+
+def test_plot_surf_contours_axis_title(pyplot, in_memory_mesh, parcellation):
     fig = plot_surf(in_memory_mesh)
     plot_surf_contours(in_memory_mesh, parcellation, figure=fig)
     display = plot_surf_contours(
@@ -1558,6 +1571,7 @@ def test_plot_surf_contours(pyplot, in_memory_mesh):
     # Non-regression assertion: we switched from _suptitle to axis title
     assert display._suptitle is None
     assert display.axes[0].get_title() == "title"
+
     fig = plot_surf(in_memory_mesh, title="title 2")
     display = plot_surf_contours(
         in_memory_mesh,
@@ -1577,24 +1591,24 @@ def test_plot_surf_contours(pyplot, in_memory_mesh):
         )
 
 
-def test_plot_surf_contours_error(rng, in_memory_mesh):
+def test_plot_surf_contours_error(rng, in_memory_mesh, parcellation):
     # we need an invalid parcellation for testing
     invalid_parcellation = rng.uniform(size=(in_memory_mesh.n_vertices))
-    parcellation = np.zeros((in_memory_mesh.n_vertices,))
-    parcellation[in_memory_mesh.faces[3]] = 1
-    parcellation[in_memory_mesh.faces[5]] = 2
     with pytest.raises(
         ValueError, match="Vertices in parcellation do not form region."
     ):
         plot_surf_contours(in_memory_mesh, invalid_parcellation)
+
     _, axes = plt.subplots(1, 1)
     with pytest.raises(ValueError, match="Axes must be 3D."):
         plot_surf_contours(in_memory_mesh, parcellation, axes=axes)
+
     msg = "All elements of colors .* matplotlib .* RGBA"
     with pytest.raises(ValueError, match=msg):
         plot_surf_contours(
             in_memory_mesh, parcellation, levels=[1, 2], colors=[[1, 2], 3]
         )
+
     msg = "Levels, labels, and colors argument .* same length or None."
     with pytest.raises(ValueError, match=msg):
         plot_surf_contours(
