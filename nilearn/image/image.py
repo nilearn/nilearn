ed589e4fb25c2872d24f627f8f0f560a7b56347f
Hande Gözükan <hande@ginkgo.work>
2024-12-31 10:53:33 +0100

    update docstring format

--- nilearn/image/image.py
+++ nilearn/image/image.py
@@ -928,46 +928,42 @@ def threshold_img(
 
       - When ``two_sided`` is True:
 
-        the given value should be
-        within the range of minimum and maximum intensity
-        of the input image.
-        All instensities in the interval ``[-threshold, threshold]``
-        will be set to zero.
+        The given value should be within the range of minimum and maximum
+        intensity of the input image.
+        All instensities in the interval ``[-threshold, threshold]`` will be
+        set to zero.
 
       - When ``two_sided`` is False:
 
         - If the threshold is negative:
 
-          then it should be greater than the minimum intensity
-          of the input data.
-          All intensities greater than or equal
-          to the specified threshold will be set to zero.
+          It should be greater than the minimum intensity of the input data.
+          All intensities greater than or equal to the specified threshold will
+          be set to zero.
           All other instensities keep their original values.
 
         - If the threshold is positive:
 
-          then it should be less than the maximum intensity
-          of the input data.
-          All intensities less than or equal
-          to the specified threshold will be set to zero.
+          then it should be less than the maximum intensity of the input data.
+          All intensities less than or equal to the specified threshold will be
+          set to zero.
           All other instensities keep their original values.
 
     - If threshold is :obj:`str`:
 
-      the number part should be in interval ``[0, 100]``.
-      We threshold the image based on the score
-      obtained using this percentile on the image data.
+      The number part should be in interval ``[0, 100]``.
+      We threshold the image based on the score obtained using this percentile
+      on the image data.
       The percentile rank is computed using
       :func:`scipy.stats.scoreatpercentile`.
 
       - When ``two_sided`` is True:
 
-        the score is calculated on the absolute values of data.
+        The score is calculated on the absolute values of data.
 
       - When ``two_sided`` is False:
 
-        the score is calculated
-        only on the non-negative values of data.
+        The score is calculated only on the non-negative values of data.
 
     .. versionchanged:: 0.9.0
         New ``cluster_threshold`` and ``two_sided`` parameters added.
