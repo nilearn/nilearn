ba4789035e8affcaf4062936e7854d0206472566
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-01-01 12:12:09 +0100

    Update nilearn/reporting/tests/test_reporting.py
    Co-authored-by: Hande Gözükan <2099645+hndgzkn@users.noreply.github.com>

--- nilearn/reporting/tests/test_reporting.py
+++ nilearn/reporting/tests/test_reporting.py
@@ -39,10 +39,6 @@ def test_local_max_two_maxima(shape, affine_eye):
     assert np.array_equal(vals, np.array([6]))
 
 
-@pytest.mark.skipif(
-    not is_matplotlib_installed(),
-    reason="Matplotlib not installed; required for this test",
-)
 def test_local_max_two_global_maxima(shape, affine_eye):
     """Basic test of nilearn.reporting._get_clusters_table._local_max()."""
     # Two global (equal) maxima, 10 voxels apart.
