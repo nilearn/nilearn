21dff50dbc83ca60915e413bc3a880a6181f1102
Remi Gau <remi_gau@hotmail.com>
2025-01-06 17:40:58 +0100

    Update nilearn/_utils/docs.py

--- nilearn/_utils/docs.py
+++ nilearn/_utils/docs.py
@@ -947,7 +947,7 @@ vmin : :obj:`float`, optional
 
 ##############################################################################
 
-docdict_indented: dict[int, dict[str, str]] = {}
+docdict_indented = {}
 
 
 def _indentcount_lines(lines):
