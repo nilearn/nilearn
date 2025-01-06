b716b28747cc36948fa869ce82af5779d52bd1b8
Remi Gau <remi_gau@hotmail.com>
2025-01-06 09:43:45 +0100

    update scripts

--- build_tools/github/merge_upstream.sh
+++ build_tools/github/merge_upstream.sh
@@ -2,6 +2,8 @@
 
 # Add upstream as remote and merge main branch into the PR branch
 # Generate a gitlog.txt file that stores the last commit message.
+# Generate a merge.txt file that stores the name of the current branch
+# (equivalent to the GITHUB_REF_NAME in github action environment).
 
 set -x -e
 
@@ -15,14 +17,14 @@ echo "gitlog.txt = $(cat gitlog.txt)"
 
 if [ -z ${CI+x} ]; then
     echo "Running locally";
-    GITHUB_REF_NAME=$(git rev-parse --abbrev-ref HEAD)
+    git rev-parse --abbrev-ref HEAD | tee merge.txt
     GITHUB_REF_TYPE="branch"
 else
     echo "Running in CI";
+    echo "$GITHUB_REF_NAME" | tee merge.txt
 fi
 
-echo "$GITHUB_REF_NAME" | tee merge.txt
-if [ "$GITHUB_REF_NAME" != "main" ] && [ "$GITHUB_REF_TYPE" == "branch" ]; then
+if ! [ -z ${CI+x} ] && [ "$GITHUB_REF_NAME" != "main" ] && [ "$GITHUB_REF_TYPE" == "branch" ]; then
     echo "Merging $(cat merge.txt)";
     git pull --ff-only upstream "refs/pull/$(cat merge.txt)";
 fi
