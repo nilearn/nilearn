5c8756c2ce0904fd2ec7050d2c85598e9ec4b715
Remi Gau <remi_gau@hotmail.com>
2025-01-13 14:52:48 +0100

    nope

--- nilearn/connectome/tests/test_group_sparse_cov.py
+++ nilearn/connectome/tests/test_group_sparse_cov.py
@@ -14,7 +14,6 @@ extra_valid_checks = [
     "check_no_attributes_set_in_init",
     "check_estimators_unfitted",
     "check_do_not_raise_errors_in_init_or_set_params",
-    "check_estimator_sparse_tag",
 ]
 
 
