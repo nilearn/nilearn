14c79e55db238ef2caadbcd6e4e5f30ada4339ef
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-01-15 12:18:03 +0100

    fix when display_maps is a list

--- nilearn/maskers/surface_maps_masker.py
+++ nilearn/maskers/surface_maps_masker.py
@@ -653,7 +653,7 @@ class SurfaceMapsMasker(_BaseSurfaceMasker):
                 bg_map=bg_img,
                 bg_on_data=True,
                 threshold=threshold,
-                hemi="left",  # TODO: use "both" after #4991 is merged
+                hemi="both",  # TODO: use "both" after #4991 is merged
             ).get_iframe()
         elif self._report_content["engine"] == "matplotlib":
             # TODO: possibly allow to generate a report with other views
