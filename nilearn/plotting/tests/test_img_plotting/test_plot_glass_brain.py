88b605aa38ea73f1ea2dda6f7ab4a7f58110e47b
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-01-20 16:38:24 +0100

    pyplot to matplotlib_pyplot

--- nilearn/plotting/tests/test_img_plotting/test_plot_glass_brain.py
+++ nilearn/plotting/tests/test_img_plotting/test_plot_glass_brain.py
@@ -11,7 +11,7 @@ from nilearn.image import get_data
 from nilearn.plotting import plot_glass_brain
 
 
-def test_plot_glass_brain(pyplot, img_3d_mni):
+def test_plot_glass_brain(matplotlib_pyplot, img_3d_mni):
     """Smoke tests for plot_glass_brain with colorbar and negative values."""
     plot_glass_brain(
         img_3d_mni, colorbar=True, resampling_interpolation="nearest"
@@ -36,13 +36,13 @@ def test_plot_glass_brain(pyplot, img_3d_mni):
 @pytest.mark.parametrize("black_bg", [True, False])
 @pytest.mark.parametrize("plot_abs", [True, False])
 def test_plot_glass_brain_background_absolute(
-    pyplot, img_3d_mni, plot_abs, black_bg
+    matplotlib_pyplot, img_3d_mni, plot_abs, black_bg
 ):
     """Somke test for absolute value plotting and background."""
     plot_glass_brain(img_3d_mni, black_bg=black_bg, plot_abs=plot_abs)
 
 
-def test_plot_glass_brain_file_output(pyplot, img_3d_mni, tmp_path):
+def test_plot_glass_brain_file_output(matplotlib_pyplot, img_3d_mni, tmp_path):
     """Smoke-test for hemispheric glass brain with file output."""
     filename = tmp_path / "test.png"
     plot_glass_brain(
@@ -52,7 +52,7 @@ def test_plot_glass_brain_file_output(pyplot, img_3d_mni, tmp_path):
     )
 
 
-def test_plot_noncurrent_axes(pyplot, rng):
+def test_plot_noncurrent_axes(matplotlib_pyplot, rng):
     """Regression test for Issue #450."""
     maps_img = Nifti1Image(rng.random((10, 10, 10)), np.eye(4))
     fh1 = plt.figure()
@@ -69,7 +69,7 @@ def test_plot_noncurrent_axes(pyplot, rng):
         assert ax_fh == fh1, f"New axis {ax_name} should be in fh1."
 
 
-def test_add_markers_using_plot_glass_brain(pyplot):
+def test_add_markers_using_plot_glass_brain(matplotlib_pyplot):
     """Tests for adding markers through plot_glass_brain."""
     fig = plot_glass_brain(None)
     coords = [(-34, -39, -9)]
@@ -77,7 +77,7 @@ def test_add_markers_using_plot_glass_brain(pyplot):
     fig.close()
 
 
-def test_add_markers_right_hemi(pyplot):
+def test_add_markers_right_hemi(matplotlib_pyplot):
     """Add a single marker in right hemisphere.
 
     No marker should appear in the left hemisphere when plotting.
@@ -98,7 +98,7 @@ def test_add_markers_right_hemi(pyplot):
         )
 
 
-def test_add_markers_left_hemi(pyplot):
+def test_add_markers_left_hemi(matplotlib_pyplot):
     """Add two markers in left hemisphere.
 
     No marker should appear in the right hemisphere when plotting.
@@ -120,7 +120,9 @@ def test_add_markers_left_hemi(pyplot):
         )
 
 
-def test_plot_glass_brain_colorbar_having_nans(pyplot, affine_eye, img_3d_mni):
+def test_plot_glass_brain_colorbar_having_nans(
+    matplotlib_pyplot, affine_eye, img_3d_mni
+):
     """Smoke-test for plot_glass_brain and nans in the data image."""
     data = get_data(img_3d_mni)
     data[6, 5, 2] = np.inf
@@ -128,25 +130,29 @@ def test_plot_glass_brain_colorbar_having_nans(pyplot, affine_eye, img_3d_mni):
 
 
 @pytest.mark.parametrize("display_mode", ["lr", "lzry"])
-def test_plot_glass_brain_display_modes_without_img(pyplot, display_mode):
+def test_plot_glass_brain_display_modes_without_img(
+    matplotlib_pyplot, display_mode
+):
     """Smoke test for work around from PR #1888."""
     plot_glass_brain(None, display_mode=display_mode)
 
 
 @pytest.mark.parametrize("display_mode", ["lr", "lzry"])
 def test_plot_glass_brain_with_completely_masked_img(
-    pyplot, img_3d_mni, display_mode
+    matplotlib_pyplot, img_3d_mni, display_mode
 ):
     """Smoke test for PR #1888 with display modes having 'l'."""
     plot_glass_brain(img_3d_mni, display_mode=display_mode)
 
 
-def test_plot_glass_brain_vmin_vmax(pyplot, img_3d_mni):
+def test_plot_glass_brain_vmin_vmax(matplotlib_pyplot, img_3d_mni):
     """Smoke tests for plot_glass_brain being passed vmin and vmax."""
     plot_glass_brain(img_3d_mni, vmin=-2, vmax=2)
 
 
-def test_plot_glass_brain_negative_vmin_with_plot_abs(pyplot, img_3d_mni):
+def test_plot_glass_brain_negative_vmin_with_plot_abs(
+    matplotlib_pyplot, img_3d_mni
+):
     """Test that warning is thrown if plot_abs is True and vmin is negative."""
     warning_message = "vmin is negative but plot_abs is True"
     with pytest.warns(UserWarning, match=warning_message):
