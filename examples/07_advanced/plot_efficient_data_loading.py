f3884c999083d1e5724b6b41c8d2c6f0e13c0c21
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-01-30 15:52:59 +0100

    docstring and params

--- examples/07_advanced/plot_efficient_data_loading.py
+++ examples/07_advanced/plot_efficient_data_loading.py
@@ -36,10 +36,10 @@ def get_atlas_path():
 
 
 @profile
-def atlas_to_masks(atlas_path, fmri_path):
+def atlas_to_masks(atlas_path, fmri_path, n_regions=6):
     masks = load_img(atlas_path)
     # only keep the first 6 regions
-    masks = index_img(masks, slice(0, 6))
+    masks = index_img(masks, slice(0, n_regions))
     mask_paths = []
     Path("temp").mkdir(parents=True, exist_ok=True)
     for i, mask in enumerate(iter_img(masks)):
@@ -75,8 +75,7 @@ def mask_fmri_single_efficient(fmri_path, mask_path):
 
 
 @profile
-def mask_fmri_parallel(fmri_path, mask_paths):
-    n_jobs = 6
+def mask_fmri_parallel(fmri_path, mask_paths, n_jobs=6):
     fmri_ts = Parallel(n_jobs=n_jobs)(
         delayed(mask_fmri_single)(fmri_path, mask) for mask in mask_paths
     )
@@ -84,8 +83,7 @@ def mask_fmri_parallel(fmri_path, mask_paths):
 
 
 @profile
-def mask_fmri_efficient_parallel(fmri_path, mask_paths):
-    n_jobs = 6
+def mask_fmri_efficient_parallel(fmri_path, mask_paths, n_jobs=6):
     fmri_ts = Parallel(n_jobs=n_jobs)(
         delayed(mask_fmri_single_efficient)(fmri_path, mask)
         for mask in mask_paths
@@ -94,23 +92,48 @@ def mask_fmri_efficient_parallel(fmri_path, mask_paths):
 
 
 @profile
-def main(N=1):
-    fmri_path = get_fmri_path(n_subjects=N)
+def main(n_images=1, n_regions=6):
+    """
+    Compare the performance of NiftiMasker vs. numpy masking by:
+    1. fetching `n_images` subjects from development fMRI dataset and
+    `n_regions` regions from the Difumo atlas.
+    2. then converting these regions to binary masks.
+    3. then using the NiftiMasker to mask the data.
+    4. then also using dataobj to load the data directly from the nifti file
+    and masking it simply using numpy.
+
+    Parameters
+    ----------
+    n_images : int, default=1
+        Number of subjects to fetch from the development fMRI dataset. These
+        subject images would be concatenated to form a single nifti file.
+        Can be increased to simulate larger data.
+
+    n_regions : int, default=6
+        Number of regions to fetch from the Difumo atlas. These regions would
+        be converted to binary masks and used to mask the fMRI data. This is
+        also the number of jobs to run in parallel.
+    """
+    fmri_path = get_fmri_path(n_subjects=n_images)
     atlas_path = get_atlas_path()
-    mask_paths = atlas_to_masks(atlas_path, fmri_path)
+    mask_paths = atlas_to_masks(atlas_path, fmri_path, n_regions=n_regions)
 
     print("waiting")
     time.sleep(30)
     print("start nifti masker")
 
-    ts_nifti_masker = mask_fmri_parallel(fmri_path, mask_paths)
+    ts_nifti_masker = mask_fmri_parallel(
+        fmri_path, mask_paths, n_jobs=n_regions
+    )
     del ts_nifti_masker
 
     print("waiting")
     time.sleep(30)
     print("start efficient masker")
 
-    ts_efficient = mask_fmri_efficient_parallel(fmri_path, mask_paths)
+    ts_efficient = mask_fmri_efficient_parallel(
+        fmri_path, mask_paths, n_jobs=n_regions
+    )
     del ts_efficient
 
 
