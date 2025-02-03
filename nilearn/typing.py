ebb139fa350b19d231fbf8e2da224061f3402595
Remi Gau <remi_gau@hotmail.com>
2025-02-03 15:48:33 +0100

    fix

--- nilearn/typing.py
+++ nilearn/typing.py
@@ -8,13 +8,26 @@ declared in nilearn._utils.doc.
 from __future__ import annotations
 
 import pathlib
+import sys
 
 from joblib.memory import Memory
 
-DataDir = str | pathlib.Path | None
-MemoryLike = Memory | str | pathlib.Path | None
 MemoryLevel = int
-Resolution = int | None
 Resume = bool
-Url = str | None
 Verbose = int
+
+# TODO update when dropping python 3.9
+if sys.version_info[1] < 10:
+    DataDir = (str, pathlib.Path)
+    MemoryLike = (Memory, str, pathlib.Path)
+    Resolution = int
+    Url = str
+
+
+else:
+    from typing import TypeAlias
+
+    DataDir: TypeAlias = str | pathlib.Path | None
+    MemoryLike: TypeAlias = Memory | str | pathlib.Path | None
+    Resolution: TypeAlias = int | None
+    Url: TypeAlias = str | None
