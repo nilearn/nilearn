c57e8faf53e0da45d265a1b9dcf87b06cd93757a
Remi Gau <remi_gau@hotmail.com>
2025-02-06 10:33:07 +0100

    [REF] refactor group_sparse_covariance

--- nilearn/connectome/group_sparse_cov.py
+++ nilearn/connectome/group_sparse_cov.py
@@ -258,15 +258,7 @@ def _group_sparse_covariance(
 
     _check_diagonal_normalization(emp_covs, n_subjects)
 
-    if precisions_init is None:
-        # Fortran order make omega[..., k] contiguous, which is often useful.
-        omega = np.ndarray(shape=emp_covs.shape, dtype=np.float64, order="F")
-        for k in range(n_subjects):
-            # Values on main diagonals are far from zero, because they
-            # are timeseries energy.
-            omega[..., k] = np.diag(1.0 / np.diag(emp_covs[..., k]))
-    else:
-        omega = precisions_init.copy()
+    omega = _init_omega(emp_covs, precisions_init)
 
     # Preallocate arrays
     y = np.ndarray(shape=(n_subjects, n_features - 1), dtype=np.float64)
@@ -464,6 +456,20 @@ def _group_sparse_covariance(
         )
 
     return omega
+
+
+def _init_omega(emp_covs, precisions_init):
+    """Initialize omega value."""
+    if precisions_init is None:
+        n_subjects = emp_covs.shape[-1]
+        # Fortran order make omega[..., k] contiguous, which is often useful.
+        omega = np.ndarray(shape=emp_covs.shape, dtype=np.float64, order="F")
+        for k in range(n_subjects):
+            # Values on main diagonals are far from zero, because they
+            # are timeseries energy.
+            omega[..., k] = np.diag(1.0 / np.diag(emp_covs[..., k]))
+    else:
+        omega = precisions_init.copy()
 
 
 def _check_alpha(alpha):
