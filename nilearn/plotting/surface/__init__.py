838de7144319dd51bf5d0bf223c7785d8fd72c66
Hande Gözükan <hande@ginkgo.work>
2025-02-11 10:33:22 +0100

    add init file for surface package

---
+++ nilearn/plotting/surface/__init__.py
@@ -0,0 +1,15 @@
+from nilearn.plotting.surface.surf_plotting import (
+    plot_img_on_surf,
+    plot_surf,
+    plot_surf_contours,
+    plot_surf_roi,
+    plot_surf_stat_map,
+)
+
+__all__ = [
+    "plot_img_on_surf",
+    "plot_surf",
+    "plot_surf_contours",
+    "plot_surf_roi",
+    "plot_surf_stat_map",
+]
