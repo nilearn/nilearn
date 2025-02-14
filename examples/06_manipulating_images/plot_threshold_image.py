88f7d2b8315e14b8e0e391d82675230a845e72ac
Remi Gau <remi_gau@hotmail.com>
2025-02-14 08:16:01 +0100

    [REF] refactor example thresolding

--- examples/06_manipulating_images/plot_threshold_image.py
+++ examples/06_manipulating_images/plot_threshold_image.py
@@ -10,7 +10,6 @@ The goal of this example is to illustrate the use of the function
 The ``threshold`` parameter can take both positive and negative values.
 ``two_sided`` parameter is complementary to ``threshold`` effecting its
 behavior.
-
 """
 
 # %%
@@ -19,28 +18,37 @@ behavior.
 #
 # We will first load the dataset and display the image without manipulation.
 
-from nilearn import datasets, plotting
-
-cut_coords = [-26, -33, 59]
+from nilearn import datasets
+from nilearn.plotting import plot_stat_map
 
 image = datasets.load_sample_motor_activation_image()
 
-plotting.plot_stat_map(
-    image,
-    colorbar=True,
-    title="image without threshold",
-    cut_coords=cut_coords,
-)
+plot_param = {
+    "display_mode": "ortho",
+    "cut_coords": [5, -26, 21],
+    "draw_cross": False,
+}
+
+plot_stat_map(image, title="image without threshold", **plot_param)
 
 # %%
 # Image thresholded at 2 when two_sided=True
 # ------------------------------------------
 #
-# Now we will use ``threshold=2`` together with ``two_sided=True`` to
-# threshold the image. When ``two_sided=True``, we can only use positive
-# values for ``threshold``.
+# Now we will use ``threshold=2`` together with ``two_sided=True``
+# to threshold the image.
+# When ``two_sided=True``,
+# we can only use positive values for ``threshold``.
 #
 # This will set all image values between -2 and 2 to 0.
+#
+# .. note::
+#
+#   You can get a similar result by passing threshold
+#   directly when plotting.
+#   In this case the colorbar will adapt to show
+#   which part of the image was masked.
+#
 
 import matplotlib.pyplot as plt
 
@@ -52,43 +60,44 @@ thresholded_img = threshold_img(
     cluster_threshold=0,
     two_sided=True,
     copy=True,
+    copy_header=True,
 )
 
+figure_width = 8
 
 fig, axes = plt.subplots(
-    2,
+    3,
     1,
-    figsize=(8, 8),
+    figsize=(figure_width, 13),
 )
 
-plotting.plot_stat_map(
-    image,
-    colorbar=True,
-    title="image without threshold",
-    axes=axes[0],
-    cut_coords=cut_coords,
+plot_stat_map(
+    image, title="image without threshold", axes=axes[0], **plot_param
 )
 
-plotting.plot_stat_map(
+plot_stat_map(
     thresholded_img,
-    colorbar=True,
-    title="image thresholded at 2 with two_sided=True",
-    cut_coords=cut_coords,
+    title="image thresholded before plotting at 2 with two_sided=True",
     axes=axes[1],
+    **plot_param,
+)
+
+plot_stat_map(
+    image,
+    title="image thresholded during plotting",
+    threshold=2,
+    axes=axes[2],
+    **plot_param,
 )
 
 # %%
 # Image thresholded at 2 when two_sided=False
 # -------------------------------------------
 #
-# Now we will use ``threshold=2`` together with ``two_sided=False`` to
-# see the effect.
+# Now we will use ``threshold=2`` together with ``two_sided=False``
+# to see the effect.
 #
 # This will set all image values below 2 to 0.
-
-import matplotlib.pyplot as plt
-
-from nilearn.image import threshold_img
 
 thresholded_img = threshold_img(
     image,
@@ -96,31 +105,27 @@ thresholded_img = threshold_img(
     cluster_threshold=0,
     two_sided=False,
     copy=True,
+    copy_header=True,
 )
 
 
 fig, axes = plt.subplots(
     2,
     1,
-    figsize=(8, 8),
+    figsize=(figure_width, 8),
 )
 
 
-plotting.plot_stat_map(
-    image,
-    colorbar=True,
-    title="image without threshold",
-    axes=axes[0],
-    cut_coords=cut_coords,
+plot_stat_map(
+    image, title="image without threshold", axes=axes[0], **plot_param
 )
 
-plotting.plot_stat_map(
+plot_stat_map(
     thresholded_img,
-    colorbar=True,
     cmap="Reds",
     title="image thresholded at 2 with two_sided=False",
-    cut_coords=cut_coords,
     axes=axes[1],
+    **plot_param,
 )
 
 # %%
@@ -132,39 +137,30 @@ plotting.plot_stat_map(
 #
 # This will set all image values above -2 to 0.
 
-import matplotlib.pyplot as plt
-
 thresholded_img = threshold_img(
     image,
     threshold=-2,
     cluster_threshold=0,
     two_sided=False,
     copy=True,
+    copy_header=True,
 )
 
 
 fig, axes = plt.subplots(
     2,
     1,
-    figsize=(8, 8),
+    figsize=(figure_width, 8),
 )
 
-plotting.plot_stat_map(
-    image,
-    colorbar=True,
-    title="image without threshold",
-    axes=axes[0],
-    cut_coords=cut_coords,
+plot_stat_map(
+    image, title="image without threshold", axes=axes[0], **plot_param
 )
 
-plotting.plot_stat_map(
+plot_stat_map(
     thresholded_img,
-    colorbar=True,
     cmap="Blues_r",
     title="image thresholded at -2 with two_sided=False",
-    cut_coords=cut_coords,
     axes=axes[1],
+    **plot_param,
 )
-# -
-
-# %%
