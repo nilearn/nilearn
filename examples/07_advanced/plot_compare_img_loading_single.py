7ce7832b397d544921b440177514baa50cdeb4e1
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-02-17 21:08:03 +0100

    add call events

--- examples/07_advanced/plot_compare_img_loading_single.py
+++ examples/07_advanced/plot_compare_img_loading_single.py
@@ -4,6 +4,7 @@ Experiment to compare image loading methods
 """
 
 # %%
+import time
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -56,44 +57,68 @@ def get_mask_path(fmri_path):
 
 
 def nifti_masker_concat_img(img, mask_img):
+    t_load = time.time()
     img = concat_imgs(img)
     mask_img = concat_imgs(mask_img)
+    t0_mask = time.time()
     NiftiMasker(mask_img=mask_img).fit_transform(img)
+    tend_mask = time.time()
+    return t_load, t0_mask, tend_mask
 
 
 def nifti_masker_load_img(img, mask_img):
+    t_load = time.time()
     img = load_img(img)
-    img.uncache()
     mask_img = load_img(mask_img)
-    mask_img.uncache()
+    t0_mask = time.time()
     NiftiMasker(mask_img=mask_img).fit_transform(img)
+    tend_mask = time.time()
+    return t_load, t0_mask, tend_mask
 
 
 def nifti_masker_nibabel_load(img, mask_img):
+    t_load = time.time()
     img = load(img)
     mask_img = load(mask_img)
+    t0_mask = time.time()
     NiftiMasker(mask_img=mask_img).fit_transform(img)
+    tend_mask = time.time()
+    return t_load, t0_mask, tend_mask
 
 
 def numpy_masker_concat_img(fmri_path, mask_path):
-    return np.asarray(concat_imgs(fmri_path).dataobj)[
-        np.squeeze(np.asarray(concat_imgs(mask_path).dataobj).astype(bool))
-    ]
+    t_load = time.time()
+    img = np.asarray(concat_imgs(fmri_path).dataobj)
+    mask_img = np.squeeze(
+        np.asarray(concat_imgs(mask_path).dataobj).astype(bool)
+    )
+    t0_mask = time.time()
+    np.asarray(img)[mask_img]
+    tend_mask = time.time()
+    return t_load, t0_mask, tend_mask
 
 
 def numpy_masker_load_img(fmri_path, mask_path):
-    return np.asarray(load_img(fmri_path).dataobj)[
-        np.asarray(load_img(mask_path).dataobj).astype(bool)
-    ]
+    t_load = time.time()
+    img = np.asarray(load_img(fmri_path).dataobj)
+    mask_img = np.squeeze(np.asarray(load_img(mask_path).dataobj).astype(bool))
+    t0_mask = time.time()
+    np.asarray(img)[mask_img]
+    tend_mask = time.time()
+    return t_load, t0_mask, tend_mask
 
 
 def numpy_masker_nibabel_load(fmri_path, mask_path):
-    return np.asarray(load(fmri_path).dataobj)[
-        np.asarray(load(mask_path).dataobj).astype(bool)
-    ]
+    t_load = time.time()
+    img = np.asarray(load(fmri_path).dataobj)
+    mask_img = np.squeeze(np.asarray(load(mask_path).dataobj).astype(bool))
+    t0_mask = time.time()
+    np.asarray(img)[mask_img]
+    tend_mask = time.time()
+    return t_load, t0_mask, tend_mask
 
 
-def plot_memory_usage(fig, ax, usages):
+def plot_memory_usage(fig, ax, usages, call_times):
     for usage in usages:
         if not usages[usage]:
             continue
@@ -124,6 +149,22 @@ def plot_memory_usage(fig, ax, usages):
             color=line_color,
         )
 
+        # plot call times
+        for call_idx, call_time in enumerate(call_times[usage]):
+            ax.axvline(call_time - zero_time, color=line_color, linestyle="--")
+            if call_idx == 0:
+                annotation = "load"
+            if call_idx == 1:
+                annotation = "start masking"
+            if call_idx == 2:
+                annotation = "end masking"
+            ax.annotate(
+                annotation,
+                xy=(call_time - zero_time, 0),
+                xytext=(call_time - zero_time, peak_mem + (peak_mem * 0.1)),
+                color=line_color,
+            )
+
     # increase the y-axis limit by 20% to make the plot more readable
     ax.set_ylim(ax.get_ylim()[0], ax.get_ylim()[1] * 1.2)
 
@@ -137,6 +178,7 @@ if __name__ == "__main__":
     mask_path = get_mask_path(img_path)
 
     usages = {}
+    call_times = {}
 
     funcs = [
         nifti_masker_concat_img,
@@ -147,22 +189,45 @@ if __name__ == "__main__":
         numpy_masker_nibabel_load,
     ]
 
-    for func in funcs:
+    # run all the nifti_masker functions first
+    for func in funcs[:3]:
         print(f"Running {func.__name__}")
-        mem_usage = memory_usage(
-            (func, (img_path, mask_path)), timestamps=True
+        mem_usage, call_time = memory_usage(
+            (func, (img_path, mask_path)), timestamps=True, retval=True
         )
         usages[func.__name__] = mem_usage
+        call_times[func.__name__] = call_time
 
     plot_path = Path.cwd() / "results" / "plot_compare_img_loading_single"
     plot_path.mkdir(parents=True, exist_ok=True)
     fig, ax = plt.subplots()
-    fig, ax = plot_memory_usage(fig, ax, usages)
+    fig, ax = plot_memory_usage(fig, ax, usages, call_times)
     ax.set_xlabel("Time (s)")
     ax.set_ylabel("Memory (MiB)")
     ax.set_title(f"Memory usage over time with N_SUBJECTS={N_SUBJECTS}")
     ax.legend()
     plt.savefig(
-        plot_path / f"memory_usage_n{N_SUBJECTS}.png", bbox_inches="tight"
+        plot_path / f"nifti_memory_usage_n{N_SUBJECTS}.png",
+        bbox_inches="tight",
+    )
+    plt.show()
+
+    # run all the numpy masker functions
+    for func in funcs[3:]:
+        print(f"Running {func.__name__}")
+        mem_usage, call_time = memory_usage(
+            (func, (img_path, mask_path)), timestamps=True, retval=True
+        )
+        usages[func.__name__] = mem_usage
+        call_times[func.__name__] = call_time
+    fig, ax = plt.subplots()
+    fig, ax = plot_memory_usage(fig, ax, usages, call_times)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Memory (MiB)")
+    ax.set_title(f"Memory usage over time with N_SUBJECTS={N_SUBJECTS}")
+    ax.legend()
+    plt.savefig(
+        plot_path / f"numpy_memory_usage_n{N_SUBJECTS}.png",
+        bbox_inches="tight",
     )
     plt.show()
