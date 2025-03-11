63a40f0ce345dd5f5514623e9dffa0bb2c5f311a
Hande Gözükan <hande@ginkgo.work>
2025-03-11 16:32:40 +0100

    move SurfaceFigure tests under plotly.displays.tests

--- nilearn/plotting/tests/test_surf_plotting.py
+++ nilearn/plotting/tests/test_surf_plotting.py
@@ -42,14 +42,6 @@ from nilearn.surface import (
 )
 from nilearn.surface._testing import assert_surface_mesh_equal
 
-try:
-    import IPython.display  # noqa:F401
-except ImportError:
-    IPYTHON_INSTALLED = False
-else:
-    IPYTHON_INSTALLED = True
-
-
 EXPECTED_CAMERAS_PLOTLY = [
     (
         "left",
@@ -446,96 +438,6 @@ def test_get_view_plot_surf_matplotlib(hemi, views):
         assert (
             _get_view_plot_surf(hemi, v) == EXPECTED_VIEW_MATPLOTLIB[hemi][v]
         )
-
-
-def test_surface_figure():
-    s = SurfaceFigure()
-    assert s.output_file is None
-    assert s.figure is None
-    with pytest.raises(NotImplementedError):
-        s.show()
-    with pytest.raises(ValueError, match="You must provide an output file"):
-        s._check_output_file()
-    s._check_output_file("foo.png")
-    assert s.output_file == "foo.png"
-    s = SurfaceFigure(output_file="bar.png")
-    assert s.output_file == "bar.png"
-
-
-@pytest.mark.skipif(is_plotly_installed(), reason="Plotly is installed.")
-def test_plotly_surface_figure_import_error():
-    """Test that an ImportError is raised when instantiating \
-       a PlotlySurfaceFigure without having Plotly installed.
-    """
-    with pytest.raises(ImportError, match="Plotly is required"):
-        PlotlySurfaceFigure()
-
-
-@pytest.mark.skipif(
-    not is_plotly_installed() or is_kaleido_installed(),
-    reason=("This test only runs if Plotly is installed, but not kaleido."),
-)
-def test_plotly_surface_figure_savefig_error():
-    """Test that an ImportError is raised when saving \
-       a PlotlySurfaceFigure without having kaleido installed.
-    """
-    with pytest.raises(ImportError, match="`kaleido` is required"):
-        PlotlySurfaceFigure().savefig()
-
-
-@pytest.mark.skipif(
-    not is_plotly_installed() or not is_kaleido_installed(),
-    reason=("Plotly and/or kaleido not installed; required for this test."),
-)
-def test_plotly_surface_figure():
-    ps = PlotlySurfaceFigure()
-    assert ps.output_file is None
-    assert ps.figure is None
-    ps.show()
-    with pytest.raises(ValueError, match="You must provide an output file"):
-        ps.savefig()
-    ps.savefig("foo.png")
-
-
-@pytest.mark.skipif(
-    not is_plotly_installed() or not IPYTHON_INSTALLED,
-    reason=("Plotly and/or Ipython is not installed; required for this test."),
-)
-@pytest.mark.parametrize("renderer", ["png", "jpeg", "svg"])
-def test_plotly_show(renderer):
-    import plotly.graph_objects as go
-
-    ps = PlotlySurfaceFigure(go.Figure())
-    assert ps.output_file is None
-    assert ps.figure is not None
-    with mock.patch("IPython.display.display") as mock_display:
-        ps.show(renderer=renderer)
-    assert len(mock_display.call_args.args) == 1
-    key = "svg+xml" if renderer == "svg" else renderer
-    assert f"image/{key}" in mock_display.call_args.args[0]
-
-
-@pytest.mark.skipif(
-    not is_plotly_installed() or not is_kaleido_installed(),
-    reason=("Plotly and/or kaleido not installed; required for this test."),
-)
-def test_plotly_savefig(tmp_path):
-    import plotly.graph_objects as go
-
-    ps = PlotlySurfaceFigure(go.Figure(), output_file=tmp_path / "foo.png")
-    assert ps.output_file == tmp_path / "foo.png"
-    assert ps.figure is not None
-    ps.savefig()
-    assert (tmp_path / "foo.png").exists()
-
-
-@pytest.mark.parametrize("input_obj", ["foo", Figure(), ["foo", "bar"]])
-def test_instantiation_error_plotly_surface_figure(plotly, input_obj):
-    with pytest.raises(
-        TypeError,
-        match=("`PlotlySurfaceFigure` accepts only plotly figure objects."),
-    ):
-        PlotlySurfaceFigure(input_obj)
 
 
 def test_value_error_get_faces_on_edge(plotly, in_memory_mesh):
