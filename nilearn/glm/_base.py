87ee4220218cb69af9f71193de840a9beb96f02c
Remi Gau <remi_gau@hotmail.com>
2025-04-02 19:55:03 +0200

    [FIX] fix filenames when saving GLM to BIDS

--- nilearn/glm/_base.py
+++ nilearn/glm/_base.py
@@ -523,16 +523,6 @@ def _generate_statistical_maps(
     if not isinstance(contrast_types, dict):
         contrast_types = {}
 
-    fields = {
-        "prefix": prefix,
-        "suffix": "statmap",
-        "extension": "nii.gz",
-        "entities": deepcopy(entities),
-    }
-
-    if generate_bids_name:
-        fields["prefix"] = None
-
     statistical_maps: dict[str, dict[str, str]] = {}
 
     for contrast_name in contrasts:
@@ -545,6 +535,16 @@ def _generate_statistical_maps(
             stat_type = "F"
         # Override automatic detection with explicit type if provided
         stat_type = contrast_types.get(contrast_name, stat_type)
+
+        fields = {
+            "prefix": prefix,
+            "suffix": "statmap",
+            "extension": "nii.gz",
+            "entities": deepcopy(entities),
+        }
+
+        if generate_bids_name:
+            fields["prefix"] = None
 
         fields["entities"]["contrast"] = _clean_contrast_name(contrast_name)
 
