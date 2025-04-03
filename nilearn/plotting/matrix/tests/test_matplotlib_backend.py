66f05197ff92394fca0a6c7fb060dc47023c7739
Hande Gözükan <hande@ginkgo.work>
2025-04-03 10:44:05 +0200

    move test code in test_backend to test_matplotlib_backend and remove _backend and test_backend files

--- nilearn/plotting/matrix/tests/test_matplotlib_backend.py
+++ nilearn/plotting/matrix/tests/test_matplotlib_backend.py
@@ -1,9 +1,21 @@
+import re
+
 import matplotlib.pyplot as plt
+import numpy as np
 import pytest
 
 from nilearn.plotting.matrix._matplotlib_backend import (
     _sanitize_figure_and_axes,
+    _sanitize_labels,
+    _sanitize_reorder,
+    _sanitize_tri,
 )
+from nilearn.plotting.matrix.tests.test_matrix_plotting import VALID_TRI_VALUES
+
+VALID_REORDER_VALUES = (True, False, "single", "complete", "average")
+
+##############################################################################
+# Some smoke testing for graphics-related code
 
 
 @pytest.mark.parametrize(
@@ -31,3 +43,48 @@ def test_sanitize_figure_and_axes(fig, axes, expected):
     assert isinstance(fig2, plt.Figure)
     assert isinstance(axes2, plt.Axes)
     assert own_fig == expected
+
+
+def test_sanitize_labels():
+    labs = ["foo", "bar"]
+    with pytest.raises(
+        ValueError,
+        match=re.escape(
+            "Length of labels (2) unequal to length of matrix (6)."
+        ),
+    ):
+        _sanitize_labels((6, 6), labs)
+    for lab in [labs, np.array(labs)]:
+        assert _sanitize_labels((2, 2), lab) == labs
+
+
+@pytest.mark.parametrize("tri", VALID_TRI_VALUES)
+def test_sanitize_tri(tri):
+    _sanitize_tri(tri)
+
+
+@pytest.mark.parametrize("tri", [None, "foo", 2])
+def test_sanitize_tri_error(tri):
+    with pytest.raises(
+        ValueError,
+        match=(
+            f"Parameter tri needs to be one of: {', '.join(VALID_TRI_VALUES)}"
+        ),
+    ):
+        _sanitize_tri(tri)
+
+
+@pytest.mark.parametrize("reorder", VALID_REORDER_VALUES)
+def test_sanitize_reorder(reorder):
+    if reorder is not True:
+        assert _sanitize_reorder(reorder) == reorder
+    else:
+        assert _sanitize_reorder(reorder) == "average"
+
+
+@pytest.mark.parametrize("reorder", [None, "foo", 2])
+def test_sanitize_reorder_error(reorder):
+    with pytest.raises(
+        ValueError, match=("Parameter reorder needs to be one of")
+    ):
+        _sanitize_reorder(reorder)
