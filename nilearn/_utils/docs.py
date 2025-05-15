55beacb9dadd2d6270befa0c64c4445411deca67
Remi Gau <remi_gau@hotmail.com>
2025-05-15 18:49:32 +0200

    [DOC] use template doc string for dummy y (#5383)

--- nilearn/_utils/docs.py
+++ nilearn/_utils/docs.py
@@ -1229,6 +1229,13 @@ vmin : :obj:`float`  or obj:`int` or None, optional
     Passed to :func:`matplotlib.pyplot.imshow`.
 """
 
+# y
+docdict["y_dummy"] = """
+y : None
+    This parameter is unused.
+    It is solely included for scikit-learn compatibility.
+"""
+
 
 ##############################################################################
 #
