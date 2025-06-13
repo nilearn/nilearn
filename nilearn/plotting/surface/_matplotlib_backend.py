ee3396f80b1532b0087e555018ced27d1ebb1926
Remi Gau <remi_gau@hotmail.com>
2025-06-13 10:58:05 +0200

    revert

--- nilearn/plotting/surface/_matplotlib_backend.py
+++ nilearn/plotting/surface/_matplotlib_backend.py
@@ -574,13 +574,6 @@ def _plot_surf(
         if colorbar:
             cbar_vmin = cbar_vmin if cbar_vmin is not None else vmin
             cbar_vmax = cbar_vmax if cbar_vmax is not None else vmax
-
-            # in rare cases where plotting an image of zeroes
-            # this avoids a matplolib error
-            if cbar_vmax == cbar_vmin == 0:
-                cbar_vmax = 1
-                cbar_vmin = -1
-
             ticks = _get_ticks(
                 cbar_vmin, cbar_vmax, cbar_tick_format, threshold
             )
