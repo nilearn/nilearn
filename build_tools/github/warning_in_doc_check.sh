8c29bf85281f924fd7d64d1fa8c321da736d9318
Remi Gau <remi_gau@hotmail.com>
2025-06-18 16:00:33 +0200

    fix

--- build_tools/github/warning_in_doc_check.sh
+++ build_tools/github/warning_in_doc_check.sh
@@ -10,8 +10,8 @@ set -e
 set -x
 
 mkdir -p doc/tmp
-grep --include "*.html" -rn "doc/_build/html/auto_examples" -e "Warning: " > doc/tmp/all_warnings.txt
-grep --include "*.html" -rn "doc/_build/html/auto_examples" -e "DeprecationWarning: " > doc/tmp/deprecation_warnings.txt
-grep --include "*.html" -rn "doc/_build/html/auto_examples" -e "FutureWarning: " > doc/tmp/future_warnings.txt
-grep --include "*.html" -rn "doc/_build/html/auto_examples" -e "UserWarning: " > doc/tmp/user_warnings.txt
-grep --include "*.html" -rn "doc/_build/html/auto_examples" -e "RuntimeWarning: " > doc/tmp/runtime_warnings.txt
+grep --include "*.html" -rn "doc/_build/html/auto_examples" -e "Warning: " > doc/tmp/all_warnings.txt || true
+grep --include "*.html" -rn "doc/_build/html/auto_examples" -e "DeprecationWarning: " > doc/tmp/deprecation_warnings.txt || true
+grep --include "*.html" -rn "doc/_build/html/auto_examples" -e "FutureWarning: " > doc/tmp/future_warnings.txt || true
+grep --include "*.html" -rn "doc/_build/html/auto_examples" -e "UserWarning: " > doc/tmp/user_warnings.txt || true
+grep --include "*.html" -rn "doc/_build/html/auto_examples" -e "RuntimeWarning: " > doc/tmp/runtime_warnings.txt || true
