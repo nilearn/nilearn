0c02d7f927ef9ad4f9a712b21def46cf050f7d0d
Hande Gözükan <hande@ginkgo.work>
2025-06-20 10:47:41 +0200

    remove symmetric_cmap from plot_surf_stat_map signature and get from kwargs

--- nilearn/plotting/surface/surf_plotting.py
+++ nilearn/plotting/surface/surf_plotting.py
@@ -411,7 +411,6 @@ def plot_surf_stat_map(
     view=None,
     engine=DEFAULT_ENGINE,
     cmap=DEFAULT_DIVERGING_CMAP,
-    symmetric_cmap=None,
     colorbar=True,
     avg_method=None,
     threshold=None,
@@ -484,18 +483,6 @@ def plot_surf_stat_map(
 
     %(cmap)s
         default="RdBu_r"
-
-    symmetric_cmap : :obj:`bool`, default=None
-        Whether to use a symmetric colormap or not.
-
-        .. note::
-            This option is currently only implemented for the ``plotly``
-            engine.
-
-        When using ``plotly`` as engine, ``symmetric_cmap`` will default to
-        `False` if `None` is passed.
-
-        .. versionadded:: 0.12.0
 
     %(colorbar)s
 
@@ -608,6 +595,8 @@ def plot_surf_stat_map(
             symmetric_cbar=symmetric_cbar,
         )
     )
+
+    symmetric_cmap = kwargs.pop("symmetric_cmap", None)
 
     fig = plot_surf(
         surf_mesh,
