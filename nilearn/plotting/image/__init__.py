03b8c38f7b420e827972e69252bd073b535385bc
Hande Gözükan <hande@ginkgo.work>
2025-06-30 17:18:30 +0200

    move img_comparison under plotting.image

--- nilearn/plotting/image/__init__.py
+++ nilearn/plotting/image/__init__.py
@@ -1,3 +1,7 @@
+from nilearn.plotting.image.img_comparison import (
+    plot_bland_altman,
+    plot_img_comparison,
+)
 from nilearn.plotting.image.img_plotting import (
     plot_anat,
     plot_carpet,
@@ -14,11 +18,13 @@ from nilearn.plotting.image.img_plotting import (
 
 __all__ = [
     "plot_anat",
+    "plot_bland_altman",
     "plot_carpet",
     "plot_connectome",
     "plot_epi",
     "plot_glass_brain",
     "plot_img",
+    "plot_img_comparison",
     "plot_markers",
     "plot_prob_atlas",
     "plot_roi",
