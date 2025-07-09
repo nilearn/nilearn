715d6fbc344dbb72ba3c1c3270d7f0d618cd106f
Remi Gau <remi_gau@hotmail.com>
2025-07-09 16:54:14 +0200

    [ENH] enforce dtype for all parts of SurfaceImage and Polydata

--- nilearn/surface/surface.py
+++ nilearn/surface/surface.py
@@ -1352,6 +1352,10 @@ class PolyData:
             the typical shape of the
             data for a hemisphere is ``(n_vertices, n_time_points)``.
 
+    dtype : DTypeLike object, default=None
+        dtype to enforce on the data.
+        If ``None`` the original dtype if used.
+
     Examples
     --------
     >>> import numpy as np
@@ -1372,7 +1376,7 @@ class PolyData:
     ValueError: Cannot create an empty PolyData. ...
     """
 
-    def __init__(self, left=None, right=None):
+    def __init__(self, left=None, right=None, dtype=None):
         if left is None and right is None:
             raise ValueError(
                 "Cannot create an empty PolyData. "
@@ -1386,6 +1390,7 @@ class PolyData:
                     param = load_surf_data(param)
                 parts[hemi] = param
         self.parts = parts
+        self._set_data_dtype(dtype)
 
         self._check_parts()
 
@@ -1404,6 +1409,13 @@ class PolyData:
                 f"Data arrays for keys 'left' and 'right' "
                 "have incompatible shapes: "
                 f"{parts['left'].shape} and {parts['right'].shape}"
+            )
+
+        if parts["left"].dtype != parts["right"].dtype:
+            raise TypeError(
+                "All parts should have same dtype. "
+                f"Got {parts['left'].dtype=} and {parts['right'].dtype=}. "
+                "You can fix this by passing a 'dtype' at instantiation."
             )
 
     @property
@@ -1491,6 +1503,11 @@ class PolyData:
             data = self.parts["right"]
 
         _data_to_gifti(data, filename)
+
+    def _set_data_dtype(self, dtype):
+        if dtype is not None:
+            for h, v in self.parts.items():
+                self.parts[h] = v.astype(dtype)
 
 
 def at_least_2d(input):
@@ -1869,12 +1886,9 @@ class SurfaceImage:
            :obj:`pathlib.Path`
            Data for the both hemispheres.
 
-    squeeze_on_save : :obj:`bool` or None, default=None
-            If ``True`` axes of length one from the data
-            will be removed before saving them to file.
-            If ``None`` is passed,
-            then the value will be set to ``True``
-            if any of the data parts is one dimensional.
+    dtype : DTypeLike object, default=None
+        dtype to enforce on the data.
+        If ``None`` the original dtype if used.
 
     Attributes
     ----------
@@ -1882,7 +1896,7 @@ class SurfaceImage:
         shape of the surface data array
     """
 
-    def __init__(self, mesh, data):
+    def __init__(self, mesh, data, dtype=None):
         """Create a SurfaceImage instance."""
         self.mesh = mesh if isinstance(mesh, PolyMesh) else PolyMesh(**mesh)
 
@@ -1893,8 +1907,9 @@ class SurfaceImage:
 
         if isinstance(data, PolyData):
             self.data = data
+            self.data._set_data_dtype(dtype)
         elif isinstance(data, dict):
-            self.data = PolyData(**data)
+            self.data = PolyData(**data, dtype=dtype)
 
         _check_data_and_mesh_compat(self.mesh, self.data)
 
