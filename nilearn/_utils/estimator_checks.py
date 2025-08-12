4abe27ac2f4f46ee647caec7fc3ed3f4b637d11e
Remi Gau <remi_gau@hotmail.com>
2025-08-12 15:32:47 +0200

    comment

--- nilearn/_utils/estimator_checks.py
+++ nilearn/_utils/estimator_checks.py
@@ -990,6 +990,8 @@ def check_img_estimator_fit_idempotent(estimator_orig):
                 and sys.version_info[1] < 11
                 and method == "decision_function"
             ):
+                # On some version of python on mac this flat out fails
+                # for FREMClassifier
                 continue
 
             # TODO
