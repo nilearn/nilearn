132516d91b8fdb1bbdc6b30d88dad86af52c9f42
Remi Gau <remi_gau@hotmail.com>
2025-08-13 10:51:58 +0200

    skip

--- nilearn/_utils/estimator_checks.py
+++ nilearn/_utils/estimator_checks.py
@@ -986,11 +986,11 @@ def check_img_estimator_fit_idempotent(estimator_orig):
 
             if (
                 isinstance(estimator, FREMClassifier)
-                and sys.platform == "darwin"
                 and method == "decision_function"
             ):
-                # On some version of python on mac this flat out fails
-                # for FREMClassifier
+                # TODO
+                # Fails for FREMClassifier
+                # mostly on Mac and sometimes linux
                 continue
 
             # TODO
