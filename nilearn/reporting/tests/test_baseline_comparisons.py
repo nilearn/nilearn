d9e8327a17be0fa7c2b485a1d48e461c3e337a94
Remi Gau <remi_gau@hotmail.com>
2025-08-15 17:42:42 +0200

    fix

--- nilearn/reporting/tests/test_baseline_comparisons.py
+++ nilearn/reporting/tests/test_baseline_comparisons.py
@@ -12,6 +12,7 @@ import pandas as pd
 import pytest
 
 from nilearn.datasets import (
+    load_fsaverage_data,
     load_mni152_template,
     load_sample_motor_activation_image,
 )
@@ -21,9 +22,15 @@ from nilearn.reporting.glm_reporter import _stat_map_to_png
 
 @pytest.mark.mpl_image_compare
 @pytest.mark.parametrize("plot_type", ["slice", "glass"])
-@pytest.mark.parametrize("height_control", [None, "fpr"])
 @pytest.mark.parametrize(
-    "two_sided, threshold", [(False, 3), (False, -3), (True, 3)]
+    "height_control, two_sided, threshold",
+    [
+        (None, False, 3),
+        (None, False, -3),
+        (None, True, 3),
+        ("fpr", True, 3),
+        ("fpr", False, 3),
+    ],
 )
 @pytest.mark.parametrize("cluster_threshold", [0, 200])
 def test_stat_map_to_png_volume(
@@ -59,6 +66,56 @@ def test_stat_map_to_png_volume(
         cut_coords=None,
         display_mode="ortho",
         plot_type=plot_type,
+        table_details=table_details,
+        two_sided=two_sided,
+    )
+
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+@pytest.mark.parametrize(
+    "height_control, two_sided, threshold",
+    [
+        (None, False, 0.5),
+        (None, False, -0.5),
+        (None, True, 0.5),
+        ("bonferroni", True, 3),
+        ("bonferroni", False, 3),
+    ],
+)
+def test_stat_map_to_png_surface(height_control, two_sided, threshold):
+    """Check figures plotting for GLM report for surface data."""
+    alpha = 0.05
+    cluster_threshold = 0
+
+    surf_img = load_fsaverage_data()
+
+    thresholded_img, threshold = threshold_stats_img(
+        stat_img=surf_img,
+        threshold=threshold,
+        alpha=alpha,
+        cluster_threshold=cluster_threshold,
+        height_control=height_control,
+        two_sided=two_sided,
+    )
+
+    table_details = OrderedDict()
+    table_details.update({"Threshold Z": np.around(threshold, 3)})
+    table_details.update({"two_sided": two_sided})
+    table_details.update({"height_control": height_control})
+    table_details = pd.DataFrame.from_dict(
+        table_details,
+        orient="index",
+    )
+
+    _, fig = _stat_map_to_png(
+        stat_img=thresholded_img,
+        threshold=threshold,
+        bg_img=surf_img,
+        cut_coords=None,
+        display_mode="ortho",
+        plot_type="slice",
         table_details=table_details,
         two_sided=two_sided,
     )
