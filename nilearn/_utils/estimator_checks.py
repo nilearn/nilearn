fde867c53419d1b6362986f19ae6478ea38ea76b
Remi Gau <remi_gau@hotmail.com>
2025-08-22 07:37:41 +0200

    fix

--- nilearn/_utils/estimator_checks.py
+++ nilearn/_utils/estimator_checks.py
@@ -447,7 +447,7 @@ def expected_failed_checks_decoders(estimator) -> dict[str, str]:
         ),
         "check_fit_idempotent": (
             "replaced by check_img_estimator_fit_idempotent"
-        )
+        ),
         "check_fit_score_takes_y": (
             "replaced by check_img_estimator_fit_score_takes_y"
         ),
