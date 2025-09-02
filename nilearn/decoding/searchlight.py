66e8751bddd6bd58fea396331f5e730cb0e2d422
Remi Gau <remi_gau@hotmail.com>
2025-09-02 16:51:04 +0200

    fix

--- nilearn/decoding/searchlight.py
+++ nilearn/decoding/searchlight.py
@@ -273,8 +273,13 @@ class SearchLight(TransformerMixin, BaseEstimator):
 
     %(verbose0)s
 
+    %(estimator_args)s
+
     Attributes
     ----------
+    estimator_args_ : dict[str, Any] or None, default=None
+        Extra parameters to pass to the sklearn learn estimators.
+
     mask_img_ : Nifti1Image or :obj:`~nilearn.surface.SurfaceImage`
         Mask computed by the masker object.
 
@@ -330,6 +335,7 @@ class SearchLight(TransformerMixin, BaseEstimator):
         scoring=None,
         cv=None,
         verbose=0,
+        estimator_args=None,
     ):
         self.mask_img = mask_img
         self.process_mask_img = process_mask_img
@@ -339,6 +345,7 @@ class SearchLight(TransformerMixin, BaseEstimator):
         self.scoring = scoring
         self.cv = cv
         self.verbose = verbose
+        estimator_args = estimator_args
 
     def _more_tags(self):
         """Return estimator tags.
@@ -451,11 +458,17 @@ class SearchLight(TransformerMixin, BaseEstimator):
             mask_img=self.mask_img_,
         )
 
+        self.estimator_args_ = (
+            {} if self.estimator_args is None else self.estimator_args
+        )
+
         estimator = self.estimator
         if estimator == "svc":
-            estimator = ESTIMATOR_CATALOG[estimator](dual=True)
+            estimator = ESTIMATOR_CATALOG[estimator](
+                dual=True, **self.estimator_args_
+            )
         elif isinstance(estimator, str):
-            estimator = ESTIMATOR_CATALOG[estimator]()
+            estimator = ESTIMATOR_CATALOG[estimator](**self.estimator_args_)
 
         scores = search_light(
             X,
@@ -515,7 +528,11 @@ class SearchLight(TransformerMixin, BaseEstimator):
 
         estimator = self.estimator
         if estimator == "svc":
-            estimator = ESTIMATOR_CATALOG[estimator](dual=True)
+            estimator = ESTIMATOR_CATALOG[estimator](
+                dual=True, **self.estimator_args_
+            )
+        elif isinstance(estimator, str):
+            estimator = ESTIMATOR_CATALOG[estimator](**self.estimator_args_)
 
         # Use the modified `_group_iter_search_light` logic to avoid `y` issues
         result = search_light(
