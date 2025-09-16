adf7b6a795e44acdbef3c1c5c333c03e77586762
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-09-16 15:06:04 +0200

    fix?

--- nilearn/image/tests/test_image.py
+++ nilearn/image/tests/test_image.py
@@ -938,7 +938,7 @@ def test_validity_threshold_value_in_threshold_img(
         threshold_img(maps, threshold=None)
 
     threshold = object()
-    with pytest.raises(TypeError, match="must be of type(s)"):
+    with pytest.raises(TypeError, match="be of type"):
         threshold_img(
             maps,
             threshold=threshold,
