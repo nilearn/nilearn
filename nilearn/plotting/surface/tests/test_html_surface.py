50c37f5c14406d116534828bcaa38fa18127d874
Remi Gau <remi_gau@hotmail.com>
2025-09-18 14:28:10 +0200

    fix

--- nilearn/plotting/surface/tests/test_html_surface.py
+++ nilearn/plotting/surface/tests/test_html_surface.py
@@ -14,7 +14,6 @@ from nilearn.plotting.surface.html_surface import (
     _fill_html_template,
     _full_brain_info,
     _one_mesh_info,
-    full_brain_info,
     view_img_on_surf,
     view_surf,
 )
@@ -95,15 +94,6 @@ def test_full_brain_info(mni152_template_res_2):
         assert len(decode(info[f"pial_{hemi}"]["_j"], "<i4")) == len(
             mesh.faces
         )
-
-    # TODO (nilearn >= 0.13.0)
-    with pytest.warns(
-        DeprecationWarning,
-        match="full_brain_info is a private function and is renamed to "
-        "_full_brain_info. Using the deprecated name will raise an error "
-        "in release 0.13",
-    ):
-        full_brain_info(mni152_template_res_2)
 
 
 def test_fill_html_template(tmp_path, mni152_template_res_2):
