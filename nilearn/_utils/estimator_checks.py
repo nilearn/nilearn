bc18d5e7243d8ff8abe51147e0dd7217e305a3c6
Remi Gau <remi_gau@hotmail.com>
2025-09-25 20:51:30 +0200

    timeout

--- nilearn/_utils/estimator_checks.py
+++ nilearn/_utils/estimator_checks.py
@@ -1184,6 +1184,8 @@ def check_img_estimator_fit_idempotent(estimator_orig):
     the same results.
 
     replaces sklearn check_fit_idempotent
+
+    TODO: speed up this test for decoders
     """
     check_methods = ["predict", "transform", "decision_function"]
 
