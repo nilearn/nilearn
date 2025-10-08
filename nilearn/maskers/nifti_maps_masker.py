7694b6e01c8bde8c5b6c16928fd890aaac38ca9b
Remi Gau <remi_gau@hotmail.com>
2025-10-08 13:15:54 +0200

    reduce warning maps masker

--- nilearn/maskers/nifti_maps_masker.py
+++ nilearn/maskers/nifti_maps_masker.py
@@ -478,7 +478,7 @@ class NiftiMapsMasker(ClassNamePrefixFeaturesOutMixin, BaseMasker):
 
                 self.maps_img_ = self._cache(resample_img)(
                     self.maps_img_,
-                    interpolation="linear",
+                    interpolation="nearest",
                     target_shape=ref_img.shape[:3],
                     target_affine=ref_img.affine,
                 )
@@ -618,7 +618,7 @@ class NiftiMapsMasker(ClassNamePrefixFeaturesOutMixin, BaseMasker):
                 )
                 maps_img_ = self._cache(resample_img)(
                     self.maps_img_,
-                    interpolation="linear",
+                    interpolation="nearest",
                     target_shape=ref_img.shape[:3],
                     target_affine=ref_img.affine,
                 )
