305284a90e498ac76454ed6080476e44c944fdb1
Hande Gözükan <hande@ginkgo.work>
2025-10-28 16:39:40 +0100

    add ReportingMixin

---
+++ nilearn/reporting/_mixin.py
@@ -0,0 +1,41 @@
+"""Module that provides mixin class to add reporting functionality to
+estimators.
+"""
+
+import abc
+
+from nilearn.reporting.html_report import generate_report
+
+
+class ReportingMixin:
+    """A mixin class to be used with classes that require reporting
+    functionality.
+    """
+
+    def __init__(self):
+        self._report_content = {
+            "description": None,
+            "summary": None,
+            "warning_message": None,
+        }
+        self._reporting_data = None
+
+    def generate_report(self, title=None):
+        """Generate an HTML report for the current object.
+
+        Parameters
+        ----------
+        title : :obj:`str`, default=None
+            title for the report. If None, title will be the class name.
+
+        Returns
+        -------
+        report : `nilearn.reporting.html_report.HTMLReport`
+            HTML report for the masker.
+        """
+        self.update_report_content("title", title)
+        return generate_report(self)
+
+    @abc.abstractmethod
+    def _reporting(self):
+        raise NotImplementedError()
