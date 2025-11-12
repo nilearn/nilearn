e80d5361a7b3e0dac3b794450ae415e347ba3d4c
Remi Gau <remi_gau@hotmail.com>
2025-11-12 10:06:44 +0100

    [FIX] remove estimator_type from input tags (#5805)

--- nilearn/maskers/_mixin.py
+++ nilearn/maskers/_mixin.py
@@ -17,7 +17,6 @@ from nilearn._utils.bids import (
 from nilearn._utils.docs import fill_doc
 from nilearn._utils.niimg_conversions import iter_check_niimg
 from nilearn._utils.numpy_conversions import csv_to_array
-from nilearn._utils.tags import SKLEARN_LT_1_6
 from nilearn.image import high_variance_confounds
 from nilearn.image.utils import get_indices_from_image
 from nilearn.surface.surface import SurfaceImage
@@ -26,24 +25,6 @@ from nilearn.typing import NiimgLike
 
 class _MultiMixin:
     """Mixin class to add common MultiMasker functionalities."""
-
-    def __sklearn_tags__(self):
-        """Return estimator tags.
-
-        See the sklearn documentation for more details on tags
-        https://scikit-learn.org/1.6/developers/develop.html#estimator-tags
-        """
-        # TODO (sklearn  >= 1.6.0) remove if block
-        if SKLEARN_LT_1_6:
-            from nilearn._utils.tags import tags
-
-            return tags(masker=True, multi_masker=True)
-
-        from nilearn._utils.tags import InputTags
-
-        tags = super().__sklearn_tags__()
-        tags.input_tags = InputTags(masker=True, multi_masker=True)
-        return tags
 
     @fill_doc
     def fit_transform(self, imgs, y=None, confounds=None, sample_mask=None):
