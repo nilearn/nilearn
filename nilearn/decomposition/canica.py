27a113ca3f4a57e0ac71252592680fa7e10cd62f
Remi Gau <remi_gau@hotmail.com>
2025-11-18 11:19:47 +0100

    reset

--- nilearn/decomposition/canica.py
+++ nilearn/decomposition/canica.py
@@ -140,7 +140,6 @@ class CanICA(_MultiPCA):
         memory_level=0,
         n_jobs=1,
         verbose=0,
-        estimator_args=None,
     ):
         super().__init__(
             n_components=n_components,
@@ -166,15 +165,11 @@ class CanICA(_MultiPCA):
 
         self.threshold = threshold
         self.n_init = n_init
-        self.estimator_args = estimator_args
 
     def _unmix_components(self, components):
         """Core function of CanICA than rotate components_ to maximize \
         independence.
         """
-        estimator_args = (
-            {} if self.estimator_args is None else self.estimator_args
-        )
         random_state = check_random_state(self.random_state)
 
         seeds = random_state.randint(np.iinfo(np.int32).max, size=self.n_init)
@@ -185,7 +180,6 @@ class CanICA(_MultiPCA):
                 whiten="arbitrary-variance",
                 fun="cube",
                 random_state=seed,
-                **estimator_args,
             )
             for seed in seeds
         )
