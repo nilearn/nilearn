c06d9911bbefec6599967f82ab47f81609383244
Remi Gau <remi_gau@hotmail.com>
2025-11-24 18:06:25 +0100

    Update nilearn/plotting/image/tests/test_plot_img.py

--- nilearn/plotting/image/tests/test_plot_img.py
+++ nilearn/plotting/image/tests/test_plot_img.py
@@ -150,16 +150,12 @@ def test_plot_img_transparency_binary_image(
     )
 
 
-def test_out_of_bounds_error(matplotlib_pyplot):
-    """Ensure plotting out-of-bounds slice raises
-    ValueError.
+def test_out_of_bounds_error(matplotlib_pyplot, img_3d_mni):
+    """Ensure plotting out-of-bounds slice raises ValueError.
+    
+    This should crash with ValueError
+    because the default 3D image fixture
+    has less than 50 slices on the x axis.
     """
-    # Create a small dummy image (10x10x10 voxels)
-    data = np.zeros((10, 10, 10))
-    img = nib.Nifti1Image(data, np.eye(4))
-
-    # This should crash with ValueError because 50 is > 10
-    # We use pytest.raises to "catch" the crash
-    #  and mark the test as PASS if it crashes correctly
     with pytest.raises(ValueError, match="out of bounds"):
-        plotting.plot_img(img, display_mode="x", cut_coords=[50])
+        plotting.plot_img(img_3d_mni, display_mode="x", cut_coords=[50])
