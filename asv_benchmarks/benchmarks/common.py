24972e5b2387ba60f35e6d6ce68d25d64267b573
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-12-08 13:41:05 +0100

    try 100 images

--- asv_benchmarks/benchmarks/common.py
+++ asv_benchmarks/benchmarks/common.py
@@ -13,7 +13,7 @@ class Benchmark:
 
     timeout = 2400  # 40 mins
 
-    def setup_cache(self, n_subjects=500, n_masks=1):
+    def setup_cache(self, n_subjects=100, n_masks=1):
         """Set up the cache directory with the necessary images and masks.
 
         The fMRI image is created by concatenating n_subjects subject images
