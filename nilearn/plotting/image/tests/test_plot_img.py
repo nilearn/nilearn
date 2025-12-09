6ad752563c5cd7ec3c2a97acb65b70343232c2e9
Hande Gözükan <hande@ginkgo.work>
2025-12-09 10:29:19 +0100

    review and rewrite tests

--- nilearn/plotting/image/tests/test_plot_img.py
+++ nilearn/plotting/image/tests/test_plot_img.py
@@ -148,59 +148,77 @@ def test_plot_img_transparency_binary_image(
     )
 
 
-@pytest.mark.parametrize("display_mode", "xyz")
-def test_out_of_bounds_warning_error(
-    matplotlib_pyplot, img_3d_rand_eye, display_mode
+@pytest.mark.parametrize(
+    "display_mode, cut_coords",
+    [
+        ("x", [7]),
+        ("x", [7, 8]),
+        ("y", [8]),
+        ("y", [8, 9]),
+        ("z", [9]),
+        ("z", [9, 10]),
+        ("xz", [7, 9]),
+        ("yz", [8, 9]),
+        ("yx", [7, 8]),
+        ("ortho", [7, 8, 9]),
+        ("tiled", [7, 8, 9]),
+    ],
+)
+def test_cut_coords_out_of_bounds_error(
+    matplotlib_pyplot, img_3d_rand_eye, display_mode, cut_coords
 ):
-    """Ensure plotting out-of-bounds slices raises warnings and errors."""
-    # Things are OK as long as at least one cut is in the image bounds
-    with pytest.warns(
-        UserWarning,
-        match=("At least one of the specified cut_coords"),
-    ):
-        plot_img(
-            img_3d_rand_eye, display_mode=display_mode, cut_coords=[1, 50]
-        )
-
-    # This should crash with ValueError
-    # because the default 3D image fixture
-    # has less than 50 slices on the x axis:  all cuts are out of bounds
-    with pytest.raises(ValueError, match="is out of the bounds of the image"):
-        plot_img(
-            img_3d_rand_eye, display_mode=display_mode, cut_coords=[50, 100]
-        )
-
-
-@pytest.mark.parametrize("display_mode", ["ortho", "tiled", "xz", "yz"])
-def test_out_of_bounds_warning_error_3d(
-    matplotlib_pyplot, img_3d_rand_eye, display_mode
-):
-    """Plot out-of-bounds slices in ortho and tiled mode raises warnings and
-    errors.
+    """Test if nilearn.plotting.image.plot_img raises error when all elements
+    of cut_coords are out of bounds of the image for corresponding coordinate.
     """
-    # Things are OK as long as at least one cut is in the image bounds
+    # img_3d_rand_eye has bounds:
+    # [(0.0, 6.0), (0.0, 7.0), (0.0, 8.0)]
+    with pytest.raises(ValueError, match="is out of the bounds of the image"):
+        plot_img(
+            img_3d_rand_eye, display_mode=display_mode, cut_coords=cut_coords
+        )
+
+
+@pytest.mark.parametrize(
+    "display_mode, cut_coords",
+    [
+        ("x", [7, 6]),
+        ("x", [6, 7, 8]),
+        ("y", [7, 8]),
+        ("y", [6, 8, 9]),
+        ("z", [8, 9]),
+        ("z", [9, 10, 8]),
+        ("xz", [6, 9]),
+        ("xz", [7, 8]),
+        ("yz", [7, 9]),
+        ("yz", [8, 7]),
+        ("yx", [6, 8]),
+        ("yx", [8, 7]),
+        ("ortho", [6, 8, 9]),
+        ("tiled", [8, 7, 9]),
+    ],
+)
+def test_cut_coords_out_of_bounds_warning(
+    matplotlib_pyplot, img_3d_rand_eye, display_mode, cut_coords
+):
+    """Test if nilearn.plotting.image.plot_img raises error when at least one
+    but not all elements of cut_coords is out of bounds of the image for
+    corresponding coordinate.
+    """
+    # img_3d_rand_eye has bounds:
+    # [(0.0, 6.0), (0.0, 7.0), (0.0, 8.0)]
     with pytest.warns(
         UserWarning,
         match=("At least one of the specified cut_coords"),
     ):
         plot_img(
-            img_3d_rand_eye, display_mode=display_mode, cut_coords=[1, 1, 100]
-        )
-
-    # This should crash with ValueError
-    # because the default 3D image fixture
-    # has less than 50 slices on the x axis:  all cuts are out of bounds
-    with pytest.raises(ValueError, match="is out of the bounds of the image"):
-        plot_img(
-            img_3d_rand_eye,
-            display_mode=display_mode,
-            cut_coords=[50, 100, 150],
+            img_3d_rand_eye, display_mode=display_mode, cut_coords=cut_coords
         )
 
 
-@pytest.mark.parametrize("display_mode", ["ortho", "tiled", "xz", "yz", "yx"])
+@pytest.mark.parametrize("display_mode", ["ortho", "tiled"])
+@pytest.mark.parametrize("cut_coords", [5, [3, 5]])
 def test_error_incompatible_cut_coords_3d(
-    matplotlib_pyplot, img_3d_rand_eye, display_mode
+    matplotlib_pyplot, img_3d_rand_eye, display_mode, cut_coords
 ):
     """Test error when incompatible cut_coords is specified for slicers of type
     `ortho` and `tiled`.
@@ -209,11 +227,23 @@ def test_error_incompatible_cut_coords_3d(
         ValueError,
         match=("cut_coords passed does not match the display mode"),
     ):
-        plot_img(img_3d_rand_eye, display_mode=display_mode, cut_coords=5)
-
-    with pytest.raises(ValueError, match="cut_coords passed does not match"):
         plot_img(
-            img_3d_rand_eye,
-            display_mode=display_mode,
-            cut_coords=[3, 5],
+            img_3d_rand_eye, display_mode=display_mode, cut_coords=cut_coords
+        )
+
+
+@pytest.mark.parametrize("display_mode", ["xz", "yz", "yx"])
+@pytest.mark.parametrize("cut_coords", [5, [3, 5, 7]])
+def test_error_incompatible_cut_coords_2d(
+    matplotlib_pyplot, img_3d_rand_eye, display_mode, cut_coords
+):
+    """Test error when incompatible cut_coords is specified for slicers of type
+    `ortho` and `tiled`.
+    """
+    with pytest.raises(
+        ValueError,
+        match=("cut_coords passed does not match the display mode"),
+    ):
+        plot_img(
+            img_3d_rand_eye, display_mode=display_mode, cut_coords=cut_coords
         )
