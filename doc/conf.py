ad976380ca9b03f650be029a7a21a633483ba7f4
Remi Gau <remi_gau@hotmail.com>
2025-12-28 10:08:03 +0100

    [REL] 0.13.0 (#5895)
    Co-authored-by: Hande Gözükan <2099645+hndgzkn@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- doc/conf.py
+++ doc/conf.py
@@ -213,6 +213,7 @@ linkcheck_ignore = [
     r"https://rrid.site/data/record.*",
     r"https://sites.wustl.edu/oasisbrains.*",
     r"https://www.cambridge.org/be/universitypress.*",
+    r"../../_static/notebook_reports_.*",
     "http://brainomics.cea.fr/localizer/",
     "https://childmind.org/science/global-open-science/healthy-brain-network/",
     "https://digicosme.cnrs.fr/en/digicosme-paris-saclay-english/",
@@ -222,6 +223,7 @@ linkcheck_ignore = [
     "https://pages.saclay.inria.fr/bertrand.thirion/",
     "https://pages.stern.nyu.edu/~wgreene/Text/econometricanalysis.htm",
     "https://surfer.nmr.mgh.harvard.edu/",
+    "https://www.gin.cnrs.fr/en/tools/aal",
 ]
 
 linkcheck_exclude_documents = [r".*/sg_execution_times.rst"]
