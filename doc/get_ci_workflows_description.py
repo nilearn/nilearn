235f5fc818dda7741ca6e7a1b8dd02ffdb653d43
Hande Gözükan <hande@ginkgo.work>
2026-01-20 11:31:00 +0100

    move plotly conf to conf.py [skip test]

--- doc/get_ci_workflows_description.py
+++ doc/get_ci_workflows_description.py
@@ -18,22 +18,6 @@ output_file_path = doc_dir / "ci.rst"
 
 SEPARATOR = "###"
 
-# -- Plotly Configuration ----------------------------------------------------
-
-try:
-    import plotly.io as pio
-
-    pio.renderers.default = "sphinx_gallery"
-    pio.kaleido.scope.default_scale = 2
-except ImportError:
-    import warnings
-
-    warnings.warn(
-        stacklevel=2,
-        message="Plotly is not installed. Plotly figures will not be shown.",
-        category=UserWarning,
-    )
-
 
 def main() -> None:
     """Get top comment of each github action workflows \
