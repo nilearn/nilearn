008f5d85f0a404b4c6d38f2d1d4fe707c5136a08
Hande Gözükan <hande@ginkgo.work>
2026-02-11 16:40:32 +0100

    add a function to check compatibility of kaleido and plotly versions

--- nilearn/_utils/helpers.py
+++ nilearn/_utils/helpers.py
@@ -2,6 +2,9 @@ import functools
 import os
 import sys
 import warnings
+from importlib.metadata import version as importlib_version
+
+from packaging import version
 
 from nilearn._utils.logger import find_stack_level
 from nilearn._utils.versions import (
@@ -278,6 +281,22 @@ def is_kaleido_installed():
     except ImportError:
         return False
     return True
+
+
+def is_kaleido_plotly_compatible():
+    """Check if installed versions of plotly and kaleido are compatible.
+
+    This function assumes that both plotly and kaleido are installed.
+    """
+    min_plotly = "6.1.1"
+    min_kaleido = "0.2.1"
+    plotly_version = importlib_version("plotly")
+    kaleido_version = importlib_version("kaleido")
+
+    return not (
+        version.parse(kaleido_version) > version.parse(min_kaleido)
+        and version.parse(plotly_version) < version.parse(min_plotly)
+    )
 
 
 def is_windows_platform():
