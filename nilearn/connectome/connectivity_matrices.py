c3609e00e8d2fa9c9e3dfabfcb01dd534f8805d4
Hao-Ting Wang <htwangtw@gmail.com>
2026-02-19 15:13:29 -0500

    Refactor the estimator in ConnectivityMeasure

--- nilearn/connectome/connectivity_matrices.py
+++ nilearn/connectome/connectivity_matrices.py
@@ -389,11 +389,22 @@ class ConnectivityMeasure(TransformerMixin, NilearnBaseEstimator):
         The covariance estimator.
         This implies that correlations are slightly shrunk
         towards zero compared to a maximum-likelihood estimate
+        (
+        When passing a customized estimator, the covariance estimator must
+        have a ``fit`` method that takes as input a 2D array of shape
+        (n_samples, n_features) and has an attribute ``covariance_`` of shape
+        (n_features, n_features) after fitting. If a custom covariance
+        estimator is used, the ``kind`` parameter will be ignored and the
+        output will be the covariance matrices estimated by the provided
+        covariance estimator.
 
     kind : {"covariance", "correlation", "partial correlation",\
             "tangent", "precision"}, default='covariance'
         The matrix kind.
         For the use of "tangent" see :footcite:t:`Varoquaux2010b`.
+        If a customized covariance estimator is used, this argument will
+        be ignored and the output will be the covariance matrices estimated by
+        the provided covariance estimator.
 
     vectorize : :obj:`bool`, default=False
         If True, connectivity matrices are reshaped into 1D arrays and only
@@ -519,6 +530,15 @@ class ConnectivityMeasure(TransformerMixin, NilearnBaseEstimator):
         """Avoid duplication of computation."""
         if self.cov_estimator is None:
             self.cov_estimator = LedoitWolf(store_precision=False)
+        elif self.cov_estimator is not LedoitWolf(store_precision=False):
+            warnings.warn(
+                "You are using a custom covariance estimator. The 'kind' "
+                "parameter will be ignored and the output will be the "
+                "covariance matrices estimated by the provided covariance "
+                "estimator.",
+                stacklevel=find_stack_level(),
+            )
+            self.kind = None
 
         if not hasattr(X, "__iter__"):
             raise TypeError(
@@ -560,10 +580,11 @@ class ConnectivityMeasure(TransformerMixin, NilearnBaseEstimator):
                 "tangent",
                 "covariance",
                 "precision",
+                None,
             )
             check_parameter_in_allowed(self.kind, allowed_kinds, "kind")
 
-            if self.kind in ("covariance", "tangent"):
+            if self.kind in ("covariance", "tangent", None):
                 connectivities = covariances
             elif self.kind == "precision":
                 connectivities = [linalg.inv(cov) for cov in covariances]
