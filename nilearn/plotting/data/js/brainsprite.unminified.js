2cb937106c28a0f2b277a75b95497566ed15bc86
Rohan <114685156+rohan3412@users.noreply.github.com>
2026-03-16 19:28:24 +0530

    Update brainsprite.unminified.js

--- nilearn/plotting/data/js/brainsprite.unminified.js
+++ nilearn/plotting/data/js/brainsprite.unminified.js
@@ -567,7 +567,16 @@ function brainsprite(params) {
     brain.draw(brain.numSlice.Z, 'Z')
   }
   
-  
+  brain.setSlice = function (newSlice) {
+    if (newSlice) {
+      if (newSlice.X !== undefined) brain.numSlice.X = Math.round(newSlice.X);
+      if (newSlice.Y !== undefined) brain.numSlice.Y = Math.round(newSlice.Y);
+      if (newSlice.Z !== undefined) brain.numSlice.Z = Math.round(newSlice.Z);
+    }
+    updateValue();
+    updateCoordinates();
+    brain.drawAll();
+  }
   
   brain.canvas.addEventListener('click', brain.clickBrain, false)
 
