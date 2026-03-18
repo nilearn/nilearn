9bce09d0bdcf0e95062fc1bedc557d30f8fe0bf7
Hande Gözükan <hande@ginkgo.work>
2026-03-18 11:42:50 +0100

    minor fix

--- nilearn/maskers/base_masker.py
+++ nilearn/maskers/base_masker.py
@@ -263,7 +263,7 @@ def sanitize_displayed_maps(
                 f"But masker only has {n_maps} {var_name}(s). "
                 f"'displayed_{var_name}s' was set to {n_maps}."
             )
-            estimator.append_report_warning(msg)
+            estimator._append_report_warning(msg)
 
             displayed_maps = n_maps
 
