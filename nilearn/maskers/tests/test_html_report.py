b6fa798837b732cfa3c187d86e63648ae81fdb03
Hande Gözükan <hande@ginkgo.work>
2026-04-02 09:56:03 +0200

    fix test when matplotlib not installed

--- nilearn/maskers/tests/test_html_report.py
+++ nilearn/maskers/tests/test_html_report.py
@@ -571,7 +571,7 @@ def test_surface_maps_masker_generate_report_engine_error(
 
 @pytest.mark.thread_unsafe
 @pytest.mark.skipif(
-    is_plotly_installed(),
+    not is_matplotlib_installed() or is_plotly_installed(),
     reason="Test requires plotly not to be installed.",
 )
 def test_surface_maps_masker_generate_report_engine_no_plotly_warning(
