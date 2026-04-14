170fa26973dd2e34b7bd09536237b249df26f014
Remi Gau <remi_gau@hotmail.com>
2026-04-14 11:21:19 +0200

    eport

--- tests/js/nifti_masker_report.test.js
+++ tests/js/nifti_masker_report.test.js
@@ -2,5 +2,7 @@ const template = require('./template.js')
 
 const file = 'nifti_masker_report.html'
 template.fullTest(file,
-  { x: 0, y: 0, width: 800, height: 340 }
+  // height was adapted to crop footer from png
+  // to avoid the change in nilearn version from appearing
+  { x: 0, y: 0, width: 800, height: 550 }
 )
