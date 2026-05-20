f313f8199c764a0c9989eda75ece6e3364437e50
Remi Gau <remi_gau@hotmail.com>
2026-05-20 18:27:31 +0200

    [STY] standardize javascript code (#6205)
    Co-authored-by: Elizabeth DuPre <15017191+emdupre@users.noreply.github.com>
    Co-authored-by: Michelle Wang <tomichellewang@gmail.com>
    Co-authored-by: Hande Gözükan <2099645+hndgzkn@users.noreply.github.com>
    Co-authored-by: Xichun Xu <99116226+XichunXu@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- nilearn/_assets/js/surface-plot-utils.js
+++ nilearn/_assets/js/surface-plot-utils.js
@@ -1,59 +1,63 @@
-function makePlot(surface, hemisphere, divId) {
-    decodeHemisphere(surfaceMapInfo, surface, hemisphere);
-    var info = surfaceMapInfo[surface + "_" + hemisphere];
-    info["type"] = "mesh3d";
-    info["vertexcolor"] = surfaceMapInfo["vertexcolor_" + hemisphere];
+/* global surfaceMapInfo, textColor, addColorbar, updateLayout, getLayout, decodeHemisphere, getLighting, getConfig */
 
-    let data = [info];
+function makePlot (surface, hemisphere, divId) {
+  decodeHemisphere(surfaceMapInfo, surface, hemisphere)
+  const info = surfaceMapInfo[surface + '_' + hemisphere]
+  info.type = 'mesh3d'
+  info.vertexcolor = surfaceMapInfo['vertexcolor_' + hemisphere]
 
-    info["lighting"] = getLighting();
-    let layout = getLayout(
-        "surface-plot",
-        "select-view",
-        surfaceMapInfo["black_bg"],
-    );
-    layout["title"] = {
-        text: surfaceMapInfo["title"],
-        font: {
-            size: surfaceMapInfo["title_fontsize"],
-            color: textColor(surfaceMapInfo["black_bg"]),
-        },
-        yref: "paper",
-        y: 0.95,
-    };
-    let config = getConfig();
+  const data = [info]
 
-    Plotly.react(divId, data, layout, config);
+  info.lighting = getLighting()
+  const layout = getLayout(
+    'surface-plot',
+    'select-view',
+    surfaceMapInfo.black_bg
+  )
+  layout.title = {
+    text: surfaceMapInfo.title,
+    font: {
+      size: surfaceMapInfo.title_fontsize,
+      color: textColor(surfaceMapInfo.black_bg)
+    },
+    yref: 'paper',
+    y: 0.95
+  }
+  const config = getConfig()
 
-    if (surfaceMapInfo["colorbar"]) {
-        addColorbar(
-            surfaceMapInfo["colorscale"],
-            surfaceMapInfo["cmin"],
-            surfaceMapInfo["cmax"],
-            divId,
-            layout,
-            config,
-            surfaceMapInfo["cbar_fontsize"],
-            surfaceMapInfo["cbar_height"],
-            (color = textColor(surfaceMapInfo["black_bg"])),
-        );
-    }
+  Plotly.react(divId, data, layout, config)
+
+  if (surfaceMapInfo.colorbar) {
+    const color = textColor(surfaceMapInfo.black_bg)
+
+    addColorbar(
+      surfaceMapInfo.colorscale,
+      surfaceMapInfo.cmin,
+      surfaceMapInfo.cmax,
+      divId,
+      layout,
+      config,
+      surfaceMapInfo.cbar_fontsize,
+      surfaceMapInfo.cbar_height,
+      (color)
+    )
+  }
 }
 
-function addPlot() {
-    let hemisphere = $("#select-hemisphere").val();
-    let kind = $("#select-kind").val();
+function addPlot () { // eslint-disable-line no-unused-vars
+  const hemisphere = $('#select-hemisphere').val()
+  const kind = $('#select-kind').val()
 
-    if (surfaceMapInfo["view"]) {
-        $("#select-view").val(surfaceMapInfo["view"]);
-    }
-    makePlot(kind, hemisphere, "surface-plot");
+  if (surfaceMapInfo.view) {
+    $('#select-view').val(surfaceMapInfo.view)
+  }
+  makePlot(kind, hemisphere, 'surface-plot')
 }
 
-function surfaceRelayout() {
-    return updateLayout(
-        "surface-plot",
-        "select-view",
-        surfaceMapInfo["black_bg"],
-    );
+function surfaceRelayout () { // eslint-disable-line no-unused-vars
+  return updateLayout(
+    'surface-plot',
+    'select-view',
+    surfaceMapInfo.black_bg
+  )
 }
