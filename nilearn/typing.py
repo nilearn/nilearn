e6159087a88c4e461bcc9bb9413e8e77f0dbaa1a
Remi Gau <remi_gau@hotmail.com>
2026-05-28 07:42:15 +0200

    rm dup

--- nilearn/typing.py
+++ nilearn/typing.py
@@ -120,9 +120,7 @@ Threshold: TypeAlias = Scalar | str
 Tfce: TypeAlias = bool
 Title: TypeAlias = str | None
 Tr: TypeAlias = Scalar
-Transparency: TypeAlias = (
-    float | np.floating | Integer | NiimgLike | Path | None
-)
+Transparency: TypeAlias = float | np.floating | Integer | NiimgLike | None
 TransparencyRange: TypeAlias = list | tuple | None
 TwoSidedTest: TypeAlias = bool
 Url: TypeAlias = str | None
