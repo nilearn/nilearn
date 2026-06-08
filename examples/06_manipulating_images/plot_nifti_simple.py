443862036b195acdd78f1f85491dc3b89059393c
Remi Gau <remi_gau@hotmail.com>
2026-06-08 08:54:17 +0200

    [FIX] fix typos and grammatical errors in examples

--- examples/06_manipulating_images/plot_nifti_simple.py
+++ examples/06_manipulating_images/plot_nifti_simple.py
@@ -53,7 +53,7 @@ masker.fit(func_filename)
 #
 #   You can also note that after fitting,
 #   the HTML representation of the estimator looks different
-#   than before before fitting.
+#   than before fitting.
 #
 masker
 
@@ -65,7 +65,7 @@ masker
 # for this functional image by plotting the mask.
 #
 # We get the estimated mask from the ``mask_img_`` attribute of the masker:
-# the final ``_`` ofd this attribute name means it was generated
+# the final ``_`` of this attribute name means it was generated
 # by the :meth:`~nilearn.maskers.NiftiMasker.fit` method.
 #
 # We can then plot it using the :func:`~nilearn.plotting.plot_roi` function
