db7bf60353b0b62d0bb4fe9025fac6452305835f
RDoerfel <doerfelruben@aol.com>
2026-06-12 11:46:14 +0200

    [DOC] fix linting / formatting issues

--- nilearn/image/image.py
+++ nilearn/image/image.py
@@ -2426,7 +2426,7 @@ def copy_img(img):
     -------
     img_copy : image
         copy of input (data, affine and header)
-    
+
     Examples
     --------
     >>> from nilearn.image import copy_img
@@ -2438,7 +2438,8 @@ def copy_img(img):
     >>> img_3d = Nifti1Image(data, affine)
     >>> # copy the image vs reference assignment
     >>> img_3d_copy = copy_img(img_3d)
-    >>> # Use reference assignment. This is not copying, img_3d_notcopy points to the same object as img_3d!
+    >>> # Use reference assignment. This is not copying, img_3d_notcopy
+    >>> # points to the same object as img_3d!
     >>> img_3d_notcopy = img_3d
     >>> # show initial dtypes; they are all the same
     >>> img_3d.get_data_dtype()
@@ -2452,10 +2453,11 @@ def copy_img(img):
     >>> # show the new dtypes
     >>> img_3d.get_data_dtype()
     dtype('uint8')
-    >>> # img_3d_copy was copied before the change, hence it keeps the original dtype
+    >>> # img_3d_copy was copied before the change and keeps the original dtype
     >>> img_3d_copy.get_data_dtype()
     dtype('<f8')
-    >>> # img_3d_notcopy refers to the same object as img_3d, hence its dtype has changed
+    >>> # img_3d_notcopy refers to the same object as img_3d, hence its dtype
+    >>> # has changed
     >>> img_3d_notcopy.get_data_dtype()
     dtype('uint8')
     """
