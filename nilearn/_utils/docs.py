4cf6df7882995b19a54619f9c6605644ca8aef4e
Maximilian Cosmo Sitter <48606431+mcsitter@users.noreply.github.com>
2026-06-18 07:31:58 +0200

    [Maint] Add str to threshold docs; remove unnecessary TYPE_CHECKING block for Nifti1Image (#6333)

--- nilearn/_utils/docs.py
+++ nilearn/_utils/docs.py
@@ -1364,7 +1364,7 @@ tfce : :obj:`bool`, default=False
 
 # threshold
 docdict["threshold"] = """
-threshold : :obj:`int` or :obj:`float`, None, or 'auto', optional
+threshold : :obj:`int` or :obj:`float`, :obj:`str`, None, or 'auto', optional
     If `None` is given, the image is not thresholded.
     If number is given, it must be non-negative. The specified value is used to
     threshold the image: values below the threshold (in absolute value) are
