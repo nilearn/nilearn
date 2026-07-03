ac52f9a1b8d615eef3ca56fd3ee336a1e16def37
Hande Gözükan <hande@ginkgo.work>
2026-07-03 11:10:18 +0200

    increase timeout to 20000

--- tests/js/view.test.js
+++ tests/js/view.test.js
@@ -6,7 +6,7 @@ const functions = ['img', 'surf', 'surf_niivue', 'connectome', 'markers', 'img_o
 
 const tolerance = 1100
 
-const timeout = 15000
+const timeout = 20000
 
 for (let i = 0; i < functions.length; i++) {
   template.fullTest('view_' + functions[i] + '.html', VIEWPORT, tolerance, timeout)
