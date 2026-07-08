82cf17f9fd372b85cbdd2962e3dc74a08cd06289
Nirmitee Mulay <nirmiteemulay@gmail.com>
2026-07-08 23:50:21 +0200

    [DOC] add example to plot_design_matrix_correlation

--- nilearn/plotting/matrix/matrix_plotting.py
+++ nilearn/plotting/matrix/matrix_plotting.py
@@ -622,6 +622,33 @@ def plot_design_matrix_correlation(
     -------
     display : :class:`matplotlib.axes.Axes`
         Axes image.
+
+    Examples
+    --------
+
+    .. plot::
+        >>> import numpy as np
+        >>> from pandas import DataFrame
+        >>> from nilearn.glm.first_level import make_first_level_design_matrix
+        >>> from nilearn.plotting import plot_design_matrix_correlation
+        >>> from nilearn.plotting.image.img_plotting import show
+        >>>
+        >>> #creating a design matrix
+        >>>
+        >>> frame_times = np.arange(9)
+        >>> onsets = np.arange(9)
+        >>> duration = np.linspace(1,9,9)
+        >>> trial_type = ["ET_0", "ET_0", "ET_0",
+        ...               "ET_1", "ET_1", "ET_1",
+        ...               "ET_2", "ET_2", "ET_2"]
+        >>> events = DataFrame({"trial_type": trial_type,
+        ...                   "onset": onsets,
+        ...                   "duration": duration})
+        >>>
+        >>> design_matrix = make_first_level_design_matrix(frame_times, events)
+        >>>
+        >>> ax = plot_design_matrix_correlation(design_matrix)
+        >>> show()
     """
     check_params(locals())
 
