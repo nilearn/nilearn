cccce840ee81c4cae583474e8e38c9b5e0b9abfd
Hande Gözükan <2099645+hndgzkn@users.noreply.github.com>
2026-07-08 07:03:48 +0200

    [MAINT] add reference image for view_surf with niivue engine (#6359)
    Co-authored-by: Hande Gözükan <hande@ginkgo.work>
    Co-authored-by: Remi Gau <remi_gau@hotmail.com>

--- tests/js/view.test.js
+++ tests/js/view.test.js
@@ -2,11 +2,11 @@ const template = require('./template.js')
 
 const VIEWPORT = { x: 0, y: 0, width: 1200, height: 800 }
 
-const functions = ['img', 'surf', 'connectome', 'markers', 'img_on_surf']
+const functions = ['img', 'surf', 'surf_niivue', 'connectome', 'markers', 'img_on_surf']
 
 const tolerance = 1100
 
-const timeout = 10000
+const timeout = 20000
 
 for (let i = 0; i < functions.length; i++) {
   template.fullTest('view_' + functions[i] + '.html', VIEWPORT, tolerance, timeout)
