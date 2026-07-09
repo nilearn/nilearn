3f5f72ecc7ae03322f139146da9a2ce2f539de0f
Remi Gau <remi_gau@hotmail.com>
2026-07-09 06:04:40 +0200

    Apply suggestions from code review
    Co-authored-by: Remi Gau <remi_gau@hotmail.com>

--- nilearn/plotting/matrix/matrix_plotting.py
+++ nilearn/plotting/matrix/matrix_plotting.py
@@ -627,6 +627,7 @@ def plot_design_matrix_correlation(
     --------
 
     .. plot::
+    
         >>> import numpy as np
         >>> from pandas import DataFrame
         >>> from nilearn.glm.first_level import make_first_level_design_matrix
@@ -647,7 +648,7 @@ def plot_design_matrix_correlation(
         >>>
         >>> design_matrix = make_first_level_design_matrix(frame_times, events)
         >>>
-        >>> ax = plot_design_matrix_correlation(design_matrix)
+        >>> plot_design_matrix_correlation(design_matrix)
         >>> show()
     """
     check_params(locals())
