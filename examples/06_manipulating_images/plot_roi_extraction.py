c0e45f9aef7e07253ef94bf2d64c49a15bd97b7e
Michelle Wang <tomichellewang@gmail.com>
2026-07-20 13:48:32 -0400

    reword parts of example

--- examples/06_manipulating_images/plot_roi_extraction.py
+++ examples/06_manipulating_images/plot_roi_extraction.py
@@ -5,17 +5,17 @@ Computing a Region of Interest (ROI) mask manually
 This example shows manual steps to create and further modify an ROI spatial
 mask. They represent a means for "data folding", i.e., extracting and then
 analyzing brain data from a subset of voxels rather than whole brain images.
-Example can also help alleviate curse of dimensionality (i.e., statistical
+Masking can also help alleviate the curse of dimensionality (i.e., statistical
 problems that arise in the context of high-dimensional input variables).
 
-We demonstrate how to compute a ROI mask using **T-test** and then how simple
-image operations can be used before and after computing ROI to improve the
+We demonstrate how to compute a ROI mask using a **T-test** and then how simple
+image operations can be used before and after computing the ROI to improve the
 quality of the computed mask.
 
 These chains of operations are easy to set up using Nilearn and Scipy Python
 libraries. Here we give clear guidelines about these steps, starting with
 pre-image operations to post-image operations. The main point is that
-visualization & results checking be possible at each step.
+visualization & results checking are possible at each step.
 
 .. seealso::
 
@@ -26,8 +26,8 @@ visualization & results checking be possible at each step.
 """
 
 # %%
-# Coordinates of the slice we are interested in each direction. We will be
-# using them for visualization.
+# Here are the coordinates of the slice we are interested in each direction.
+# We will be using them for visualization.
 
 # cut in x-direction
 sagittal = -25
@@ -42,17 +42,14 @@ cut_coords = [sagittal, coronal, axial]
 # %%
 # Loading the data
 # ----------------
-# We rely on the Haxby datasets and its experiments to demonstrate the complete
-# list of operations. Fetching datasets is easy, shipping with Nilearn using a
-# function named as `fetch_haxby`. The data will then be automatically stored
-# in a home directory with "nilearn_data" folder in your computer. From which,
-# we process data using paths of the Nifti images.
+# We will use the Haxby dataset to demonstrate the complete list of operations.
+# The data will then be automatically stored in our home directory under
+# ``nilearn_data/``.
 
-# We load data from nilearn by import datasets
 from nilearn import datasets
 
-# First, we fetch single subject specific data with haxby datasets: to have
-# anatomical image, EPI images and masks images
+# First, we fetch anatomical image, EPI images and masks images from the Haxby
+# dataset
 haxby_dataset = datasets.fetch_haxby()
 
 # print basic information on the dataset
@@ -73,12 +70,13 @@ print(
 import pandas as pd
 
 run_target = pd.read_csv(haxby_dataset.session_target[0], sep=" ")
-# Now, we have the labels and will be useful while computing student's t-test
+# Now, we have the labels that will be useful while computing the
+# student's t-test
 haxby_labels = run_target["labels"]
 
 # %%
-# We have the datasets in hand especially paths to the locations. Now, we do
-# simple pre-processing step called as image smoothing on functional images
+# We now have the paths to the images in this dataset. The next step is to do a
+# simple pre-processing step called `image smoothing` on the functional images
 # and then build a statistical test on smoothed images.
 
 # %%
@@ -88,9 +86,9 @@ haxby_labels = run_target["labels"]
 # When using methods that are not robust to noise, it is useful to apply a
 # spatial filtering kernel on the data. Such data smoothing is usually applied
 # using a Gaussian function with 4mm to 12mm
-# :term:`full-width at half-maximum<FWHM>` (this is where the :term:`FWHM`
-# comes from). The function :func:`~nilearn.image.smooth_img` accounts for
-# potential anisotropy in the image affine (i.e., non-indentical
+# :term:`full-width at half-maximum<FWHM>` (this is where the ``fwhm``
+# parameter below comes from). The function :func:`~nilearn.image.smooth_img`
+# accounts for potential anisotropy in the image affine (i.e., non-indentical
 # :term:`voxel` size in all the three dimensions). Analogous to the
 # majority of nilearn functions, :func:`~nilearn.image.smooth_img` can
 # also use file names as input parameters.
@@ -101,32 +99,25 @@ from nilearn import image
 # Functional data
 fmri_filename = haxby_dataset.func[0]
 # smoothing: first argument as functional data filename and smoothing value
-# (integer) in second argument. Output returns in Nifti image.
+# (integer) in second argument. Output is a Nifti image.
 fmri_img = image.smooth_img(fmri_filename, fwhm=6)
 
 # Visualize the mean of the smoothed EPI image using plotting function
 # `plot_epi`
 from nilearn.plotting import plot_epi
 
-# First, compute the voxel-wise mean of smooth EPI image
-# (first argument) using image processing module `image`
+# First, compute the voxel-wise mean of the smooth EPI image
+# (first argument) using the image processing module `image`
 mean_img = image.mean_img(fmri_img)
 # Second, we visualize the mean image with coordinates positioned manually
 plot_epi(mean_img, title="Smoothed mean EPI", cut_coords=cut_coords)
 
 # %%
-# Given the smoothed functional data stored in variable 'fmri_img', we then
-# select two features of interest with face and house experimental conditions.
-# The method we will be using is a simple Student's t-test. The below section
-# gives us brief motivation example about why selecting features in high
-# dimensional FMRI data setting.
-
-# %%
 # Functional MRI data can be considered "high dimensional" given the p-versus-n
 # ratio (e.g., p=~20,000-200,000 voxels for n=1000 samples or less). In this
 # setting, machine-learning algorithms can perform poorly due to the so-called
-# curse of dimensionality. However, simple means from the realms of classical
-# statistics can help reducing the number of voxels.
+# curse of dimensionality. However, simple means from classical statistics
+# can help reduce the number of voxels.
 from nilearn.image import get_data
 
 fmri_data = get_data(fmri_img)
@@ -158,11 +149,11 @@ _, p_values = stats.ttest_ind(
 
 # Use a log scale for p-values
 log_p_values = -np.log10(p_values)
-# NAN values to zero
+# Set NAN values to zero
 log_p_values[np.isnan(log_p_values)] = 0.0
 log_p_values[log_p_values > 10.0] = 10.0
 
-# Before visualizing, we transform the computed p-values to Nifti-like image
+# Before visualizing, we transform the computed p-values to a Nifti-like image
 # using function `new_img_like` from nilearn.
 from nilearn.image import new_img_like
 
@@ -172,12 +163,12 @@ from nilearn.plotting import plot_stat_map
 # First argument being a reference image
 # and second argument should be p-values data
 # to convert to a new image as output.
-# This new image will have same header information as reference image.
+# This new image will have same header information as the reference image.
 log_p_values_img = new_img_like(fmri_img, log_p_values)
 
-# Now, we visualize log p-values image on functional mean image as background
-# with coordinates given manually and colorbar on the right side of plot (by
-# default colorbar=True)
+# Now, we visualize the log p-values image on the functional mean image as
+# a background with coordinates given manually and a colorbar on the right side
+# of the plot (by default, colorbar=True)
 plot_stat_map(
     log_p_values_img,
     mean_img,
@@ -187,15 +178,15 @@ plot_stat_map(
 )
 
 # %%
-# **Selecting features using f_classif**: Feature selection method is also
-# available in the scikit-learn Python package, where it has been extended to
-# several classes, using the `sklearn.feature_selection.f_classif` function.
+# **Selecting features using f_classif**: It is also possible to use the
+# :func:`sklearn.feature_selection.f_classif` function, which works for
+# feature selection in multi-class settings.
 
 # %%
 # Build a mask from this statistical map (Improving the quality of the mask)
 # --------------------------------------------------------------------------
 # **Thresholding** - We build the t-map to have better representation of voxels
-# of interest and voxels with lower p-values correspond to the most intense
+# of interest, where voxels with lower p-values correspond to the most intense
 # voxels. This can be done easily by applying a threshold to a t-map data in
 # array.
 
@@ -228,7 +219,7 @@ plot_stat_map(
 # %%
 # **Binarization** and **Intersection** with Ventral Temporal (VT) mask - We
 # now want to restrict our investigation to the VT area. The corresponding
-# spatial mask is provided in haxby_dataset.mask_vt. We want to compute the
+# spatial mask is provided in ``haxby_dataset.mask_vt``. We want to compute the
 # intersection of this provided mask with our self-computed mask.
 
 # self-computed mask
@@ -236,15 +227,16 @@ bin_p_values = log_p_values != 0
 # VT mask
 mask_vt_filename = haxby_dataset.mask_vt[0]
 
-# The first step is to load VT mask and same time convert data type
-# numbers to boolean type
+# The first step is to load VT mask and at the same time to convert the
+# datatype from "number" to "boolean"
 from nilearn.image import load_img
 
 vt = get_data(load_img(mask_vt_filename)).astype(bool)
 
-# We can then use a logical "and" operation - numpy.logical_and - to keep only
-# voxels that have been selected in both masks. In neuroimaging jargon, this
-# is called an "AND conjunction". We use already imported numpy as np
+# We can then use a logical "and" operation - `numpy.logical_and` - to
+# keep only voxels that have been selected in both masks.
+# In neuroimaging jargon, this is called an "AND conjunction".
+# We use the already imported numpy as np
 bin_p_values_and_vt = np.logical_and(bin_p_values, vt)
 
 # Visualizing the mask intersection results using plotting function `plot_roi`,
@@ -258,7 +250,8 @@ from nilearn.plotting import plot_roi, show
 bin_p_values_and_vt_img = new_img_like(
     fmri_img, bin_p_values_and_vt.astype(np.int32)
 )
-# Visualizing goes here with background as computed mean of functional images
+# We visualize the mask using the computed mean of functional images as
+# background.
 plot_roi(
     bin_p_values_and_vt_img,
     mean_img,
@@ -278,13 +271,11 @@ plot_roi(
 # We use ndimage function from scipy Python library for mask dilation
 from scipy.ndimage import binary_dilation
 
-# Input here is a binarized and intersected mask data from previous section
+# Input here is a binarized and intersected mask data from the previous section
 dil_bin_p_values_and_vt = binary_dilation(bin_p_values_and_vt)
 
-# Now, we visualize the same using `plot_roi` with data being converted
-# to Nifti image.
-# In all new image like, reference image is the same but second argument
-# varies with data specific
+# Now, we visualize the same using `plot_roi` with the data being converted
+# to Nifti image. In all `new_img_like` calls, we use the same reference image
 dil_bin_p_values_and_vt_img = new_img_like(
     fmri_img, dil_bin_p_values_and_vt.astype(np.int32)
 )
@@ -299,8 +290,8 @@ plot_roi(
 )
 # %%
 # Finally, we end with splitting the connected ROIs to two hemispheres into two
-# separate regions (ROIs). The function `scipy.ndimage.label` from the scipy
-# Python library.
+# separate regions (ROIs). We use the function :func:`scipy.ndimage.label` from
+# the scipy Python library.
 
 # %%
 # **Identification of connected components** - The function
@@ -317,34 +308,34 @@ second_roi_data = (labels == 3).astype(np.int32)
 # Visualizing the connected components
 # First, we create a Nifti image type from first roi data in a array
 first_roi_img = new_img_like(fmri_img, first_roi_data)
-# Then, visualize the same created Nifti image in first argument and mean of
-# functional images as background (second argument), cut_coords is default now
-# and coordinates are selected automatically pointed exactly on the roi data
+# Then, we visualize the same created Nifti image (first argument) with the
+# mean of functional images as background (second argument). The cut_coords are
+# the default now: coordinates are selected automatically and will be pointed
+# exactly on the roi data
 plot_roi(first_roi_img, mean_img, title="Connected components: first ROI")
-# we do the same for second roi data
+# we do the same for the second roi data
 second_roi_img = new_img_like(fmri_img, second_roi_data)
-# Visualization goes here with second roi image and cut_coords are default with
-# coordinates selected automatically pointed on the data
 plot_roi(second_roi_img, mean_img, title="Connected components: second ROI")
 
 
 # %%
-# Use the new ROIs, to extract data maps in both ROIs
-# We extract data from ROIs using nilearn's NiftiLabelsMasker
+# Use the new ROIs to extract data maps in both ROIs
+# We extract data from ROIs using Nilearn's
+# :class:`~nilearn.maskers.NiftiLabelsMasker`
 from nilearn.maskers import NiftiLabelsMasker
 
 # %%
-# Before data extraction, we convert an array labels to Nifti like image. All
-# inputs to NiftiLabelsMasker must be Nifti-like images or filename to Nifti
-# images. We use the same reference image as used above in previous sections
+# Before data extraction, we convert array labels to a Nifti like image. All
+# inputs to ``NiftiLabelsMasker`` must be Nifti-like images or filenames to
+# Nifti images. We use the same reference image as used above in previous
+# sections
 labels_img = new_img_like(fmri_img, labels)
 
 # %%
-# First, initialize masker with parameters suited for data extraction using
-# labels as input image, resampling_target is None as affine,
-# shape/size is same
-# for all the data used here, time series signal processing parameters
-# standardize and detrend are set to False
+# First, we initialize a masker with parameters suited for data extraction:
+# labels as input image, ``resampling_target`` is None as the affine and
+# shape/size are the same for all the data used here, time series signal
+# processing parameters ``standardize`` and ``detrend`` are set to ``False``
 masker = NiftiLabelsMasker(
     labels_img,
     resampling_target=None,
@@ -354,7 +345,7 @@ masker = NiftiLabelsMasker(
 )
 
 # %%
-# Preparing for data extraction: setting number of conditions, size, etc from
+# Preparing for data extraction: setting number of conditions, size, etc. from
 # haxby dataset
 condition_names = haxby_labels.unique()
 n_cond_img = fmri_data[..., haxby_labels == "house"].shape[-1]
@@ -363,9 +354,10 @@ n_conds = len(condition_names)
 X1, X2 = np.zeros((n_cond_img, n_conds)), np.zeros((n_cond_img, n_conds))
 
 # %%
-# Gathering data for each condition and then use transformer from masker
-# object transform() on each data. The transformer extracts data in condition
-# maps where the target regions are specified by labels images
+# Gathering data for each condition and then use
+# :meth:`~nilearn.maskers.NiftiLabelsMasker.fit_transform` on each data.
+# The transformer extracts data in condition maps where the target regions are
+# specified by labels images
 for i, cond in enumerate(condition_names):
     cond_maps = new_img_like(
         fmri_img, fmri_data[..., haxby_labels == cond][..., :n_cond_img]
