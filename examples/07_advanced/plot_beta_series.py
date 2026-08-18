0cfe69d4bb12bd2050b6f97506ca8e371f4b0266
Elizabeth DuPre <15017191+emdupre@users.noreply.github.com>
2026-08-18 13:26:29 -0400

    Integrate review comments, update plots

--- examples/07_advanced/plot_beta_series.py
+++ examples/07_advanced/plot_beta_series.py
@@ -30,9 +30,10 @@ from nilearn.plotting import plot_design_matrix, plot_stat_map, show
 
 data = fetch_language_localizer_demo_dataset()
 
-# find the first subject's functional run, confounds, and events file
+# %%
+# Find the first subject's functional run, confounds, and events file
 # and use them to instantiate a
-# :class:`~nilearn.glm.first_level.FirstLevelModel`
+# :class:`~nilearn.glm.first_level.FirstLevelModel`.
 models, models_run_imgs, events_dfs, models_confounds = first_level_from_bids(
     dataset_path=data.data_dir,
     task_label="languagelocalizer",
@@ -56,8 +57,8 @@ for _idx, model in enumerate(models):
         fmri_file = run_imgs
         events_df = events
 
-# we can verify that only one fMRI run was found for
-# this subject as expected
+# We can verify that only one fMRI run was found for
+# this subject as expected.
 print("fmri_file:", fmri_file)
 
 # %%
@@ -71,23 +72,23 @@ print("fmri_file:", fmri_file)
 # :func:`~nilearn.glm.first_level.first_level_from_bids`.
 import matplotlib.pyplot as plt
 
-# Fit the model
+# Fit the model.
 standard_glm.fit(fmri_file, events_df)
 
 # The standard design matrix has one column for each condition, along with
-# columns for the confound regressors and drifts
+# columns for the confound regressors and drifts.
 fig, ax = plt.subplots(figsize=(5, 10))
 plot_design_matrix(standard_glm.design_matrices_[0], axes=ax)
 show()
 
+# %%
 # We will reuse the parameters supplied above to
 # `first_level_from_bids` for all other models, so
 # we can extract these directly from the `standard_glm`
 # object.
 glm_parameters = standard_glm.get_params()
 
-# We need to override one parameter (signal_scaling)
-# with the value of scaling_axis
+# We need to override one parameter (``signal_scaling``).
 glm_parameters["signal_scaling"] = standard_glm.signal_scaling
 
 # %%
@@ -95,7 +96,7 @@ glm_parameters["signal_scaling"] = standard_glm.signal_scaling
 # ----------------------------------------
 # We will now create a Least Squares-All (LSA) model.
 # This involves creating a new condition in the design matrix
-# for each trial of interest
+# for each trial of interest.
 # It's important to ensure that the original conditions can be inferred from
 # the new trial-wise conditions, in order to collect the resulting
 # :term:`beta` maps into condition-wise beta-series.
@@ -110,7 +111,7 @@ for i_trial, trial in lsa_events_df.iterrows():
     trial_condition = trial["trial_type"]
     condition_counter[trial_condition] += 1
     # We use a unique delimiter here (``__``) which shouldn't be in the
-    # original condition names
+    # original condition names.
     trial_name = f"{trial_condition}__{condition_counter[trial_condition]:03d}"
     lsa_events_df.loc[i_trial, "trial_type"] = trial_name
 
@@ -124,7 +125,7 @@ show()
 # %%
 # Aggregate beta maps from the LSA model based on condition
 # `````````````````````````````````````````````````````````
-# Collect the :term:`Parameter Estimate` maps
+# Collect the :term:`Parameter Estimate` maps.
 from nilearn.image import concat_imgs
 
 lsa_beta_maps = {cond: [] for cond in events_df["trial_type"].unique()}
@@ -132,12 +133,12 @@ trialwise_conditions = lsa_events_df["trial_type"].unique()
 for condition in trialwise_conditions:
     beta_map = lsa_glm.compute_contrast(condition, output_type="effect_size")
     # Drop the trial number from the condition name to get the original name,
-    # splitting on our delimiter (``__``)
+    # splitting on our delimiter (``__``).
     condition_name = condition.split("__")[0]
     lsa_beta_maps[condition_name].append(beta_map)
 
 # We concatenate the lists of 3D maps into a single 4D beta-series for
-# each condition
+# each condition.
 lsa_beta_maps = {
     name: concat_imgs(maps) for name, maps in lsa_beta_maps.items()
 }
@@ -147,7 +148,7 @@ lsa_beta_maps = {
 # ----------------------------------------------
 # We next create a separate Least Squares-Separate (LSS) model,
 # one for each trial in the conditions of interest.
-# Thisis much like the LSA approach,
+# This is much like the LSA approach,
 # except that we only relabel *one* trial in the `events_df` data frame.
 # We loop through the trials, create a version of the data frame where the
 # targeted trial has a unique trial type, fit the model to that data frame,
@@ -175,7 +176,7 @@ def lss_transformer(events_df, row_number):
     """
     events_df = events_df.copy()
 
-    # Determine which number trial it is *within the condition of interest*
+    # Determine which number trial it is *within the condition of interest*.
     trial_condition = events_df.loc[row_number, "trial_type"]
     trial_type_series = events_df["trial_type"]
     trial_type_series = trial_type_series.loc[
@@ -195,18 +196,18 @@ def lss_transformer(events_df, row_number):
     return events_df, trial_name
 
 
-# Loop through the trials of interest and transform the data frame for LSS
+# Loop through the trials of interest and transform the data frame for LSS.
 lss_beta_maps = {cond: [] for cond in events_df["trial_type"].unique()}
 lss_design_matrices = []
 
 for i_trial in range(events_df.shape[0]):
     lss_events_df, trial_condition = lss_transformer(events_df, i_trial)
 
-    # Compute and collect beta maps
+    # Compute and collect beta maps.
     lss_glm = FirstLevelModel(**glm_parameters)
     lss_glm.fit(fmri_file, lss_events_df)
 
-    # We save the design matrices across trials to visualize them later
+    # We save the design matrices across trials to visualize them later.
     lss_design_matrices.append(lss_glm.design_matrices_[0])
 
     beta_map = lss_glm.compute_contrast(
@@ -220,7 +221,7 @@ for i_trial in range(events_df.shape[0]):
     lss_beta_maps[condition_name].append(beta_map)
 
 # We again concatenate the lists of 3D maps into a single 4D beta-series for
-# each condition
+# each condition.
 lss_beta_maps = {
     name: concat_imgs(maps) for name, maps in lss_beta_maps.items()
 }
@@ -265,7 +266,7 @@ show()
 # ---------------------------
 # Beta-series can be used much like :term:`resting-state` data,
 # though generally with vastly reduced degrees of freedom
-# than a typical :term:`resting-state` run,
+# compared to a typical :term:`resting-state` run,
 # given that the number of trials should always be less
 # than the number of volumes in a :term:`fMRI` run.
 #
@@ -277,8 +278,10 @@ show()
 # analysis.
 #
 # In the following section, we perform a task-based functional
-# connectivity analysis using one of the two task conditions
-# ('string') from the LSS beta-series.
+# connectivity analysis using the two task conditions
+# ("language" and "string") from the LSS beta-series.
+# The seed coordinate is chosen based on a previous
+# `Neurosynth <https://neurosynth.org/>`_ meta-analysis.
 # This section is based on
 # :ref:`sphx_glr_auto_examples_03_connectivity\
 # _plot_seed_to_voxel_correlation.py`,
@@ -288,29 +291,58 @@ import numpy as np
 
 from nilearn.maskers import NiftiMasker, NiftiSpheresMasker
 
-# Coordinate taken from Neurosynth's 'language' meta-analysis
+# Use coordinate taken from Neurosynth's "language" meta-analysis.
 coords = [(-54, -42, 3)]
 
-# Initialize masker for the "language"seed
+# Initialize masker for the Neurosynth seed.
 seed_masker = NiftiSpheresMasker(
     coords,
     radius=8,
     detrend=True,
+    standardize="zscore_sample",
     memory="nilearn_cache",
     memory_level=1,
     verbose=1,
 )
 
-# Initialize a separate masker for the whole brain
+# Initialize a separate masker for the whole brain.
 brain_masker = NiftiMasker(
     smoothing_fwhm=6,
     detrend=True,
+    standardize="zscore_sample",
     memory="nilearn_cache",
     memory_level=1,
     verbose=1,
 )
 
-# Perform the seed-to-voxel correlation for the LSS 'string' beta-series
+# %%
+# Perform the seed-to-voxel correlation for the LSS beta-series.
+# ``````````````````````````````````````````````````````````````
+# Using the defined ``seed_masker``, we extract the signal from our
+# `Neurosynth <https://neurosynth.org/>`_ coordinate for the LSS beta-series
+# maps defined for each trial, separately for each task type ("language" and
+# "string").
+# We then extract the whole-brain signal using the ``brain_masker`` from the
+# same LSS beta-series maps.
+# Finally, we take the dot-product of these two matrices and normalize it by
+# the number of samples; i.e., the number of trials.
+#
+# We perform this analysis for each task condition separately.
+#
+language_seed_beta_series = seed_masker.fit_transform(
+    lss_beta_maps["language"]
+)
+language_beta_series = brain_masker.fit_transform(lss_beta_maps["language"])
+language_corrs = (
+    np.dot(
+        language_beta_series.T,
+        language_seed_beta_series,
+    )
+    / language_seed_beta_series.shape[0]
+)
+language_connectivity_img = brain_masker.inverse_transform(language_corrs.T)
+
+# Perform the same seed-to-voxel correlation for the LSS 'string' beta-series
 string_seed_beta_series = seed_masker.fit_transform(lss_beta_maps["string"])
 string_beta_series = brain_masker.fit_transform(lss_beta_maps["string"])
 string_corrs = (
@@ -322,31 +354,31 @@ string_corrs = (
 )
 string_connectivity_img = brain_masker.inverse_transform(string_corrs.T)
 
-# Show both correlation maps
-fig, ax = plt.subplots(figsize=(10, 8))
+# %%
+# Visualize both correlation maps.
+# ````````````````````````````````
+fig, axes = plt.subplots(figsize=(10, 8), nrows=2)
+conn_imgs = [language_connectivity_img, string_connectivity_img]
+conn_img_lbls = ["language", "string"]
 
-display = plot_stat_map(
-    string_connectivity_img,
-    threshold=0.05,
-    vmax=0.25,
-    cut_coords=coords[0],
-    title="String",
-    figure=fig,
-    axes=ax,
-)
-display.add_markers(
-    marker_coords=coords,
-    marker_color="g",
-    marker_size=300,
-)
+for img, lbl, ax in zip(conn_imgs, conn_img_lbls, axes, strict=False):
+    display = plot_stat_map(
+        img,
+        threshold=0.5,
+        vmax=1,
+        cut_coords=coords[0],
+        title=lbl,
+        figure=fig,
+        axes=ax,
+    )
+    display.add_markers(
+        marker_coords=coords,
+        marker_color="g",
+        marker_size=200,
+    )
+
 fig.suptitle(
     "Least Squares-Separate (LSS) Beta-Series Functional Connectivity"
 )
 
 show()
-
-# %%
-# References
-# ----------
-#
-# .. footbibliography::
