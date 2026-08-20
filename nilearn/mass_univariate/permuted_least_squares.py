FIX validate numerical tested_vars in permuted_ols

nilearn/mass_univariate/permuted_least_squares.py
@@ -380,7 +380,8 @@ def permuted_ols(
     Parameters
     ----------
     tested_vars : array-like, shape=(n_samples, n_regressors)
-        Explanatory variates, fitted and tested independently from each others.
+        Numerical explanatory variates, fitted and tested independently from
+        each others.
 
     target_vars : array-like, shape=(n_samples, n_descriptors)
         :term:`fMRI` data to analyze according
@@ -938,6 +939,13 @@ def _check_inputs_permuted_ols(
 def _sanitize_inputs_permuted_ols(
     n_jobs, output_type, tfce, threshold, target_vars, tested_vars
 ):
+    tested_vars = np.asanyarray(tested_vars)
+    if not (
+        np.issubdtype(tested_vars.dtype, np.number)
+        or np.issubdtype(tested_vars.dtype, np.bool_)
+    ):
+        raise TypeError("'tested_vars' must contain numerical values.")
+
     # check n_jobs (number of CPUs)
     if n_jobs < 0:
         n_jobs = max(1, joblib.cpu_count() - int(n_jobs) + 1)