e1e5010ad6812915398eb70457f9ec950d4fe4ee
Mohammad Sadeghi <74879414+mohammedsadeghi@users.noreply.github.com>
2026-08-21 13:25:58 +0330

    Update nilearn/reporting/get_clusters_table.py
    Co-authored-by: Remi Gau <remi_gau@hotmail.com>

--- nilearn/reporting/get_clusters_table.py
+++ nilearn/reporting/get_clusters_table.py
@@ -331,7 +331,7 @@ def get_clusters_table(
 
         .. nilearn_deprecated:: 0.14.1dev
 
-            This parameter will be removed in version 0.16.0, when
+            This parameter will be removed in version 0.17.0, when
             :func:`~nilearn.reporting.get_clusters_table` will always return
             the cluster label maps together with the table. Set
             ``return_label_maps=True`` and update your code to unpack both
