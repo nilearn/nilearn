fd07d4bb00caad7fc0911ad2818b89ae33670d86
Remi Gau <remi_gau@hotmail.com>
2026-08-26 16:32:38 +0200

    Apply suggestion from @Remi-Gau

--- examples/02_decoding/plot_haxby_full_analysis.py
+++ examples/02_decoding/plot_haxby_full_analysis.py
@@ -1,6 +1,6 @@
 """
 ROI-based decoding analysis in the Haxby dataset
-===================================================
+================================================
 
 In this script we reproduce the data analysis
 conducted by :footcite:t:`Haxby2001`.
