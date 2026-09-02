20e04d90c66b4b1f2020f93349fe8d27e6544b3e
Remi Gau <remi_gau@hotmail.com>
2026-09-02 18:12:31 +0200

    fix

--- nilearn/utils/tests/test_discovery.py
+++ nilearn/utils/tests/test_discovery.py
@@ -38,9 +38,9 @@ def test_all_functions():
     fn = all_functions()
     print(fn)
     if is_matplotlib_installed():
-        assert len(fn) == 172
+        assert len(fn) == 170
     else:
-        assert len(fn) == 138
+        assert len(fn) == 136
 
 
 @pytest.mark.parametrize(
