a867a57c07e608ee14a513071034f7ff159362a9
Remi Gau <remi_gau@hotmail.com>
2026-09-03 15:14:01 +0200

    rm skip test

--- nilearn/maskers/tests/test_multi_nifti_labels_masker.py
+++ nilearn/maskers/tests/test_multi_nifti_labels_masker.py
@@ -1,7 +1,5 @@
 """Test the multi_nifti_labels_masker module."""
 
-import sys
-
 import numpy as np
 import pytest
 from nibabel import Nifti1Image
@@ -369,10 +367,6 @@ def test_multi_nifti_labels_masker_atlas_data_different_fov(
     assert_array_equal(masker.labels_img_.affine, affine2)
 
 
-# TODO (python >= 3.11) remove skipif
-@pytest.mark.skipif(
-    sys.version_info[1] < 11, reason="UserWarning not thrown in py310"
-)
 def test_multi_nifti_labels_masker_resampling_target():
     """Test labels masker with resampling target in 'data', 'labels'.
 
