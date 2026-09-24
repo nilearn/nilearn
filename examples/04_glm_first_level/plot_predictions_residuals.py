797e3e42c40bba3f56493a85825a428bb533d039
Remi Gau <remi_gau@hotmail.com>
2026-09-24 14:29:22 +0200

    Update examples/04_glm_first_level/plot_predictions_residuals.py

--- examples/04_glm_first_level/plot_predictions_residuals.py
+++ examples/04_glm_first_level/plot_predictions_residuals.py
@@ -97,7 +97,7 @@ print(table)
 coords = table.loc[range(1, 7), ["X", "Y", "Z"]].to_numpy()
 print(coords)
 
-masker = NiftiSpheresMasker(coords, verbose=1)
+masker = NiftiSpheresMasker(coords, verbose=1).fit()
 
 # let's have a look at the report to make sure the spheres are well placed.
 report = masker.generate_report()
