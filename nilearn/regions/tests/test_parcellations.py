489fe35e6709e632e040089319f2cb4b94575925
Hande Gözükan <2099645+hndgzkn@users.noreply.github.com>
2026-09-25 10:35:37 +0200

    [ENH] Move sklearn estimator checks (#6585)
    Co-authored-by: Hande Gözükan <hande@ginkgo.work>
    Co-authored-by: Remi Gau <remi_gau@hotmail.com>

--- nilearn/regions/tests/test_parcellations.py
+++ nilearn/regions/tests/test_parcellations.py
@@ -6,12 +6,7 @@ import numpy as np
 import pandas as pd
 import pytest
 from nibabel import Nifti1Image
-from sklearn.utils.estimator_checks import parametrize_with_checks
 
-from nilearn._utils.estimator_checks import (
-    nilearn_check_estimator,
-    return_expected_failed_checks,
-)
 from nilearn._utils.helpers import is_windows_platform
 from nilearn.conftest import _affine_eye
 from nilearn.maskers import (
@@ -35,28 +30,6 @@ METHODS = [
     "rena",
     "hierarchical_kmeans",
 ]
-
-ESTIMATORS_TO_CHECK = [Parcellations(method=x, n_parcels=5) for x in METHODS]
-
-
-@pytest.mark.slow
-@parametrize_with_checks(
-    estimators=ESTIMATORS_TO_CHECK,
-    expected_failed_checks=return_expected_failed_checks,
-)
-def test_check_estimator_sklearn(estimator, check):
-    """Check compliance with sklearn estimators."""
-    check(estimator)
-
-
-@pytest.mark.slow
-@pytest.mark.parametrize(
-    "estimator, check, name",
-    nilearn_check_estimator(estimators=ESTIMATORS_TO_CHECK),
-)
-def test_check_estimator_nilearn(estimator, check, name):  # noqa: ARG001
-    """Check compliance with nilearn estimators rules."""
-    check(estimator)
 
 
 @pytest.fixture
